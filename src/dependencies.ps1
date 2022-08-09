23ad307997603f68a3cd178d117b0da5707b3c6c
Rodrigo Tintor <Rodrigo.Tintor@microsoft.com>
2022-08-09 15:47:17 -0700

    Removing comments
    Signed-off-by: Rodrigo Tintor <Rodrigo.Tintor@microsoft.com>

--- src/dependencies.ps1
+++ src/dependencies.ps1
@@ -8,19 +8,3 @@ Set-StrictMode -Version 5
 . (join-path $psscriptroot CmdletTranslation.ps1)
 . (join-path $psscriptroot ModuleMap.ps1)
 . (join-path $psscriptroot cmdletMapper.ps1)
-
-# $map = [CmdletMapper]::new()
-# $map.AddCustomizationsFile("C:\Src\Microsoft.Graph.Compatibility.AzureAD\TestInput.json")
-# $map.AddCustomization("Set-AzureADMSPermissionGrantPolicy","Update-MgPolicyPermissionGrantPolicy", $null, $null)
-# $map.AddCustomization("Remove-AzureADMSPermissionGrantPolicy","Remove-MgPolicyPermissionGrantPolicy", $null, $null)
-# $param = @{}
-# $script = @"
-# `$Value = @{
-#     forceChangePasswordNextSignIn = `$TmpValue.ForceChangePasswordNextLogin
-#     password = `$TmpValue.Password 
-# }
-# "@
-# $param.Add("PasswordProfile", [DataMap]::New("PasswordProfile", "PasswordProfile", 99, [Scriptblock]::Create($script)))
-# $map.AddCustomization("New-AzureADUser","New-MgUser", $param, $null)
-# $map.GenerateInputDataFile()
-Write-Host "Done!"
