5b94fdf370066ab542dca7feec9a80dbb8c16e76
Rodrigo Tintor <rotintor@microsoft.com>
2022-12-09 10:50:54 -0800

    Fix UT
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

--- test/General.Tests.ps1
+++ test/General.Tests.ps1
@@ -24,7 +24,7 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD
-        $module.ExportedCommands.Keys.Count | Should -Be 118
+        $module.ExportedCommands.Keys.Count | Should -Be 113
     }
 
     It 'Known number of missing commands' {        
