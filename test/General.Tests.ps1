0b1fa56b19376abc6fd86c9a1cdc34ca59140024
Rodrigo Tintor <rotintor@microsoft.com>
2022-12-09 10:13:41 -0800

    Changing test for the new added command
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

--- test/General.Tests.ps1
+++ test/General.Tests.ps1
@@ -24,11 +24,11 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD
-        $module.ExportedCommands.Keys.Count | Should -Be 112
+        $module.ExportedCommands.Keys.Count | Should -Be 113
     }
 
     It 'Known number of missing commands' {        
-        $MISSING_CMDS.Count | Should -Be 117
+        $MISSING_CMDS.Count | Should -Be 118
     }
 
     It 'Running a simple command Set-CompatADAlias'{
