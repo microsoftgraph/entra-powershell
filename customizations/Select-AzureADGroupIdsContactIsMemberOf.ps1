8dd9ac414e61f499db87fac7e2413aee46924090
Rodrigo Tintor <rodrigo.tintor@microsoft.com>
2022-12-14 10:16:11 -0800

    Adding basic customization for missing commands part1 (#8)
    * Adding basic customization for commands:
    Get-AzureADContactMembership.ps1
    Get-AzureADMSAdministrativeUnit.ps1
    Get-AzureADMSDeletedDirectoryObject.ps1
    Get-AzureADMSDeletedGroup.ps1
    Get-AzureADServiceAppRoleAssignedTo.ps1
    New-AzureADApplicationKeyCredential.ps1
    New-AzureADServicePrincipalPasswordCredential.ps1
    Remove-AzureADApplicationPasswordCredential.ps1
    Remove-AzureADDeletedApplication.ps1
    Remove-AzureADServiceAppRoleAssignment.ps1
    Reset-AzureADMSLifeCycleGroup.ps1
    Set-AzureADTenantDetail.ps1
    
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>
    
    * Adding more missing commands:
    Add-AzureADMSScopedRoleMembership.ps1
    Get-AzureADExtensionProperty.ps1
    Get-AzureADMSConditionalAccessPolicy.ps1
    Get-AzureADObjectByObjectId.ps1
    New-AzureADMSConditionalAccessPolicy.ps1
    Remove-AzureADMSAdministrativeUnitMember.ps1
    Remove-AzureADMSConditionalAccessPolicy.ps1
    Remove-AzureADMSDeletedDirectoryObject.ps1
    Remove-AzureADMSLifecyclePolicyGroup.ps1
    Remove-AzureADMSNamedLocationPolicy.ps1
    Remove-AzureADServicePrincipalKeyCredential.ps1
    Select-AzureADGroupIdsContactIsMemberOf.ps1
    Set-AzureADMSConditionalAccessPolicy.ps1
    Set-AzureADMSNamedLocationPolicy.ps1
    Set-AzureADUserThumbnailPhoto.ps1
    
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>
    
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>
    Co-authored-by: Rodrigo Tintor <rotintor@microsoft.com>

---
+++ customizations/Select-AzureADGroupIdsContactIsMemberOf.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Select-AzureADGroupIdsContactIsMemberOf"
+    TargetName = "Get-MgContactMemberOf"
+    Parameters = $null
+    Outputs = $null
+}
