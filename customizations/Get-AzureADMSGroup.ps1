57bc2fb5a68f121f1a20e81d9295815f41bd98ae
Rodrigo Tintor <rotintor@microsoft.com>
2022-12-14 15:46:40 -0800

    Adding missing command for Ms graph.
    Get-AzureADMSApplication
    Get-AzureADMSGroup
    
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

---
+++ customizations/Get-AzureADMSGroup.ps1
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADMSGroup"
+    TargetName = "Get-MgGroup"
+    Parameters = @(
+        @{
+            SourceName = "SearchString"
+            TargetName = "Filter"
+            ConversionType = 99
+            SpecialMapping = @"
+`$Value = "mailNickName eq '`$TmpValue' or (mail eq '`$TmpValue' or (displayName eq '`$TmpValue' or startswith(displayName,'`$TmpValue')))"
+"@
+        }
+    )
+    Outputs = $null
+}
