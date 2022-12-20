332f62109ae662f9eda127a6710706b09fddc6fd
Rodrigo Tintor <rotintor@microsoft.com>
2022-12-20 13:50:34 -0800

    Adding commands: Add-AzureADDeviceRegisteredOwner.ps1 Add-AzureADDirectoryRoleMember.ps1 Add-AzureADMSAdministrativeUnitMember.ps1 Add-AzureADMSApplicationOwner.ps1 Add-AzureADMSLifecyclePolicyGroup.ps1 Add-AzureADServicePrincipalOwner.ps1 Select-AzureADGroupIdsGroupIsMemberOf.ps1 Select-AzureADGroupIdsServicePrincipalIsMemberOf.ps1 Select-AzureADGroupIdsUserIsMemberOf.ps1 Update-AzureADSignedInUserPassword.ps1

---
+++ customizations/Add-AzureADDirectoryRoleMember.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Add-AzureADDirectoryRoleMember"
+    TargetName = "New-MgDirectoryRoleMemberByRef"
+    Parameters = $null
+    Outputs = $null
+}
