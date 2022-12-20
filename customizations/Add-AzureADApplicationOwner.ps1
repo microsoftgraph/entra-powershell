f83670882f99a1a223824fa755b3255a01aee5d8
Rodrigo Tintor <rotintor@microsoft.com>
2022-12-20 13:19:11 -0800

    Adding commands: Add-AzureADApplicationOwner.ps1 Get-AzureADDeletedApplication.ps1 Get-AzureADDeviceConfiguration.ps1 Get-AzureADMSAdministrativeUnitMember.ps1 Get-AzureADMSNamedLocationPolicy.ps1 Get-AzureADMSScopedRoleMembership.ps1 Get-AzureADServicePrincipalMembership.ps1 Get-AzureADUserMembership.ps1 Get-AzureADUserThumbnailPhoto.ps1 Remove-AzureADApplicationKeyCredential.ps1

---
+++ customizations/Add-AzureADApplicationOwner.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Add-AzureADApplicationOwner"
+    TargetName = "New-MgApplicationOwnerByRef"
+    Parameters = $null
+    Outputs = $null
+}
--- test/General.Tests.ps1
+++ test/General.Tests.ps1
@@ -24,11 +24,11 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD
-        $module.ExportedCommands.Keys.Count | Should -Be 140
+        $module.ExportedCommands.Keys.Count | Should -Be 150
     }
 
     It 'Known number of missing commands' {        
-        $MISSING_CMDS.Count | Should -Be 89
+        $MISSING_CMDS.Count | Should -Be 79
     }
 
     It 'Running a simple command Set-CompatADAlias'{
