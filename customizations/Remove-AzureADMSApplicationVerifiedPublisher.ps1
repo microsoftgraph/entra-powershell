6bee7daf628a233ec9d3fe202b49f93f7d9e24c5
Rodrigo Tintor <rotintor@microsoft.com>
2023-03-15 20:40:24 -0700

    Adding commands that calls MS Graph
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

---
+++ customizations/Remove-AzureADMSApplicationVerifiedPublisher.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Remove-AzureADMSApplicationVerifiedPublisher"
+    TargetName = "Clear-MgApplicationVerifiedPublisher"
+    Parameters = $null
+    Outputs = $null
+}
