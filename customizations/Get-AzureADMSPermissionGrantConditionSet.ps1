2e4b43f196255aab7bf3261bbb17ddab9e89c7af
Rodrigo Tintor <rotintor@microsoft.com>
2023-04-12 14:11:23 -0700

    Adding Get-AzureADMSPermissionGrantConditionSet
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

---
+++ customizations/Get-AzureADMSPermissionGrantConditionSet.ps1
@@ -0,0 +1,49 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADMSPermissionGrantConditionSet"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @"
+    `$params = @{}
+    if(`$PSBoundParameters.ContainsKey("Verbose"))
+    {
+        `$params["Verbose"] = `$Null
+    }
+    if(`$null -ne `$PSBoundParameters["Id"])
+    {
+        `$params["PermissionGrantConditionSetId"] = `$PSBoundParameters["Id"]
+    }
+    if(`$null -ne `$PSBoundParameters["ConditionSetType"])
+    {
+        `$conditionalSet = `$PSBoundParameters["ConditionSetType"]
+    }
+    if(`$null -ne `$PSBoundParameters["PolicyId"])
+    {
+        `$params["PermissionGrantPolicyId"] = `$PSBoundParameters["PolicyId"]
+    }
+    if(`$PSBoundParameters.ContainsKey("Debug"))
+    {
+        `$params["Debug"] = `$Null
+    }
+
+    Write-Debug("============================ TRANSFORMATIONS ============================")
+    $`params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+    Write-Debug("=========================================================================`n")
+    
+    if("`$conditionalSet" -eq "includes"){
+        `$response = Get-MgPolicyPermissionGrantPolicyInclude @params
+    }
+    elseif("`$conditionalSet" -eq "excludes"){
+        `$response = Get-MgPolicyPermissionGrantPolicyExclude @params
+    }
+    else{
+        Write-Error("Message: Resource not found for the segment '`$conditionalSet'.")
+        return
+    }
+    
+    `$response
+"@
+}
