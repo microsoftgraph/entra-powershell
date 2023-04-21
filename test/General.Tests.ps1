3fffe5f3f35d17f0f83e228cc464812dfcbe3dad
Rodrigo Tintor <rotintor@microsoft.com>
2023-04-21 15:36:22 -0700

    Changing UT for new commands
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

--- test/General.Tests.ps1
+++ test/General.Tests.ps1
@@ -25,11 +25,11 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD
-        $module.ExportedCommands.Keys.Count | Should -Be 199
+        $module.ExportedCommands.Keys.Count | Should -Be 201
     }
 
     It 'Known number of missing commands' {        
-        $MISSING_CMDS.Count | Should -Be 30
+        $MISSING_CMDS.Count | Should -Be 28
     }
 
     It 'Running a simple command Set-CompatADAlias'{
