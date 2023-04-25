bc51cc1f4281ad307b7ac4c4dc1be3602f51b790
Rodrigo Tintor <rotintor@microsoft.com>
2023-04-25 08:40:51 -0700

    Adding missing commands: Restore-AzureADDeletedApplication Restore-AzureADMSDeletedDirectoryObject Revoke-AzureADSignedInUserAllRefreshToken Revoke-AzureADUserAllRefreshToken
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

---
+++ customizations/Restore-AzureADDeletedApplication.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Restore-AzureADDeletedApplication"
+    TargetName = "Restore-MgDirectoryDeletedItem"
+    Parameters = $null
+    Outputs = $null
+}
--- test/General.Tests.ps1
+++ test/General.Tests.ps1
@@ -25,11 +25,11 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD
-        $module.ExportedCommands.Keys.Count | Should -Be 201
+        $module.ExportedCommands.Keys.Count | Should -Be 205
     }
 
     It 'Known number of missing commands' {        
-        $MISSING_CMDS.Count | Should -Be 28
+        $MISSING_CMDS.Count | Should -Be 24
     }
 
     It 'Running a simple command Set-CompatADAlias'{
