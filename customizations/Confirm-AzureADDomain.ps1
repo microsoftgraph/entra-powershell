15d2eb47c5025ae96058c9622e3fc02ce50a9f2d
Rodrigo Tintor <rotintor@microsoft.com>
2023-06-20 09:28:04 -0700

    Fix bug on *-AzureADDomain commands
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

---
+++ customizations/Confirm-AzureADDomain.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Confirm-AzureADDomain"
+    TargetName = "Confirm-MgDomain"
+    Parameters = @(
+        @{
+            SourceName = "Name"
+            TargetName = "DomainId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+    )
+    Outputs = $null
+}
---
+++ customizations/Remove-AzureADDomain.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Remove-AzureADDomain"
+    TargetName = "Remove-MgDomain"
+    Parameters = @(
+        @{
+            SourceName = "Name"
+            TargetName = "DomainId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+    )
+    Outputs = $null
+}
