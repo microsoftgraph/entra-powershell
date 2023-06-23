2aebb7b36c7ce7711e8ecab3382232cf6abd6bf9
Rodrigo Tintor <rotintor@microsoft.com>
2023-06-23 09:01:46 -0700

    Removing Get-AzureADDeviceConfiguration because the proper endpoint is not yet ready on Microsoft Graph. Feature team is working on parity.
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

--- test/General.Tests.ps1
+++ test/General.Tests.ps1
@@ -25,11 +25,11 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD
-        $module.ExportedCommands.Keys.Count | Should -Be 210
+        $module.ExportedCommands.Keys.Count | Should -Be 209
     }
 
     It 'Known number of missing commands' {        
-        $MISSING_CMDS.Count | Should -Be 20
+        $MISSING_CMDS.Count | Should -Be 21
     }
 
     It 'Running a simple command Set-CompatADAlias'{
