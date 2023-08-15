5c51faaa7ebf7151942bef811b508d608507b410
Rodrigo Tintor <rotintor@microsoft.com>
2023-08-15 13:05:00 -0700

    Setting the required version to 2.0.0 adding UT for AzureADPreview

---
+++ test/module/AzureADPreview/AddtionalFunctions.Tests.ps1
@@ -0,0 +1,19 @@
+BeforeAll {    
+    if((Get-Module -Name Microsoft.Graph.Compatibility.AzureAD.Preview) -eq $null){
+        Import-Module Microsoft.Graph.Compatibility.AzureAD.Preview
+    }
+}
+
+Describe 'Checking Files'{
+    BeforeAll {
+        $files = Get-ChildItem -Path (join-path $psscriptroot "..\..\..\module\AzureADPreview\AdditionalFunctions") -Filter '*.ps1'        
+    }
+
+    It 'Checking that AdditionalFunctions produce commands' {
+        $files | ForEach-Object {
+            $name = $_.Name.Replace(".ps1","")
+            $module = Get-Module Microsoft.Graph.Compatibility.AzureAD.Preview
+            $module.ExportedCommands.ContainsKey($name) | Should -BeTrue
+        }
+    }
+}
---
+++ test/module/AzureADPreview/General.Tests.ps1
@@ -0,0 +1,40 @@
+BeforeAll {
+    if((Get-Module -Name Microsoft.Graph.Compatibility.AzureAD.Preview) -eq $null){
+        Import-Module Microsoft.Graph.Compatibility.AzureAD.Preview
+    }
+}
+
+Describe 'PowerShell Version Check' {
+    It 'Version 5.1 or Greater' {
+        $semanticVersion = $PSVersionTable.PSVersion
+        $version = $semanticVersion.Major + ($semanticVersion.Minor * 0.1)
+        $version | Should -BeGreaterOrEqual 5.1
+    }
+}
+
+Describe 'Module checks' {
+    It 'Module imported' {                
+        $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD.Preview
+        $module | Should -Not -Be $null
+    }
+
+    It 'Have more that zero exported functions' {
+        $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD.Preview
+        $module.ExportedCommands.Keys.Count | Should -BeGreaterThan 0
+    }
+
+    It 'Known number translated commands' {
+        $module = Get-Module -Name Microsoft.Graph.Compatibility.AzureAD.Preview
+        $module.ExportedCommands.Keys.Count | Should -Be 145
+    }
+
+    It 'Known number of missing commands' {        
+        $MISSING_CMDS.Count | Should -Be 167
+    }
+
+    It 'Running a simple command Set-CompatADAlias'{
+        Set-CompatADAlias
+        $Alias = Get-Alias -Name Get-AzureADUser
+        $Alias | Should -Not -Be $null
+    }
+}
