0739c3ea9d63c7be3ce3432e4e0fab50c8e82908
varsha <v-varshamane@microsoft.com>
2023-08-31 01:30:43 +0530

    customization of cmdlets Add-AzureADScopedRoleMembership,Get-AzureADAuditSignInLogs,Get-AzureADMSFeatureRolloutPolicy,Get-AzureADScopedRoleMembership,New-AzureADMSFeatureRolloutPolicy,Remove-AzureADMSFeatureRolloutPolicy,Remove-AzureADScopedRoleMembership,Set-AzureADMSFeatureRolloutPolicy

--- module/AzureADPreview/customizations/New-AzureADMSFeatureRolloutPolicy.ps1
+++ module/AzureADPreview/customizations/New-AzureADMSFeatureRolloutPolicy.ps1
@@ -4,43 +4,6 @@
 @{
     SourceName = "New-AzureADMSFeatureRolloutPolicy"
     TargetName = "New-MgBetaPolicyFeatureRolloutPolicy"
-    Parameters = @(
-        @{
-            SourceName = "Feature"
-            TargetName = "Feature"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "DisplayName"
-            TargetName = "DisplayName"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "Description"
-            TargetName = "Description"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "IsEnabled"
-            TargetName = "IsEnabled"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "IsAppliedToOrganization"
-            TargetName = "IsAppliedToOrganization"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "AppliesTo"
-            TargetName = "AppliesTo"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        }
-    )
+    Parameters = $null
     Outputs = $null
 }
