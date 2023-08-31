8f730a64aaabcfc7d59c4299c3af02cf77f9e9fb
v-varshamane <142500640+v-varshamane@users.noreply.github.com>
2023-08-31 20:32:58 +0530

    Azureadpreview august sprint1 (#58)
    Adding commands:
    Add-AzureADScopedRoleMembership,
    Get-AzureADAuditSignInLogs,
    Get-AzureADMSFeatureRolloutPolicy,
    Get-AzureADScopedRoleMembership,
    New-AzureADMSFeatureRolloutPolicy,
    Remove-AzureADMSFeatureRolloutPolicy,
    Remove-AzureADScopedRoleMembership,
    Set-AzureADMSFeatureRolloutPolicy

---
+++ module/AzureADPreview/customizations/Get-AzureADScopedRoleMembership.ps1
@@ -0,0 +1,49 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADScopedRoleMembership"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @"
+    PROCESS {    
+        `$params = @{}
+        `$keysChanged = @{ObjectId = "Id"}
+        if(`$PSBoundParameters.ContainsKey("Verbose"))
+        {
+            `$params["Verbose"] = `$Null
+        }
+        if(`$null -ne `$PSBoundParameters["ObjectId"])
+        {
+            `$params["AdministrativeUnitId"] = `$PSBoundParameters["ObjectId"]
+        }
+        if(`$PSBoundParameters.ContainsKey("Debug"))
+        {
+            `$params["Debug"] = `$Null
+        }
+        if(`$null -ne `$PSBoundParameters["ScopedRoleMembershipId"])
+        {
+            `$params["ScopedRoleMembershipId"] = `$PSBoundParameters["ScopedRoleMembershipId"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        `$response = Get-MgBetaDirectoryAdministrativeUnitScopedRoleMember @params
+        `$response | ForEach-Object {
+            if (`$null -ne `$_) {
+                
+                `$propsToConvert = @('RoleMemberInfo')
+        
+                foreach (`$prop in `$propsToConvert) {
+                    `$value = `$_.`$prop | ConvertTo-Json | ConvertFrom-Json
+                    `$_ | Add-Member -MemberType NoteProperty -Name `$prop -Value (`$value) -Force
+                }
+            }
+        }
+        `$response
+        }
+"@
+}
