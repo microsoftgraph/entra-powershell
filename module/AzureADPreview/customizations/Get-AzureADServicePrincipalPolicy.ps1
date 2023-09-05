3b5e657e31cb749a3453be15810bbffae054dce5
Ashwini Karke <ashwini.karke@perennialsys.com>
2023-09-05 15:10:32 +0530

    Added customscript for Get-AzureADServicePrincipalPolicy

---
+++ module/AzureADPreview/customizations/Get-AzureADServicePrincipalPolicy.ps1
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADServicePrincipalPolicy"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @"
+    PROCESS {    
+        `$params = @{}
+        `$Id = `$PSBoundParameters["Id"]
+        `$params["Uri"] = "https://graph.microsoft.com/beta/serviceprincipals/`$Id/policies"
+        `$params["Method"] = "GET"
+        if (`$PSBoundParameters.ContainsKey("ID")) {
+            `$params["Uri"] = "https://graph.microsoft.com/beta/serviceprincipals/`$Id/policies"
+        }
+        
+        if (`$PSBoundParameters.ContainsKey("Debug")) {
+            `$params["Debug"] = `$Null
+        }
+        if (`$PSBoundParameters.ContainsKey("Verbose")) {
+            `$params["Verbose"] = `$Null
+        }
+        
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+        Write-Debug("=========================================================================``n")
+                
+        `$response = (Invoke-GraphRequest -Method `$params.method -Uri `$params.uri).value   
+            `$response | ForEach-Object {
+                if (`$null -ne `$_) {
+                    foreach (`$Keys in `$_.Keys) { 
+                        `$_ | Add-Member -MemberType NoteProperty -Name `$Keys -Value (`$_.`$Keys) -Force
+                    }
+                }
+            }
+            if (`$MyInvocation.PipelineLength -gt 1) {
+                `$response  
+            }
+            else {
+                `$response | Select-Object Id, DisplayName, Type, IsOrganizationDefault
+            }  
+    }
+"@
+}
