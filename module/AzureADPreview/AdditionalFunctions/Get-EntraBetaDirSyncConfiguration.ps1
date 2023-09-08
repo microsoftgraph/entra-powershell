13e7732db976b4c6b557b076382bb70d896cae32
kshitiz sharma <v-kshsharma@microsoft.com>
2023-09-08 22:37:32 +0530

    Updated code with write-error mirroing msol

--- module/AzureADPreview/AdditionalFunctions/Get-EntraBetaDirSyncConfiguration.ps1
+++ module/AzureADPreview/AdditionalFunctions/Get-EntraBetaDirSyncConfiguration.ps1
@@ -1,10 +1,9 @@
 function Get-EntraBetaDirSyncConfiguration {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
-        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)][System.String] $TenantId
+        [Parameter(ParameterSetName = "GetById", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)][System.String] $TenantId
     )
 
-    
     PROCESS {    
         $params = @{}
         $keysChanged = @{$TenantId = "OnPremisesDirectorySynchronizationId" }
@@ -20,12 +19,18 @@ function Get-EntraBetaDirSyncConfiguration {
         Write-Debug("============================ TRANSFORMATIONS ============================")
         $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
-        $response = ((Get-MgBetaDirectoryOnPremiseSynchronization @params).configuration | Select-Object -Property AccidentalDeletionPrevention).AccidentalDeletionPrevention
+        $response = ((Get-MgBetaDirectoryOnPremiseSynchronization @params -ErrorVariable Eq -erroraction SilentlyContinue).configuration | Select-Object -Property AccidentalDeletionPrevention).AccidentalDeletionPrevention
         # Create a custom table
-        $customTable = [PSCustomObject]@{
-            "AccidentalDeletionThreshold" = $response.AlertThreshold
-            "DeletionPreventionType"      = $response.SynchronizationPreventionType
+        if ($null -ne $eq) {
+            write-error "Get-EntraBetaDirSyncConfiguration : Cannot bind parameter 'TenantId'. Cannot convert value `"$($params["OnPremisesDirectorySynchronizationId"])`" to type `"System.Guid`". Error: `"Guid should contain 32 digits with 4 dashes (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).`"
+            "
         }
-        $customTable | Format-Table
+        else {
+            $customTable = [PSCustomObject]@{
+                "AccidentalDeletionThreshold" = $response.AlertThreshold
+                "DeletionPreventionType"      = $response.SynchronizationPreventionType
+            }
+            $customTable | Format-Table
+        }
     }
 }
