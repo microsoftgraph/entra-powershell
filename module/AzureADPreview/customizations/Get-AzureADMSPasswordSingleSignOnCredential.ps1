e1f9eb4601bc7a4b5bbee55b8e2bfe75df16d5e3
Ashwini Karke <ashwini.karke@perennialsys.com>
2023-09-18 12:19:19 +0530

    Added custom script

--- module/AzureADPreview/customizations/Get-AzureADMSPasswordSingleSignOnCredential.ps1
+++ module/AzureADPreview/customizations/Get-AzureADMSPasswordSingleSignOnCredential.ps1
@@ -3,20 +3,40 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADMSPasswordSingleSignOnCredential"
-    TargetName = "Get-MgBetaServicePrincipalPasswordSingleSignOnCredential"
-    Parameters = @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "ServicePrincipalId"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "PasswordSSOObjectId"
-            TargetName = "Id"
-            ConversionType = "ScriptBlock"
-            SpecialMapping = $null
-        }
-    )
+    TargetName = $null
+    Parameters = $null
     Outputs = $null
+    CustomScript = @"
+    `$params = @{}
+    if(`$null -ne `$PSBoundParameters["ObjectId"])
+    {
+        `$params["ServicePrincipalId"] = `$PSBoundParameters["ObjectId"]
+    }
+    if(`$null -ne `$PSBoundParameters["PasswordSSOObjectId"])
+    {
+        `$body = `$PSBoundParameters["PasswordSSOObjectId"] | ConvertTo-Json
+    }
+    `$headers = @{
+        "Content-Type" = "application/json"
+    }
+
+    `$params["Uri"] = "https://graph.microsoft.com/beta/serviceprincipals/"+`$params.ServicePrincipalId+"/getPasswordSingleSignOnCredentials"
+    `$params["Method"] = "POST"
+
+    if(`$PSBoundParameters.ContainsKey("Debug"))
+    {
+        `$params["Debug"] = `$Null
+    }
+    if(`$PSBoundParameters.ContainsKey("Verbose"))
+    {
+        `$params["Verbose"] = `$Null
+    }
+
+    Write-Debug("============================ TRANSFORMATIONS ============================")
+    `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+    Write-Debug("=========================================================================`n")
+    
+    `$response = Invoke-GraphRequest -Uri `$params.uri -Method `$params.method -Headers `$headers -Body `$body | ConvertTo-Json
+    `$response | ConvertFrom-Json  
+"@
 }
