cfe7adb33e98dc126bb9d43444442ac12110a0de
Ashwini Karke <ashwini.karke@perennialsys.com>
2023-09-19 19:34:54 +0530

    Added customscript

--- module/AzureADPreview/customizations/New-AzureADMSPasswordSingleSignOnCredential.ps1
+++ module/AzureADPreview/customizations/New-AzureADMSPasswordSingleSignOnCredential.ps1
@@ -3,20 +3,41 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "New-AzureADMSPasswordSingleSignOnCredential"
-    TargetName = "New-MgBetaServicePrincipalPasswordSingleSignOnCredential"
-    Parameters =  @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "ServicePrincipalId"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        },
-        @{
-            SourceName = "PasswordSSOCredential"
-            TargetName = "Credentials"
-            ConversionType = "Name"
-            SpecialMapping = $null
-        }
-    )
+    TargetName = $null
+    Parameters = $null
     Outputs = $null
+    CustomScript = @"
+    `$params = @{}
+    if(`$null -ne `$PSBoundParameters["ObjectId"])
+    {
+        `$params["ServicePrincipalId"] = `$PSBoundParameters["ObjectId"]
+    }
+    if(`$null -ne `$PSBoundParameters["PasswordSSOCredential"])
+    {
+        `$body = [PasswordSSOCredentials]`$PSBoundParameters["PasswordSSOCredential"] | ConvertTo-Json
+    }
+    `$headers = @{
+        "Content-Type" = "application/json"
+    }
+    `$params["Uri"] = "https://graph.microsoft.com/beta/serviceprincipals/"+`$params.ServicePrincipalId+"/getPasswordSingleSignOnCredentials"
+    `$params["Method"] = "POST"
+
+    if(`$PSBoundParameters.ContainsKey("Debug"))
+    {
+        `$params["Debug"] = `$Null
+    }
+    if(`$PSBoundParameters.ContainsKey("Verbose"))
+    {
+        `$params["Verbose"] = `$Null
+    }
+
+    Write-Debug("============================ TRANSFORMATIONS ============================")
+    `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+    Write-Debug("=========================================================================`n")
+    
+    `$response = Invoke-GraphRequest -Uri `$params.uri -Method `$params.method -Headers `$headers -Body `$body | ConvertTo-Json
+    `$result = `$response | ConvertFrom-Json      
+    `$result | Add-Member -MemberType AliasProperty -Value '@odata.context' -Name 'odata.context' -Force
+    `$result
+"@
 }
