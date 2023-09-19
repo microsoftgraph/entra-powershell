e157576bf510d14d8946b65be9cfe41948feb72d
kshitiz sharma <v-kshsharma@microsoft.com>
2023-09-19 17:10:20 +0530

    Updated for anomaly

--- module/AzureADPreview/AdditionalFunctions/Set-EntraBetaDirSyncFeature.ps1
+++ module/AzureADPreview/AdditionalFunctions/Set-EntraBetaDirSyncFeature.ps1
@@ -42,77 +42,82 @@ function Set-EntraBetaDirSyncFeature {
         
         Enables the SoftMatchOnUpn feature for the tenant.
     #>
-    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
-    param (
-        [Parameter(ParameterSetName = "GetQuery", Mandatory = $true, ValueFromPipelineByPropertyName = $true)][System.String] $Feature,
-        [Parameter(ParameterSetName = "GetQuery", Mandatory = $true, ValueFromPipelineByPropertyName = $true)][System.Boolean] $Enabled,
-        [Parameter(ParameterSetName = "GetQuery", ValueFromPipelineByPropertyName = $true)][System.Guid] $TenantId,
-        [switch] $Force
-    )
-    PROCESS {
-        $params = @{}
-        if ($PSBoundParameters.ContainsKey("Verbose")) {
-            $Verbose = $Null
-        }
-        if ($null -ne $PSBoundParameters["Feature"]) {
-            $Feature = $PSBoundParameters["Feature"] + "Enabled"
-        }
-        if ($null -ne $PSBoundParameters["Enabled"]) {
-            $Enabled = $PSBoundParameters["Enabled"]
-        }
-        if ($PSBoundParameters.ContainsKey("Debug")) {
-            $params["Debug"] = $Null
-        }
-        Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
-        Write-Debug("=========================================================================`n")
-        [string]::IsNullOrWhiteSpace($TenantId)
-        if ([string]::IsNullOrWhiteSpace($TenantId)) {
-            $OnPremisesDirectorySynchronizationId = (Get-MgBetaDirectoryOnPremiseSynchronization).Id
-        }
-        else {
-            $OnPremisesDirectorySynchronizationId = Get-MgBetaDirectoryOnPremiseSynchronization -OnPremisesDirectorySynchronizationId $TenantId -ErrorAction SilentlyContinue -ErrorVariable er
-            if (![string]::IsNullOrWhiteSpace($er)) {
-                $OnPremisesDirectorySynchronizationId = $OnPremisesDirectorySynchronizationId.Id
+        [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+        param (
+            [Parameter(ParameterSetName = "GetQuery", Mandatory = $true, ValueFromPipelineByPropertyName = $true)][System.String] $Feature,
+            [Parameter(ParameterSetName = "GetQuery", Mandatory = $true, ValueFromPipelineByPropertyName = $true)][System.Boolean] $Enabled,
+            [Parameter(ParameterSetName = "GetQuery", ValueFromPipelineByPropertyName = $true)][System.Guid] $TenantId,
+            [switch] $Force
+        )
+        PROCESS {
+    
+            $params = @{}
+            if ($PSBoundParameters.ContainsKey("Verbose")) {
+                $Verbose = $Null
+            }
+            if ($null -ne $PSBoundParameters["Feature"]) {
+                $Feature = $PSBoundParameters["Feature"] + "Enabled"
+            }
+            if ($null -ne $PSBoundParameters["Enabled"]) {
+                $Enabled = $PSBoundParameters["Enabled"]
+            }
+            if ($PSBoundParameters.ContainsKey("Debug")) {
+                $params["Debug"] = $Null
+            }
+    
+            Write-Debug("============================ TRANSFORMATIONS ============================")
+            $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+            Write-Debug("=========================================================================`n")
+            if ([string]::IsNullOrWhiteSpace($TenantId)) {
+                $OnPremisesDirectorySynchronizationId = (Get-MgBetaDirectoryOnPremiseSynchronization).Id
             }
             else {
-                Write-Error "Cannot bind parameter 'TenantId'. Cannot convert value `"$TenantId`" to type 
+                $OnPremisesDirectorySynchronizationId = Get-MgBetaDirectoryOnPremiseSynchronization -OnPremisesDirectorySynchronizationId $TenantId -ErrorAction SilentlyContinue -ErrorVariable er
+                if(![string]::IsNullOrWhiteSpace($er)) {
+                    $OnPremisesDirectorySynchronizationId = $OnPremisesDirectorySynchronizationId.Id
+                }
+                else {
+                    Write-Error "Cannot bind parameter 'TenantId'. Cannot convert value `"$TenantId`" to type 
                     `"System.Guid`". Error: `"Guid should contain 32 digits with 4 dashes (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).`" "
+                    break
+                }
+            }
+    
+            $body = @{
+                features = @{ $Feature = $Enabled }
+            }
+            $body = $body | ConvertTo-Json
+            if ($Force) {
+                # If -Force is used, skip confirmation and proceed with the action.
+                $decision = 0
+            }
+            else {
+                $title = 'Confirm'
+                $question = 'Do you want to continue?'
+                $Suspend = new-Object System.Management.Automation.Host.ChoiceDescription "&Suspend", "S"
+                $Yes = new-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Y"
+                $No = new-Object System.Management.Automation.Host.ChoiceDescription "&No", "N"
+                $choices = [System.Management.Automation.Host.ChoiceDescription[]]( $Yes, $No, $Suspend)
+                $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+            }
+            if ($decision -eq 0) {
+                $response = Update-MgBetaDirectoryOnPremiseSynchronization -OnPremisesDirectorySynchronizationId $OnPremisesDirectorySynchronizationId -BodyParameter $body -ErrorAction SilentlyContinue -ErrorVariable "er"
+                $er
                 break
-            }
-        }
-        $body = @{
-            features = @{ $Feature = $Enabled }
-        }
-        $body = $body | ConvertTo-Json
-        if ($Force) {
-            # If -Force is used, skip confirmation and proceed with the action.
-            $decision = 0
-        }
-        else {
-            $title = 'Confirm'
-            $question = 'Do you want to continue?'
-            $Suspend = new-Object System.Management.Automation.Host.ChoiceDescription "&Suspend", "S"
-            $Yes = new-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Y"
-            $No = new-Object System.Management.Automation.Host.ChoiceDescription "&No", "N"
-            $choices = [System.Management.Automation.Host.ChoiceDescription[]]( $Yes, $No, $Suspend)
-            $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-        }
-        if ($decision -eq 0) {
-            $response = Update-MgBetaDirectoryOnPremiseSynchronization -OnPremisesDirectorySynchronizationId $OnPremisesDirectorySynchronizationId -BodyParameter $body -ErrorAction SilentlyContinue -ErrorVariable "er"
-            $er
-            break
-            if ([string]::IsNullOrWhiteSpace($er)) {
-                $response
+                if([string]::IsNullOrWhiteSpace($er)) {
+                    $response
+                }
+                else {
+                    Write-Error "Cannot bind parameter 'TenantId'. Cannot convert value `"$TenantId`" to type 
+                    `"System.Guid`". Error: `"Guid should contain 32 digits with 4 dashes (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).`" "
+                }
+    
             }
             else {
-                Write-Error "Cannot bind parameter 'TenantId'. Cannot convert value `"$TenantId`" to type 
-                    `"System.Guid`". Error: `"Guid should contain 32 digits with 4 dashes (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).`" "
+                return
             }
-        }
-        else {
-            return
+    
+        
         }
     }
-}
     
