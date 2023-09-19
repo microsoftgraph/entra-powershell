f195fea2f89d0fdb9f137171bf210d629bca6b09
Rodrigo Tintor <rodrigo.tintor@microsoft.com>
2023-09-19 10:14:32 -0700

    Migration Remove commands from AzureAD to AzureADPreview (#91)

---
+++ module/AzureADPreview/customizations/Restore-AzureADMSDeletedDirectoryObject.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Restore-AzureADMSDeletedDirectoryObject"
+    TargetName = "Restore-MgBetaDirectoryDeletedItem"
+    Parameters = $null
+    Outputs = $null
+}
