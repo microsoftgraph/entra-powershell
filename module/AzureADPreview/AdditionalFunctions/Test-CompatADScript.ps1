94ffd8cea8e3c3c44b6a440d6146b53f72894590
Rodrigo Tintor <rotintor@microsoft.com>
2023-09-19 09:22:14 -0700

    Migrating all previously customized commands in AzureAD to AzureADPreview.

--- module/AzureADPreview/AdditionalFunctions/Test-CompatADScript.ps1
+++ module/AzureADPreview/AdditionalFunctions/Test-CompatADScript.ps1
@@ -2,6 +2,10 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
+<#
+.Description
+Test-CompatADScript looks for unsupported commands in a script.
+#>
 function Test-CompatADScript {
     param (
         $Script
---
+++ module/AzureADPreview/customizations/Get-AzureADServiceAppRoleAssignedTo.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADServiceAppRoleAssignedTo"
+    TargetName = "Get-MgBetaServicePrincipalAppRoleAssignedTo"
+    Parameters = $null
+    Outputs = $null
+}
