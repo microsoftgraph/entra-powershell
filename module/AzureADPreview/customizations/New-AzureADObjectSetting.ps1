b5fc3fd569cfe861b624bd64b45100e15672a40e
Komal Bachchani <komal.p@perennialsys.com>
2023-09-19 13:10:31 +0530

    Adding initial New-AzureADObjectSetting

---
+++ module/AzureADPreview/customizations/New-AzureADObjectSetting.ps1
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "New-AzureADObjectSetting"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @"
+    PROCESS {  
+        `$params = @{}  
+                if (`$null -ne `$PSBoundParameters["TargetType"]) {
+                    `$params["TargetType"] = `$PSBoundParameters["TargetType"]
+                }
+                if (`$null -ne `$PSBoundParameters["TargetObjectId"]) {
+                    `$params["TargetObjectId"] = `$PSBoundParameters["TargetObjectId"]
+                }
+                if (`$null -ne `$PSBoundParameters["DirectorySetting"]) {
+                    `$params["DirectorySetting"] = `$PSBoundParameters["DirectorySetting"]
+                }
+                if (`$PSBoundParameters.ContainsKey("Debug")) {
+                    `$params["Debug"] = `$Null
+                }
+                if (`$PSBoundParameters.ContainsKey("Verbose")) {
+                    `$params["Verbose"] = `$Null
+                }
+                    Write-Debug("============================ TRANSFORMATIONS ============================")
+                    `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+                    Write-Debug("=========================================================================``n")
+                    Invoke-GraphRequest -Method POST -Uri https://graph.microsoft.com/beta/`$TargetType/`$TargetObjectId/settings -Body (`$DirectorySetting.ToJson())
+    }
+"@
+}
