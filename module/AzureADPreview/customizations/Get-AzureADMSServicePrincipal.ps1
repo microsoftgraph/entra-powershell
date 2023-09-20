9f568bed9c934fe8d0b84a40cc08eef77302e749
Ashwini Karke <ashwini.karke@perennialsys.com>
2023-09-20 15:31:47 +0530

    Added Get-AzureADMSServicePrincipal

---
+++ module/AzureADPreview/customizations/Get-AzureADMSServicePrincipal.ps1
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADMSServicePrincipal"
+    TargetName = "Get-MgBetaServicePrincipal"
+    Parameters = @(
+        @{
+            SourceName = "SearchString"
+            TargetName = "Filter"
+            ConversionType = "ScriptBlock"
+            SpecialMapping = @"
+`$Value = "displayName eq '`$TmpValue' or startswith(displayName,'`$TmpValue')"
+"@
+        }
+    )
+    Outputs = $null
+}
