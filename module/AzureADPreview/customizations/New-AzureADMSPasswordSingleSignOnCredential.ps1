ae9c915ebee7c4a755befbd744d6524a4b27839f
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-09-27 15:45:50 +0530

    Added special mapping

--- module/AzureADPreview/customizations/New-AzureADMSPasswordSingleSignOnCredential.ps1
+++ module/AzureADPreview/customizations/New-AzureADMSPasswordSingleSignOnCredential.ps1
@@ -3,42 +3,22 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "New-AzureADMSPasswordSingleSignOnCredential"
-    TargetName = $null
-    Parameters = $null
-    Outputs = $null
-    CustomScript = @"
-    `$params = @{}
-    if(`$null -ne `$PSBoundParameters["ObjectId"])
-    {
-        `$params["ServicePrincipalId"] = `$PSBoundParameters["ObjectId"]
-    }
-    if(`$null -ne `$PSBoundParameters["PasswordSSOCredential"])
-    {
-        `$params["PasswordSSOCredential"] = `$PSBoundParameters["PasswordSSOCredential"] 
-    }
-    `$headers = @{
-        "Content-Type" = "application/json"
-    }
-    `$params["Uri"] = "https://graph.microsoft.com/beta/serviceprincipals/"+`$params.ServicePrincipalId+"/getPasswordSingleSignOnCredentials"
-    `$params["Method"] = "POST"
-    `$body = `$params["PasswordSSOCredential"] | ConvertTo-Json
-
-    if(`$PSBoundParameters.ContainsKey("Debug"))
-    {
-        `$params["Debug"] = `$Null
-    }
-    if(`$PSBoundParameters.ContainsKey("Verbose"))
-    {
-        `$params["Verbose"] = `$Null
-    }
-
-    Write-Debug("============================ TRANSFORMATIONS ============================")
-    `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
-    Write-Debug("=========================================================================`n")
-    
-    `$response = Invoke-GraphRequest -Uri `$params.uri -Method `$params.method -Headers `$headers -Body `$body 
-    `$result = `$response | ConvertFrom-Json      
-    `$result | Add-Member -MemberType AliasProperty -Value '@odata.context' -Name 'odata.context' -Force
-    `$result
+    TargetName = "New-MgBetaServicePrincipalPasswordSingleSignOnCredential"
+    Parameters = @(
+        @{
+            SourceName = "ObjectId"
+            TargetName = "ServicePrincipalId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        },
+        @{
+            SourceName = "PasswordSSOCredential"
+            TargetName = "BodyParameter"
+            ConversionType = "ScriptBlock"
+            SpecialMapping = @"
+`$Value = `$TmpValue | ConvertTo-Json
 "@
+        }
+    )
+    Outputs = $null
 }
