e53c4720ad04ae00adbb3498708f4aa65c19e0df
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-10-13 16:55:47 +0530

    Get-EntraBetaPasswordPolicy

---
+++ module/AzureADPreview/AdditionalFunctions/Get-EntraBetaPasswordPolicy.ps1
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+function Get-EntraBetaPasswordPolicy {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)][System.String] $DomainName
+    )
+
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{}
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $Null
+        }
+        if ($null -ne $PSBoundParameters["DomainName"]) {
+            $params["DomainId"] = $PSBoundParameters["DomainName"]
+        }
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $Null
+        }
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        $response = Get-MgBetaDomain @params
+        # Create a custom table
+        $customTable = [PSCustomObject]@{
+            "NotificationDays" = $response.PasswordNotificationWindowInDays
+            "ValidityPeriod"      = $response.PasswordValidityPeriodInDays
+        }
+        $customTable 
+    }
+}
