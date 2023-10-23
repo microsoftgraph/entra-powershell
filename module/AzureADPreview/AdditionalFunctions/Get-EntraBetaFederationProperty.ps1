8bf783f04c557a67063a9121080e67ef500a07b9
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-10-23 11:10:54 +0530

    Added get-help

--- module/AzureADPreview/AdditionalFunctions/Get-EntraBetaFederationProperty.ps1
+++ module/AzureADPreview/AdditionalFunctions/Get-EntraBetaFederationProperty.ps1
@@ -2,9 +2,33 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 function Get-EntraBetaFederationProperty {
+    <#
+    .SYNOPSIS
+        For the specified domain, displays the properties of the Active Directory Federation Services 2.0 server and Microsoft Online.
+    
+    
+    .DESCRIPTION
+        The Get-EntraBetaFederationProperty cmdlet gets key settings from both the Active Directory Federation Services 2.0 server and Microsoft Online. You can use this
+    information to troubleshoot authentication problems caused by mismatched settings between the Active Directory Federation Services 2.0 server and Microsoft Online.
+
+    .PARAMETER DomainName
+        The domain name for which the properties from both the Active Directory Federation Services 2.0 server and Microsoft Online will be displayed.
+
+    .PARAMETER SupportMultipleDomain
+       
+
+    .PARAMETER <CommonParameters>
+        This cmdlet supports the common parameters: Verbose, Debug,
+        ErrorAction, ErrorVariable, WarningAction, WarningVariable,
+        OutBuffer, PipelineVariable, and OutVariable. For more information, see 
+        about_CommonParameters (https:/go.microsoft.com/fwlink/?LinkID=113216).
+    
+        
+    #>
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
-        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)][System.String] $DomainName
+        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $false)][System.String] $DomainName,
+        [Parameter(ParameterSetName = "GetQuery", Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $false)][Switch] $SupportMultipleDomain
         )
 
     PROCESS {    
