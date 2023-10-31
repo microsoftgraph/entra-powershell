bdd9cfe891affed9ab29209ae850567055923ec4
varsha <v-varshamane@microsoft.com>
2023-10-31 21:34:30 +0530

    code updates for Get-MsolDomainFederationSettings,Convert-MsolFederatedUser for 1.0 and beta version

--- test/module/AzureAD/General.Tests.ps1
+++ test/module/AzureAD/General.Tests.ps1
@@ -25,7 +25,7 @@ Describe 'Module checks' {
 
     It 'Known number translated commands' {
         $module = Get-Module -Name Microsoft.Graph.Entra
-        $module.ExportedCommands.Keys.Count | Should -Be 218
+        $module.ExportedCommands.Keys.Count | Should -Be 220
     }
 
     It 'Running a simple command Set-EntraAlias'{
