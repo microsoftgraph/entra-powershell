c3146d83194018b76c5b7021a45266abe8eb3174
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-11-28 14:58:54 +0530

    Get-AzureADGroupOwner

---
+++ module/AzureAD/customizations/Get-AzureADGroupOwner.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADGroupOwner"
+    TargetName = "Get-MgBetaGroupOwner"
+    Parameters = @(
+        @{
+            SourceName = "ObjectId"
+            TargetName = "GroupId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+    )
+    Outputs = $null
+}
