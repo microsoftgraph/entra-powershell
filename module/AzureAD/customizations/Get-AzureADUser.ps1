bf51253851c8bb1bc3176d998ec2f5190e30d750
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-11-29 16:06:52 +0530

    Added properties

--- module/AzureAD/customizations/Get-AzureADUser.ps1
+++ module/AzureAD/customizations/Get-AzureADUser.ps1
@@ -3,16 +3,65 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADUser"
-    TargetName = "Get-MgBetaUser"
-    Parameters = @(
-        @{
-            SourceName = "SearchString"
-            TargetName = "Filter"
-            ConversionType = "ScriptBlock"
-            SpecialMapping = @"
-`$Value = "userPrincipalName eq '`$TmpValue' or (state eq '`$TmpValue' or (mailNickName eq '`$TmpValue' or (mail eq '`$TmpValue' or (jobTitle eq '`$TmpValue' or (displayName eq '`$TmpValue' or (startswith(displayName,'`$TmpValue') or (department eq '`$TmpValue' or (country eq '`$TmpValue' or city eq '`$TmpValue'))))))))"
-"@
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{SearchString = "Filter"; ObjectId = "Id"}
+        if($null -ne $PSBoundParameters["SearchString"])
+        {
+            $TmpValue = $PSBoundParameters["SearchString"]
+            $Value = "userPrincipalName eq '$TmpValue' or (state eq '$TmpValue' or (mailNickName eq '$TmpValue' or (mail eq '$TmpValue' or (jobTitle eq '$TmpValue' or (displayName eq '$TmpValue' or (startswith(displayName,'$TmpValue') or (department eq '$TmpValue' or (country eq '$TmpValue' or city eq '$TmpValue'))))))))"
+            $params["Filter"] = $Value
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["UserId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($null -ne $PSBoundParameters["Filter"])
+        {
+            $TmpValue = $PSBoundParameters["Filter"]
+            foreach($i in $keysChanged.GetEnumerator()){
+                $TmpValue = $TmpValue.Replace($i.Key, $i.Value)
+            }
+            $Value = $TmpValue
+            $params["Filter"] = $Value
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $Null
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgUser @params -Property AccountEnabled, AgeGroup, AssignedLicenses, AssignedPlans, City, CompanyName, ConsentProvidedForMinor, Country, CreationType, DeletionTimestamp, Department, DirSyncEnabled, DisplayName, ExtensionProperty, FacsimileTelephoneNumber, GivenName, ImmutableId, IsCompromised, JobTitle, LastDirSyncTime, LegalAgeGroupClassification, Mail, MailNickName, Mobile, ObjectId, ObjectType, OnPremisesSecurityIdentifier, OtherMails, PasswordPolicies, PasswordProfile, PhysicalDeliveryOfficeName, PostalCode, PreferredLanguage, ProvisionedPlans, ProvisioningErrors, ProxyAddresses, RefreshTokensValidFromDateTime, ShowInAddressList, SignInNames, SipProxyAddress, State, StreetAddress, Surname, BusinessPhones, UsageLocation, UserPrincipalName, UserState, UserStateChangedOn, UserType
+        
+        $response | ForEach-Object {
+            if($null -ne $_) {
+            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+    
+            }
+        }
+        $response | ConvertTo-Json | ConvertFrom-Json
+        }
+'@
 }
