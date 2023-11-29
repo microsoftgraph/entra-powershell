12d7c891f9158ab2c44fc1d48f8db79c0f3148d8
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-11-29 16:56:44 +0530

    Added customization

---
+++ module/AzureAD/customizations/Get-AzureADSubscribedSku.ps1
@@ -0,0 +1,34 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADSubscribedSku"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["SubscribedSkuId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================
+")
+        
+        $response = Get-MgSubscribedSku @params
+        $response | ConvertTo-Json | ConvertFrom-Json
+        }
+'@
+}
