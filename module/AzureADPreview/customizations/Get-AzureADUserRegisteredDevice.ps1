929e57fd1c1fc2c7a8571da29c880fa713cdb66d
varsha <v-varshamane@microsoft.com>
2023-11-30 23:23:05 +0530

    fixed cmdlets Get-AzureADUserAppRoleAssignment-UserRegisteredDevice

--- module/AzureADPreview/customizations/Get-AzureADUserRegisteredDevice.ps1
+++ module/AzureADPreview/customizations/Get-AzureADUserRegisteredDevice.ps1
@@ -3,14 +3,48 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADUserRegisteredDevice"
-    TargetName = "Get-MgBetaUserOwnedDevice"
-    Parameters = @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "UserId"
-            ConversionType = "Name"
-            SpecialMapping = $null
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["UserId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $Null
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgBetaUserRegisteredDevice @params
+        $response | ForEach-Object {
+            if($null -ne $_) {
+            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+            }
+        }
+        $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+        } 
+'@
 }
