3d3f87a9868d25b0a7d9edb755f07ad27a49351a
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-01 10:37:56 +0530

    Added Get-AzureADUserOwnedDevice

--- module/AzureADPreview/customizations/Get-AzureADUserOwnedDevice.ps1
+++ module/AzureADPreview/customizations/Get-AzureADUserOwnedDevice.ps1
@@ -1,13 +1,51 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADUserOwnedDevice"
-    TargetName = "Get-MgBetaUserOwnedDevice"
-    Parameters = @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "UserId"
-            ConversionType = "Name"
-            SpecialMapping = $null
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["UserId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $Null
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgBetaUserOwnedDevice @params
+        $response | ForEach-Object {
+            if($null -ne $_) {
+            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+    
+            }
+        }
+        $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+        }
+'@
 }
