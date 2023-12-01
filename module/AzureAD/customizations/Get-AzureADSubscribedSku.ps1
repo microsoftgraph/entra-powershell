3a2c69ca10565cdf8a361c2c2c9e7754f29ccad7
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-01 15:45:04 +0530

    Resolved wrapping issue

--- module/AzureAD/customizations/Get-AzureADSubscribedSku.ps1
+++ module/AzureAD/customizations/Get-AzureADSubscribedSku.ps1
@@ -6,29 +6,37 @@
     TargetName = $null
     Parameters = $null
     Outputs = $null
-    CustomScript = @'
+    CustomScript = @"
     PROCESS {    
-        $params = @{}
-        $keysChanged = @{ObjectId = "Id"}
-        if($PSBoundParameters.ContainsKey("Verbose"))
+        `$params = @{}
+        `$keysChanged = @{ObjectId = "Id"}
+        if(`$PSBoundParameters.ContainsKey("Verbose"))
         {
-            $params["Verbose"] = $Null
+            `$params["Verbose"] = `$Null
         }
-        if($null -ne $PSBoundParameters["ObjectId"])
+        if(`$null -ne `$PSBoundParameters["ObjectId"])
         {
-            $params["SubscribedSkuId"] = $PSBoundParameters["ObjectId"]
+            `$params["SubscribedSkuId"] = `$PSBoundParameters["ObjectId"]
         }
-        if($PSBoundParameters.ContainsKey("Debug"))
+        if(`$PSBoundParameters.ContainsKey("Debug"))
         {
-            $params["Debug"] = $Null
+            `$params["Debug"] = `$Null
         }
         Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-        Write-Debug("=========================================================================
-")
+        `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
         
-        $response = Get-MgSubscribedSku @params
-        $response | ConvertTo-Json | ConvertFrom-Json
+        `$response = Get-MgSubscribedSku @params
+        `$response | ForEach-Object {
+            if (`$null -ne `$_) {
+                `$propsToConvert = @('PrepaidUnits')
+                foreach (`$prop in `$propsToConvert) {
+                    `$value = `$_.`$prop | ConvertTo-Json | ConvertFrom-Json
+                    `$_ | Add-Member -MemberType NoteProperty -Name `$prop -Value (`$value) -Force
+                }
+            }
         }
-'@
+        `$response
+        }
+"@
 }
