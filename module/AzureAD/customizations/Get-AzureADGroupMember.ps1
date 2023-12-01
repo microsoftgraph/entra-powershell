1d9bbd03fdee025e145ed8ee3db16f8861d2d4d7
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-01 17:56:45 +0530

    Get-AzureADGroupMember

--- module/AzureAD/customizations/Get-AzureADGroupMember.ps1
+++ module/AzureAD/customizations/Get-AzureADGroupMember.ps1
@@ -3,14 +3,125 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADGroupMember"
-    TargetName = "Get-MgBetaGroupMember"
-    Parameters = @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "GroupId"
-            ConversionType = "Name"
-            SpecialMapping = $null
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["GroupId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $Null
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgGroupMember @params | Select-Object -Property Id, DeletedDateTime,
+        @{
+            Name = 'accountEnabled'
+            Expression = {$_.AdditionalProperties.accountEnabled}
+        },
+        @{
+            Name = 'assignedLicenses'
+            Expression = {$_.AdditionalProperties.assignedLicenses}
+        },
+        @{
+            Name = 'assignedPlans'
+            Expression = {$_.AdditionalProperties.assignedPlans}
+        },
+        @{
+            Name = 'country'
+            Expression = {$_.AdditionalProperties.country}
+        },
+        @{
+            Name = 'displayName'
+            Expression = {$_.AdditionalProperties.displayName}
+        },
+        @{
+            Name = 'givenName'
+            Expression = {$_.AdditionalProperties.givenName}
+        },
+        @{
+            Name = 'mail'
+            Expression = {$_.AdditionalProperties.mail}
+        },
+        @{
+            Name = 'mailNickname'
+            Expression = {$_.AdditionalProperties.mailNickname}
+        },
+        @{
+            Name = 'mobilePhone'
+            Expression = {$_.AdditionalProperties.mobilePhone}
+        },
+        @{
+            Name = 'otherMails'
+            Expression = {$_.AdditionalProperties.otherMails}
+        },
+        @{
+            Name = 'preferredLanguage'
+            Expression = {$_.AdditionalProperties.preferredLanguage}
+        },
+        @{
+            Name = 'provisionedPlans'
+            Expression = {$_.AdditionalProperties.provisionedPlans}
+        },
+        @{
+            Name = 'onPremisesProvisioningErrors'
+            Expression = {$_.AdditionalProperties.onPremisesProvisioningErrors}
+        },
+        @{
+            Name = 'proxyAddresses'
+            Expression = {$_.AdditionalProperties.proxyAddresses}
+        },
+        @{
+            Name = 'refreshTokensValidFromDateTime'
+            Expression = {$_.AdditionalProperties.refreshTokensValidFromDateTime}
+        },
+        @{
+            Name = 'surname'
+            Expression = {$_.AdditionalProperties.surname}
+        },
+        @{
+            Name = 'businessPhones'
+            Expression = {$_.AdditionalProperties.businessPhones}
+        },
+        @{
+            Name = 'userType'
+            Expression = {$_.AdditionalProperties.userType}
+        },
+        @{
+            Name = 'userPrincipalName'
+            Expression = {$_.AdditionalProperties.userPrincipalName}
+        }
+    
+        $response | ForEach-Object {
+            if($null -ne $_) {
+            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+            }
+        }
+        $response
+        }
+'@
 }
