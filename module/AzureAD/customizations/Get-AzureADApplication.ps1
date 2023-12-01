c3dd2a01a9df84a96be29f2590d3d35b93e22ae6
Uzair Ansari (Perennial Systems Inc) <v-uansari@microsoft.com>
2023-12-01 16:37:30 +0530

    refactor and table format issue

--- module/AzureAD/customizations/Get-AzureADApplication.ps1
+++ module/AzureAD/customizations/Get-AzureADApplication.ps1
@@ -6,60 +6,71 @@
     TargetName = $null
     Parameters = $null
     Outputs = $null
-    CustomScript = @"
+    CustomScript = @'
     PROCESS {  
-    `$params = @{}
-    `$keysChanged = @{SearchString = "Filter"; ObjectId = "Id"}
-    if(`$null -ne `$PSBoundParameters["SearchString"])
+    $params = @{}
+    $keysChanged = @{SearchString = "Filter"; ObjectId = "Id"}
+    if($null -ne $PSBoundParameters["SearchString"])
     {
-        `$TmpValue = `$PSBoundParameters["SearchString"]
-        `$Value = "displayName eq '`$TmpValue' or startswith(displayName,'`$TmpValue')"
-        `$params["Filter"] = `$Value
+        $TmpValue = $PSBoundParameters["SearchString"]
+        $Value = "displayName eq '$TmpValue' or startswith(displayName,'$TmpValue')"
+        $params["Filter"] = $Value
     }
-    if(`$null -ne `$PSBoundParameters["ObjectId"])
+    if($null -ne $PSBoundParameters["ObjectId"])
     {
-        `$params["ApplicationId"] = `$PSBoundParameters["ObjectId"]
+        $params["ApplicationId"] = $PSBoundParameters["ObjectId"]
     }
-    if(`$null -ne `$PSBoundParameters["Filter"])
+    if($null -ne $PSBoundParameters["Filter"])
     {
-        `$TmpValue = `$PSBoundParameters["Filter"]
-        foreach(`$i in `$keysChanged.GetEnumerator()){
-            `$TmpValue = `$TmpValue.Replace(`$i.Key, `$i.Value)
+        $TmpValue = $PSBoundParameters["Filter"]
+        foreach($i in $keysChanged.GetEnumerator()){
+            $TmpValue = $TmpValue.Replace($i.Key, $i.Value)
         }
-        `$Value = `$TmpValue
-        `$params["Filter"] = `$Value
+        $Value = $TmpValue
+        $params["Filter"] = $Value
     }
-    if(`$PSBoundParameters.ContainsKey("Verbose"))
+    if($PSBoundParameters.ContainsKey("Verbose"))
     {
-        `$params["Verbose"] = `$Null
+        $params["Verbose"] = $Null
     }
-    if(`$null -ne `$PSBoundParameters["All"])
+    if($null -ne $PSBoundParameters["All"])
     {
-        if(`$PSBoundParameters["All"])
+        if($PSBoundParameters["All"])
         {
-            `$params["All"] = `$Null
+            $params["All"] = $Null
         }
     }
-    if(`$PSBoundParameters.ContainsKey("Debug"))
+    if($PSBoundParameters.ContainsKey("Debug"))
     {
-        `$params["Debug"] = `$Null
+        $params["Debug"] = $Null
     }
-    if(`$null -ne `$PSBoundParameters["Top"])
+    if($null -ne $PSBoundParameters["Top"])
     {
-        `$params["Top"] = `$PSBoundParameters["Top"]
+        $params["Top"] = $PSBoundParameters["Top"]
     }
 
     Write-Debug("============================ TRANSFORMATIONS ============================")
-        `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================``n")
     
-    `$response = Get-MgApplication @params
-    `$response | ForEach-Object {
-        if(`$null -ne `$_) {
-        Add-Member -InputObject `$_ -MemberType AliasProperty -Name ObjectId -Value Id
-        `$_ | ConvertTo-Json | ConvertFrom-Json
+    $response = Get-MgApplication @params
+    $response | ForEach-Object {
+        if($null -ne $_) {
+        Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+        $propsToConvert = @(
+             'AddIns','Logo','AppRoles','GroupMembershipClaims','IdentifierUris','Info',
+             'IsDeviceOnlyAuthSupported','KeyCredentials','Oauth2RequirePostResponse','OptionalClaims',
+             'ParentalControlSettings','PasswordCredentials','Api','PublicClient',
+             'PublisherDomain','Web','RequiredResourceAccess','SignInAudience')
+             
+                foreach ($prop in $propsToConvert) {
+                    $value = $_.$prop | ConvertTo-Json | ConvertFrom-Json
+                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                }
         }
     }
+
+    $response
 }    
-"@
+'@
 }
