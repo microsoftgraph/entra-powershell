85054a5287ed48097951d13e65100172860ef965
Uzair Ansari (Perennial Systems Inc) <v-uansari@microsoft.com>
2023-12-05 11:53:18 +0530

    customscript changed from double quote to single quote

--- module/AzureADPreview/customizations/New-AzureADApplication.ps1
+++ module/AzureADPreview/customizations/New-AzureADApplication.ps1
@@ -6,211 +6,211 @@
     TargetName = $null
     Parameters = $null
     outputs = $null
-    CustomScript = @"
-    `$params = @{}
-    if(`$PSBoundParameters.ContainsKey("Verbose"))
+    CustomScript = @'
+    $params = @{}
+    if($PSBoundParameters.ContainsKey("Verbose"))
     {
-        `$params["Verbose"] = `$Null
+        $params["Verbose"] = $Null
     }
-    if(`$null -ne `$PSBoundParameters["DisplayName"])
+    if($null -ne $PSBoundParameters["DisplayName"])
     {
-        `$params["DisplayName"] = `$PSBoundParameters["DisplayName"]
+        $params["DisplayName"] = $PSBoundParameters["DisplayName"]
     }
     
-    if(`$null -ne `$PSBoundParameters["AddIns"])
+    if($null -ne $PSBoundParameters["AddIns"])
     {
-        `$a = @()
-        foreach(`$value in `$PSBoundParameters["AddIns"])
+        $a = @()
+        foreach($value in $PSBoundParameters["AddIns"])
         {
-           `$Temp = `$value | ConvertTo-Json
-            `$hash = @{}
+           $Temp = $value | ConvertTo-Json
+            $hash = @{}
 
-            (ConvertFrom-Json `$Temp).psobject.properties | Foreach { `$hash[`$_.Name] = `$_.Value }
-            `$a += `$hash
+            (ConvertFrom-Json $Temp).psobject.properties | Foreach { $hash[$_.Name] = $_.Value }
+            $a += $hash
         }
 
-        `$params["AddIns"] = `$a
+        $params["AddIns"] = $a
     }
 
-    if(`$null -ne `$PSBoundParameters["AppLogoUrl"])
+    if($null -ne $PSBoundParameters["AppLogoUrl"])
     {
-        `$params["LogoInputFile"] = `$PSBoundParameters["AppLogoUrl"]
+        $params["LogoInputFile"] = $PSBoundParameters["AppLogoUrl"]
     }
-    if(`$null -ne `$PSBoundParameters["AppRoles"])
+    if($null -ne $PSBoundParameters["AppRoles"])
     {
-        `$a = @()
-        `$input = `$PSBoundParameters["AppRoles"]
-        foreach(`$value in `$input)
+        $a = @()
+        $input = $PSBoundParameters["AppRoles"]
+        foreach($value in $input)
         {
-           `$Temp = `$value | ConvertTo-Json
-            `$hash = @{}
+           $Temp = $value | ConvertTo-Json
+            $hash = @{}
 
-            (ConvertFrom-Json `$Temp).psobject.properties | Foreach { `$hash[`$_.Name] = `$_.Value }
-            `$a += `$hash
+            (ConvertFrom-Json $Temp).psobject.properties | Foreach { $hash[$_.Name] = $_.Value }
+            $a += $hash
         }
         
-        `$params["AppRoles"] = `$a
+        $params["AppRoles"] = $a
     }
-    if(`$null -ne `$PSBoundParameters["GroupMembershipClaims"])
+    if($null -ne $PSBoundParameters["GroupMembershipClaims"])
     {
-        `$params["GroupMembershipClaims"] =  `$PSBoundParameters["GroupMembershipClaims"]
+        $params["GroupMembershipClaims"] =  $PSBoundParameters["GroupMembershipClaims"]
     }
 
-    `$Web = @{}
+    $Web = @{}
 
-    if(`$null -ne `$PSBoundParameters["Homepage"])
+    if($null -ne $PSBoundParameters["Homepage"])
     {
-        `$Web["HomePageUrl"]  =  `$PSBoundParameters["Homepage"]
+        $Web["HomePageUrl"]  =  $PSBoundParameters["Homepage"]
     }
     
-    if(`$null -ne `$PSBoundParameters["ReplyUrls"])
+    if($null -ne $PSBoundParameters["ReplyUrls"])
     {
-        `$Web["RedirectUris"]  =  `$PSBoundParameters["ReplyUrls"]
+        $Web["RedirectUris"]  =  $PSBoundParameters["ReplyUrls"]
     }
     
-    if(`$null -ne `$PSBoundParameters["IdentifierUris"])
+    if($null -ne $PSBoundParameters["IdentifierUris"])
     {
-        `$params["IdentifierUris"] =  `$PSBoundParameters["IdentifierUris"]
+        $params["IdentifierUris"] =  $PSBoundParameters["IdentifierUris"]
     }
     
-    if(`$null -ne `$PSBoundParameters["InformationalUrls"])
+    if($null -ne $PSBoundParameters["InformationalUrls"])
     {
-        `$Temp = `$PSBoundParameters["InformationalUrls"]
-        `$hash = @{}
+        $Temp = $PSBoundParameters["InformationalUrls"]
+        $hash = @{}
 
-        `$hash["MarketingUrl"] = `$Temp.Marketing
-        `$hash["PrivacyStatementUrl"] = `$Temp.Privacy
-        `$hash["SupportUrl"] = `$Temp.Support 
-        `$hash["TermsOfServiceUrl"] = `$Temp.TermsOfService
+        $hash["MarketingUrl"] = $Temp.Marketing
+        $hash["PrivacyStatementUrl"] = $Temp.Privacy
+        $hash["SupportUrl"] = $Temp.Support 
+        $hash["TermsOfServiceUrl"] = $Temp.TermsOfService
         
-        `$params["Info"] =  `$hash
+        $params["Info"] =  $hash
     }
 
-    if(`$null -ne `$PSBoundParameters["IsDeviceOnlyAuthSupported"])
+    if($null -ne $PSBoundParameters["IsDeviceOnlyAuthSupported"])
     {
-        `$params["IsDeviceOnlyAuthSupported"] =  `$PSBoundParameters["IsDeviceOnlyAuthSupported"]
+        $params["IsDeviceOnlyAuthSupported"] =  $PSBoundParameters["IsDeviceOnlyAuthSupported"]
     }
 
-    if(`$null -ne `$PSBoundParameters["KeyCredentials"])
+    if($null -ne $PSBoundParameters["KeyCredentials"])
     {
-        `$a = @()
-        `$input = `$PSBoundParameters["KeyCredentials"]
-        foreach(`$value in `$input)
+        $a = @()
+        $input = $PSBoundParameters["KeyCredentials"]
+        foreach($value in $input)
         {
-            `$hash = @{
-                CustomKeyIdentifier= `$value.CustomKeyIdentifier
-                EndDateTime =  `$value.EndDate
-                Key= `$value.Value
-                StartDateTime= `$value.StartDate
-                Type= `$value.Type
-                Usage= `$value.Usage
+            $hash = @{
+                CustomKeyIdentifier= $value.CustomKeyIdentifier
+                EndDateTime =  $value.EndDate
+                Key= $value.Value
+                StartDateTime= $value.StartDate
+                Type= $value.Type
+                Usage= $value.Usage
             }
 
-            `$a += `$hash
+            $a += $hash
         }
         
-        `$params["KeyCredentials"] = `$a
+        $params["KeyCredentials"] = $a
     }
 
-    `$Api = @{}
+    $Api = @{}
 
-    if(`$null -ne `$PSBoundParameters["KnownClientApplications"])
+    if($null -ne $PSBoundParameters["KnownClientApplications"])
     {
-        `$Api["KnownClientApplications"] =  `$PSBoundParameters["KnownClientApplications"]
+        $Api["KnownClientApplications"] =  $PSBoundParameters["KnownClientApplications"]
     }
-    if(`$null -ne `$PSBoundParameters["LogoutUrl"])
+    if($null -ne $PSBoundParameters["LogoutUrl"])
     {
-        `$Web["LogoutUrl"] =  `$PSBoundParameters["LogoutUrl"]
-    }
-
-    if(`$null -ne `$PSBoundParameters["Oauth2RequirePostResponse"])
-    {
-        `$params["Oauth2RequirePostResponse"] =  `$PSBoundParameters["Oauth2RequirePostResponse"]
-    }
-    if(`$null -ne `$PSBoundParameters["ParentalControlSettings"])
-    {
-        `$Temp = `$PSBoundParameters["ParentalControlSettings"] | ConvertTo-Json
-        `$hash = @{}
-
-        (ConvertFrom-Json `$Temp).psobject.properties | Foreach { `$hash[`$_.Name] = `$_.Value }
-
-        `$params["ParentalControlSettings"] = `$hash 
+        $Web["LogoutUrl"] =  $PSBoundParameters["LogoutUrl"]
     }
 
-    if(`$null -ne `$PSBoundParameters["PasswordCredentials"])
+    if($null -ne $PSBoundParameters["Oauth2RequirePostResponse"])
     {
-        `$a = @()
-        `$input = `$PSBoundParameters["PasswordCredentials"]
-        foreach(`$value in `$input)
+        $params["Oauth2RequirePostResponse"] =  $PSBoundParameters["Oauth2RequirePostResponse"]
+    }
+    if($null -ne $PSBoundParameters["ParentalControlSettings"])
+    {
+        $Temp = $PSBoundParameters["ParentalControlSettings"] | ConvertTo-Json
+        $hash = @{}
+
+        (ConvertFrom-Json $Temp).psobject.properties | Foreach { $hash[$_.Name] = $_.Value }
+
+        $params["ParentalControlSettings"] = $hash 
+    }
+
+    if($null -ne $PSBoundParameters["PasswordCredentials"])
+    {
+        $a = @()
+        $input = $PSBoundParameters["PasswordCredentials"]
+        foreach($value in $input)
         {
-            `$hash = @{
-                CustomKeyIdentifier= `$value.CustomKeyIdentifier
-                EndDateTime =  `$value.EndDate
-                SecretText= `$value.Value
-                StartDateTime= `$value.StartDate
+            $hash = @{
+                CustomKeyIdentifier= $value.CustomKeyIdentifier
+                EndDateTime =  $value.EndDate
+                SecretText= $value.Value
+                StartDateTime= $value.StartDate
                 }
-            `$a += `$hash
+            $a += $hash
         }
         
-        `$params["PasswordCredentials"] = `$a
+        $params["PasswordCredentials"] = $a
     }
 
-    if(`$null -ne `$PSBoundParameters["PreAuthorizedApplications"])
+    if($null -ne $PSBoundParameters["PreAuthorizedApplications"])
     {
-        `$a = @()
-        `$input = `$PSBoundParameters["PreAuthorizedApplications"]
-        foreach(`$value in `$input)
+        $a = @()
+        $input = $PSBoundParameters["PreAuthorizedApplications"]
+        foreach($value in $input)
         {
-           `$Temp = `$value | ConvertTo-Json
-            `$hash = @{}
+           $Temp = $value | ConvertTo-Json
+            $hash = @{}
 
-            (ConvertFrom-Json `$Temp).psobject.properties | Foreach { `$hash[`$_.Name] = `$_.Value }
-            `$a += `$hash
+            (ConvertFrom-Json $Temp).psobject.properties | Foreach { $hash[$_.Name] = $_.Value }
+            $a += $hash
         }
         
-        `$Api["PreAuthorizedApplications"] = `$a   
+        $Api["PreAuthorizedApplications"] = $a   
     }
 
-    if(`$null -ne `$PSBoundParameters["PublicClient"])
+    if($null -ne $PSBoundParameters["PublicClient"])
     {
-        `$params["PublicClient"] =  `$PSBoundParameters["PublicClient"]
+        $params["PublicClient"] =  $PSBoundParameters["PublicClient"]
     }
-    if(`$null -ne `$PSBoundParameters["PublisherDomain"])
+    if($null -ne $PSBoundParameters["PublisherDomain"])
     {
-        `$params["PublisherDomain"] =  `$PSBoundParameters["PublisherDomain"]
+        $params["PublisherDomain"] =  $PSBoundParameters["PublisherDomain"]
     }
 
-    if(`$null -ne `$PSBoundParameters["RequiredResourceAccess"])
+    if($null -ne $PSBoundParameters["RequiredResourceAccess"])
     {
-        `$input = `$PSBoundParameters["RequiredResourceAccess"]
+        $input = $PSBoundParameters["RequiredResourceAccess"]
         
-        `$params["RequiredResourceAccess"] =  `$input | ConvertTo-Json 
+        $params["RequiredResourceAccess"] =  $input | ConvertTo-Json 
     }
 
-    if(`$null -ne `$PSBoundParameters["SamlMetadataUrl"])
+    if($null -ne $PSBoundParameters["SamlMetadataUrl"])
     {
-        `$params["SamlMetadataUrl"] =  `$PSBoundParameters["SamlMetadataUrl"]
+        $params["SamlMetadataUrl"] =  $PSBoundParameters["SamlMetadataUrl"]
     }
-    if(`$null -ne `$PSBoundParameters["SignInAudience"])
+    if($null -ne $PSBoundParameters["SignInAudience"])
     {
-        `$params["SignInAudience"] =  `$PSBoundParameters["SignInAudience"]
+        $params["SignInAudience"] =  $PSBoundParameters["SignInAudience"]
     }
 
-    `$params["Web"] = `$Web
-    `$params["Api"] = `$Api
+    $params["Web"] = $Web
+    $params["Api"] = $Api
 
-    if(`$PSBoundParameters.ContainsKey("Debug"))
+    if($PSBoundParameters.ContainsKey("Debug"))
     {
-        `$params["Debug"] = `$Null
+        $params["Debug"] = $Null
     }
 
     Write-Debug("============================ TRANSFORMATIONS ============================")
-    $`params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
     Write-Debug("=========================================================================``n")
 
-    `$response = New-MgBetaApplication @params
+    $response = New-MgBetaApplication @params
 
     
-    `$response
-"@
+    $response
+'@
 }
