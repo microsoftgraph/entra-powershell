9bab7f4e219f86eb926bfa13f74ff65c7413a8e3
v-akarke <142799789+v-akarke@users.noreply.github.com>
2023-12-06 10:35:20 -0800

    Get-AzureADServiceAppRoleAssignedTo, Get-AzureADServicePrincipalOAuth2PermissionGrant, Get-AzureADUserAppRoleAssignment, Get-AzureADUserLicenseDetail, Get-AzureADUserRegisteredDevice (#217)

--- module/AzureAD/customizations/Get-AzureADServicePrincipalOAuth2PermissionGrant.ps1
+++ module/AzureAD/customizations/Get-AzureADServicePrincipalOAuth2PermissionGrant.ps1
@@ -2,9 +2,9 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 @{
-    SourceName = "Get-AzureADServiceAppRoleAssignedTo"
-    TargetName = "Get-MgServicePrincipalAppRoleAssignment"
-    Parameters =  @(
+    SourceName = "Get-AzureADServicePrincipalOAuth2PermissionGrant"
+    TargetName = "Get-MgServicePrincipalOauth2PermissionGrant"
+    Parameters = @(
         @{
             SourceName = "ObjectId"
             TargetName = "ServicePrincipalId"
