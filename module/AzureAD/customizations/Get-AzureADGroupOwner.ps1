1d51ad0637b7fdb3b19bc4af6ddce0a789468bea
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-07 17:05:31 +0530

    Added customization

--- module/AzureAD/customizations/Get-AzureADGroupOwner.ps1
+++ module/AzureAD/customizations/Get-AzureADGroupOwner.ps1
@@ -3,14 +3,47 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADGroupOwner"
-    TargetName = "Get-MgBetaGroupOwner"
-    Parameters = @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "GroupId"
-            ConversionType = "Name"
-            SpecialMapping = $null
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'   
+    PROCESS {    
+        $params = @{}
+        $topCount = $null
+        $baseUri = 'https://graph.microsoft.com/v1.0/groups'
+        $properties = '$select=*'
+        $Method = "GET"
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["GroupId"] = $PSBoundParameters["ObjectId"]
+            $URI = "$baseUri/$($params.GroupId)/owners?$properties"
+        }
+        
+        if($null -ne $PSBoundParameters["All"])
+        {
+            $URI = "$baseUri/$($params.GroupId)/owners?$properties"
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $topCount = $PSBoundParameters["Top"]
+            $URI = "$baseUri/$($params.GroupId)/owners?`$top=$topCount&$properties"
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = (Invoke-GraphRequest -Uri $URI -Method $Method).value
+        $response | ConvertTo-Json | ConvertFrom-Json
+        }
+'@
 }
