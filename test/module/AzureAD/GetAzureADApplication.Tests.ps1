91b312b71f309cf5d68e0f3a9835cfd9a360a6e6
Uzair Ansari (Perennial Systems Inc) <v-uansari@microsoft.com>
2023-12-20 19:52:11 +0530

    poc of test cases for Get-AzureADApplication

--- test/module/AzureAD/GetAzureADApplication.Tests.ps1
+++ test/module/AzureAD/GetAzureADApplication.Tests.ps1
@@ -1,45 +1,301 @@
+BeforeAll {
+  Import-Module -Name Microsoft.Graph.Applications -Force
+
+  if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+      Import-Module ..\..\..\bin\Microsoft.Graph.Entra.psm1 -Force
+  }
+}
+
 # Test cases for Get-EntraApplication
 Describe "Get-EntraApplication" {
-  BeforeAll {
-    Import-Module -Name Microsoft.Graph.Applications -Force
-
-    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
-        Import-Module ..\..\..\bin\Microsoft.Graph.Entra.psm1 -Force
-    }
-  }
   Context "Test for Get-EntraApplication" {
 
-      It "Should return all application" {
+      It "Should return all applications" {
       # Mock the Get-MgApplication cmdlet for this specific test case
         $scriptblock = {
-        # Your mock implementation here
-        param($params)
-        Write-Host "Mocking Get-MgApplication with parameters: $($params | ConvertTo-Json -Depth 3)"
+        
+        param($args)
+        Write-Host "Mocking Get-MgApplication with parameters: $($args | ConvertTo-Json -Depth 3)"
 
         return @(
             [PSCustomObject]@{
-                "CreatedDateTime" = "/Date(1699611758000)/"
-                "CreatedOnBehalfOf" = @{
-                    "DeletedDateTime" = $null
-                    "Id" = $null
-                }
-                "DisplayName" = "test app"
-                "Id" = "010cc9b5-fce9-485e-9566-c68debafac5f"
+              "AddIns"                       = @{}
+              "Api"                          = @{AcceptMappedClaims=$null; KnownClientApplications=$null;
+                                             Oauth2PermissionScopes=$null; PreAuthorizedApplications=$null;
+                                             RequestedAccessTokenVersion=2}
+              "AppId"                        = "5f783237-3457-45d8-93e7-a0edb1cfbfd1"
+              "AppManagementPolicies"        = $null
+              "AppRoles"                     = $null
+              "ApplicationTemplateId"        = $null
+              "CreatedDateTime"              = $null
+              "CreatedOnBehalfOf"            = @{DeletedDateTime=""; Id=""}
+              "DefaultRedirectUri"           = $null
+              "DeletedDateTime"              = $null
+              "Description"                  = $null
+              "DisabledByMicrosoftStatus"    = $null
+              "GroupMembershipClaims"        = $null
+              "Id"                           = "010cc9b5-fce9-485e-9566-c68debafac5f"
+              "IdentifierUris"               = @{}
+              "Info"                         = @{LogoUrl=""; MarketingUrl=""; PrivacyStatementUrl=""; SupportUrl=""; TermsOfServiceUrl=""}
+              "IsDeviceOnlyAuthSupported"    = $True
+              "IsFallbackPublicClient"       = $true
+              "KeyCredentials"               = @{}
+              "Logo"                         = $null
+              "Oauth2RequirePostResponse"    = $null
+              "OptionalClaims"               = @{AccessToken=""; IdToken=""; Saml2Token=""}
+              "Owners"                       = $null
+              "ParentalControlSettings"      = @{CountriesBlockedForMinors=$null; LegalAgeGroupRule="Allow"}
+              "PasswordCredentials"          = @{}
+              "PublicClient"                 = @{RedirectUris=$null}
+              "PublisherDomain"              = "M365x99297270.onmicrosoft.com"
+              "RequiredResourceAccess"       = @{}
+              "SamlMetadataUrl"              = $null
+              "ServiceManagementReference"   = $null
+              "SignInAudience"               = "AzureADandPersonalMicrosoftAccount"
+              "Spa"                          = @{RedirectUris=$null}
+              "Synchronization"              = @{Id=""; Jobs=""; Secrets=""; Templates=""}
+              "Tags"                         = @{}
+              "Web"                          = @{HomePageUrl="https://localhost/demoapp"; ImplicitGrantSettings=""; LogoutUrl="";}
             }
         )
-      }     
+        }     
 
-        $null = Mock -CommandName Get-MgApplication -MockWith $scriptBlock -ModuleName Microsoft.Graph.Entra -Verifiable
+          Mock -CommandName Get-MgApplication -MockWith $scriptBlock -ModuleName Microsoft.Graph.Entra
 
           $result = Get-EntraApplication -All $true
           $result | Should -Not -BeNullOrEmpty
-          $result.Count | Should -Be 1
-
-          # Add assertions based on your expected behavior
-          $result | Should -Not -BeNullOrEmpty
-
-          # Verify that the mock was invoked with the correct parameters
-          Assert-MockCalled -CommandName Get-MgApplication -Exactly 1 -Scope It -Verifiable
       }
-  }
+
+      It "Should return specific application" {
+        # Mock the Get-MgApplication cmdlet for this specific test case
+          $scriptblock = {
+          
+          param($args)
+          Write-Host "Mocking Get-MgApplication with parameters: $($args | ConvertTo-Json -Depth 3)"
+  
+          return @(
+              [PSCustomObject]@{
+                "AddIns"                       = @{}
+                "Api"                          = @{AcceptMappedClaims=$null; KnownClientApplications=$null;
+                                               Oauth2PermissionScopes=$null; PreAuthorizedApplications=$null;
+                                               RequestedAccessTokenVersion=2}
+                "AppId"                        = "5f783237-3457-45d8-93e7-a0edb1cfbfd1"
+                "AppManagementPolicies"        = $null
+                "AppRoles"                     = $null
+                "ApplicationTemplateId"        = $null
+                "CreatedDateTime"              = $null
+                "CreatedOnBehalfOf"            = @{DeletedDateTime=""; Id=""}
+                "DefaultRedirectUri"           = $null
+                "DeletedDateTime"              = $null
+                "Description"                  = $null
+                "DisabledByMicrosoftStatus"    = $null
+                "GroupMembershipClaims"        = $null
+                "Id"                           = "010cc9b5-fce9-485e-9566-c68debafac5f"
+                "IdentifierUris"               = @{}
+                "Info"                         = @{LogoUrl=""; MarketingUrl=""; PrivacyStatementUrl=""; SupportUrl=""; TermsOfServiceUrl=""}
+                "IsDeviceOnlyAuthSupported"    = $True
+                "IsFallbackPublicClient"       = $true
+                "KeyCredentials"               = @{}
+                "Logo"                         = $null
+                "Oauth2RequirePostResponse"    = $null
+                "OptionalClaims"               = @{AccessToken=""; IdToken=""; Saml2Token=""}
+                "Owners"                       = $null
+                "ParentalControlSettings"      = @{CountriesBlockedForMinors=$null; LegalAgeGroupRule="Allow"}
+                "PasswordCredentials"          = @{}
+                "PublicClient"                 = @{RedirectUris=$null}
+                "PublisherDomain"              = "M365x99297270.onmicrosoft.com"
+                "RequiredResourceAccess"       = @{}
+                "SamlMetadataUrl"              = $null
+                "ServiceManagementReference"   = $null
+                "SignInAudience"               = "AzureADandPersonalMicrosoftAccount"
+                "Spa"                          = @{RedirectUris=$null}
+                "Synchronization"              = @{Id=""; Jobs=""; Secrets=""; Templates=""}
+                "Tags"                         = @{}
+                "Web"                          = @{HomePageUrl="https://localhost/demoapp"; ImplicitGrantSettings=""; LogoutUrl="";}
+              }
+          )
+          }     
+  
+            Mock -CommandName Get-MgApplication -MockWith $scriptBlock -ModuleName Microsoft.Graph.Entra
+  
+            $result = Get-EntraApplication -ObjectId "010cc9b5-fce9-485e-9566-c68debafac5f"
+            $result | Should -Not -BeNullOrEmpty
+            $result.Id | should -Be @('010cc9b5-fce9-485e-9566-c68debafac5f')
+    }
+
+    It "Should return specific application by searchstring" {
+        # Mock the Get-MgApplication cmdlet for this specific test case
+          $scriptblock = {
+          
+          param($args)
+          Write-Host "Mocking Get-MgApplication with parameters: $($args | ConvertTo-Json -Depth 3)"
+  
+          return @(
+              [PSCustomObject]@{
+                "AddIns"                       = @{}
+                "Api"                          = @{AcceptMappedClaims=$null; KnownClientApplications=$null;
+                                               Oauth2PermissionScopes=$null; PreAuthorizedApplications=$null;
+                                               RequestedAccessTokenVersion=2}
+                "AppId"                        = "5f783237-3457-45d8-93e7-a0edb1cfbfd1"
+                "AppManagementPolicies"        = $null
+                "AppRoles"                     = $null
+                "ApplicationTemplateId"        = $null
+                "CreatedDateTime"              = $null
+                "CreatedOnBehalfOf"            = @{DeletedDateTime=""; Id=""}
+                "DefaultRedirectUri"           = $null
+                "DeletedDateTime"              = $null
+                "Description"                  = $null
+                "DisplayName"                  = "test app"
+                "DisabledByMicrosoftStatus"    = $null
+                "GroupMembershipClaims"        = $null
+                "Id"                           = "010cc9b5-fce9-485e-9566-c68debafac5f"
+                "IdentifierUris"               = @{}
+                "Info"                         = @{LogoUrl=""; MarketingUrl=""; PrivacyStatementUrl=""; SupportUrl=""; TermsOfServiceUrl=""}
+                "IsDeviceOnlyAuthSupported"    = $True
+                "IsFallbackPublicClient"       = $true
+                "KeyCredentials"               = @{}
+                "Logo"                         = $null
+                "Oauth2RequirePostResponse"    = $null
+                "OptionalClaims"               = @{AccessToken=""; IdToken=""; Saml2Token=""}
+                "Owners"                       = $null
+                "ParentalControlSettings"      = @{CountriesBlockedForMinors=$null; LegalAgeGroupRule="Allow"}
+                "PasswordCredentials"          = @{}
+                "PublicClient"                 = @{RedirectUris=$null}
+                "PublisherDomain"              = "M365x99297270.onmicrosoft.com"
+                "RequiredResourceAccess"       = @{}
+                "SamlMetadataUrl"              = $null
+                "ServiceManagementReference"   = $null
+                "SignInAudience"               = "AzureADandPersonalMicrosoftAccount"
+                "Spa"                          = @{RedirectUris=$null}
+                "Synchronization"              = @{Id=""; Jobs=""; Secrets=""; Templates=""}
+                "Tags"                         = @{}
+                "Web"                          = @{HomePageUrl="https://localhost/demoapp"; ImplicitGrantSettings=""; LogoutUrl="";}
+              }
+          )
+          }     
+  
+            Mock -CommandName Get-MgApplication -MockWith $scriptBlock -ModuleName Microsoft.Graph.Entra
+    
+              $result = Get-EntraApplication -SearchString 'test app'
+              $result | Should -Not -BeNullOrEmpty
+              $result.DisplayName | should -Be 'test app'
+    }  
+        
+    It "Should return specific application by filter" {
+          # Mock the Get-MgApplication cmdlet for this specific test case
+            $scriptblock = {
+            # Your mock implementation here
+            param($args)
+            Write-Host "Mocking Get-MgApplication with parameters: $($args | ConvertTo-Json -Depth 3)"
+    
+            return @(
+                [PSCustomObject]@{
+                  "AddIns"                       = @{}
+                  "Api"                          = @{AcceptMappedClaims=$null; KnownClientApplications=$null;
+                                                 Oauth2PermissionScopes=$null; PreAuthorizedApplications=$null;
+                                                 RequestedAccessTokenVersion=2}
+                  "AppId"                        = "5f783237-3457-45d8-93e7-a0edb1cfbfd1"
+                  "AppManagementPolicies"        = $null
+                  "AppRoles"                     = $null
+                  "ApplicationTemplateId"        = $null
+                  "CreatedDateTime"              = $null
+                  "CreatedOnBehalfOf"            = @{DeletedDateTime=""; Id=""}
+                  "DefaultRedirectUri"           = $null
+                  "DeletedDateTime"              = $null
+                  "Description"                  = $null
+                  "DisplayName"                  = "test app"
+                  "DisabledByMicrosoftStatus"    = $null
+                  "GroupMembershipClaims"        = $null
+                  "Id"                           = "010cc9b5-fce9-485e-9566-c68debafac5f"
+                  "IdentifierUris"               = @{}
+                  "Info"                         = @{LogoUrl=""; MarketingUrl=""; PrivacyStatementUrl=""; SupportUrl=""; TermsOfServiceUrl=""}
+                  "IsDeviceOnlyAuthSupported"    = $True
+                  "IsFallbackPublicClient"       = $true
+                  "KeyCredentials"               = @{}
+                  "Logo"                         = $null
+                  "Oauth2RequirePostResponse"    = $null
+                  "OptionalClaims"               = @{AccessToken=""; IdToken=""; Saml2Token=""}
+                  "Owners"                       = $null
+                  "ParentalControlSettings"      = @{CountriesBlockedForMinors=$null; LegalAgeGroupRule="Allow"}
+                  "PasswordCredentials"          = @{}
+                  "PublicClient"                 = @{RedirectUris=$null}
+                  "PublisherDomain"              = "M365x99297270.onmicrosoft.com"
+                  "RequiredResourceAccess"       = @{}
+                  "SamlMetadataUrl"              = $null
+                  "ServiceManagementReference"   = $null
+                  "SignInAudience"               = "AzureADandPersonalMicrosoftAccount"
+                  "Spa"                          = @{RedirectUris=$null}
+                  "Synchronization"              = @{Id=""; Jobs=""; Secrets=""; Templates=""}
+                  "Tags"                         = @{}
+                  "Web"                          = @{HomePageUrl="https://localhost/demoapp"; ImplicitGrantSettings=""; LogoutUrl="";}
+                }
+            )
+            }     
+    
+          Mock -CommandName Get-MgApplication -MockWith $scriptBlock -ModuleName Microsoft.Graph.Entra
+  
+            $result = Get-EntraApplication -Filter "DisplayName -eq 'test app'"
+            $result | Should -Not -BeNullOrEmpty
+            $result.DisplayName | should -Be 'test app'
+    }  
+    
+    It "Should return top application" {
+              # Mock the Get-MgApplication cmdlet for this specific test case
+                $scriptblock = {
+                
+                param($args)
+                Write-Host "Mocking Get-MgApplication with parameters: $($args | ConvertTo-Json -Depth 3)"
+        
+                return @(
+                    [PSCustomObject]@{
+                      "AddIns"                       = @{}
+                      "Api"                          = @{AcceptMappedClaims=$null; KnownClientApplications=$null;
+                                                     Oauth2PermissionScopes=$null; PreAuthorizedApplications=$null;
+                                                     RequestedAccessTokenVersion=2}
+                      "AppId"                        = "5f783237-3457-45d8-93e7-a0edb1cfbfd1"
+                      "AppManagementPolicies"        = $null
+                      "AppRoles"                     = $null
+                      "ApplicationTemplateId"        = $null
+                      "CreatedDateTime"              = $null
+                      "CreatedOnBehalfOf"            = @{DeletedDateTime=""; Id=""}
+                      "DefaultRedirectUri"           = $null
+                      "DeletedDateTime"              = $null
+                      "Description"                  = $null
+                      "DisplayName"                  = "test app"
+                      "DisabledByMicrosoftStatus"    = $null
+                      "GroupMembershipClaims"        = $null
+                      "Id"                           = "010cc9b5-fce9-485e-9566-c68debafac5f"
+                      "IdentifierUris"               = @{}
+                      "Info"                         = @{LogoUrl=""; MarketingUrl=""; PrivacyStatementUrl=""; SupportUrl=""; TermsOfServiceUrl=""}
+                      "IsDeviceOnlyAuthSupported"    = $True
+                      "IsFallbackPublicClient"       = $true
+                      "KeyCredentials"               = @{}
+                      "Logo"                         = $null
+                      "Oauth2RequirePostResponse"    = $null
+                      "OptionalClaims"               = @{AccessToken=""; IdToken=""; Saml2Token=""}
+                      "Owners"                       = $null
+                      "ParentalControlSettings"      = @{CountriesBlockedForMinors=$null; LegalAgeGroupRule="Allow"}
+                      "PasswordCredentials"          = @{}
+                      "PublicClient"                 = @{RedirectUris=$null}
+                      "PublisherDomain"              = "M365x99297270.onmicrosoft.com"
+                      "RequiredResourceAccess"       = @{}
+                      "SamlMetadataUrl"              = $null
+                      "ServiceManagementReference"   = $null
+                      "SignInAudience"               = "AzureADandPersonalMicrosoftAccount"
+                      "Spa"                          = @{RedirectUris=$null}
+                      "Synchronization"              = @{Id=""; Jobs=""; Secrets=""; Templates=""}
+                      "Tags"                         = @{}
+                      "Web"                          = @{HomePageUrl="https://localhost/demoapp"; ImplicitGrantSettings=""; LogoutUrl="";}
+                    }
+                )
+                }     
+        
+                  Mock -CommandName Get-MgApplication -MockWith $scriptBlock -ModuleName Microsoft.Graph.Entra
+          
+                  $result = Get-EntraApplication -Top 1
+                  $result | Should -Not -BeNullOrEmpty
+    }  
+
+  
+ }
 }
