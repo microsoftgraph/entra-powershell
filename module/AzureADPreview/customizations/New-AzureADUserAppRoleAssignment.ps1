5bd4d71346bdffa3d1b1fbf3bc60006818fd222e
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-21 15:47:36 +0530

    New-AzureADUserAppRoleAssignment

---
+++ module/AzureADPreview/customizations/New-AzureADUserAppRoleAssignment.ps1
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "New-AzureADUserAppRoleAssignment"
+    TargetName = "New-MgBetaUserAppRoleAssignment"
+    Parameters = @(
+        @{
+            SourceName = "ObjectId"
+            TargetName = "UserId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "Id"
+            TargetName = "AppRoleId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+    )
+    Outputs = $null
+}
