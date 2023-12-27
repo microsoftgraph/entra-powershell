683fa44d124271e1b50c3becfa271c8c6c0d199b
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-27 14:20:37 +0530

    Get-AzureADDirectoryRoleMember

--- module/AzureADPreview/customizations/Get-AzureADDirectoryRoleMember.ps1
+++ module/AzureADPreview/customizations/Get-AzureADDirectoryRoleMember.ps1
@@ -27,19 +27,22 @@
         $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
         
-        $response = Get-MgBetaDirectoryRoleMember @params
-        $response | ForEach-Object {
-            if ($null -ne $_) {
-                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
-                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
-                $propsToConvert = @('assignedLicenses','assignedPlans','identities','provisionedPlans')
-                foreach ($prop in $propsToConvert) {
-                    $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
-                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+        try {
+            $response = Get-MgBetaDirectoryRoleMember @params
+            $response | ForEach-Object {
+                if ($null -ne $_) {
+                    Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
+                    Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+                    $propsToConvert = @('assignedLicenses','assignedPlans','identities','provisionedPlans')
+                    foreach ($prop in $propsToConvert) {
+                        $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                        $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                    }
                 }
             }
+            $response
         }
-        $response
+        catch {}
         }
 '@
 }
