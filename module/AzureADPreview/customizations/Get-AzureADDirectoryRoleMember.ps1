482b61871e6785e2c82f2fe60fa09ad568308c94
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2023-12-27 14:05:26 +0530

    Get-AzureADDirectoryRoleMember

--- module/AzureADPreview/customizations/Get-AzureADDirectoryRoleMember.ps1
+++ module/AzureADPreview/customizations/Get-AzureADDirectoryRoleMember.ps1
@@ -3,14 +3,43 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADDirectoryRoleMember"
-    TargetName = "Get-MgBetaDirectoryRoleMember"
+    TargetName = $null
     Parameters = $null
-    Outputs = @(
-        @{
-            SourceName = "AdditionalProperties"
-            TargetName = "AdditionalProperties"
-            ConversionType = "FlatObject"
-            SpecialMapping = $null
+    Outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["DirectoryRoleId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgBetaDirectoryRoleMember @params
+        $response | ForEach-Object {
+            if ($null -ne $_) {
+                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+                $propsToConvert = @('assignedLicenses','assignedPlans','identities','provisionedPlans')
+                foreach ($prop in $propsToConvert) {
+                    $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                }
+            }
+        }
+        $response
+        }
+'@
 }
