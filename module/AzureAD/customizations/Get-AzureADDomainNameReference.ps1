1b35cfd42d05420bc73c2d86cc1187e0082e89e0
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-01-05 18:04:25 +0530

    Get-AzureADDomainNameReference

--- module/AzureAD/customizations/Get-AzureADDomainNameReference.ps1
+++ module/AzureAD/customizations/Get-AzureADDomainNameReference.ps1
@@ -33,7 +33,7 @@
         Write-Debug("=========================================================================`n")
         
         $response = (Invoke-GraphRequest -Uri $URI -Method $Method).value
-        $response = $response | ConvertTo-Json | ConvertFrom-Json
+        $response = $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json
         $response | ForEach-Object {
             if($null -ne $_) {
                 Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
