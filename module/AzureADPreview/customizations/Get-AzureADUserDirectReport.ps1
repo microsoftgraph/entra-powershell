f4bb1c9829af15894db4cba589bc024d46f21ac4
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-01-17 10:29:53 +0530

    Get-AzureADUserDirectReport

--- module/AzureADPreview/customizations/Get-AzureADUserDirectReport.ps1
+++ module/AzureADPreview/customizations/Get-AzureADUserDirectReport.ps1
@@ -3,14 +3,62 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADUserDirectReport"
-    TargetName = "Get-MgBetaUserDirectReport"
-    Parameters = @(
-        @{
-            SourceName = "ObjectId"
-            TargetName = "UserId"
-            ConversionType = "Name"
-            SpecialMapping = $null
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'   
+    PROCESS {    
+        $params = @{}
+        $topCount = $null
+        $baseUri = 'https://graph.microsoft.com/beta/users'
+        $properties = '$select=*'
+        $Method = "GET"
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["UserId"] = $PSBoundParameters["ObjectId"]
+            $URI = "$baseUri/$($params.UserId)/directReports?$properties"
+        }
+        
+        if($null -ne $PSBoundParameters["All"])
+        {
+            $URI = "$baseUri/$($params.UserId)/directReports?$properties"
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $topCount = $PSBoundParameters["Top"]
+            $URI = "$baseUri/$($params.UserId)/directReports?`$top=$topCount&$properties"
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = (Invoke-GraphRequest -Uri $URI -Method $Method).value
+        $response = $response | ConvertTo-Json | ConvertFrom-Json
+        $response | ForEach-Object {
+            if($null -ne $_) {
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name DeletionTimestamp -Value DeletedDateTime
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name DirSyncEnabled -Value OnPremisesSyncEnabled
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ImmutableId -Value onPremisesImmutableId
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name LastDirSyncTime -Value OnPremisesLastSyncDateTime
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name Mobile -Value mobilePhone
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ProvisioningErrors -Value onPremisesProvisioningErrors
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name TelephoneNumber -Value BusinessPhones
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name UserState -Value ExternalUserState
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name UserStateChangedOn -Value ExternalUserStateChangeDateTime
+            }
+        }
+        $response 
+        }
+'@
 }
