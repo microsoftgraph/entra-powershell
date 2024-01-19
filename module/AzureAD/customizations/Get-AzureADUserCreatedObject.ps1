bea9a3de5bcc7e23639c99313234a1ae60c113fb
snehalkotwal <142995585+snehalkotwal@users.noreply.github.com>
2024-01-20 02:14:06 +0530

    Get-AzureADUserCreatedObject-GA (#206)
    * Get-AzureADUserCreatedObject-GA
    * Get-AzureADUserCreatedObject

---
+++ module/AzureAD/customizations/Get-AzureADUserCreatedObject.ps1
@@ -0,0 +1,72 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADUserCreatedObject"
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'   
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["UserId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $Null
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgUserCreatedObject @params
+        $properties = @{
+            ObjectId = "Id"
+            DeletionTimestamp = "deletedDateTime"
+            AppOwnerTenantId = "appOwnerOrganizationId"
+        }
+        $response | ForEach-Object {
+            if($null -ne $_) {
+                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties 
+                foreach ($prop in $properties.GetEnumerator()) {
+                    $propertyName = $prop.Name
+                    $propertyValue = $prop.Value
+                    if ($_.PSObject.Properties.Match($propertyName)) {
+                        $_ | Add-Member -MemberType AliasProperty -Name $propertyName -Value $propertyValue
+                    }
+                }
+                $propsToConvert = @('keyCredentials','passwordCredentials','requiredResourceAccess')
+                foreach ($prop in $propsToConvert) {
+                    try {
+                        if($_.PSObject.Properties.Match($prop)) {
+                            $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                            $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force   
+                        }
+                    }
+                    catch {}    
+                }
+            }
+        }
+        $response
+        }
+'@
+}
