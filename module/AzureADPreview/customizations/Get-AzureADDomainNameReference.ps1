7841a9c2cce4622a9f033d00b4aafbcd889b5ff1
v-akarke <142799789+v-akarke@users.noreply.github.com>
2024-01-18 17:33:51 -0800

    Get-AzureADDomainNameReference (#311)
    * Get-AzureADDomainNameReference

--- module/AzureADPreview/customizations/Get-AzureADDomainNameReference.ps1
+++ module/AzureADPreview/customizations/Get-AzureADDomainNameReference.ps1
@@ -3,14 +3,66 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADDomainNameReference"
-    TargetName = "Get-MgBetaDomainNameReference"
-    Parameters = @(
-        @{
-            SourceName = "Name"
-            TargetName = "DomainId"
-            ConversionType = "Name"
-            SpecialMapping = $null
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'   
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{Name = "DomainId"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["Name"])
+        {
+            $params["DomainId"] = $PSBoundParameters["Name"]
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgBetaDomainNameReference @params
+        $properties = @{
+            ObjectId = "Id"
+            DeletionTimestamp = "deletedDateTime"
+            DirSyncEnabled = "onPremisesSyncEnabled"
+            ImmutableId = "onPremisesImmutableId"
+            Mobile = "mobilePhone"
+            ProvisioningErrors = "onPremisesProvisioningErrors"
+            TelephoneNumber = "businessPhones"
+            UserState = "externalUserState"
+            UserStateChangedOn = "externalUserStateChangeDate"
+        }
+        $response | ForEach-Object {
+            if($null -ne $_) {
+                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties 
+                foreach ($prop in $properties.GetEnumerator()) {
+                    $propertyName = $prop.Name
+                    $propertyValue = $prop.Value
+                    if ($_.PSObject.Properties.Match($propertyName)) {
+                        $_ | Add-Member -MemberType AliasProperty -Name $propertyName -Value $propertyValue
+                    }
+                }
+                $propsToConvert = @('provisionedPlans','assignedPlans','assignedLicenses','appRoles','keyCredentials','identities')
+                foreach ($prop in $propsToConvert) {
+                    try {
+                        if($_.PSObject.Properties.Match($prop)) {
+                            $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                            $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force   
+                        }
+                    }
+                    catch {}    
+                }
+    
+            }
+        }
+        $response 
+    }   
+'@
 }
