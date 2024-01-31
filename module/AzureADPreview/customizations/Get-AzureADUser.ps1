b99fcf9d933ee339f744c8238fdb96f283c4d01d
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-01-31 20:41:38 +0530

    Get-AzureADUser

--- module/AzureADPreview/customizations/Get-AzureADUser.ps1
+++ module/AzureADPreview/customizations/Get-AzureADUser.ps1
@@ -3,16 +3,76 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADUser"
-    TargetName = "Get-MgBetaUser"
-    Parameters = @(
-        @{
-            SourceName = "SearchString"
-            TargetName = "Filter"
-            ConversionType = "ScriptBlock"
-            SpecialMapping = @"
-`$Value = "userPrincipalName eq '`$TmpValue' or (state eq '`$TmpValue' or (mailNickName eq '`$TmpValue' or (mail eq '`$TmpValue' or (jobTitle eq '`$TmpValue' or (displayName eq '`$TmpValue' or (startswith(displayName,'`$TmpValue') or (department eq '`$TmpValue' or (country eq '`$TmpValue' or city eq '`$TmpValue'))))))))"
-"@
+    TargetName = $null
+    Parameters = $null
+    outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{SearchString = "Filter"; ObjectId = "Id"}
+        if($null -ne $PSBoundParameters["SearchString"])
+        {
+            $TmpValue = $PSBoundParameters["SearchString"]
+            $Value = "userPrincipalName eq '$TmpValue' or (state eq '$TmpValue' or (mailNickName eq '$TmpValue' or (mail eq '$TmpValue' or (jobTitle eq '$TmpValue' or (displayName eq '$TmpValue' or (startswith(displayName,'$TmpValue') or (department eq '$TmpValue' or (country eq '$TmpValue' or city eq '$TmpValue'))))))))"
+            $params["Filter"] = $Value
         }
-    )
-    Outputs = $null
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["UserId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($null -ne $PSBoundParameters["Filter"])
+        {
+            $keysChanged = @{
+                "UserState" = "ExternalUserState"
+            }
+            $TmpValue = $PSBoundParameters["Filter"]
+            foreach($i in $keysChanged.GetEnumerator()){
+                $TmpValue = $TmpValue.Replace($i.Key, $i.Value)
+            }
+            $Value = $TmpValue
+            $params["Filter"] = $Value
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $Null
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgBetaUser @params 
+        $response | ForEach-Object {
+            if ($null -ne $_) {
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name UserState -Value ExternalUserState
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name UserStateChangedOn -Value ExternalUserStateChangeDateTime
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name Mobile -Value mobilePhone
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name DeletionTimestamp -Value DeletedDateTime
+                $propsToConvert = @('AssignedLicenses','AssignedPlans','PasswordProfile')
+                foreach ($prop in $propsToConvert) {
+                    $value = $_.$prop | ConvertTo-Json | ConvertFrom-Json
+                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                }
+            }
+        }
+
+        $response 
+        }    
+'@
 }
