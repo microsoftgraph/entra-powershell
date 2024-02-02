8f99251943b61fd4b998704598164722d9dc54b7
Rodrigo Tintor <rotintor@microsoft.com>
2024-02-02 09:20:20 -0800

    Fix Module Tests.
    Signed-off-by: Rodrigo Tintor <rotintor@microsoft.com>

--- test/module/AzureAD/Module.Tests.ps1
+++ test/module/AzureAD/Module.Tests.ps1
@@ -3,42 +3,46 @@
 # ------------------------------------------------------------------------------
 Describe "Microsoft.Graph.Entra Module" {
     Context "On module import" {
-        BeforeAll {    
+        BeforeAll {
             if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
-                $ModulePath = ".\bin\Microsoft.Graph.Entra.psd1"
-                Import-Module $ModulePath -force
-                $ModuleName = "Microsoft.Graph.Entra"
-                $PSModuleInfo = Get-Module $ModuleName
-                
+                Import-Module Microsoft.Graph.Entra
             }
         }
 
         It "Should have exported commands" {
+            $PSModuleInfo = Get-Module Microsoft.Graph.Entra
             $PSModuleInfo | Should -Not -BeNullOrEmpty
             $PSModuleInfo.ExportedFunctions.Count | Should -Not -Be 0
 
         }
 
         It 'Should be compatible with PS core and desktop' {
+            $PSModuleInfo = Get-Module Microsoft.Graph.Entra
             $PSModuleInfo.CompatiblePSEditions | Should -BeIn @("Core", "Desktop")
         }
 
         It 'Should point to script module' {
-            $PSModuleInfo.RootModule | Should -BeLikeExactly "*$ModuleName.psm1"
+            $PSModuleInfo = Get-Module Microsoft.Graph.Entra
+            $PSModuleInfo.RootModule | Should -BeLikeExactly "*Microsoft.Graph.Entra.psm1"
         }
 
         It 'Should lock GUID' {
-            $PSModuleInfo.Guid | Should -Be "3f58c85c-3729-4008-a81f-3338c2556d64"
+            $PSModuleInfo = Get-Module Microsoft.Graph.Entra
+            $PSModuleInfo.Guid | Should -Be "50fbdc2c-9bbc-419c-9e6f-a9ee31582ffe"
         }
 
         It "Module import should not write to error and information streams" {
             $ps = [powershell]::Create()
-            $ps.AddScript("Import-Module $ModulePath -ErrorAction SilentlyContinue").Invoke()
-
+            $ps.AddScript("Import-Module Microsoft.Graph.Entra -ErrorAction SilentlyContinue").Invoke()
+            "Checking Information stream" | Out-Host
             $ps.Streams.Information.Count | Should -Be 0
+            "Checking Error stream" | Out-Host
             $ps.Streams.Error.Count | Should -Be 0
+            "Checking Verbose stream" | Out-Host
             $ps.Streams.Verbose.Count | Should -Be 0
+            "Checking Debug stream" | Out-Host
             $ps.Streams.Warning.Count | Should -Be 0
+            "Checking Progress stream" | Out-Host
             $ps.Streams.Progress.Count | Should -Be 0
 
             $ps.Dispose()
