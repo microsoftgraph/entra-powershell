5dabbf9fe30e741b23a5447bcb592966a2c04ef5
v-uansari <v-uansari@microsoft.com>
2024-02-15 12:28:47 +0530

    Remove-AzureADMSServicePrincipalDelegatedPermissionClassification

---
+++ module/EntraBeta/customizations/Remove-EntraMSServicePrincipalDelegatedPermissionClassification.ps1
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Remove-AzureADMSServicePrincipalDelegatedPermissionClassification"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @'
+        Remove-MgBetaServicePrincipalDelegatedPermissionClassification -ServicePrincipalId $PSBoundParameters["ServicePrincipalId"] -DelegatedPermissionClassificationId $PSBoundParameters["Id"]
+'@
+}
