991bd80df92dfa797678749988feaaa8f64e5400
v-uansari <v-uansari@microsoft.com>
2024-02-20 14:51:31 +0530

    Disconnect-AzureAD

---
+++ module/Entra/customizations/Disconnect-Entra.ps1
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Disconnect-AzureAD"
+    TargetName = "Disconnect-MgGraph"
+    Parameters = $null
+    Outputs = $null
+}
