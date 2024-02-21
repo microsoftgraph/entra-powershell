ad9f1344ff97322d97acaf58e1943d7565e46440
Kennedy Munga <kemunga@microsoft.com>
2024-02-21 14:02:34 +0300

    Remove-EntraBetaApplicationProxyApplication

---
+++ module/EntraBeta/customizations/Remove-EntraBetaApplicationProxyApplication.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Remove-AzureADApplicationProxyApplication"
+    TargetName = "Remove-MgApplication"
+    Parameters =  @(
+        @{
+            SourceName = "ObjectId"
+            TargetName = "ApplicationId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+    )
+    Outputs = $null
+}
