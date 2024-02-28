d6d7f3f319c24ed31bc92a0befd6732e3d9b12cf
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-02-28 16:15:19 +0530

    New-AzureADMSNamedLocationPolicy

--- module/EntraBeta/customizations/New-EntraBetaMSNamedLocationPolicy.ps1
+++ module/EntraBeta/customizations/New-EntraBetaMSNamedLocationPolicy.ps1
@@ -3,7 +3,70 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "New-AzureADMSNamedLocationPolicy"
-    TargetName = "New-MgBetaIdentityConditionalAccessNamedLocation"
+    TargetName = $null
     Parameters = $null
     Outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $body = @{}
+        $params = @{}
+        $keysChanged = @{}
+        if($null -ne $PSBoundParameters["IncludeUnknownCountriesAndRegions"])
+        {
+            $body["IncludeUnknownCountriesAndRegions"] = $PSBoundParameters["IncludeUnknownCountriesAndRegions"]
+        }
+        if($null -ne $PSBoundParameters["Id"])
+        {
+            $body["Id"] = $PSBoundParameters["Id"]
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["IsTrusted"])
+        {
+            $body["IsTrusted"] = $PSBoundParameters["IsTrusted"]
+        }
+        if($null -ne $PSBoundParameters["OdataType"])
+        {
+            $body["@odata.type"] = $PSBoundParameters["OdataType"]
+        }
+        if($null -ne $PSBoundParameters["CountriesAndRegions"])
+        {
+            $body["CountriesAndRegions"] = $PSBoundParameters["CountriesAndRegions"]
+        }
+        if($null -ne $PSBoundParameters["IpRanges"])
+        {
+            $Tmp = $PSBoundParameters["IpRanges"]
+                $hash =@()
+                foreach($i in $Tmp){
+                    $hash += @{cidrAddress=$i.CidrAddress}
+                }
+            $body["IpRanges"] = $hash
+        }
+        if($null -ne $PSBoundParameters["DisplayName"])
+        {
+            $body["DisplayName"] = $PSBoundParameters["DisplayName"]
+        }
+    
+        $params["BodyParameter"] = $body
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = New-MgBetaIdentityConditionalAccessNamedLocation @params
+        $response | ForEach-Object {
+            if($null -ne $_) {
+            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+    
+            }
+        }
+        $response
+        }
+'@
 }
