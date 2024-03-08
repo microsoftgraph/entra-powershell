a2fcce707ffef62ac653d4c9e1bb3974963f4a97
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-03-08 17:52:11 +0530

    New-AzureADMSNamedLocationPolicy

--- module/EntraBeta/customizations/New-EntraBetaMSNamedLocationPolicy.ps1
+++ module/EntraBeta/customizations/New-EntraBetaMSNamedLocationPolicy.ps1
@@ -62,8 +62,16 @@
         $response = New-MgBetaIdentityConditionalAccessNamedLocation @params
         $response | ForEach-Object {
             if($null -ne $_) {
-            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
-    
+                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+            $propsToConvert = @('ipRanges')
+            try {
+                foreach ($prop in $propsToConvert) {
+                    $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                }
+            }
+            catch {}   
             }
         }
         $response
