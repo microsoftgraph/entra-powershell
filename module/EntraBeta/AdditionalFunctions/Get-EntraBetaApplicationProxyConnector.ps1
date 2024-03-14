2758d0703b77014d40439910663afa16b07a1402
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-03-14 21:50:32 +0530

    Get-AzureADApplicationProxyConnector

---
+++ module/EntraBeta/AdditionalFunctions/Get-EntraBetaApplicationProxyConnector.ps1
@@ -0,0 +1,66 @@
+function Get-EntraBetaApplicationProxyConnector {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+    [Parameter(ParameterSetName = "GetVague", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $SearchString,
+    [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.Nullable`1[System.Int32]] $Top,
+    [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $ObjectId,
+    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.Nullable`1[System.Boolean]] $All,
+    [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $Filter
+    )
+
+    PROCESS {    
+        $params = @{}
+        $params["Method"] = "GET"
+        $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectors"
+        if($null -ne $PSBoundParameters["SearchString"])
+        {
+            $f = '$' + 'Filter'
+            $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectors?$f=machineName eq '$SearchString' OR startswith(machineName,'$SearchString')"    
+        }
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectors/$ObjectId"
+        }
+        if($null -ne $PSBoundParameters["Filter"])
+        {
+            $f = '$' + 'Filter'
+            $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectors?$f=$filter"
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectors"
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $t = '$' + 'Top'
+            $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectors?$t=$top"
+        }
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+
+        $response = Invoke-GraphRequest -Method $params.method -Uri $params.uri 
+        try {    
+            $call = $response.value 
+            $call
+        }
+        catch {
+            $response
+        }
+
+    }        
+}
