08da227169982b71b08931a16f9c2097c1b29e3a
Kennedy Munga <kemunga@microsoft.com>
2024-03-14 20:56:48 +0300

    fix debug

--- module/Entra/customizations/Restore-EntraDeletedApplication.ps1
+++ module/Entra/customizations/Restore-EntraDeletedApplication.ps1
@@ -30,7 +30,7 @@
     
         Write-Debug("============================ TRANSFORMATIONS ============================")
         $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-        Write-Debug("=========================================================================`n")
+        Write-Debug("=========================================================================")
         
         $response = Restore-MgDirectoryDeletedItem @params
         $response | ForEach-Object {
