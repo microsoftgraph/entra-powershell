c41ae96a0cd95926b134c26df983f164643c5cf8
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-03-20 14:02:02 +0530

    New-ApplicationProxyConnectorGroup

---
+++ module/EntraBeta/AdditionalFunctions/New-EntraBetaApplicationProxyConnectorGroup.ps1
@@ -0,0 +1,36 @@
+function New-EntraBetaApplicationProxyConnectorGroup {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $Name
+    )
+
+    PROCESS {    
+        $params = @{}
+        $params["Method"] = "POST"
+        $body = @{}
+        $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectorGroups"
+    
+        if($null -ne $PSBoundParameters["Name"])
+        {
+            $body = @{
+                "name" = $PSBoundParameters["Name"]
+            } 
+            $body = $body | ConvertTo-Json
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+
+        Invoke-MgGraphRequest -Method $params.method -Uri $params.uri -Body $body -ContentType "application/json"
+    }        
+}
