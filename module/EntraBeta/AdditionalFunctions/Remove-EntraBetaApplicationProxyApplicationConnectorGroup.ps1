e321ae8378bdb3dfe11052570550372874c4288a
Ashwini Karke (Perennial Systems Inc) <v-akarke@microsoft.com>
2024-03-26 17:40:47 +0530

    Remove-AzureADApplicationProxyApplicationConnectorGroup

---
+++ module/EntraBeta/AdditionalFunctions/Remove-EntraBetaApplicationProxyApplicationConnectorGroup.ps1
@@ -0,0 +1,31 @@
+function Remove-EntraBetaApplicationProxyApplicationConnectorGroup {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $ObjectId
+    )
+
+    PROCESS {    
+        $params = @{}
+        $params["Method"] = "DELETE"
+        $body = @{}
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["Uri"] = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationProxy/connectorGroups/$ObjectId"
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+
+        Invoke-MgGraphRequest -Method $params.method -Uri $params.uri 
+    }        
+}
