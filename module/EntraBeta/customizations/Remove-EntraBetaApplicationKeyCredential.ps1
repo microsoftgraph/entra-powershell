02c68e719f24d4437e636537d4dfe49a3bbaa8e5
Snehal Kotwal (Perennial Systems Inc) <v-skotwal@microsoft.com>
2024-04-05 16:30:36 +0530

    deleted doc

--- module/EntraBeta/customizations/Remove-EntraBetaApplicationKeyCredential.ps1
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
-# ------------------------------------------------------------------------------
-@{
-    SourceName = "Remove-AzureADApplicationKeyCredential"
-    TargetName = "Remove-MgBetaApplicationKey"
-    Parameters = $null
-    Outputs = $null
-}
