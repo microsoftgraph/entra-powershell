430c42add70490564476b408e3a894fa249e026b
v-uansari <143997438+v-uansari@users.noreply.github.com>
2024-04-08 19:51:07 +0530

    msol docs updates (#677)

--- module/Entra/AdditionalFunctions/Get-EntraDomainFedrationSettings.ps1
+++ module/Entra/AdditionalFunctions/Get-EntraDomainFedrationSettings.ps1
@@ -3,49 +3,6 @@
 # ------------------------------------------------------------------------------
 
 function Get-EntraDomainFedrationSettings {
-<#
-.SYNOPSIS
-    Retrieves settings for a federated domain.
-
-.DESCRIPTION
-    The Get-EntraDomainFederationSettings cmdlet gets key settings from Microsoft Azure Active Directory. Use the Get-EntraFederationProperty cmdlet to get settings for both Microsoft Azure Active Directory and the Active Directory Federation Services server.
-
-.PARAMETER DomainName
-    The fully qualified domain name to retrieve.
-    
-.PARAMETER TenantId 
-    The unique ID of the tenant to perform the operation on. If this is not provided then the value will default to the tenant of the current user. This parameter is only applicable to partner users.
-
-.PARAMETER <CommonParameters>
-        This cmdlet supports the common parameters: Verbose, Debug, ErrorAction, ErrorVariable, WarningAction, WarningVariable, OutBuffer, PipelineVariable and OutVariable. For more information, see about_CommonParameters (https:/go.microsoft.com/fwlink/?LinkID=113216). 
-    
-.OUTPUTS
-    Microsoft.Online.Administration.DomainFederationSettings
-    This cmdlet returns the following settings:
-    
-            ActiveLogOnUri
-            FederationBrandName
-            IssuerUri
-            LogOffUri
-            MetadataExchangeUri
-            NextSigningCertificate
-            PassiveLogOnUri
-            SigningCertificate
-
-
-.EXAMPLE
-    
-C:\PS>Get-EntraDomainFederationSettings -DomainName contoso.com
-    
-    Returns the federation settings for contoso.com.
-
-    Description
-    
-    -----------
-    
-    Returns the federation settings for contoso.com.
-
-#>
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param(
         [Parameter(Mandatory=$true,Position=0,ValueFromPipelineByPropertyName=$true)][string]$DomainName,
