417b6f4b94bccc4c3df5eb516616896a6c26bd1a
v-uansari <v-uansari@microsoft.com>
2024-04-09 16:15:53 +0530

    updates

---
+++ test/module/Entra/Mock-Remove-EntraDevice.Tests.ps1
@@ -0,0 +1,39 @@
+BeforeAll {  
+    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+        #Import-Module .\bin\Microsoft.Graph.Entra.psm1 -Force
+        Import-Module Microsoft.Graph.Entra
+    }
+    Import-Module .\test\module\Common-Functions.ps1 -Force
+
+    Mock -CommandName Remove-MgDevice -MockWith {} -ModuleName Microsoft.Graph.Entra
+}
+
+Describe "Remove-EntraDevice" {
+    Context "Test for Remove-EntraDevice" {
+        It "Should return empty object" {
+            $result = Remove-EntraDevice -ObjectId 056b2531-005e-4f3e-be78-01a71ea30a04
+            $result | Should -BeNullOrEmpty
+
+            Should -Invoke -CommandName Remove-MgDevice -ModuleName Microsoft.Graph.Entra -Times 1
+        }
+        It "Should fail when ObjectId is empty" {
+            { Remove-EntraDevice -ObjectId "" }
+        }   
+        It "Should contain DeviceId in parameters when passed ObjectId to it" {
+            Mock -CommandName Remove-MgDevice -MockWith {$args} -ModuleName Microsoft.Graph.Entra
+
+            $result = Remove-EntraDevice -ObjectId 056b2531-005e-4f3e-be78-01a71ea30a04
+            $params = Get-Parameters -data $result
+            $params.DeviceId | Should -Be "056b2531-005e-4f3e-be78-01a71ea30a04"
+        }
+        It "Should contain 'User-Agent' header" {
+            Mock -CommandName Remove-MgDevice -MockWith {$args} -ModuleName Microsoft.Graph.Entra
+
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Remove-EntraDevice"
+
+            $result = Remove-EntraDevice -ObjectId 056b2531-005e-4f3e-be78-01a71ea30a04
+            $params = Get-Parameters -data $result
+            $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+        } 
+    }
+}
---
+++ test/module/Entra/Mock-Set-EntraDevice.Tests.ps1
@@ -0,0 +1,39 @@
+BeforeAll {  
+    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+        #Import-Module .\bin\Microsoft.Graph.Entra.psm1 -Force
+        Import-Module Microsoft.Graph.Entra      
+    }
+    Import-Module .\test\module\Common-Functions.ps1 -Force
+
+    Mock -CommandName Update-MgDevice -MockWith {} -ModuleName Microsoft.Graph.Entra
+}
+
+Describe "Set-EntraDevice"{
+    Context "Test for Set-EntraDevice" {
+        It "Should return empty object"{
+            $result = Set-EntraDevice -ObjectId 056b2531-005e-4f3e-be78-01a71ea30a04 -DisplayName "Mock-App" -AccountEnabled $true
+            $result | Should -BeNullOrEmpty           
+
+            Should -Invoke -CommandName Update-MgDevice -ModuleName Microsoft.Graph.Entra -Times 1
+        }
+        It "Should fail when ObjectId is empty" {
+            { Set-EntraDevice -ObjectId ""  } | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
+        }
+        It "Should contain DeviceId in parameters when passed ObjectId to it" {
+            Mock -CommandName Update-MgDevice -MockWith {$args} -ModuleName Microsoft.Graph.Entra
+
+            $result = Set-EntraDevice -ObjectId 056b2531-005e-4f3e-be78-01a71ea30a04
+            $params = Get-Parameters -data $result
+            $params.DeviceId | Should -Be "056b2531-005e-4f3e-be78-01a71ea30a04"
+        }
+        It "Should contain 'User-Agent' header" {
+            Mock -CommandName Update-MgDevice -MockWith {$args} -ModuleName Microsoft.Graph.Entra
+
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Set-EntraDevice"
+
+            $result = Set-EntraDevice -ObjectId 056b2531-005e-4f3e-be78-01a71ea30a04
+            $params = Get-Parameters -data $result
+            $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+        }
+    }
+}
