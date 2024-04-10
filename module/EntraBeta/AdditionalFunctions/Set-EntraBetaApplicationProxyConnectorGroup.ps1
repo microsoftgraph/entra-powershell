36635b0fe025531f7878ef96125833e1500a323c
Ashwini Karke <v-akarke@microsoft.com>
2024-04-10 15:35:27 +0530

    Set-AzureADApplicationProxyConnectorGroup

--- module/EntraBeta/AdditionalFunctions/Set-EntraBetaApplicationProxyConnectorGroup.ps1
+++ module/EntraBeta/AdditionalFunctions/Set-EntraBetaApplicationProxyConnectorGroup.ps1
@@ -9,6 +9,7 @@ function Set-EntraBetaApplicationProxyConnectorGroup {
 
     PROCESS {    
         $params = @{}
+        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
         $params["Method"] = "PATCH"
         $body = @{}
         if($null -ne $PSBoundParameters["Id"])
@@ -34,6 +35,6 @@ function Set-EntraBetaApplicationProxyConnectorGroup {
         $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
 
-        Invoke-GraphRequest -Method $params.method -Uri $params.uri -Body $body
+        Invoke-GraphRequest -Headers $customHeaders -Method $params.method -Uri $params.uri -Body $body
     }        
 }
