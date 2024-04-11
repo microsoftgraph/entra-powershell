1c5e15e3088eb90fb580facb89e196a4d9b707cc
v-uansari <v-uansari@microsoft.com>
2024-04-11 10:01:32 +0530

    update tests

--- test/module/Entra/Entra.Tests.ps1
+++ test/module/Entra/Entra.Tests.ps1
@@ -1,12 +1,21 @@
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra)){
+    Import-Module Microsoft.Graph.Entra
+}
+
+Import-Module Pester
+
+$psmPath = (Get-Module Microsoft.Graph.Entra).Path
 $testReportPath = join-path $psscriptroot "..\..\..\TestReport\Entra"
 $mockScriptsPath = join-path $psscriptroot "..\..\..\test\module\Entra\Mock-*.Tests.ps1"
 $reportGenPath = join-path $psscriptroot "..\..\..\reportgenerator\ReportUnit.exe"
 
+
+Write-Host 'psmPath: '$psmPath
 Write-Host 'testReportPath: '$testReportPath
 Write-Host 'mockScriptsPath: '$mockScriptsPath
 Write-Host 'reportGenPath: '$reportGenPath
 
-Import-Module Pester
+
 
 $testOutputFile = "$testReportPath\TestResults.xml"
 if (!(test-path -path $testReportPath)) {new-item -path $testReportPath -itemtype directory}
@@ -17,9 +26,9 @@ $config = New-PesterConfiguration
 $config.Run.Path = $mockScripts
 $config.Run.PassThru = $true
 $config.Run.Exit = $true
-# $config.CodeCoverage.Enabled = $true
-# $config.CodeCoverage.CoveragePercentTarget = 100
-# $config.CodeCoverage.Path = "Microsoft.Graph.Entra.psm1"
+$config.CodeCoverage.Enabled = $true
+$config.CodeCoverage.CoveragePercentTarget = 100
+$config.CodeCoverage.Path = $psmPath
 $config.TestResult.Enabled = $true
 $config.TestResult.OutputPath = $testOutputFile
 $config.Output.Verbosity = "Detailed"
