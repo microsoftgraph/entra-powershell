423c17b333ed5ce33b3ecae8401caa49767f2bbb
snehalkotwal <v-skotwal@microsoft.com>
2024-04-16 17:35:49 +0530

    mock data updated

--- test/module/Entra/Mock-Get-EntraSubscribedSku.Tests.ps1
+++ test/module/Entra/Mock-Get-EntraSubscribedSku.Tests.ps1
@@ -8,18 +8,18 @@ BeforeAll {
 $scriptblock = {
     return @(
         [PSCustomObject]@{
-           	PrepaidUnits         = @{Enabled="20"; LockedOut=""; Suspended=0; Warning=""; AdditionalProperties=""}
-			AccountId            = "d5aec55f-2d12-4442-8d2f-ccca95d4390e"
-			AccountName          = "M365x99297270"
-			AppliesTo            = "User"
-			CapabilityStatus     = "Enabled"
-			ConsumedUnits        = "20"
-			Id                   = "d5aec55f-2d12-4442-8d2f-ccca95d4390e_b05e124f-c7cc-45a0-a6aa-8cf78c946968"
-			ServicePlans         = {"M365_AUDIT_PLATFORM", "EXCHANGE_S_FOUNDATION", "ATA", "ADALLOM_S_STANDALONE"}
-			SkuId                = "b05e124f-c7cc-45a0-a6aa-8cf78c946968"
-            SkuPartNumber        = "EMSPREMIUM"
-            SubscriptionIds      = {"f56a4f3e-0812-4a08-8f08-0499121e842f"}
-            Parameters           = $args
+           "PrepaidUnits"                     = @{Enabled="20"; LockedOut=""; Suspended=0; Warning=""; AdditionalProperties=""} 
+           "AccountId"                        = "d5aec55f-2d12-4442-8d2f-ccca95d4390e"
+           "AccountName"                      = "M365x99297270"
+           "AppliesTo"                        = "User"
+           "CapabilityStatus"                 = "Enabled"
+           "ConsumedUnits"                    = "20"
+           "Id"                               = "d5aec55f-2d12-4442-8d2f-ccca95d4390e_b05e124f-c7cc-45a0-a6aa-8cf78c946968"
+           "ServicePlans"                     = {"M365_AUDIT_PLATFORM", "EXCHANGE_S_FOUNDATION", "ATA", "ADALLOM_S_STANDALONE"}
+           "SkuId"                            = "b05e124f-c7cc-45a0-a6aa-8cf78c946968"
+           "SkuPartNumber"                    = "EMSPREMIUM"
+           "SubscriptionIds"                  = {"f56a4f3e-0812-4a08-8f08-0499121e842f"}
+           "Parameters"                       = $args
           
         } 
     )
