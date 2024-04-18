67e2c51ee927571a76f98a29f246a5cf756cd1fa
Ashwini Karke <v-akarke@microsoft.com>
2024-04-18 16:17:21 +0530

    Connect-Entra

--- module/Entra/AdditionalFunctions/Connect-Entra.ps1
+++ module/Entra/AdditionalFunctions/Connect-Entra.ps1
@@ -1,32 +1,86 @@
 function Connect-Entra {    
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
-        [Parameter(ParameterSetName = "GetById", Mandatory = $false)][System.String] $TenantId,
-        [Parameter(ParameterSetName = "GetById", Mandatory = $false)][System.String] $CertificateThumbprint,
-        [Parameter(ParameterSetName = "GetById", Mandatory = $false)][System.String] $ApplicationId,
-        [Parameter(ParameterSetName = "GetById", Mandatory = $false)][System.String] $AadAccessToken
+        [Parameter()][System.String[]] $Scopes,
+        [Parameter()][Alias("AppId", "ApplicationId")][System.String] $ClientId,
+        [Parameter()][Alias("Audience", "Tenant")][System.String] $TenantId,
+        [Parameter()] $ContextScope,
+        [Parameter()][Alias("EnvironmentName", "NationalCloud")][System.String] $Environment,
+        [Parameter()][Switch] $EnvironmentVariable,
+        [Parameter()][Alias("UseDeviceAuthentication", "DeviceCode", "DeviceAuth", "Device")][System.Management.Automation.SwitchParameter] $UseDeviceCode,
+        [Parameter()][Double] $ClientTimeout,
+        [Parameter()][Switch] $NoWelcome,
+        [Parameter()][Alias("ManagedIdentity", "ManagedServiceIdentity", "MSI")][System.Management.Automation.SwitchParameter] $Identity,
+        [Parameter()][Alias("CertificateSubject", "CertificateName")][System.String] $CertificateSubjectName,
+        [Parameter()][System.String] $CertificateThumbprint,
+        [Parameter()][System.Security.Cryptography.X509Certificates.X509Certificate2] $Certificate,
+        [Parameter()][Alias("SecretCredential", "Credential")][System.Management.Automation.PSCredential] $ClientSecretCredential,
+        [Parameter()][System.Security.SecureString] $AccessToken
         )
 
     PROCESS {    
         $params = @{}
         $keysChanged = @{}
-        if ($null -ne $PSBoundParameters["TenantId"]) 
-        {
+        if ($null -ne $PSBoundParameters["Scopes"]) {
+            $params["Scopes"] = $PSBoundParameters["Scopes"]
+        }
+        
+        if ($null -ne $PSBoundParameters["ClientId"]) {
+            $params["ClientId"] = $PSBoundParameters["ClientId"]
+        }
+        
+        if ($null -ne $PSBoundParameters["TenantId"]) {
             $params["TenantId"] = $PSBoundParameters["TenantId"]
         }
-        if ($null -ne $PSBoundParameters["CertificateThumbprint"]) 
-        {
+        
+        if ($null -ne $PSBoundParameters["ContextScope"]) {
+            $params["ContextScope"] = $PSBoundParameters["ContextScope"]
+        }
+        
+        if ($null -ne $PSBoundParameters["Environment"]) {
+            $params["Environment"] = $PSBoundParameters["Environment"]
+        }
+
+        if ($PSBoundParameters.ContainsKey("EnvironmentVariable")) {
+            $params["EnvironmentVariable"] = $PSBoundParameters["EnvironmentVariable"]
+        }
+        
+        if ($null -ne $PSBoundParameters["UseDeviceCode"]) {
+            $params["UseDeviceCode"] = $PSBoundParameters["UseDeviceCode"]
+        }
+        
+        if ($null -ne $PSBoundParameters["ClientTimeout"]) {
+            $params["ClientTimeout"] = $PSBoundParameters["ClientTimeout"]
+        }
+        
+        if ($PSBoundParameters.ContainsKey("NoWelcome")) {
+            $params["NoWelcome"] = $PSBoundParameters["NoWelcome"]
+        }
+        
+        if ($PSBoundParameters.ContainsKey("Identity")) {
+            $params["Identity"] = $PSBoundParameters["Identity"]
+        }
+        
+        if ($null -ne $PSBoundParameters["CertificateSubjectName"]) {
+            $params["CertificateSubjectName"] = $PSBoundParameters["CertificateSubjectName"]
+        }
+        
+        if ($null -ne $PSBoundParameters["CertificateThumbprint"]) {
             $params["CertificateThumbprint"] = $PSBoundParameters["CertificateThumbprint"]
         }
-        if ($null -ne $PSBoundParameters["ApplicationId"]) 
-        {
-            $params["ApplicationId"] = $PSBoundParameters["ApplicationId"]
+        
+        if ($null -ne $PSBoundParameters["Certificate"]) {
+            $params["Certificate"] = $PSBoundParameters["Certificate"]
         }
-        if ($null -ne $PSBoundParameters["AccessToken"]) 
-        {
-            $secureString = ConvertTo-SecureString -String $PSBoundParameters["ApplicationId"] -AsPlainText -Force
-            $params["AccessToken"] = $secureString
+        
+        if ($null -ne $PSBoundParameters["ClientSecretCredential"]) {
+            $params["ClientSecretCredential"] = $PSBoundParameters["ClientSecretCredential"]
         }
+        
+        if ($null -ne $PSBoundParameters["AccessToken"]) {
+            $params["AccessToken"] = $PSBoundParameters["AccessToken"]
+        }
+        
         if ($PSBoundParameters.ContainsKey("Verbose")) {
             $params["Verbose"] = $Null
         }
@@ -36,7 +90,6 @@ function Connect-Entra {
         Write-Debug("============================ TRANSFORMATIONS ============================")
         $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
-        $response = Connect-MgGraph @params 
-        $response
+        Connect-MgGraph @params 
     }
 }
