48ca49a43be3afe9fb4809321c1b0ffd335c1db4
Ashwini Karke <v-akarke@microsoft.com>
2024-04-20 17:47:17 +0530

    Get-EntraUser

--- module/Entra/customizations/Get-EntraUser.ps1
+++ module/Entra/customizations/Get-EntraUser.ps1
@@ -14,19 +14,18 @@
         $baseUri = 'https://graph.microsoft.com/v1.0/users'
         $properties = '$select=Id,AccountEnabled,AgeGroup,OfficeLocation,AssignedLicenses,AssignedPlans,City,CompanyName,ConsentProvidedForMinor,Country,CreationType,Department,DisplayName,GivenName,OnPremisesImmutableId,JobTitle,LegalAgeGroupClassification,Mail,MailNickName,MobilePhone,OnPremisesSecurityIdentifier,OtherMails,PasswordPolicies,PasswordProfile,PostalCode,PreferredLanguage,ProvisionedPlans,OnPremisesProvisioningErrors,ProxyAddresses,RefreshTokensValidFromDateTime,ShowInAddressList,State,StreetAddress,Surname,BusinessPhones,UsageLocation,UserPrincipalName,ExternalUserState,ExternalUserStateChangeDateTime,UserType,OnPremisesLastSyncDateTime,ImAddresses,SecurityIdentifier,OnPremisesUserPrincipalName,ServiceProvisioningErrors,IsResourceAccount,OnPremisesExtensionAttributes,DeletedDateTime,OnPremisesSyncEnabled,EmployeeType,EmployeeHireDate,CreatedDateTime,EmployeeOrgData,preferredDataLocation,Identities,onPremisesSamAccountName,EmployeeId,EmployeeLeaveDateTime,AuthorizationInfo,FaxNumber,OnPremisesDistinguishedName,OnPremisesDomainName,IsLicenseReconciliationNeeded,signInSessionsValidFromDateTime'
         $params["Method"] = "GET"
+        $params["Uri"] = "$baseUri/?$properties"
 
-        if($PSBoundParameters.ContainsKey("Top") -and $PSBoundParameters.ContainsKey("Filter"))
+        if($null -ne $PSBoundParameters["Top"])
         {
             $topCount = $PSBoundParameters["Top"]
-            $Filter = $PSBoundParameters["Filter"]
-            $params["Uri"] = "$baseUri/?`$top=$topCount&`$filter=$Filter&$properties"
-            $params["Headers"] = @{ ConsistencyLevel = 'eventual'}
+            $params["Uri"] += "&`$top=$topCount"
         }
         if($null -ne $PSBoundParameters["SearchString"])
         {
             $TmpValue = $PSBoundParameters["SearchString"]
             $SearchString = "`$search=`"userprincipalname:$TmpValue`" OR `"state:$TmpValue`" OR `"mailNickName:$TmpValue`" OR `"mail:$TmpValue`" OR `"jobTitle:$TmpValue`" OR `"displayName:$TmpValue`" OR `"department:$TmpValue`" OR `"country:$TmpValue`" OR `"city:$TmpValue`""
-            $params["Uri"] = "$baseUri/?$SearchString&$properties"
+            $params["Uri"] += "&$SearchString"
             $params["Headers"] = @{ ConsistencyLevel = 'eventual'}
         }
         if($null -ne $PSBoundParameters["ObjectId"])
@@ -38,26 +37,17 @@
         {
             $Filter = $PSBoundParameters["Filter"]
             $f = '$' + 'Filter'
-            $params["Uri"] = "$baseUri/?$f=$Filter&$properties"
+            $params["Uri"] += "&$f=$Filter"
         }
         if($PSBoundParameters.ContainsKey("Verbose"))
         {
             $params["Verbose"] = $Null
         }
-        if($null -ne $PSBoundParameters["All"])
-        {
-            $params["Uri"] = "$baseUri/?$properties"
-        }
         if($PSBoundParameters.ContainsKey("Debug"))
         {
             $params["Debug"] = $Null
         }
-        if($null -ne $PSBoundParameters["Top"])
-        {
-            $topCount = $PSBoundParameters["Top"]
-            $params["Uri"] = "$baseUri/?`$top=$topCount&$properties"
-        }
-    
+       
         Write-Debug("============================ TRANSFORMATIONS ============================")
         $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
@@ -84,6 +74,6 @@
             }
         }
         $data
-    }    
+    }     
 '@
 }
