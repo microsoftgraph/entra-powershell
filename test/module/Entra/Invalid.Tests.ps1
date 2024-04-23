ff5144f7304cb2520b0d555f2396b586603b9d02
v-uansari <v-uansari@microsoft.com>
2024-04-23 17:33:45 +0530

    update test

--- test/module/Entra/Invalid.Tests.ps1
+++ test/module/Entra/Invalid.Tests.ps1
@@ -2,19 +2,7 @@ if($null -eq (Get-Module -Name Microsoft.Graph.Entra)){
     Import-Module Microsoft.Graph.Entra
 }
 
-Describe "demo"{
-    It "Should fail when parameters are empty"{
-        $module = Get-Module -Name Microsoft.Graph.Entra
-        $module.ExportedCommands.Keys | ForEach-Object{
-            $command = Get-Command $_
-            $GetViaIdentityParameterSet = $command.ParameterSets | Where-Object Name -eq "GetById"
-            if($GetViaIdentityParameterSet.Parameters.Name -contains 'ObjectId'){     
-                $objectId = ""
-                $commandScriptBlock = [scriptblock]::Create("$command -ObjectId $objectId")
-                {Invoke-Command -ScriptBlock $commandScriptBlock} | Should -Throw "Missing an argument for parameter 'ObjectId'.*"
-            }
-        }
-    }
+Describe "Invalid Tests"{
     It "Should fail when parameters are invalid"{
         $module = Get-Module -Name Microsoft.Graph.Entra
         $module.ExportedCommands.Keys | ForEach-Object{
@@ -22,4 +10,74 @@ Describe "demo"{
                 { Invoke-Command $command -demo "" } | Should -Throw "A parameter cannot be found that matches parameter name 'demo'."
             }
     }
+    It "Should fail with 'TenantId' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'TenantId'){
+                $commandScriptBlock = [scriptblock]::Create("$command -TenantId $objectId")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'TenantId'.*"
+            }
+        }
+    }
+    It "Should fail with 'Id' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'Id'){
+                $commandScriptBlock = [scriptblock]::Create("$command -Id $objectId")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'Id'.*"
+            }
+        }
+    }
+    It "Should fail with 'ObjectId' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'ObjectId'){                
+                $commandScriptBlock = [scriptblock]::Create("$command -ObjectId $objectId")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'ObjectId'.*"
+            }
+        }
+    }
+    It "Should fail with 'All' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'All'){                
+                $commandScriptBlock = [scriptblock]::Create("$command -All ")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'All'*"
+            }
+        }
+    }
+    It "Should fail with 'Top' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'Top'){                
+                $commandScriptBlock = [scriptblock]::Create("$command -Top ")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'Top'*"
+            }
+        }
+    }
+    It "Should fail with 'Filter' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'Filter'){                
+                $commandScriptBlock = [scriptblock]::Create("$command -Filter ")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'Filter'*"
+            }
+        }
+    }
+    It "Should fail with 'SearchString' parameter" {
+        $module = Get-Module -Name Microsoft.Graph.Entra
+        $module.ExportedCommands.Keys | ForEach-Object {
+            $command = Get-Command $_
+            if ($command.ParameterSets.Parameters.Name -contains 'SearchString'){                
+                $commandScriptBlock = [scriptblock]::Create("$command -SearchString ")
+                { Invoke-Command -ScriptBlock $commandScriptBlock } | Should -Throw "Missing an argument for parameter 'SearchString'*"
+            }
+        }
+    }      
 }
