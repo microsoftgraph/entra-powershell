92c18732868a4cd8aefa3713b2ff920006e49422
Ashwini Karke <v-akarke@microsoft.com>
2024-05-02 21:35:43 +0530

    mail pull

--- test/module/Entra/Get-EntraApplication.Tests.ps1
+++ test/module/Entra/Get-EntraApplication.Tests.ps1
@@ -1,48 +1,44 @@
-BeforeAll {  
-    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
-        Import-Module Microsoft.Graph.Entra      
-    }
-    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
-    
-    $scriptblock = {
-        return @(
-            [PSCustomObject]@{
-              "Id"                           = "Google-OAUTH"
-              "DisplayName"                  = "Mock-App"
-              "identityProviderType"         = "Google"
-              "AdditionalProperties"         = @{
-                                                  "@odata.context"       = "https://graph.microsoft.com/v1.0/$metadata#identity/identityProviders/$entity"
-                                                  "@odata.type"          = "#microsoft.graph.socialIdentityProvider"
-                                                  "clientId"             = "Google123"
-                                                  "clientSecret"         = "******"
-                                                }
-              "Parameters"                   = $args
-            }
-        )
-    }
-
-    Mock -CommandName New-MgIdentityProvider -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
-}
-
-Describe "New-EntraMSIdentityProvider" {
-Context "Test for New-EntraMSIdentityProvider" {
-        
-        It "Should contain displayName in parameters when passed Name to it" {    
-            
-            $result = New-EntraMSIdentityProvider -Type "Google" -Name "Mock-App" -ClientId "Google123" -ClientSecret "GoogleId"
-            $params = Get-Parameters -data $result.Parameters
-            $Para = $params | convertTo-json -depth 10 | convertFrom-json
-            # $Para.BodyParameter.AdditionalProperties | Should -Be "Mock-App"
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+Describe "Get-EntraApplication" {
+    BeforeAll {
+        if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+            Import-Module Microsoft.Graph.Entra
         }
-        # It "Should contain 'User-Agent' header" {
-        #     $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion New-EntraMSIdentityProvider"
+    }    
 
-        #     $result = New-EntraMSIdentityProvider -Type "Google" -Name "Mock-App" -ClientId "Google123" -ClientSecret "GoogleId"
-        #     $params = Get-Parameters -data $result.Parameters
-        #     $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
-        # }
-
-
+    It "Should support minimum set of parameter sets" {
+        $GetAzureADApplication = Get-Command Get-EntraApplication
+        $GetAzureADApplication.ParameterSets.Name | Should -BeIn @("GetQuery", "GetVague", "GetById")
+        $GetAzureADApplication.Visibility | Should -Be "Public"
+        $GetAzureADApplication.CommandType | Should -Be "Function"
     }
 
+    It "Should return a list of applications by default" {
+        $GetAzureADApplication = Get-Command Get-EntraApplication
+        $GetAzureADApplication.ModuleName | Should -Be "Microsoft.Graph.Entra"
+        $GetAzureADApplication.DefaultParameterSet | Should -Be "GetQuery"
+    }
+
+    It 'Should have List parameterSet' {
+        $GetAzureADApplication = Get-Command Get-EntraApplication
+        $ListParameterSet = $GetAzureADApplication.ParameterSets | Where-Object Name -eq "GetQuery"
+        $ListParameterSet.Parameters.Name | Should -Contain All
+        $ListParameterSet.Parameters.Name | Should -Contain Filter
+        $ListParameterSet.Parameters.Name | Should -Contain Top
+    }
+
+    It 'Should have Get parameterSet' {
+        $GetAzureADApplication = Get-Command Get-EntraApplication
+        $GetParameterSet = $GetAzureADApplication.ParameterSets | Where-Object Name -eq "GetById"
+        $GetParameterSet.Parameters.Name | Should -Contain ObjectId
+    }
+
+    It 'Should have GetViaIdentity parameterSet' {
+        $GetAzureADApplication = Get-Command Get-EntraApplication
+        $GetViaIdentityParameterSet = $GetAzureADApplication.ParameterSets | Where-Object Name -eq "GetVague"
+        $GetViaIdentityParameterSet.Parameters.Name | Should -Contain SearchString
+        $GetViaIdentityParameterSet.Parameters.Name | Should -Contain All
+    }
 }
