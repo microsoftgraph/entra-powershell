c49260ee08ac3a5db95404cc3b44a69ad4523697
Sanjeev Kumar <sanjeev.kumar@vendorpartners.com.ph>
2024-05-02 18:20:22 +0530

    integrated EntraMSGroup, EntraMSGroupLifecyclePolicy into EntraMSLifecyclePolicyGroup and EntraMSApplication into EntraMSApplicationExtensionProperty

--- test/module/Entra/Integration/EntraMSApplicationExtensionProperty.Tests.ps1
+++ test/module/Entra/Integration/EntraMSApplicationExtensionProperty.Tests.ps1
@@ -21,29 +21,43 @@ Describe "The EntraMSApplicationExtensionProperty command executing unmocked" {
             $thisTestInstanceId = New-Guid | Select-Object -expandproperty guid
             $testApplicationName = 'Test Demo Name' + $thisTestInstanceId
             $global:newMSApplication = New-EntraMSApplication -DisplayName $testApplicationName
-            Write-Host "Id :$($newMSApplication.Id)"
-            Write-Host "AppId :$($newMSApplication.AppId)"
+        }
+        
+        It "should successfully get an application by display name" {
+            $application = Get-EntraMSApplication -Filter "DisplayName eq '$($newMSApplication.DisplayName)'"
+            $application.ObjectId | Should -Be $newMSApplication.Id
+            $application.AppId | Should -Be $newMSApplication.AppId
+            $application.DisplayName | Should -Be $newMSApplication.DisplayName
+        }
 
+        It "should successfully update a application display name" {
+            $updatedDisplayName = "Update Application Name"
+            Set-EntraMSApplication -ObjectId $newMSApplication.ObjectId -DisplayName $updatedDisplayName
+            $result = Get-EntraMSApplication -Filter "AppId eq '$($newMSApplication.AppId)'"
+            $result.ObjectId | Should -Be $newMSApplication.Id
+            $result.AppId | Should -Be $newMSApplication.AppId
+            $result.DisplayName | Should -Be "Update Application Name"
+        }
+
+        It "should successfully create application extension property" {
             # Create an extension property
             $global:newMSApplicationExtensionProperty = New-EntraMSApplicationExtensionProperty -ObjectId $newMSApplication.Id -DataType "string" -Name "NewAttribute" -TargetObjects "Application"
-            Write-Host "ObjectId :$($newMSApplicationExtensionProperty.ObjectId)"
-            Write-Host "Name :$($newMSApplicationExtensionProperty.Name)"
         }
 
         It "should successfully get application extension property" {
             # Get application extension property using object id 
-            $applicationExtensionProperty = Get-EntraMSApplicationExtensionProperty -ObjectId $newMSApplicationExtensionProperty.ObjectId
-            $applicationExtensionProperty.ObjectId | Should -Be $newMSApplicationExtensionProperty.ObjectId
+            $applicationExtensionProperty = Get-EntraMSApplicationExtensionProperty -ObjectId $newMSApplication.Id
+            $applicationExtensionProperty.ObjectId | Should -Be $newMSApplicationExtensionProperty.Id
             $applicationExtensionProperty.Name | Should -Be $newMSApplicationExtensionProperty.Name
-            Write-Host $applicationExtensionProperty
+           
         }
 
         AfterAll {
-            if ($newMSGroup) {
-                Remove-EntraMSApplication -ObjectId $newMSApplication.ObjectId | Out-Null
+            if ($newMSApplicationExtensionProperty) {
+                Remove-EntraMSApplicationExtensionProperty -ObjectId $newMSApplication.Id -ExtensionPropertyId $newMSApplicationExtensionProperty.Id | Out-Null
             }
-            if ($testGroupPolicy) {
-                # Remove-EntraMSApplicationExtensionProperty -ObjectId $newMSApplicationExtensionProperty.ObjectId -ExtensionPropertyId "344ed560-f8e7-410e-ab9f-c79df5c36" | Out-Null
+            if ($newMSApplication) {
+                Remove-EntraMSApplication -ObjectId $newMSApplication.Id | Out-Null
             }
         }
     }
--- test/module/Entra/Integration/EntraMSLifecyclePolicyGroup.Tests.ps1
+++ test/module/Entra/Integration/EntraMSLifecyclePolicyGroup.Tests.ps1
@@ -19,50 +19,84 @@ Describe "The EntraMSLifecyclePolicyGroup command executing unmocked" {
 
             # Create a group with Description parameter.
             $thisTestInstanceId = New-Guid | Select-Object -ExpandProperty Guid
-            $testName = 'Demo Help Group' + $thisTestInstanceId
+            $global:displayName = 'Demo Help Group' + $thisTestInstanceId
             $testNickname = "testhelpDeskAdminGroup"
-            $global:newMSGroup = New-EntraMSGroup -DisplayName $testName -MailEnabled $false -MailNickname $testNickname -SecurityEnabled $true -GroupTypes "unified"
-            Write-Host "Group Id:$($newMSGroup.Id)" 
-            # Validate group creation
-            if (-not $newMSGroup) {
-                throw "Failed to create a new group."
-            }
+            $global:newMSGroup = New-EntraMSGroup -DisplayName $displayName -MailEnabled $false -MailNickname $testNickname -SecurityEnabled $true -GroupTypes "unified"
             Start-Sleep -Seconds 10
+        }
 
+        It "should successfully get a specific group by using an group Id" {
+            $group = Get-EntraMSGroup -Id $newMSGroup.Id
+            $group.ObjectId | Should -Be $newMSGroup.Id
+            $group.DisplayName | Should -Be $displayName
+        }
+
+        It "should successfully update a group display name" {
+            $updatedDisplayName = "Update Help Group Name"
+            Set-EntraMSGroup -Id $newMSGroup.Id -DisplayName $updatedDisplayName
+            $result = Get-EntraGroup -ObjectId $newMSGroup.Id
+            $result.Id | Should -Contain $newMSGroup.Id
+        }
+
+        It "should successfully Create a lifecycle policy" {
             # Create a lifecycle policy
             $global:testGroupPolicy = New-EntraMSGroupLifecyclePolicy -GroupLifetimeInDays 99 -ManagedGroupTypes "Selected" -AlternateNotificationEmails "example@contoso.un"
-            Write-Host "Policy Id:$($testGroupPolicy.Id)"
-            # Validate policy creation
-            if (-not $testGroupPolicy) {
-                throw "Failed to create a new group lifecycle policy."
-            }
+        }
+
+        It "should successfully retrieve properties of an groupLifecyclePolicy" {
+            $groupLifecyclePolicy = Get-EntraMSGroupLifecyclePolicy -Id $testGroupPolicy.Id
+
+            # Ensure that the retrieved group lifecycle policy matches the expected one
+            $groupLifecyclePolicy.Id | Should -Be $testGroupPolicy.Id
+            $groupLifecyclePolicy.GroupLifetimeInDays | Should -Be 99
+            $groupLifecyclePolicy.ManagedGroupTypes | Should -Contain "Selected"
+            $groupLifecyclePolicy.AlternateNotificationEmails | Should -Contain "example@contoso.un"
+        }
+
+        It "should successfully update groupLifecyclePolicy" {
+            $alternateNotificationEmails = "admingroup@contoso.en"
+            $global:updatedGroupLifecyclePolicy = Set-EntraMSGroupLifecyclePolicy -Id $testGroupPolicy.Id -GroupLifetimeInDays 200 -AlternateNotificationEmails $alternateNotificationEmails -ManagedGroupTypes "Selected"
+            Start-Sleep -Seconds 10
+
+            # Ensure that the retrieved group lifecycle policy matches the expected one
+            $updatedGroupLifecyclePolicy.Id | Should -Be $testGroupPolicy.Id
+            $updatedGroupLifecyclePolicy.GroupLifetimeInDays | Should -Be 200
+            $updatedGroupLifecyclePolicy.ManagedGroupTypes | Should -Contain "Selected"
+            $updatedGroupLifecyclePolicy.AlternateNotificationEmails | Should -Contain $alternateNotificationEmails
+        }
+
+        # It "should throw an exception if a nonexistent ID parameter is specified" {
+        #     $Id = (New-Guid).Guid
+        #     Get-EntraMSGroupLifecyclePolicy -Id $Id -ErrorAction ignore 
+        #     $errorMessage = $Error[0].Exception.Message
+        #     $errorMessage | Should -match "([\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12})"
+        # }
+
+        It "should successfully associate the group with the lifecycle policy" {
+            # Associate the group with the lifecycle policy
+            $testLifePolicyGroup = Add-EntraMSLifecyclePolicyGroup -Id $testGroupPolicy.Id -GroupId $newMSGroup.Id
+            $testLifePolicyGroup.ObjectId | Should -BeNullOrEmpty
             Start-Sleep -Seconds 10
         }
 
         It "should successfully retrieve details of a LifecyclePolicyGroup" {
-            # Associate the group with the lifecycle policy
-            $testLifePolicyGroup = Add-EntraMSLifecyclePolicyGroup -Id $testGroupPolicy.Id -GroupId $newMSGroup.Id
-            Write-Host "Lifecycle Policy Group Id:$($testLifePolicyGroup.Id)"  
-            $testLifePolicyGroup.ObjectId | Should -BeNullOrEmpty
-            
             # Get lifecycle policy group using group id 
-            $lifecyclePolicyGroup = Get-EntraMSLifecyclePolicyGroup -Id $newMSGroup.Id
+            $global:lifecyclePolicyGroup = Get-EntraMSLifecyclePolicyGroup -Id $newMSGroup.Id
             $lifecyclePolicyGroup.ObjectId | Should -Be $testGroupPolicy.Id
-            $lifecyclePolicyGroup.GroupLifetimeInDays | Should -Be 99
+            $lifecyclePolicyGroup.GroupLifetimeInDays | Should -Be 200
             $lifecyclePolicyGroup.ManagedGroupTypes | Should -Contain "Selected"
-            $lifecyclePolicyGroup.AlternateNotificationEmails | Should -Contain "example@contoso.un"
-            Write-Host $lifecyclePolicyGroup
+            $lifecyclePolicyGroup.AlternateNotificationEmails | Should -Contain $updatedGroupLifecyclePolicy.AlternateNotificationEmails
         }
 
         AfterAll {
+            if ($lifecyclePolicyGroup) {
+                Remove-EntraMSLifecyclePolicyGroup -Id $lifecyclePolicyGroup.Id -GroupId $newMSGroup.Id | Out-Null
+            }
+            if ($updatedGroupLifecyclePolicy) {
+                Remove-EntraMSGroupLifecyclePolicy -Id $updatedGroupLifecyclePolicy.Id | Out-Null
+            }
             if ($newMSGroup) {
                 Remove-EntraMSGroup -Id $newMSGroup.Id | Out-Null
-            }
-            if ($testGroupPolicy) {
-                Remove-EntraMSGroupLifecyclePolicy -Id $testGroupPolicy.Id | Out-Null
-            }
-            if ($testLifePolicyGroup) {
-                Remove-EntraMSLifecyclePolicyGroup -Id $testLifePolicyGroup.Id -GroupId $newMSGroup.Id | Out-Null
             }
         }
     }
