dd8a9126167adf8544a4333226f7d449b73b31f4
Ashwini Karke <v-akarke@microsoft.com>
2024-05-03 18:17:47 +0530

    Get-EntraCurrentSessionInfo

--- module/EntraBeta/customizations/Get-EntraBetaCurrentSessionInfo.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaCurrentSessionInfo.ps1
@@ -3,7 +3,41 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADCurrentSessionInfo"
-    TargetName = "Get-MgContext"
+    TargetName = $null
     Parameters = $null
     Outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $keysChanged = @{}
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Confirm"])
+        {
+            $params["Confirm"] = $PSBoundParameters["Confirm"]
+        }
+        if($null -ne $PSBoundParameters["WhatIf"])
+        {
+            $params["WhatIf"] = $PSBoundParameters["WhatIf"]
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Get-MgContext @params 
+        $response | ForEach-Object {
+            if($null -ne $_) {
+            Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+            }
+        }
+        $response
+        }
+'@
 }
