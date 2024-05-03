d7e1f97b3b3a1e407ca6cbc7d57ef6a41f849586
v-akarke <142799789+v-akarke@users.noreply.github.com>
2024-05-03 05:55:43 -0700

    New-AzureADMSNamedLocationPolicy (#491)

--- module/EntraBeta/customizations/New-EntraBetaMSNamedLocationPolicy.ps1
+++ module/EntraBeta/customizations/New-EntraBetaMSNamedLocationPolicy.ps1
@@ -3,7 +3,79 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "New-AzureADMSNamedLocationPolicy"
-    TargetName = "New-MgBetaIdentityConditionalAccessNamedLocation"
+    TargetName = $null
     Parameters = $null
     Outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $body = @{}
+        $params = @{}
+        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
+        $keysChanged = @{}
+        if($null -ne $PSBoundParameters["IncludeUnknownCountriesAndRegions"])
+        {
+            $body["IncludeUnknownCountriesAndRegions"] = $PSBoundParameters["IncludeUnknownCountriesAndRegions"]
+        }
+        if($null -ne $PSBoundParameters["Id"])
+        {
+            $body["Id"] = $PSBoundParameters["Id"]
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["IsTrusted"])
+        {
+            $body["IsTrusted"] = $PSBoundParameters["IsTrusted"]
+        }
+        if($null -ne $PSBoundParameters["OdataType"])
+        {
+            $body["@odata.type"] = $PSBoundParameters["OdataType"]
+        }
+        if($null -ne $PSBoundParameters["CountriesAndRegions"])
+        {
+            $body["CountriesAndRegions"] = $PSBoundParameters["CountriesAndRegions"]
+        }
+        if($null -ne $PSBoundParameters["IpRanges"])
+        {
+            $Tmp = $PSBoundParameters["IpRanges"]
+                $hash =@()
+                foreach($i in $Tmp){
+                    $hash += @{cidrAddress=$i.CidrAddress}
+                }
+            $body["IpRanges"] = $hash
+        }
+        if($null -ne $PSBoundParameters["DisplayName"])
+        {
+            $body["DisplayName"] = $PSBoundParameters["DisplayName"]
+        }
+    
+        $params["BodyParameter"] = $body
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = New-MgBetaIdentityConditionalAccessNamedLocation @params -Headers $customHeaders
+        $response | ForEach-Object {
+            if($null -ne $_) {
+                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+            $propsToConvert = @('ipRanges')
+            try {
+                foreach ($prop in $propsToConvert) {
+                    $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                }
+            }
+            catch {}   
+            }
+        }
+        $response
+        }
+'@
 }
