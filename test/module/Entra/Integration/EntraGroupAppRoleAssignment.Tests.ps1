f0b32fa86d10f831db59bb9bda167e8e918c623a
Sanjeev Kumar <sanjeev.kumar@vendorpartners.com.ph>
2024-05-06 19:12:58 +0530

    updated test

--- test/module/Entra/Integration/EntraGroupAppRoleAssignment.Tests.ps1
+++ test/module/Entra/Integration/EntraGroupAppRoleAssignment.Tests.ps1
@@ -1,6 +1,6 @@
-Describe "The EntraGroup command executing unmocked" {
+Describe "The EntraGroupAppRoleAssignment command executing unmocked" {
 
-    Context "When getting user and group" {
+    Context "When getting GroupAppRoleAssignment" {
         BeforeAll {
             $testReportPath = join-path $psscriptroot "\setenv.ps1"
             Import-Module -Name $testReportPath
@@ -13,7 +13,6 @@ Describe "The EntraGroup command executing unmocked" {
             $global:displayName = 'DemoName' + $thisTestInstanceId
             
             $global:newGroup = New-EntraGroup -DisplayName $displayName -MailEnabled $false -SecurityEnabled $true -MailNickName $displayName
-            Write-Host $newGroup
         }
 
         It "should successfully get a specific group by using an Id" {
@@ -29,8 +28,69 @@ Describe "The EntraGroup command executing unmocked" {
             $result.Id | Should -Contain $newGroup.Id
         }
 
+        It "should successfully create application" {
+            $applicationDisplayName = "Demo new application"
+            $global:createdApplication = New-EntraApplication -DisplayName $applicationDisplayName
+            $createdApplication.DisplayName  | Should -Be $applicationDisplayName
+        }
+
+        It "should successfully get application" {
+            $global:getCreatedApplication = Get-EntraApplication -ObjectId $createdApplication.Id
+            $getCreatedApplication.DisplayName  | Should -Be $createdApplication.DisplayName
+            $getCreatedApplication.Id  | Should -Be $createdApplication.Id
+            $getCreatedApplication.AppId  | Should -Be $createdApplication.AppId
+        }
+
+        It "should successfully update application display name" {
+            $global:updateApplicationDisplayName = "Update demo application"
+            Set-EntraApplication -ObjectId $getCreatedApplication.Id -DisplayName $updateApplicationDisplayName
+            
+            $global:getUpdatedCreatedApplication = Get-EntraApplication -ObjectId $getCreatedApplication.Id
+            $getUpdatedCreatedApplication.DisplayName  | Should -Be $updateApplicationDisplayName
+            $getUpdatedCreatedApplication.Id  | Should -Be $getCreatedApplication.Id
+            $getUpdatedCreatedApplication.AppId  | Should -Be $getCreatedApplication.AppId
+        }
+
+        It "should successfully create and get service principal" {
+            $global:MyApp = Get-EntraApplication -Filter "DisplayName eq '$($getUpdatedCreatedApplication.DisplayName)'"
+            
+            New-EntraServicePrincipal -AccountEnabled $true -AppId $MyApp.AppId -AppRoleAssignmentRequired $true -DisplayName $MyApp.DisplayName -Tags {"WindowsAzureActiveDirectoryIntegratedApp"}
+            $global:createdServicePrincipal = Get-EntraServicePrincipal -Filter "DisplayName eq '$($MyApp.DisplayName)'"
+            $createdServicePrincipal.AppId | Should -Be $MyApp.AppId
+            $createdServicePrincipal.DisplayName | Should -Be $MyApp.DisplayName
+        }
+
+        It "should successfully update the account of a service principal" {
+            Set-EntraServicePrincipal -ObjectId $createdServicePrincipal.Id -AccountEnabled $False
+            $disableServicePrincipal = Get-EntraServicePrincipal -Filter "DisplayName eq '$($MyApp.DisplayName)'"
+            $disableServicePrincipal.AppId | Should -Be $MyApp.AppId
+            $disableServicePrincipal.DisplayName | Should -Be $MyApp.DisplayName
+
+            Set-EntraServicePrincipal -ObjectId $createdServicePrincipal.Id -AccountEnabled $True
+            $global:updatedServicePrincipal = Get-EntraServicePrincipal -Filter "DisplayName eq '$($MyApp.DisplayName)'"
+            $updatedServicePrincipal.AppId | Should -Be $MyApp.AppId
+            $updatedServicePrincipal.DisplayName | Should -Be $MyApp.DisplayName
+        }
+
+        It "should successfully assign a group of users to an application" {
+           New-EntraGroupAppRoleAssignment -ObjectId $newGroup.ObjectId -PrincipalId $newGroup.ObjectId -ResourceId $updatedServicePrincipal.ObjectId -Id $updatedServicePrincipal.AppId
+        }
+
+        It "should successfully retrieve application role assignments of a group" {
+            $global:getGroupAppRoleAssignment = Get-EntraGroupAppRoleAssignment -ObjectId $newGroup.Id
+            Write-Host $getGroupAppRoleAssignment
+        }
         
         AfterAll {
+            if ( $getGroupAppRoleAssignment) {
+                Remove-EntraGroupAppRoleAssignment -ObjectId $newGroup.Id -AppRoleAssignmentId $getGroupAppRoleAssignment.Id | Out-Null
+            }
+            if ( $updatedServicePrincipal) {
+                Remove-EntraServicePrincipal -ObjectId $updatedServicePrincipal.Id | Out-Null
+            }
+            if ( $getUpdatedCreatedApplication) {
+                Remove-EntraApplication -ObjectId $getUpdatedCreatedApplication.Id | Out-Null
+            }
             if ($newGroup) {
                 Remove-EntraGroup -ObjectId $newGroup.Id | Out-Null
             }
