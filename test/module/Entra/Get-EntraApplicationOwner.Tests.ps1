5409db9f4fecdb980d06951d5ef1d6bee5743849
v-uansari <v-uansari@microsoft.com>
2024-05-06 17:47:41 +0530

    added mock tests

--- test/module/Entra/Get-EntraApplicationOwner.Tests.ps1
+++ test/module/Entra/Get-EntraApplicationOwner.Tests.ps1
@@ -4,24 +4,28 @@ BeforeAll {
     }
     Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
     
-    $mockResponse = @{
-        value = @{
-            Id                            = "e3108c4d-86ff-4ceb-9429-24e85b4b8cea"          
-            ageGroup                      = $null
-            onPremisesLastSyncDateTime    = $null
-            creationType                  = $null
-            imAddresses                   = {"adelev@m365x99297270.onmicrosoft.com"}
-            preferredLanguage             = $null
-            mail                          = "AdeleV@M365x99297270.OnMicrosoft.com"
-            securityIdentifier            = "S-1-12-1-1093396945-1080104032-2731339150-364051459"
-            identities                    = {}
-            consentProvidedForMinor       = $null
-            onPremisesUserPrincipalName   = $null
-            Parameters                    = $args
+    $mockResponse = {
+        return @{
+            value = @(
+                @{
+                    Id                            = "e3108c4d-86ff-4ceb-9429-24e85b4b8cea"          
+                    ageGroup                      = $null
+                    onPremisesLastSyncDateTime    = $null
+                    creationType                  = $null
+                    imAddresses                   = {"adelev@m365x99297270.onmicrosoft.com"}
+                    preferredLanguage             = $null
+                    mail                          = "AdeleV@M365x99297270.OnMicrosoft.com"
+                    securityIdentifier            = "S-1-12-1-1093396945-1080104032-2731339150-364051459"
+                    identities                    = {}
+                    consentProvidedForMinor       = $null
+                    onPremisesUserPrincipalName   = $null
+                    Parameters                    = $args
+                }
+            )
         }
-    }    
+    }
     
-    Mock -CommandName Invoke-GraphRequest -MockWith {$mockResponse} -ModuleName Microsoft.Graph.Entra
+    Mock -CommandName Invoke-GraphRequest -MockWith $mockResponse -ModuleName Microsoft.Graph.Entra
 }
 
 Describe "Get-EntraApplicationOwner"{
@@ -36,11 +40,12 @@ Describe "Get-EntraApplicationOwner"{
         It "Should fail when ObjectId is empty" {
             { Get-EntraApplicationOwner -ObjectId "" } | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
         }
-        # It "Should contain 'User-Agent' header" {
-        #     $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraApplicationOwner"
-        #     $result = Get-EntraApplicationOwner -ObjectId "e3108c4d-86ff-4ceb-9429-24e85b4b8cea"            
-        #     $params = Get-Parameters -data $result.Parameters
-        #     $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
-        # }  
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraApplicationOwner"
+            $result = Get-EntraApplicationOwner -ObjectId "e3108c4d-86ff-4ceb-9429-24e85b4b8cea"
+            $params = Get-Parameters -data $result.Parameters
+            $a= $params | ConvertTo-json | ConvertFrom-Json
+            $a.headers.'User-Agent' | Should -Be $userAgentHeaderValue
+        }
     }
 }
