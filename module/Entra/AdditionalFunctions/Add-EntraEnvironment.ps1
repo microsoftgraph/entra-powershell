bb87c28f42a88f4e0a6f8f6ba5c33ecb80a58e0e
Emmanuel Nganga <enganga@microsoft.com>
2024-05-08 13:17:11 +0300

    Added Add-EntraEnvironment.ps1

---
+++ module/Entra/AdditionalFunctions/Add-EntraEnvironment.ps1
@@ -0,0 +1,47 @@
+function Add-EntraEnvironment{
+    [CmdletBinding(DefaultParameterSetName = '')]
+      param (
+      [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+      [System.String] $Name,
+      [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+      [System.String] $AzureADEndpoint,
+      [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+      [System.String] $GraphEndpoint
+      )
+  
+      PROCESS{
+        $params=@{}
+         $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+  
+         if ($PSBoundParameters.ContainsKey("Verbose")) {
+              $params["Verbose"] = $Null
+          }
+         if ($PSBoundParameters.ContainsKey("Debug")) {
+              $params["Debug"] = $Null
+         }
+  
+        if($null -ne $PSBoundParameters["Name"]){
+           $params["Name"]=$PSBoundParameters["Name"]
+         }
+  
+         if($null -ne $PSBoundParameters["AzureADEndpoint"]){
+           $params["AzureADEndpoint"]=$PSBoundParameters["AzureADEndpoint"]
+         }
+  
+         if($null -ne $PSBoundParameters["GraphEndpoint"]){
+           $params["GraphEndpoint"]=$PSBoundParameters["GraphEndpoint"]
+         }
+  
+          Write-Debug("============================ TRANSFORMATIONS ============================")
+          $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+          Write-Debug("=========================================================================`n")
+  
+          Add-MgEnvironment @params  -Headers $customHeaders
+      
+      }
+  
+  
+  }
+  
+  
+  
