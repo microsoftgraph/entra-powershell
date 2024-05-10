3462be6d3ac17b2237cf8997d7ff7c07d120ac9b
Ashwini Karke <v-akarke@microsoft.com>
2024-05-10 11:32:08 +0530

    added common parameters

--- module/EntraBeta/customizations/New-EntraBetaObjectSetting.ps1
+++ module/EntraBeta/customizations/New-EntraBetaObjectSetting.ps1
@@ -6,35 +6,71 @@
     TargetName = $null
     Parameters = $null
     Outputs = $null
-    CustomScript = @"
+    CustomScript = @'
     PROCESS {  
-        `$params = @{}
-        `$customHeaders = New-EntraBetaCustomHeaders -Command `$MyInvocation.MyCommand
-                if (`$null -ne `$PSBoundParameters["TargetType"]) {
-                    `$params["TargetType"] = `$PSBoundParameters["TargetType"]
+        $params = @{}
+        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
+                if ($null -ne $PSBoundParameters["TargetType"]) {
+                    $params["TargetType"] = $PSBoundParameters["TargetType"]
                 }
-                if (`$null -ne `$PSBoundParameters["TargetObjectId"]) {
-                    `$params["TargetObjectId"] = `$PSBoundParameters["TargetObjectId"]
+                if ($null -ne $PSBoundParameters["TargetObjectId"]) {
+                    $params["TargetObjectId"] = $PSBoundParameters["TargetObjectId"]
                 }
-                if (`$null -ne `$PSBoundParameters["DirectorySetting"]) {
-                    `$params["DirectorySetting"] = `$PSBoundParameters["DirectorySetting"]
+                if ($null -ne $PSBoundParameters["DirectorySetting"]) {
+                    $params["DirectorySetting"] = $PSBoundParameters["DirectorySetting"]
                 }
-                if (`$PSBoundParameters.ContainsKey("Debug")) {
-                    `$params["Debug"] = `$Null
+                if ($PSBoundParameters.ContainsKey("Debug")) {
+                    $params["Debug"] = $Null
                 }
-                if (`$PSBoundParameters.ContainsKey("Verbose")) {
-                    `$params["Verbose"] = `$Null
+                if ($PSBoundParameters.ContainsKey("Verbose")) {
+                    $params["Verbose"] = $Null
+                }
+                if($null -ne $PSBoundParameters["WarningVariable"])
+                {
+                    $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+                }
+                if($null -ne $PSBoundParameters["InformationVariable"])
+                {
+                    $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+                }
+                if($null -ne $PSBoundParameters["InformationAction"])
+                {
+                    $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+                }
+                if($null -ne $PSBoundParameters["OutVariable"])
+                {
+                    $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+                }
+                if($null -ne $PSBoundParameters["OutBuffer"])
+                {
+                    $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+                }
+                if($null -ne $PSBoundParameters["ErrorVariable"])
+                {
+                    $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+                }
+                if($null -ne $PSBoundParameters["PipelineVariable"])
+                {
+                    $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+                }
+                if($null -ne $PSBoundParameters["ErrorAction"])
+                {
+                    $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+                }
+                if($null -ne $PSBoundParameters["WarningAction"])
+                {
+                    $params["WarningAction"] = $PSBoundParameters["WarningAction"]
                 }
                     Write-Debug("============================ TRANSFORMATIONS ============================")
-                    `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
-                    Write-Debug("=========================================================================``n")
-                    `$directorySettingsJson =  `$DirectorySetting| ForEach-Object {
-                        `$NonEmptyProperties = `$_.psobject.Properties | Where-Object {`$_.Value} | Select-Object -ExpandProperty Name
-                        `$propertyValues = `$_ | Select-Object -Property `$NonEmptyProperties | ConvertTo-Json
-                        [regex]::Replace(`$propertyValues,'(?<=")(\w+)(?=":)',{`$args[0].Groups[1].Value.ToLower()})
+                    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+                    Write-Debug("=========================================================================`n")
+                    $directorySettingsJson =  $DirectorySetting| ForEach-Object {
+                        $NonEmptyProperties = $_.psobject.Properties | Where-Object {$_.Value} | Select-Object -ExpandProperty Name
+                        $propertyValues = $_ | Select-Object -Property $NonEmptyProperties | ConvertTo-Json
+                        [regex]::Replace($propertyValues,'(?<=")(\w+)(?=":)',{$args[0].Groups[1].Value.ToLower()})
                          }
-                    `$response = Invoke-GraphRequest -Headers `$customHeaders -Method POST -Uri https://graph.microsoft.com/beta/`$TargetType/`$TargetObjectId/settings -Body `$directorySettingsJson
-                    `$response | ConvertTo-Json | ConvertFrom-Json
-    }
-"@
+                    $response = Invoke-GraphRequest -Headers $customHeaders -Method POST -Uri https://graph.microsoft.com/beta/$TargetType/$TargetObjectId/settings -Body $directorySettingsJson
+                    $response | ConvertTo-Json | ConvertFrom-Json
+        }
+'@
 }
