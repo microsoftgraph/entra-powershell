78d7d9b62304056a9d7c6443fd1a351c9af0734a
Emmanuel Nganga <enganga@microsoft.com>
2024-05-13 12:28:16 +0300

    Added Find-EntraPermissions.ps1

---
+++ module/Entra/AdditionalFunctions/Find-EntraPermissions.ps1
@@ -0,0 +1,62 @@
+function Find-EntraPermissions {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+    [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $SearchString,
+    [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
+    [System.Nullable`1[System.Boolean]] $ExactMatch,
+    [Parameter(ParameterSetName = "GetQuery", Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $PermissionType,
+    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.Nullable`1[System.Boolean]] $All,
+     [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $Online,
+    [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $Filter
+    )
+
+    PROCESS {    
+        $params = @{}
+        if($null -ne $PSBoundParameters["SearchString"])
+        {
+            $params["SearchString"]=$PSBoundParameters["SearchString"]
+        }
+        if($null -ne $PSBoundParameters["ExactMatch"])
+        {
+            $params["ExactMatch"] = $PSBoundParameters["ExactMatch"]
+        }
+        
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["All"])
+        {
+            if($PSBoundParameters["All"])
+            {
+                $params["All"] = $PSBoundParameters["All"]
+            }
+        }
+        if($null -ne $PSBoundParameters["Online"])
+        {
+            if($PSBoundParameters["Online"])
+            {
+                $params["Online"] = $PSBoundParameters["Online"]
+            }
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["Top"])
+        {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        Find-MgGraphPermission @params 
+        }        
+}
