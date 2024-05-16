81be5a8ba5724046f2464677644e538c76d4b5ae
snehalkotwal <v-skotwal@microsoft.com>
2024-05-16 16:04:54 +0530

    Integration test added

--- test/module/Entra/Integration/Set-EntraMSRoleDefinition.Tests.ps1
+++ test/module/Entra/Integration/Set-EntraMSRoleDefinition.Tests.ps1
@@ -8,22 +8,22 @@ Describe "The Set-EntraMSRoleDefination command executing unmocked" {
             $tenantId = $env:TEST_TENANTID
             $cert = $env:CERTIFICATETHUMBPRINT
             Connect-Entra -TenantId $tenantId  -AppId $appId -CertificateThumbprint $cert
-
+        }
+            #create new role defination 
+        It "should successfully create new ms role defination " {
             $thisTestInstanceId = New-Guid | select -expandproperty guid
             $testName = 'SimpleRoleDefination' + $thisTestInstanceId
-
-            #create new role defination 
             $RolePermissions = New-object Microsoft.Open.MSGraph.Model.RolePermission
             $RolePermissions.AllowedResourceActions =  @("microsoft.directory/applications/basic/read")
            $global:newmsRoleDefinition = New-EntraMSRoleDefinition -RolePermissions $RolePermissions -IsEnabled $false -DisplayName $testName
     
         }
-
         It "should successfully update the msrole defination with expected properties when the msrole defination ID parameter is used" {
-           $thisTestInstanceId = New-Guid | select -expandproperty guid
-           $newmsrolename = 'SimpleRoleDefinationUpdated' + $thisTestInstanceId
-           Set-EntraMSRoleDefinition -Id $newmsRoleDefinition.Id -DisplayName $newmsrolename | Should -BeNullOrEmpty
-            
+            $thisTestInstanceId = New-Guid | select -expandproperty guid
+            $global:newmsrolename = 'SimpleRoleDefinationUpdated' + $thisTestInstanceId
+            Set-EntraMSRoleDefinition -Id $newmsRoleDefinition.Id -DisplayName $newmsrolename | Should -BeNullOrEmpty
+        }
+        It "should successfully retrieve details of a MS role defination" {    
             $app = Get-EntraMSRoleDefinition -Id $newmsRoleDefinition.Id
             $app | Should -Not -BeNullOrEmpty
             $app.Id | Should -Be $newmsRoleDefinition.Id
