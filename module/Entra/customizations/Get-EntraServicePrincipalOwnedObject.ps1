7a565a100cc395de3f267aa2840807a62995e2ea
v-uansari <v-uansari@microsoft.com>
2024-05-21 16:31:06 +0530

    updates

--- module/Entra/customizations/Get-EntraServicePrincipalOwnedObject.ps1
+++ module/Entra/customizations/Get-EntraServicePrincipalOwnedObject.ps1
@@ -3,86 +3,21 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Get-AzureADServicePrincipalOwnedObject"
-    TargetName = $null
-    Parameters = $null
-    Outputs = $null
-    CustomScript = @'
-    PROCESS {    
-        $params = @{}
-        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
-        $keysChanged = @{ObjectId = "Id"}
-        if($null -ne $PSBoundParameters["ErrorAction"])
-        {
-            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+    TargetName = "Get-MgServicePrincipalOwnedObject"
+    Parameters = @(
+        @{
+            SourceName = "ObjectId"
+            TargetName = "ServicePrincipalId"
+            ConversionType = "Name"
+            SpecialMapping = $null
         }
-        if($PSBoundParameters.ContainsKey("Verbose"))
-        {
-            $params["Verbose"] = $Null
+    )
+    Outputs = @(
+        @{
+            SourceName = "AdditionalProperties"
+            TargetName = "AdditionalProperties"
+            ConversionType = "FlatObject"
+            SpecialMapping = $null
         }
-        if($null -ne $PSBoundParameters["OutVariable"])
-        {
-            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-        }
-        if($null -ne $PSBoundParameters["InformationAction"])
-        {
-            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
-        }
-        if($null -ne $PSBoundParameters["WarningVariable"])
-        {
-            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
-        }
-        if($PSBoundParameters.ContainsKey("Debug"))
-        {
-            $params["Debug"] = $Null
-        }
-        if($null -ne $PSBoundParameters["PipelineVariable"])
-        {
-            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
-        }
-        if($null -ne $PSBoundParameters["OutBuffer"])
-        {
-            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
-        }
-        if($null -ne $PSBoundParameters["ErrorVariable"])
-        {
-            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
-        }
-        if($null -ne $PSBoundParameters["Top"])
-        {
-            $params["Top"] = $PSBoundParameters["Top"]
-        }
-        if($null -ne $PSBoundParameters["ObjectId"])
-        {
-            $params["ServicePrincipalId"] = $PSBoundParameters["ObjectId"]
-        }
-        if($null -ne $PSBoundParameters["All"])
-        {
-            if($PSBoundParameters["All"])
-            {
-                $params["All"] = $PSBoundParameters["All"]
-            }
-        }
-        if($null -ne $PSBoundParameters["WarningAction"])
-        {
-            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-        }
-        if($null -ne $PSBoundParameters["InformationVariable"])
-        {
-            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-        }
-    
-        Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-        Write-Debug("=========================================================================`n")
-        
-        $response = Get-MgServicePrincipalOwnedObject @params -Headers $customHeaders
-        $response | ForEach-Object {
-            if($null -ne $_) {
-                Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
-                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id    
-            }
-        }
-        $response
-    }
-'@
+    )
 }
