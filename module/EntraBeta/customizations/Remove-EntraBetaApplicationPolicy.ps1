c9fa554d3deb10052ad65f4840f93aab65ece83f
Ashwini Karke <v-akarke@microsoft.com>
2024-05-21 15:33:34 +0530

    updated indentation

--- module/EntraBeta/customizations/Remove-EntraBetaApplicationPolicy.ps1
+++ module/EntraBeta/customizations/Remove-EntraBetaApplicationPolicy.ps1
@@ -10,62 +10,62 @@
     PROCESS {  
         $params = @{}
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-                if ($null -ne $PSBoundParameters["Id"]) {
-                    $params["Id"] = $PSBoundParameters["Id"]
-                }
-                if ($null -ne $PSBoundParameters["PolicyId"]) {
-                    $params["PolicyId"] = $PSBoundParameters["PolicyId"]
-                }
-                $Method = "DELETE"
-            
-                if ($PSBoundParameters.ContainsKey("Debug")) {
-                    $params["Debug"] = $Null
-                }
-                if ($PSBoundParameters.ContainsKey("Verbose")) {
-                    $params["Verbose"] = $Null
-                }
-                if($null -ne $PSBoundParameters["WarningVariable"])
-                {
-                    $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
-                }
-                if($null -ne $PSBoundParameters["InformationVariable"])
-                {
-                    $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-                }
-                if($null -ne $PSBoundParameters["InformationAction"])
-                {
-                    $params["InformationAction"] = $PSBoundParameters["InformationAction"]
-                }
-                if($null -ne $PSBoundParameters["OutVariable"])
-                {
-                    $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-                }
-                if($null -ne $PSBoundParameters["OutBuffer"])
-                {
-                    $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
-                }
-                if($null -ne $PSBoundParameters["ErrorVariable"])
-                {
-                    $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
-                }
-                if($null -ne $PSBoundParameters["PipelineVariable"])
-                {
-                    $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
-                }
-                if($null -ne $PSBoundParameters["ErrorAction"])
-                {
-                    $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
-                }
-                if($null -ne $PSBoundParameters["WarningAction"])
-                {
-                    $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-                }
-                    Write-Debug("============================ TRANSFORMATIONS ============================")
-                    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-                    Write-Debug("=========================================================================`n")
-                    $URI = 'https://graph.microsoft.com/beta/applications/{0}/policies/{1}/$ref' -f $Id,$PolicyId
-                    $response = Invoke-GraphRequest -Headers $customHeaders -Uri $uri -Method $Method
-                    $response
+        if ($null -ne $PSBoundParameters["Id"]) {
+            $params["Id"] = $PSBoundParameters["Id"]
+        }
+        if ($null -ne $PSBoundParameters["PolicyId"]) {
+            $params["PolicyId"] = $PSBoundParameters["PolicyId"]
+        }
+        $Method = "DELETE"
+    
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $Null
+        }
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        $URI = 'https://graph.microsoft.com/beta/applications/{0}/policies/{1}/$ref' -f $Id,$PolicyId
+        $response = Invoke-GraphRequest -Headers $customHeaders -Uri $uri -Method $Method
+        $response
     } 
 '@
 }
