58a5a91e4aff57a63546b3d7390361a1132e6b21
Ashwini Karke <v-akarke@microsoft.com>
2024-05-21 15:46:55 +0530

    Common-Parameters

--- module/EntraBeta/customizations/Get-EntraBetaApplicationSignInSummary.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaApplicationSignInSummary.ps1
@@ -12,66 +12,66 @@
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
         $filterApplied = $null
         $topCount = $null
-                if ($null -ne $PSBoundParameters["Days"]) {
-                    $params["Days"] = $PSBoundParameters["Days"]
-                }
-                if ($null -ne $PSBoundParameters["Filter"]) {
-                    $params["Filter"] = $PSBoundParameters["Filter"]
-                    $filterApplied = '?$filter=' + $params["Filter"]
-                }
-                if ($null -ne $PSBoundParameters["Top"]) {
-                    $params["Top"] = $PSBoundParameters["Top"]
-                    $topCount = '?$top=' + $params["Top"]
-                }
-                $Method = "GET"
-                if ($PSBoundParameters.ContainsKey("Debug")) {
-                    $params["Debug"] = $Null
-                }
-                if ($PSBoundParameters.ContainsKey("Verbose")) {
-                    $params["Verbose"] = $Null
-                }
-                if($null -ne $PSBoundParameters["WarningVariable"])
-                {
-                    $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
-                }
-                if($null -ne $PSBoundParameters["InformationVariable"])
-                {
-                    $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-                }
-                if($null -ne $PSBoundParameters["InformationAction"])
-                {
-                    $params["InformationAction"] = $PSBoundParameters["InformationAction"]
-                }
-                if($null -ne $PSBoundParameters["OutVariable"])
-                {
-                    $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-                }
-                if($null -ne $PSBoundParameters["OutBuffer"])
-                {
-                    $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
-                }
-                if($null -ne $PSBoundParameters["ErrorVariable"])
-                {
-                    $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
-                }
-                if($null -ne $PSBoundParameters["PipelineVariable"])
-                {
-                    $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
-                }
-                if($null -ne $PSBoundParameters["ErrorAction"])
-                {
-                    $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
-                }
-                if($null -ne $PSBoundParameters["WarningAction"])
-                {
-                    $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-                }
-                    Write-Debug("============================ TRANSFORMATIONS ============================")
-                    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-                    Write-Debug("=========================================================================`n")
-                    $URI = "https://graph.microsoft.com/beta/reports/getAzureADApplicationSignInSummary(period='D{0}'){1}{2}" -f $Days, $filterApplied, $topCount
-                    $response = (Invoke-GraphRequest -Headers $customHeaders -Uri $uri -Method $Method | ConvertTo-Json | ConvertFrom-Json).value
-                    $response
-        }    
+        if ($null -ne $PSBoundParameters["Days"]) {
+            $params["Days"] = $PSBoundParameters["Days"]
+        }
+        if ($null -ne $PSBoundParameters["Filter"]) {
+            $params["Filter"] = $PSBoundParameters["Filter"]
+            $filterApplied = '?$filter=' + $params["Filter"]
+        }
+        if ($null -ne $PSBoundParameters["Top"]) {
+            $params["Top"] = $PSBoundParameters["Top"]
+            $topCount = '?$top=' + $params["Top"]
+        }
+        $Method = "GET"
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $Null
+        }
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        $URI = "https://graph.microsoft.com/beta/reports/getAzureADApplicationSignInSummary(period='D{0}'){1}{2}" -f $Days, $filterApplied, $topCount
+        $response = (Invoke-GraphRequest -Headers $customHeaders -Uri $uri -Method $Method | ConvertTo-Json | ConvertFrom-Json).value
+        $response
+    }    
 '@
 }
--- module/EntraBeta/customizations/Get-EntraBetaServicePrincipalPolicy.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaServicePrincipalPolicy.ps1
@@ -10,59 +10,59 @@
     PROCESS {  
         $params = @{}
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-                if ($null -ne $PSBoundParameters["Id"]) {
-                    $params["Id"] = $PSBoundParameters["Id"]
-                }
-                $Method = "GET"
-                if ($PSBoundParameters.ContainsKey("Debug")) {
-                    $params["Debug"] = $Null
-                }
-                if ($PSBoundParameters.ContainsKey("Verbose")) {
-                    $params["Verbose"] = $Null
-                }
-                if($null -ne $PSBoundParameters["WarningVariable"])
-                {
-                    $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
-                }
-                if($null -ne $PSBoundParameters["InformationVariable"])
-                {
-                    $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-                }
-                if($null -ne $PSBoundParameters["InformationAction"])
-                {
-                    $params["InformationAction"] = $PSBoundParameters["InformationAction"]
-                }
-                if($null -ne $PSBoundParameters["OutVariable"])
-                {
-                    $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-                }
-                if($null -ne $PSBoundParameters["OutBuffer"])
-                {
-                    $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
-                }
-                if($null -ne $PSBoundParameters["ErrorVariable"])
-                {
-                    $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
-                }
-                if($null -ne $PSBoundParameters["PipelineVariable"])
-                {
-                    $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
-                }
-                if($null -ne $PSBoundParameters["ErrorAction"])
-                {
-                    $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
-                }
-                if($null -ne $PSBoundParameters["WarningAction"])
-                {
-                    $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-                }
-                    Write-Debug("============================ TRANSFORMATIONS ============================")
-                    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-                    Write-Debug("=========================================================================`n")
-                    $URI = "https://graph.microsoft.com/beta/serviceprincipals/$Id/policies"
-                    $response = (Invoke-GraphRequest -Headers $customHeaders -Uri $uri -Method $Method | ConvertTo-Json | ConvertFrom-Json).value
-                    $response | Add-Member -MemberType AliasProperty -Value '@odata.type' -Name 'odata.type'
-                    $response
+        if ($null -ne $PSBoundParameters["Id"]) {
+            $params["Id"] = $PSBoundParameters["Id"]
+        }
+        $Method = "GET"
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $Null
+        }
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        $URI = "https://graph.microsoft.com/beta/serviceprincipals/$Id/policies"
+        $response = (Invoke-GraphRequest -Headers $customHeaders -Uri $uri -Method $Method | ConvertTo-Json | ConvertFrom-Json).value
+        $response | Add-Member -MemberType AliasProperty -Value '@odata.type' -Name 'odata.type'
+        $response
     }  
 '@
 }
