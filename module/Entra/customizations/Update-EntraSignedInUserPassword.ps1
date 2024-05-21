0dd71037b5acf8de53867d0e424dec6947f218af
v-uansari <v-uansari@microsoft.com>
2024-05-21 18:02:53 +0530

    Update-AzureADSignedInUserPassword

--- module/Entra/customizations/Update-EntraSignedInUserPassword.ps1
+++ module/Entra/customizations/Update-EntraSignedInUserPassword.ps1
@@ -3,7 +3,49 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Update-AzureADSignedInUserPassword"
-    TargetName = "Update-MgUserPassword"
+    TargetName = $null
     Parameters = $null
     Outputs = $null
+    CustomScript = @"
+    PROCESS {    
+        `$params = @{}
+        `$customHeaders = New-EntraCustomHeaders -Command `$MyInvocation.MyCommand
+        `$keysChanged = @{}
+        if(`$null -ne `$PSBoundParameters["NewPassword"])
+        {
+            `$params["NewPassword"] = `$PSBoundParameters["NewPassword"]
+        }
+        if(`$PSBoundParameters.ContainsKey("Verbose"))
+        {
+            `$params["Verbose"] = `$Null
+        }
+        if(`$PSBoundParameters.ContainsKey("Debug"))
+        {
+            `$params["Debug"] = `$Null
+        }
+        if(`$null -ne `$PSBoundParameters["CurrentPassword"])
+        {
+            `$params["CurrentPassword"] = `$PSBoundParameters["CurrentPassword"]
+        } 
+        `$currsecur = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode(`$params.CurrentPassword)
+        `$curr = [System.Runtime.InteropServices.Marshal]::PtrToStringUni(`$currsecur)
+    
+        `$newsecur = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode(`$params.NewPassword)
+        `$new = [System.Runtime.InteropServices.Marshal]::PtrToStringUni(`$newsecur)
+    
+        `$params["Url"]  = "https://graph.microsoft.com/v1.0/me/changePassword"
+        `$body = @{
+            currentPassword = `$curr
+            newPassword = `$new 
+        }
+        `$body = `$body | ConvertTo-Json
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        `$params.Keys | ForEach-Object {"`$_ : `$(`$params[`$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        `$response = Invoke-GraphRequest -Headers `$customHeaders -Uri `$params.Url -Method POST -Body `$body
+        `$response
+    }
+"@
 }
