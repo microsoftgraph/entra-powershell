ea0fc393413e61f5b0d8d39177b44e4e5388a478
v-uansari <v-uansari@microsoft.com>
2024-05-28 17:35:26 +0530

    added customizations

---
+++ module/Entra/customizations/Add-EntraScopedRoleMembership.ps1
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Add-AzureADScopedRoleMembership"
+    TargetName = "New-MgDirectoryAdministrativeUnitScopedRoleMember"
+    Parameters = @(
+        @{
+            SourceName = "ObjectId"
+            TargetName = "AdministrativeUnitId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        },
+        @{
+            SourceName = "AdministrativeUnitObjectId"
+            TargetName = "AdministrativeUnitId1"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        },
+        @{
+            SourceName = "RoleObjectId"
+            TargetName = "RoleId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        },
+        @{
+            SourceName = "RoleMemberInfo"
+            TargetName = "RoleMemberInfo"
+            ConversionType = "ScriptBlock"
+            SpecialMapping =@"
+            `$Value = @{
+                        id = (`$TmpValue).ObjectId
+                    } | ConvertTo-Json
+"@
+                }
+    )
+    Outputs = @(
+        @{
+            SourceName = "RoleMemberInfo"
+            TargetName = "RoleMemberInfo"
+            ConversionType = "ScriptBlock"
+            SpecialMapping = @"
+            `$Value = `$_.RoleMemberInfo.ToJsonString()
+"@
+        },
+        @{
+            SourceName = "RoleId"
+            TargetName = "RoleObjectId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        },
+        @{
+            SourceName = "AdministrativeUnitId"
+            TargetName = "AdministrativeUnitObjectId"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+    )
+}
---
+++ module/Entra/customizations/Get-EntraScopedRoleMembership.ps1
@@ -0,0 +1,87 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+@{
+    SourceName = "Get-AzureADScopedRoleMembership"
+    TargetName = $null
+    Parameters = $null
+    Outputs = $null
+    CustomScript = @'
+    PROCESS {    
+        $params = @{}
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $keysChanged = @{ObjectId = "Id"}
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["AdministrativeUnitId"] = $PSBoundParameters["ObjectId"]
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["ScopedRoleMembershipId"])
+        {
+            $params["ScopedRoleMembershipId"] = $PSBoundParameters["ScopedRoleMembershipId"]
+        }
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+	    if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================
+")
+        
+        $response = Get-MgDirectoryAdministrativeUnitScopedRoleMember @params -Headers $customHeaders
+        $response | ForEach-Object {
+            if ($null -ne $_) {
+                
+                $propsToConvert = @('RoleMemberInfo')
+        
+                foreach ($prop in $propsToConvert) {
+                    $value = $_.$prop | ConvertTo-Json | ConvertFrom-Json
+                    $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
+                }
+            }
+        }
+        $response
+    }   
+'@
+}
