b6b9eff090382bc8f0fba908c310021d87e90076
Kennedy Munga <kemunga@microsoft.com>
2024-05-28 16:31:09 +0300

    Update tests

--- test/module/Entra/Get-EntraUser.Tests.ps1
+++ test/module/Entra/Get-EntraUser.Tests.ps1
@@ -5,30 +5,49 @@ BeforeAll {
     Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
     
     $scriptblock = {
-        # Write-Host "Mocking Get-MgDevice with parameters: $($args | ConvertTo-Json -Depth 3)"
+        $valueObject = [PSCustomObject]@{
+            "DisplayName"                      = "Mock-User"
+            "AccountEnabled"                   = $true
+            "Mail"                             = "User@aaabbbcccc.OnMicrosoft.com"
+            "userPrincipalName"                = "User@aaabbbcccc.OnMicrosoft.com"
+            "DeletedDateTime"                  = $null
+            "CreatedDateTime"                  = $null
+            "EmployeeId"                       = $null
+            "Id"                               = "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            "Surname"                          = $null
+            "MailNickName"                     = "User"
+            "OnPremisesDistinguishedName"      = $null
+            "OnPremisesSecurityIdentifier"     = $null
+            "OnPremisesUserPrincipalName"      = $null
+            "OnPremisesSyncEnabled"            = $false
+            "onPremisesImmutableId"            = $null
+            "OnPremisesLastSyncDateTime"       = $null
+            "JobTitle"                         = $null
+            "CompanyName"                      = $null
+            "Department"                       = $null
+            "Country"                          = $null
+            "BusinessPhones"                   = @{}
+            "OnPremisesProvisioningErrors"     = @{}
+            "ImAddresses"                      = @{}
+            "ExternalUserState"                = $null
+            "ExternalUserStateChangeDateTime"  = $null
+            "MobilePhone"                      = $null
+        }
+
+        # Response from an Invoke-GraphRequest is a hashtable with @odata.context and Value objects
+
+        # Name                           Value
+        # ----                           -----
+        # @odata.context                 https://graph.microsoft.com/v1.0/$metadata#users
+        # value                          {System.Collections.Hashtable, System.Collections.Hashtable, System.Collections.Hasht...
+
+        $response = @{
+            '@odata.context'        = 'Users()'
+            Value                   = $valueObject
+        }
+
         return @(
-            [PSCustomObject]@{
-                "DisplayName"                   = "Mock-User"
-                "AccountEnabled"                = $true
-                "Mail"                          = "User@aaabbbcccc.OnMicrosoft.com"
-                "userPrincipalName"             = "User@aaabbbcccc.OnMicrosoft.com"      
-                "DeletedDateTime"               = $null
-                "CreatedDateTime"               = $null
-                "EmployeeId"                    = $null
-                "Id"                            = "bbbbbbbb-1111-2222-3333-cccccccccccc"
-                "Surname"                       = $null
-                "MailNickName"                  = "User"
-                "onPremisesDistinguishedName"   = $null
-                "onPremisesSecurityIdentifier"  = $null
-                "onPremisesUserPrincipalName"   = $null
-                "jobTitle"                      = $null
-                "companyName"                   = $null
-                "department"                    = $null           
-                "country"                       = $null
-                "businessPhones"                = @{}
-                "onPremisesProvisioningErrors"  = @{}
-                "ImAddresses"                   = @{}
-            }
+            $response
         )
     }
 
@@ -39,6 +58,7 @@ Describe "Get-EntraUser" {
     Context "Test for Get-EntraUser" {
         It "Should return specific user" {
             $result = Get-EntraUser -ObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            Write-Verbose "Result : {$result}" -Verbose
             $result | Should -Not -BeNullOrEmpty
             $result.Id | should -Be @('bbbbbbbb-1111-2222-3333-cccccccccccc')
 
@@ -47,9 +67,17 @@ Describe "Get-EntraUser" {
         It "Should contain 'User-Agent' header" {
             $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraUser"
 
-            $result = Get-EntraUser -SearchString 'Mock-User'
-            $params = Get-Parameters -data $result.Parameters
-            $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+            $result = Get-EntraUser -Top 1
+            $result | Should -Not -BeNullOrEmpty
+
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraUser"
+            $customHeaders = New-Object 'system.collections.generic.dictionary[string,string]'
+            $customHeaders["User-Agent"] = $userAgentHeaderValue
+
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }
         }
     }
 }
