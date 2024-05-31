e9979e6acc1e159d77e44b73c5d336cb6c1ff3b8
Ashwini Karke <v-akarke@microsoft.com>
2024-05-31 16:31:11 +0530

    added beta test cases

--- test/module/EntraBeta/Get-EntraBetaPolicy.Tests.ps1
+++ test/module/EntraBeta/Get-EntraBetaPolicy.Tests.ps1
@@ -1,6 +1,6 @@
 BeforeAll {
-    if ((Get-Module -Name Microsoft.Graph.Entra) -eq $null) {
-        Import-Module Microsoft.Graph.Entra      
+    if ((Get-Module -Name Microsoft.Graph.Entra.Beta) -eq $null) {
+        Import-Module Microsoft.Graph.Entra.Beta   
     }
     
     Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
@@ -38,52 +38,52 @@ BeforeAll {
         return $response
     }
    
-    Mock -CommandName Invoke-GraphRequest -MockWith $ScriptBlock -ModuleName Microsoft.Graph.Entra
+    Mock -CommandName Invoke-GraphRequest -MockWith $ScriptBlock -ModuleName Microsoft.Graph.Entra.Beta
 }
-Describe "Get-EntraPolicy" {
-    Context "Test for Get-EntraPolicy" {
+Describe "Get-EntraBetaPolicy" {
+    Context "Test for Get-EntraBetaPolicy" {
         It "Should return specific Policy" {
-            $result = Get-EntraPolicy -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraBetaPolicy -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
             
             $result | Should -Not -BeNullOrEmpty
             $result.Id | should -Contain 'bbbbbbbb-1111-2222-3333-cccccccccccc'
 
-            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should return all Policies" {
-            $result = Get-EntraPolicy -All 
+            $result = Get-EntraBetaPolicy -All 
             $result | Should -Not -BeNullOrEmpty            
             
-            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should return all Policy" {
-            $result = Get-EntraPolicy -Top 1
+            $result = Get-EntraBetaPolicy -Top 1
             $result | Should -Not -BeNullOrEmpty 
             $result | Should -HaveCount 1             
             
-            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should fail when Id is invalid" {
-            { Get-EntraPolicy -Id "" } | Should -Throw "Cannot bind argument to parameter 'Id' because it is an empty string."
+            { Get-EntraBetaPolicy -Id "" } | Should -Throw "Cannot bind argument to parameter 'Id' because it is an empty string."
         }
         It "Should fail when Id is empty" {
-            { Get-EntraPolicy -Id } | Should -Throw "Missing an argument for parameter 'Id'*"
+            { Get-EntraBetaPolicy -Id } | Should -Throw "Missing an argument for parameter 'Id'*"
         }
         It "Should fail when Top is empty" {
-            { Get-EntraPolicy -Top } | Should -Throw "Missing an argument for parameter 'Top'*"
+            { Get-EntraBetaPolicy -Top } | Should -Throw "Missing an argument for parameter 'Top'*"
         }
         It "Should fail when Top is invalid" {
-            { Get-EntraPolicy -Top XY } | Should -Throw "Cannot process argument transformation on parameter 'Top'*"
+            { Get-EntraBetaPolicy -Top XY } | Should -Throw "Cannot process argument transformation on parameter 'Top'*"
         }
         It "Should fail when All has an argument" {
-            { Get-EntraPolicy -All $true } | Should -Throw "A positional parameter cannot be found that accepts argument 'True'.*"
+            { Get-EntraBetaPolicy -All $true } | Should -Throw "A positional parameter cannot be found that accepts argument 'True'.*"
         }             
         It "Should contain 'User-Agent' header" {
-            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraPolicy"
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraBetaPolicy"
 
-            $result = Get-EntraPolicy -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraBetaPolicy -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $result | Should -Not -BeNullOrEmpty
-            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra.Beta -Times 1 -ParameterFilter {
                 $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
                 $true
             }    
