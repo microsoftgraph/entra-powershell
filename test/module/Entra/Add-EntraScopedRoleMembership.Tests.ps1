3c947e2d06ce422a4029555aeedaa041d8364fa7
v-uansari <v-uansari@microsoft.com>
2024-06-05 11:11:36 +0530

    added tests

---
+++ test/module/Entra/Add-EntraScopedRoleMembership.Tests.ps1
@@ -0,0 +1,60 @@
+BeforeAll{
+    if ((Get-Module -Name Microsoft.Graph.Entra) -eq $null) {
+        Import-Module Microsoft.Graph.Entra      
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+
+    $userObjId = "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+    $roleObjId = "aaaaaaaa-2222-3333-4444-bbbbbbbbbbbb"
+    $unitObjId = "aaaaaaaa-3333-4444-5555-bbbbbbbbbbbb"
+    $RoleMember = New-Object -TypeName Microsoft.Open.MSGraph.Model.MsRoleMemberInfo
+    $RoleMember.Id = $userObjId
+
+    $scriptblock = {
+        @{
+            "administrativeUnitId" = $unitObjId
+            "roleId" = $roleObjId
+            "roleMemberInfo" = @(
+                @{
+                    "id" = $userObjId
+                    "userPrincipalName" = "Dummy"
+                    "displayName" = "Dummy"
+                }
+            )
+            "id" = "NewDummyId"
+            "Parameters" = $args
+        }
+    }
+
+    Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
+}
+Describe "Tests for Add-EntraScopedRoleMembership"{
+    It "Result should not be empty"{
+        $result = Add-EntraScopedRoleMembership -ObjectId $unitObjId -RoleObjectId $roleObjId -RoleMemberInfo $RoleMember
+        $result | Should -Not -BeNullOrEmpty
+        $result.Id | should -Be @('NewDummyId')
+        Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
+    }
+    It "Should fail when ObjectId is invalid" {
+        { Add-EntraScopedRoleMembership -ObjectId "" } | Should -Throw "Cannot bind argument to parameter 'ObjectId'*"
+    }
+    It "Should fail when ObjectId is empty" {
+        { Add-EntraScopedRoleMembership -ObjectId } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
+    }
+    It "Should fail when RoleMemberInfo is invalid" {
+        { Add-EntraScopedRoleMembership -RoleMemberInfo "" } | Should -Throw "Cannot process argument transformation on parameter 'RoleMemberInfo'*"
+    }
+    It "Should fail when RoleMemberInfo is empty" {
+        { Add-EntraScopedRoleMembership -RoleMemberInfo } | Should -Throw "Missing an argument for parameter 'RoleMemberInfo'*"
+    }
+    It "Should fail when invalid parameter is passed" {
+        { Add-EntraScopedRoleMembership -xyz } | Should -Throw "A parameter cannot be found that matches parameter name 'xyz'*"
+    }
+    It "Should contain 'User-Agent' header" {
+        $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Add-EntraScopedRoleMembership"
+        $result = Add-EntraScopedRoleMembership -ObjectId $unitObjId -RoleObjectId $roleObjId -RoleMemberInfo $RoleMember
+        $params = Get-Parameters -data $result.Parameters
+        $a= $params | ConvertTo-json | ConvertFrom-Json
+        $a.headers.'User-Agent' | Should -Be $userAgentHeaderValue
+    }
+}
