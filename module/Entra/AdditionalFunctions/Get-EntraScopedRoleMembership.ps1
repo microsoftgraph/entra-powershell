5a38bb8d9e0e221948fa5d3b1cfdd2a344671ca5
v-uansari <v-uansari@microsoft.com>
2024-06-10 16:00:36 +0530

    Get-EntraScopedRoleMembership

---
+++ module/Entra/AdditionalFunctions/Get-EntraScopedRoleMembership.ps1
@@ -0,0 +1,94 @@
+function Get-EntraScopedRoleMembership {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $ObjectId,
+    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $ScopedRoleMembershipId
+    )
+
+    PROCESS {    
+        $params = @{}
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $keysChanged = @{ObjectId = "Id"}
+        $isList = $false
+        $baseUri = "https://graph.microsoft.com/v1.0/directory/administrativeUnits/"        
+        if($null -ne $PSBoundParameters["ObjectId"])
+        {
+            $params["AdministrativeUnitId"] = $PSBoundParameters["ObjectId"]
+            $uri = $baseUri + "/$($params.AdministrativeUnitId)/scopedRoleMembers"
+            $params["Uri"] = $uri
+            $isList = $true
+        }
+        if($null -ne $PSBoundParameters["ScopedRoleMembershipId"])
+        {
+            $isList = $false
+            $params["ScopedRoleMembershipId"] = $PSBoundParameters["ScopedRoleMembershipId"]
+            $uri = $uri + "/$($params.ScopedRoleMembershipId)"
+            $params["Uri"] = $uri
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }        
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+	    if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================")
+
+        $response = (Invoke-GraphRequest -Uri $uri -Headers $customHeaders -Method GET) | ConvertTo-Json -Depth 5 | ConvertFrom-Json
+        if($isList){
+            $response = $response.value
+        }
+
+        $response | ForEach-Object {
+            if ($null -ne $_) {                    
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name AdministrativeUnitObjectId -Value AdministrativeUnitId
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name RoleObjectId -Value RoleId
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
+            }
+        }
+
+        $response    
+    }
+}
---
+++ test/module/Entra/Get-EntraScopedRoleMembership.Tests.ps1
@@ -0,0 +1,58 @@
+BeforeAll{
+    if ((Get-Module -Name Microsoft.Graph.Entra) -eq $null) {
+        Import-Module Microsoft.Graph.Entra      
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+
+    $userObjId = "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+    $roleObjId = "aaaaaaaa-2222-3333-4444-bbbbbbbbbbbb"
+    $unitObjId = "aaaaaaaa-3333-4444-5555-bbbbbbbbbbbb"
+    $scopedRoleMembershipId = "scopedRoleMemId"
+
+    $scriptblock = {
+        @{
+            "id" = $scopedRoleMembershipId
+            "roleId"= $roleObjId
+            "administrativeUnitId"= $unitObjId
+            "roleMemberInfo"= @(
+                @{
+                    "id"= $userObjId
+                    "displayName"= "displayName-value"
+                    "userPrincipalName"= "userPrincipalName-value"
+                }
+            )
+            "Parameters" = $args
+        }
+    }
+
+    Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
+}
+Describe "Tests for Get-EntraScopedRoleMembership"{
+    It "Result should not be empty"{
+        $result = Get-EntraScopedRoleMembership -ObjectId $unitObjId -ScopedRoleMembershipId $scopedRoleMembershipId
+        $result | Should -Not -BeNullOrEmpty
+        $result.ObjectId | should -Be $scopedRoleMembershipId
+        $result.AdministrativeUnitObjectId | should -Be $unitObjId
+        $result.RoleObjectId | should -Be $roleObjId
+        Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
+    }
+    It "Should fail when ObjectId is invalid" {
+        { Get-EntraScopedRoleMembership -ObjectId "" } | Should -Throw "Cannot bind argument to parameter 'ObjectId'*"
+    }
+    It "Should fail when ObjectId is empty" {
+        { Get-EntraScopedRoleMembership -ObjectId } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
+    }    
+    It "Should fail when ScopedRoleMembershipId is empty" {
+        { Get-EntraScopedRoleMembership -ObjectId $unitObjId -ScopedRoleMembershipId } | Should -Throw "Missing an argument for parameter 'ScopedRoleMembershipId'*"
+    }
+    It "Should fail when invalid parameter is passed" {
+        { Get-EntraScopedRoleMembership -xyz } | Should -Throw "A parameter cannot be found that matches parameter name 'xyz'*"
+    }
+    It "Should contain 'User-Agent' header" {
+        $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraScopedRoleMembership"
+        $result = Get-EntraScopedRoleMembership -ObjectId $unitObjId -ScopedRoleMembershipId $scopedRoleMembershipId
+        $params = Get-Parameters -data $result.Parameters
+        $a= $params | ConvertTo-json | ConvertFrom-Json
+        $a.headers.'User-Agent' | Should -Be $userAgentHeaderValue
+    }
+}
