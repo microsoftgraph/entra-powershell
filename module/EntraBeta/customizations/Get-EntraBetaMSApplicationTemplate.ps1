4a1fbc10fdcd3338a1e8e66a20daacc958b44281
Kennedy Munga <kemunga@microsoft.com>
2024-06-12 16:59:00 +0300

    Update Get-EntraBetaMSApplicationTemplate

--- module/EntraBeta/customizations/Get-EntraBetaMSApplicationTemplate.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaMSApplicationTemplate.ps1
@@ -8,13 +8,62 @@
     Outputs = $null
     CustomScript = @'
     PROCESS {
+        $params = @{}
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-        if($null -ne $PSBoundParameters["Id"]){
-            Get-MgBetaApplicationTemplate -Headers $customHeaders -ApplicationTemplateId $PSBoundParameters["Id"]
+        if($null -ne $PSBoundParameters["Id"])
+        {
+            $params["ApplicationTemplateId"] = $PSBoundParameters["Id"]
         }
-        else{
-            Get-MgBetaApplicationTemplate -Headers $customHeaders
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
         }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+	    if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        if($null -ne $PSBoundParameters["Property"])
+        {
+            $params["Property"] = $PSBoundParameters["Property"]
+        }
+
+        Get-MgBetaApplicationTemplate @params -Headers $customHeaders
     }
 '@
 }
