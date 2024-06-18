50e0063e8f87f22ac3004cd7279802dae22e192e
v-uansari <v-uansari@microsoft.com>
2024-06-18 10:52:42 +0530

    Set-EntraBetaMSCustomSecurityAttributeDefinitionAllowedValue

---
+++ module/Entra/AdditionalFunctions/Set-EntraMSCustomSecurityAttributeDefinitionAllowedValue.ps1
@@ -0,0 +1,86 @@
+function Set-EntraMSCustomSecurityAttributeDefinitionAllowedValue {
+    [CmdletBinding(DefaultParameterSetName = 'InvokeByDynamicParameters')]
+    param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $AllowedValueId,
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+    [System.Nullable`1[System.Boolean]] $IsActive,
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $Id
+    )
+
+    PROCESS {    
+
+    $params = @{}
+    $body = @{}
+    
+    $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+    $Uri = "https://graph.microsoft.com/v1.0/directory/customSecurityAttributeDefinitions/$Id/allowedValues/$AllowedValueId"
+    $Method = "PATCH"
+    $keysChanged = @{}
+    if($null -ne $PSBoundParameters["ErrorAction"])
+    {
+        $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+    }
+    if($null -ne $PSBoundParameters["Id"])
+    {
+        $params["Id"] = $PSBoundParameters["Id"]
+    }
+    if($null -ne $PSBoundParameters["AllowedValueId"])
+    {
+        $params["AllowedValueId"] = $PSBoundParameters["AllowedValueId"]
+    }
+    if($null -ne $PSBoundParameters["IsActive"])
+    {
+        $body["IsActive"] = $PSBoundParameters["IsActive"]
+    }
+    if($PSBoundParameters.ContainsKey("Verbose"))
+    {
+        $params["Verbose"] = $Null
+    }
+    if($null -ne $PSBoundParameters["OutVariable"])
+    {
+        $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+    }
+    
+    if($null -ne $PSBoundParameters["InformationAction"])
+    {
+        $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+    }
+    if($null -ne $PSBoundParameters["WarningVariable"])
+    {
+        $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+    }
+    if($PSBoundParameters.ContainsKey("Debug"))
+    {
+        $params["Debug"] = $Null
+    }
+    if($null -ne $PSBoundParameters["PipelineVariable"])
+    {
+        $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+    }
+    if($null -ne $PSBoundParameters["ErrorVariable"])
+    {
+        $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+    }
+    if($null -ne $PSBoundParameters["OutBuffer"])
+    {
+        $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+    }
+    if($null -ne $PSBoundParameters["WarningAction"])
+    {
+        $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+    }    
+    if($null -ne $PSBoundParameters["InformationVariable"])
+    {
+        $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+    }
+
+    Write-Debug("============================ TRANSFORMATIONS ============================")
+    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+    Write-Debug("=========================================================================`n")
+    
+    $response = Invoke-GraphRequest -Uri $Uri -Method $Method -Body $body -Headers $customHeaders
+        $response
+    }
+}
