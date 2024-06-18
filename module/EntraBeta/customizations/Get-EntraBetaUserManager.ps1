c4c61f5f4ae6238e803bb1c2b968e964b712a608
v-uansari <v-uansari@microsoft.com>
2024-06-18 16:59:10 +0530

    updates

--- module/EntraBeta/customizations/Get-EntraBetaUserManager.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaUserManager.ps1
@@ -63,8 +63,8 @@
         Write-Debug("============================ TRANSFORMATIONS ============================")
         $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
-        try {
-            $response = Get-MgBetaUserManager @params -Headers $customHeaders -ErrorAction Stop
+        $response = Get-MgBetaUserManager @params -Headers $customHeaders -ErrorAction Stop
+        try {            
             $response | ForEach-Object {
                 if($null -ne $_) {
                     Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
