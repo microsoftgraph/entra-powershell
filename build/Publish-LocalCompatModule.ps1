cf629c89520f9d76a411a4bff6a6d556631b5466
Kennedy Munga <kemunga@microsoft.com>
2024-06-25 15:09:47 +0300

    Remove unnecessary code

--- build/Publish-LocalCompatModule.ps1
+++ build/Publish-LocalCompatModule.ps1
@@ -19,8 +19,7 @@ if($fullModuleName -eq 'Microsoft.Graph.Entra'){
 else{
 	$moduleName = 'EntraBeta'
 }
-# $moduleName = $moduleName.TrimStart("Microsoft.Graph.");
-# $moduleName = $moduleName.Trim(".","");
+
 $settingPath = "$PSScriptRoot/../module/$ModuleName/config/ModuleSettings.json"
 $content = Get-Content -Path $settingPath | ConvertFrom-Json
 
@@ -33,34 +32,8 @@ foreach ($destinationModuleName in $content.destinationModuleName){
 	Publish-Module -Name $destinationModuleName -RequiredVersion $content.destinationModuleVersion -Repository (Get-LocalPSRepoName)
 }
 
-# Publish-Module -Name Microsoft.Graph.Authentication -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.DirectoryObjects -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Users -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Users.Actions -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Users.Functions -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Groups -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Identity.DirectoryManagement -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Identity.Governance -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Identity.SignIns -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-# Publish-Module -Name Microsoft.Graph.Applications -RequiredVersion '2.15.0' -Repository (Get-LocalPSRepoName)
-
 Publish-Module -Path $modulePath -Repository (Get-LocalPSRepoName)
 
 if ($Install) {
 	Install-Module -Name (Get-ModuleName) -Repository (Get-LocalPSRepoName) -AllowClobber
-
-	# foreach ($destinationModuleName in $content.destinationModuleName){
-	# 	Write-Verbose("Installing Module $($destinationModuleName)")
-	# 	Publish-Module $destinationModuleName -scope currentuser -RequiredVersion $content.destinationModuleVersion -Force -AllowClobber
-	# }
-
-	# Install-Module -Name Microsoft.Graph.DirectoryObjects -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Users -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Users.Actions -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Users.Functions -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Groups -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Identity.DirectoryManagement -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Identity.Governance -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Identity.SignIns -Repository (Get-LocalPSRepoName) -AllowClobber
-	# Install-Module -Name Microsoft.Graph.Applications -Repository (Get-LocalPSRepoName) -AllowClobber
 }
