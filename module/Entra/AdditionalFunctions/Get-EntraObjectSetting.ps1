58d50dfda6a32e33a2aa804452aba6147789d1d4
v-uansari <v-uansari@microsoft.com>
2024-06-26 13:48:40 +0530

    Get-EntraBetaObjectSetting

---
+++ module/Entra/AdditionalFunctions/Get-EntraObjectSetting.ps1
@@ -0,0 +1,118 @@
+function Get-EntraObjectSetting {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)][System.String] $Id,
+        [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]                  
+        [System.Nullable`1[System.Int32]] $Top,
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [switch] $All,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $TargetType,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $TargetObjectId
+    )
+
+    PROCESS {    
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $params = @{}
+        $topCount = $null
+        $baseUri = "https://graph.microsoft.com/v1.0/$TargetType/$TargetObjectId/settings"
+        $params["Method"] = "GET"
+        $params["Uri"] = "$baseUri"+"?"
+
+        if ($null -ne $PSBoundParameters["TargetType"]) {
+            $params["TargetType"] = $PSBoundParameters["TargetType"]
+        }
+        if ($null -ne $PSBoundParameters["TargetObjectId"]) {
+            $params["TargetObjectId"] = $PSBoundParameters["TargetObjectId"]
+        }
+
+        if($null -ne $PSBoundParameters["Top"] -and  (-not $PSBoundParameters.ContainsKey("All")))
+        {
+            $topCount = $PSBoundParameters["Top"]
+            if ($topCount -gt 999) {
+                $params["Uri"] += "&`$top=999"
+            }
+            else{
+                $params["Uri"] += "&`$top=$topCount"
+            }
+        }
+
+        if($null -ne $PSBoundParameters["Id"])
+        {
+            $Id = $PSBoundParameters["Id"]
+            $params["Uri"] = "$baseUri/$($Id)"
+        }
+        
+        
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
+            $params["Verbose"] = $Null
+        }
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
+            $params["Debug"] = $Null
+        }        
+	    if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+	    if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+
+        $response = Invoke-GraphRequest @params -Headers $customHeaders
+        $data = $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+        try {
+            $data = $response.value | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+            $all = $All.IsPresent
+            $increment = $topCount - $data.Count
+            while ($response.'@odata.nextLink' -and (($all) -or ($increment -gt 0 -and -not $all))) {
+                $params["Uri"] = $response.'@odata.nextLink'
+                if (-not $all) {
+                    $topValue = [Math]::Min($increment, 999)
+                    $params["Uri"] = $params["Uri"].Replace('$top=999', "`$top=$topValue")
+                    $increment -= $topValue
+                }
+                $response = Invoke-GraphRequest @params 
+                $data += $response.value | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+            }
+        } catch {}        
+
+        $data
+    }        
+      
+}
