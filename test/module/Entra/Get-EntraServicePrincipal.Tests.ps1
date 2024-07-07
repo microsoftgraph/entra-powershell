3f6f038e93d1f45b024db511ef9f40d424c6f2d7
Ashwini Karke <v-akarke@microsoft.com>
2024-07-08 01:07:04 +0530

    removed MS prefix

--- test/module/Entra/Get-EntraServicePrincipal.Tests.ps1
+++ test/module/Entra/Get-EntraServicePrincipal.Tests.ps1
@@ -29,71 +29,71 @@ BeforeAll {
     Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
 }
   
-Describe "Get-EntraMSServicePrincipal" {
-    Context "Test for Get-EntraMSServicePrincipal" {
+Describe "Get-EntraServicePrincipal" {
+    Context "Test for Get-EntraServicePrincipal" {
         It "Should return specific ServicePrincipal" {
-            $result = Get-EntraMSServicePrincipal -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraServicePrincipal -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $result | Should -Not -BeNullOrEmpty
             $result.Id | should -Be @('bbbbbbbb-1111-2222-3333-cccccccccccc')
 
             Should -Invoke -CommandName Invoke-GraphRequest  -ModuleName Microsoft.Graph.Entra -Times 1
         }
         It "Should fail when Id is invalid" {
-            { Get-EntraMSServicePrincipal -Id "" } | Should -Throw "Cannot bind argument to parameter 'Id' because it is an empty string."
+            { Get-EntraServicePrincipal -Id "" } | Should -Throw "Cannot bind argument to parameter 'Id' because it is an empty string."
         }
         It "Should fail when Id is empty" {
-            { Get-EntraMSServicePrincipal -Id } | Should -Throw "Missing an argument for parameter 'Id'*"
+            { Get-EntraServicePrincipal -Id } | Should -Throw "Missing an argument for parameter 'Id'*"
         }
         It "Should return all ServicePrincipals" {
-            $result = Get-EntraMSServicePrincipal -All
+            $result = Get-EntraServicePrincipal -All
             $result | Should -Not -BeNullOrEmpty            
             
             Should -Invoke -CommandName Invoke-GraphRequest  -ModuleName Microsoft.Graph.Entra -Times 1
         }
         It "Should fail when All has an argument" {
-            { Get-EntraMSServicePrincipal -All $true } | Should -Throw "A positional parameter cannot be found that accepts argument 'True'.*"
+            { Get-EntraServicePrincipal -All $true } | Should -Throw "A positional parameter cannot be found that accepts argument 'True'.*"
         }   
         It "Should fail when searchstring is empty" {
-            { Get-EntraMSServicePrincipal -SearchString } | Should -Throw "Missing an argument for parameter 'SearchString'*"
+            { Get-EntraServicePrincipal -SearchString } | Should -Throw "Missing an argument for parameter 'SearchString'*"
         } 
         It "Should fail when filter is empty" {
-            { Get-EntraMSServicePrincipal -Filter } | Should -Throw "Missing an argument for parameter 'Filter'*"
+            { Get-EntraServicePrincipal -Filter } | Should -Throw "Missing an argument for parameter 'Filter'*"
         }
         It "Should fail when Top is empty" {
-            { Get-EntraMSServicePrincipal -Top } | Should -Throw "Missing an argument for parameter 'Top'*"
+            { Get-EntraServicePrincipal -Top } | Should -Throw "Missing an argument for parameter 'Top'*"
         }
         It "Should fail when Top is invalid" {
-            { Get-EntraMSServicePrincipal -Top XY } | Should -Throw "Cannot process argument transformation on parameter 'Top'*"
+            { Get-EntraServicePrincipal -Top XY } | Should -Throw "Cannot process argument transformation on parameter 'Top'*"
         }        
         It "Should return specific ServicePrincipal by searchstring" {
-            $result = Get-EntraMSServicePrincipal -SearchString 'Mock-App'
+            $result = Get-EntraServicePrincipal -SearchString 'Mock-App'
             $result | Should -Not -BeNullOrEmpty
             $result.DisplayName | should -Be 'Mock-App'
 
             Should -Invoke -CommandName Invoke-GraphRequest  -ModuleName Microsoft.Graph.Entra -Times 1
         } 
         It "Should return specific ServicePrincipal by filter" {
-            $result = Get-EntraMSServicePrincipal -Filter "DisplayName -eq 'Mock-App'"
+            $result = Get-EntraServicePrincipal -Filter "DisplayName -eq 'Mock-App'"
             $result | Should -Not -BeNullOrEmpty
             $result.DisplayName | should -Be 'Mock-App'
 
             Should -Invoke -CommandName Invoke-GraphRequest  -ModuleName Microsoft.Graph.Entra -Times 1
         }  
         It "Should return top ServicePrincipal" {
-            $result = @(Get-EntraMSServicePrincipal -Top 1)
+            $result = @(Get-EntraServicePrincipal -Top 1)
             $result | Should -Not -BeNullOrEmpty
             $result | Should -HaveCount 1 
 
             Should -Invoke -CommandName Invoke-GraphRequest  -ModuleName Microsoft.Graph.Entra -Times 1
         }  
         It "Result should Contain Id" {
-            $result = Get-EntraMSServicePrincipal -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraServicePrincipal -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $result.Id | should -Be "bbbbbbbb-1111-2222-3333-cccccccccccc"
         }    
         It "Should contain 'User-Agent' header" {
-            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraMSServicePrincipal"
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraServicePrincipal"
 
-            Get-EntraMSServicePrincipal -SearchString 'Mock-App' | Out-Null
+            Get-EntraServicePrincipal -SearchString 'Mock-App' | Out-Null
             Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
                 $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
                 $true
