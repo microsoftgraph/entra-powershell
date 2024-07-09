9cbbb1adbf045d053d6ca97a29bfffd3a6264044
Ashwini Karke <v-akarke@microsoft.com>
2024-07-09 22:47:03 +0530

    New-EntraFeatureRolloutPolicy

---
+++ module/Entra/AdditionalFunctions/New-EntraFeatureRolloutPolicy.ps1
@@ -0,0 +1,70 @@
+function New-EntraFeatureRolloutPolicy {
+    [CmdletBinding(DefaultParameterSetName = 'InvokeByDynamicParameters')]
+    param (
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+    [System.String] $Description,
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+    [System.Nullable`1[System.Boolean]] $IsAppliedToOrganization,
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters", Mandatory = $true)]
+    [System.String] $DisplayName,
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters", Mandatory = $true)]
+    [System.String] $Feature,
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters", Mandatory = $true)]
+    [System.Nullable`1[System.Boolean]] $IsEnabled,
+    [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+    [System.Collections.Generic.List`1[Microsoft.Open.MSGraph.Model.MsDirectoryObject]] $AppliesTo
+    )
+
+    PROCESS {    
+    $params = @{}
+    $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+    $params = @{}
+    $body = @{}
+    $params["Uri"] = 'https://graph.microsoft.com/v1.0/policies/featureRolloutPolicies/'
+    $params["Method"] = "POST"
+    
+    if($null -ne $PSBoundParameters["Description"])
+    {
+        $body["Description"] = $PSBoundParameters["Description"]
+    }
+    if($null -ne $PSBoundParameters["IsAppliedToOrganization"])
+    {
+        $body["IsAppliedToOrganization"] = $PSBoundParameters["IsAppliedToOrganization"]
+    }
+    if($null -ne $PSBoundParameters["DisplayName"])
+    {
+        $body["DisplayName"] = $PSBoundParameters["DisplayName"]
+    }
+    if($null -ne $PSBoundParameters["Feature"])
+    {
+        $body["Feature"] = $PSBoundParameters["Feature"]
+    }
+    if($null -ne $PSBoundParameters["IsEnabled"])
+    {
+        $body["IsEnabled"] = $PSBoundParameters["IsEnabled"]
+    }
+    if($null -ne $PSBoundParameters["AppliesTo"])
+    {
+        $body["AppliesTo"] = $PSBoundParameters["AppliesTo"]
+    }
+
+    $params["Body"] = $body
+
+    Write-Debug("============================ TRANSFORMATIONS ============================")
+    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+    Write-Debug("=========================================================================`n")
+    
+    $data = Invoke-GraphRequest @params -Headers $customHeaders | ConvertTo-Json | ConvertFrom-Json
+    $userList = @()
+        foreach ($response in $data) {
+            $userType = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphFeatureRolloutPolicy
+            $response.PSObject.Properties | ForEach-Object {
+                $propertyName = $_.Name
+                $propertyValue = $_.Value
+                $userType | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+            }
+            $userList += $userType
+        }
+    $userList 
+    }
+}
---
+++ test/module/Entra/New-EntraFeatureRolloutPolicy.Tests.ps1
@@ -0,0 +1,67 @@
+BeforeAll {  
+    if ((Get-Module -Name Microsoft.Graph.Entra) -eq $null) {
+        Import-Module Microsoft.Graph.Entra      
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+    
+    $scriptblock = {
+        return @(
+            [PSCustomObject]@{
+                "DisplayName"             = "FeatureRolloutPolicy"
+                "Id"                      = "bbbbbbbb-1111-2222-3333-cccccccccccc"
+                "IsEnabled"               = "False"
+                "Description"             = "FeatureRolloutPolicy"
+                "Feature"                 = "passwordHashSync"
+                "IsAppliedToOrganization" = "False"
+                "AppliesTo"               = ""
+            }
+        )
+    }
+    
+    Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
+}
+  
+Describe "New-EntraFeatureRolloutPolicy" {
+    Context "Test for New-EntraFeatureRolloutPolicy" {
+        It "Should return created FeatureRolloutPolicy" {
+            $result = New-EntraFeatureRolloutPolicy -Feature 'PasswordHashSync' -DisplayName 'FeatureRolloutPolicy1' -Description 'FeatureRolloutPolicy1' -IsEnabled $false 
+            $result | Should -Not -BeNullOrEmpty
+            $result.DisplayName | should -Be "FeatureRolloutPolicy"
+            $result.IsAppliedToOrganization | should -Be "False"
+            $result.IsEnabled | should -Be "False"
+            $result.Description | should -Be "FeatureRolloutPolicy" 
+
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
+        }
+        It "Should fail when Feature are invalid" {
+            { New-EntraFeatureRolloutPolicy -Feature "" } | Should -Throw "Cannot bind argument to parameter 'Feature'*"
+        }
+        It "Should fail when Feature are empty" {
+            { New-EntraFeatureRolloutPolicy -Feature } | Should -Throw "Missing an argument for parameter 'Feature'*"
+        } 
+        It "Should fail when DisplayName are invalid" {
+            { New-EntraFeatureRolloutPolicy -DisplayName "" } | Should -Throw "Cannot bind argument to parameter 'DisplayName'*"
+        }
+        It "Should fail when DisplayName are empty" {
+            { New-EntraFeatureRolloutPolicy -DisplayName } | Should -Throw "Missing an argument for parameter 'DisplayName'*"
+        } 
+        It "Should fail when Description are empty" {
+            { New-EntraFeatureRolloutPolicy -Feature 'PasswordHashSync' -DisplayName 'FeatureRolloutPolicy1' -Description -IsEnabled $false  } | Should -Throw "Missing an argument for parameter 'Description'*"
+        }
+        It "Should fail when IsEnabled are invalid" {
+            { New-EntraFeatureRolloutPolicy -IsEnabled "" } | Should -Throw "Cannot process argument transformation on parameter 'IsEnabled'.*"
+        }
+        It "Should fail when IsEnabled are empty" {
+            { New-EntraFeatureRolloutPolicy -IsEnabled } | Should -Throw "Missing an argument for parameter 'IsEnabled'*"
+        } 
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion New-EntraFeatureRolloutPolicy"
+
+            New-EntraFeatureRolloutPolicy -Feature 'PasswordHashSync' -DisplayName 'FeatureRolloutPolicy1' -Description 'FeatureRolloutPolicy1' -IsEnabled $false | Out-Null 
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }
+        }   
+    }
+}
