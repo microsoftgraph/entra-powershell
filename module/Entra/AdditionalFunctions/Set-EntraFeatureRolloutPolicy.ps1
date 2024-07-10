d25e243cda93ccf73836d42a6fd05bab9d48bfe4
Ashwini Karke <v-akarke@microsoft.com>
2024-07-10 16:40:23 +0530

    added customization

---
+++ module/Entra/AdditionalFunctions/Set-EntraFeatureRolloutPolicy.ps1
@@ -0,0 +1,56 @@
+function Set-EntraFeatureRolloutPolicy {
+    [CmdletBinding(DefaultParameterSetName = 'InvokeByDynamicParameters')]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $Id,
+        [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+        [System.Nullable`1[System.Boolean]] $IsAppliedToOrganization,
+        [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+        [System.String] $DisplayName,
+        [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+        [System.String] $Description,
+        [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+        [System.String] $Feature,
+        [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+        [System.Nullable`1[System.Boolean]] $IsEnabled,
+        [Parameter(ParameterSetName = "InvokeByDynamicParameters")]
+        [System.Collections.Generic.List`1[Microsoft.Open.MSGraph.Model.MsDirectoryObject]] $AppliesTo
+    )
+
+    PROCESS {    
+        $params = @{}
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $params = @{}
+        $body = @{}
+        $params["Uri"] = "https://graph.microsoft.com/v1.0/policies/featureRolloutPolicies/$Id"
+        $params["Method"] = "PATCH"
+    
+        if ($null -ne $PSBoundParameters["Description"]) {
+            $body["Description"] = $PSBoundParameters["Description"]
+        }
+        if ($null -ne $PSBoundParameters["IsAppliedToOrganization"]) {
+            $body["IsAppliedToOrganization"] = $PSBoundParameters["IsAppliedToOrganization"]
+        }
+        if ($null -ne $PSBoundParameters["DisplayName"]) {
+            $body["DisplayName"] = $PSBoundParameters["DisplayName"]
+        }
+        if ($null -ne $PSBoundParameters["Feature"]) {
+            $body["Feature"] = $PSBoundParameters["Feature"]
+        }
+        if ($null -ne $PSBoundParameters["IsEnabled"]) {
+            $body["IsEnabled"] = $PSBoundParameters["IsEnabled"]
+        }
+        if ($null -ne $PSBoundParameters["AppliesTo"]) {
+            $body["AppliesTo"] = $PSBoundParameters["AppliesTo"]
+        }
+    
+        $params["Body"] = $body
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+    
+        $response = Invoke-GraphRequest @params -Headers $customHeaders | ConvertTo-Json | ConvertFrom-Json
+        $response 
+    }
+}
