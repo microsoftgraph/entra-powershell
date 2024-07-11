98c08104b4395bdafb80dcff13f6766b0123f755
Ashwini Karke <v-akarke@microsoft.com>
2024-07-11 15:39:28 +0530

    added alias and removed Get-EntraCurrentSessionInfo

--- module/Entra/AdditionalFunctions/Get-EntraContext.ps1
+++ module/Entra/AdditionalFunctions/Get-EntraContext.ps1
@@ -1,18 +1,68 @@
 function Get-EntraContext{
+    [CmdletBinding(DefaultParameterSetName = '')]
+    param ()
 
-    PROCESS{
+    PROCESS {    
         $params = @{}
-        if ($PSBoundParameters.ContainsKey("Verbose")) {
+        $keysChanged = @{}
+        if($null -ne $PSBoundParameters["ErrorAction"])
+        {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if($null -ne $PSBoundParameters["PipelineVariable"])
+        {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutVariable"])
+        {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if($null -ne $PSBoundParameters["InformationAction"])
+        {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if($null -ne $PSBoundParameters["WarningVariable"])
+        {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if($PSBoundParameters.ContainsKey("Verbose"))
+        {
             $params["Verbose"] = $Null
         }
-        if ($PSBoundParameters.ContainsKey("Debug")) {
+        if($PSBoundParameters.ContainsKey("Debug"))
+        {
             $params["Debug"] = $Null
         }
-
+        if($null -ne $PSBoundParameters["Confirm"])
+        {
+            $params["Confirm"] = $PSBoundParameters["Confirm"]
+        }
+        if($null -ne $PSBoundParameters["ErrorVariable"])
+        {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if($null -ne $PSBoundParameters["OutBuffer"])
+        {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if($null -ne $PSBoundParameters["WarningAction"])
+        {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        if($null -ne $PSBoundParameters["WhatIf"])
+        {
+            $params["WhatIf"] = $PSBoundParameters["WhatIf"]
+        }
+        if($null -ne $PSBoundParameters["InformationVariable"])
+        {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+    
         Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
         
-        Get-MgContext @params  
+        $response = Get-MgContext @params
+        $response
     }
 }
