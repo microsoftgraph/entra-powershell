f0236fa025112caee9fc60e0a2410f61027a6b4e
Emmanuel Nganga <enganga@microsoft.com>
2024-07-11 16:13:47 +0300

    minor change

--- module/Entra/AdditionalFunctions/Get-EntraDomainFederationSettings.ps1
+++ module/Entra/AdditionalFunctions/Get-EntraDomainFederationSettings.ps1
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
-function Get-EntraDomainFedrationSettings {
+function Get-EntraDomainFederationSettings {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param(
         [Parameter(Mandatory=$true,Position=0,ValueFromPipelineByPropertyName=$true)][string]$DomainName,
