32965ce8a65f0eff4431e1ac1a1a49d4234cfa50
Ashwini Karke <v-akarke@microsoft.com>
2024-07-11 17:36:52 +0530

    added UT and customization

---
+++ module/Entra/AdditionalFunctions/Remove-EntraFeatureRolloutPolicyDirectoryObject.ps1
@@ -0,0 +1,24 @@
+function Remove-EntraFeatureRolloutPolicyDirectoryObject {
+    [CmdletBinding(DefaultParameterSetName = '')]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $Id,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $ObjectId
+    )
+
+    PROCESS {    
+        $params = @{}
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $params = @{}
+        $params["Uri"] = 'https://graph.microsoft.com/v1.0/policies/featureRolloutPolicies/{0}/appliesTo/{1}/$ref' -f $Id,$ObjectId
+        $params["Method"] = "DELETE"
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Invoke-GraphRequest @params -Headers $customHeaders
+        $response 
+    }
+}
