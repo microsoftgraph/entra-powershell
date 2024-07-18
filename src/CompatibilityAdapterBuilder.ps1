1ad9ebfc0f3cccbce5f514d6a2a14125b22de827
Ashwini Karke <v-akarke@microsoft.com>
2024-07-18 13:47:28 +0530

    added aliases

--- src/CompatibilityAdapterBuilder.ps1
+++ src/CompatibilityAdapterBuilder.ps1
@@ -514,7 +514,6 @@ function Get-EntraUnsupportedCommand {
             #Adding direct aliases for Connect-Entra and Disconnect-Entra
             $aliases += "   Set-Alias -Name Connect-AzureAD -Value Connect-Entra -Scope Global -Force`n"
             $aliases += "   Set-Alias -Name Disconnect-AzureAD -Value Disconnect-Entra -Scope Global -Force`n"
-            $aliases += "   Set-Alias -Name Get-EntraCurrentSessionInfo -Value Get-EntraContext -Scope Global -Force`n"
     $aliasFunction = @"
 function Enable-EntraAzureADAlias {
 $($aliases)}
