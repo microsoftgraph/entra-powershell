1337072f9717a374070fd5312c05e666da589c76
v-pughosh <v-pughosh@microsoft.com>
2024-07-25 12:34:32 +0530

    u[dates

--- module/Entra/AdditionalFunctions/New-EntraApplicationFromApplicationTemplate.ps1
+++ module/Entra/AdditionalFunctions/New-EntraApplicationFromApplicationTemplate.ps1
@@ -1,3 +1,6 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
 function New-EntraApplicationFromApplicationTemplate {
     [CmdletBinding(DefaultParameterSetName = '')]
     param (
