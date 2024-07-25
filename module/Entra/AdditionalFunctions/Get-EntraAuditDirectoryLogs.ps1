497cc483537b50ac34fe05d242af893687964128
Ashwini Karke <v-akarke@microsoft.com>
2024-07-25 10:17:50 +0530

    added license

--- module/Entra/AdditionalFunctions/Get-EntraAuditDirectoryLogs.ps1
+++ module/Entra/AdditionalFunctions/Get-EntraAuditDirectoryLogs.ps1
@@ -1,3 +1,6 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
 function Get-EntraAuditDirectoryLogs {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
