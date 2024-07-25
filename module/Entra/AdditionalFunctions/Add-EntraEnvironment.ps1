d4c613d39ad95dc01aad5c0e06d7e1447a935d3c
Ashwini Karke <v-akarke@microsoft.com>
2024-07-25 16:25:21 +0530

    added license

--- module/Entra/AdditionalFunctions/Add-EntraEnvironment.ps1
+++ module/Entra/AdditionalFunctions/Add-EntraEnvironment.ps1
@@ -1,7 +1,8 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-function Add-EntraEnvironment{
+
+function Add-EntraEnvironment {
     [CmdletBinding(DefaultParameterSetName = 'AddQuery')]
       param (
       [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
