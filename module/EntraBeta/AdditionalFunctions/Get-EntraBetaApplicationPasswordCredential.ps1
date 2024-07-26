8994c258c4262dbe0964e177521d100912c8f4b6
Ashwini Karke <v-akarke@microsoft.com>
2024-07-26 16:42:24 +0530

    Get-EntraBetaApplicationPasswordCredential

---
+++ module/EntraBeta/AdditionalFunctions/Get-EntraBetaApplicationPasswordCredential.ps1
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+function Get-EntraBetaApplicationPasswordCredential {
+    [CmdletBinding(DefaultParameterSetName = '')]
+    param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $ObjectId,
+    [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
+    [System.String[]] $Property
+    )
+
+    PROCESS {
+        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
+        $params = @{}
+        $baseUri = "https://graph.microsoft.com/v1.0/applications/$ObjectId/passwordCredentials"
+        $params["Method"] = "GET"
+        $params["Uri"] = "$baseUri"
+
+        $response = Invoke-GraphRequest @params -Headers $customHeaders | ConvertTo-Json | ConvertFrom-Json
+        try {
+            $response = $response.value
+        }
+        catch {}
+        $response | ForEach-Object {
+            if($null -ne $_) {
+                $CustomKeyIdentifier = [System.Text.Encoding]::UTF8.GetBytes([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_.CustomKeyIdentifier)))
+                $DisplayName = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_.CustomKeyIdentifier))
+                Add-Member -InputObject $_ -MemberType NoteProperty -Name CustomKeyIdentifier -Value $CustomKeyIdentifier -Force
+                Add-Member -InputObject $_ -MemberType NoteProperty -Name DisplayName -Value $DisplayName -Force
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name EndDate -Value endDateTime
+                Add-Member -InputObject $_ -MemberType AliasProperty -Name StartDate -Value startDateTime
+            }
+        }
+        $userList = @()
+        foreach ($data in $response) {
+            $userType = New-Object Microsoft.Graph.Beta.PowerShell.Models.MicrosoftGraphPasswordCredential
+            $data.PSObject.Properties | ForEach-Object {
+                $propertyName = $_.Name
+                $propertyValue = $_.Value
+                $userType | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+            }
+            $userList += $userType
+        } 
+
+        if($null -ne $PSBoundParameters["Property"])
+        {
+            $userList | Select-Object $PSBoundParameters["Property"]
+        }
+        else {
+            $userList
+        }
+    }
+}
---
+++ test/module/EntraBeta/Get-EntraBetaApplicationPasswordCredential.Tests.ps1
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+BeforeAll {  
+    if ((Get-Module -Name Microsoft.Graph.Entra.Beta) -eq $null) {
+        Import-Module Microsoft.Graph.Entra.Beta      
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+    
+    $scriptblock = {
+        return @(
+            @{
+                "startDateTime"    = "11/24/2023 6:28:39 AM"
+                "keyId" = "bbbbbbbb-1111-2222-3333-rrrrrrrrrrrr"
+                "hint" = "123"
+                "secretText"  = ""
+                "endDateTime"       = "11/24/2024 6:28:39 AM"
+                "CustomKeyIdentifier" = "dGVzdA=="
+                "DisplayName" = ""
+
+            }
+        )
+    }
+    
+    Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra.Beta
+}
+Describe "Get-EntraBetaApplicationPasswordCredential" {
+    Context "Test for Get-EntraBetaApplicationPasswordCredential" {
+        It "Should return specific Policy" {
+            $result = Get-EntraBetaApplicationPasswordCredential -ObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result.keyId | Should -Be "bbbbbbbb-1111-2222-3333-rrrrrrrrrrrr"
+            $result.DisplayName | Should -Not -BeNullOrEmpty
+            $result.CustomKeyIdentifier.gettype().name | Should -Be 'Byte[]'
+
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra.Beta -Times 1
+        }
+        It "Should fail when ObjectId is invalObjectId" {
+            { Get-EntraBetaApplicationPasswordCredential -ObjectId "" } | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
+        }
+        It "Should fail when ObjectId is empty" {
+            { Get-EntraBetaApplicationPasswordCredential -ObjectId } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
+        }            
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraBetaApplicationPasswordCredential"
+
+            $result = Get-EntraBetaApplicationPasswordCredential -ObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result | Should -Not -BeNullOrEmpty
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra.Beta -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }    
+        }
+    }
+}
