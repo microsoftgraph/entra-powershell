4f5afd46a4778ec8a69bfba7d8bb387a8e26d05b
v-uansari <v-uansari@microsoft.com>
2024-08-07 15:09:56 +0530

    removed worng tests

--- test/module/Entra/Get-EntraDeviceRegisteredOwner.Tests.ps1
+++ test/module/Entra/Get-EntraDeviceRegisteredOwner.Tests.ps1
@@ -46,18 +46,13 @@ Describe "Get-EntraDeviceRegisteredOwner" {
             { Get-EntraDeviceRegisteredOwner -ObjectId "" } | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string.*"
         }
         It "Should return all device registered owner" {
-            $result = Get-EntraDeviceRegisteredOwner -ObjectId "8542ebd1-3d49-4073-9dce-30f197c67755" -All $true
+            $result = Get-EntraDeviceRegisteredOwner -ObjectId "8542ebd1-3d49-4073-9dce-30f197c67755" -All 
             $result | Should -Not -BeNullOrEmpty
             $result.Id | should -Be '412be9d1-1460-4061-8eed-cca203fcb215'
 
             Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
         }
-        It "Should fail when All is empty" {
-            { Get-EntraDeviceRegisteredOwner -ObjectId "8542ebd1-3d49-4073-9dce-30f197c67755" -All  } | Should -Throw "Missing an argument for parameter 'All'*"
-        }
-        It "Should fail when All is invalid" {
-            { Get-EntraDeviceRegisteredOwner -ObjectId "8542ebd1-3d49-4073-9dce-30f197c67755" -All xyz } | Should -Throw "Cannot process argument transformation on parameter 'All'*"
-        }
+       
         It "Should return top device registered owner" {
             $result = Get-EntraDeviceRegisteredOwner -ObjectId "8542ebd1-3d49-4073-9dce-30f197c67755" -Top 1
             $result | Should -Not -BeNullOrEmpty
