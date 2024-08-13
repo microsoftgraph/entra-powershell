88ae48f680f361807fefc569c9d4947635c012bb
v-pughosh <v-pughosh@microsoft.com>
2024-08-13 13:59:44 +0530

    updates

--- module/Entra/AdditionalFunctions/Get-EntraApplicationTemplate.ps1
+++ module/Entra/AdditionalFunctions/Get-EntraApplicationTemplate.ps1
@@ -5,7 +5,7 @@ function Get-EntraApplicationTemplate {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
     [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.String] $Id    
+    [System.String] $Id
     )
 
     PROCESS {
@@ -18,57 +18,13 @@ function Get-EntraApplicationTemplate {
             $params["ApplicationTemplateId"] = $PSBoundParameters["Id"]
             $uri += "/$Id"
         }
-        if($PSBoundParameters.ContainsKey("Verbose"))
-        {
-            $params["Verbose"] = $PSBoundParameters["Verbose"]
-        }
-        if($PSBoundParameters.ContainsKey("Debug"))
-        {
-            $params["Debug"] = $PSBoundParameters["Debug"]
-        }
-        if($null -ne $PSBoundParameters["WarningVariable"])
-        {
-            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
-        }
-        if($null -ne $PSBoundParameters["InformationVariable"])
-        {
-            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-        }
-	    if($null -ne $PSBoundParameters["InformationAction"])
-        {
-            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
-        }
-        if($null -ne $PSBoundParameters["OutVariable"])
-        {
-            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-        }
-        if($null -ne $PSBoundParameters["OutBuffer"])
-        {
-            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
-        }
-        if($null -ne $PSBoundParameters["ErrorVariable"])
-        {
-            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
-        }
-        if($null -ne $PSBoundParameters["PipelineVariable"])
-        {
-            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
-        }
-        if($null -ne $PSBoundParameters["ErrorAction"])
-        {
-            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
-        }
-        if($null -ne $PSBoundParameters["WarningAction"])
-        {
-            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-        }
 
         $response = Invoke-GraphRequest -Uri $uri -Method GET -Headers $customHeaders
         if($response.ContainsKey('value')){
             $response = $response.value
         }
-        
-        $data = $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json        
+
+        $data = $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json
         $userList = @()
         foreach ($res in $data) {
             $userType = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphApplicationTemplate
@@ -79,6 +35,6 @@ function Get-EntraApplicationTemplate {
             }
             $userList += $userType
         }
-        $userList 
-    }    
+        $userList
+    }
 }
