2bcd49b295f4c80dab3137eda718808dc66c9c58
v-akarke <142799789+v-akarke@users.noreply.github.com>
2024-08-22 03:07:42 -0700

    Bugfix 831 (Returning Graph Types instead of PSCustomObjects) (#965)

--- module/EntraBeta/customizations/Get-EntraBetaPolicyAppliedObject.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaPolicyAppliedObject.ps1
@@ -7,7 +7,7 @@
     Parameters = $null
     Outputs = $null
     CustomScript = @'
-    PROCESS {    
+    PROCESS {
         $params = @{}
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
         $Id = $PSBoundParameters["Id"]
@@ -19,10 +19,21 @@
         Write-Debug("============================ TRANSFORMATIONS ============================")
         $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
-                
-        $response = (Invoke-GraphRequest -Headers $customHeaders -Method $params.method -Uri $params.uri | ConvertTo-Json  -Depth 10 | ConvertFrom-Json).value   
+        $response = (Invoke-GraphRequest -Headers $customHeaders -Method $params.method -Uri $params.uri | ConvertTo-Json  -Depth 10 | ConvertFrom-Json).value
         $response | Add-Member -MemberType AliasProperty -Value '@odata.type' -Name 'odata.type'
-        $response  
-    } 
+        if($response){
+            $userList = @()
+            foreach ($data in $response) {
+                $userType = New-Object Microsoft.Graph.Beta.PowerShell.Models.MicrosoftGraphDirectoryObject
+                $data.PSObject.Properties | ForEach-Object {
+                    $propertyName = $_.Name
+                    $propertyValue = $_.Value
+                    $userType | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+                }
+                $userList += $userType
+            }
+            $userList
+        }
+    }
 '@
 }
