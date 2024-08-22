85ca9a31e89c55ceedb8829493712fca57220590
snehalkotwal <142995585+snehalkotwal@users.noreply.github.com>
2024-08-22 17:06:22 +0530

    Msol beta doc (#1016)

--- module/EntraBeta/AdditionalFunctions/Get-EntraBetaDirSyncConfiguration.ps1
+++ module/EntraBeta/AdditionalFunctions/Get-EntraBetaDirSyncConfiguration.ps1
@@ -3,20 +3,6 @@
 # ------------------------------------------------------------------------------
 
 function Get-EntraBetaDirSyncConfiguration {
-    <#
-.PARAMETER TenantId
-
-.PARAMETER <CommonParameters>
-    This cmdlet supports the common parameters: Verbose, Debug,
-    ErrorAction, ErrorVariable, WarningAction, WarningVariable,
-    OutBuffer, PipelineVariable, and OutVariable. For more information, see 
-    about_CommonParameters (https:/go.microsoft.com/fwlink/?LinkID=113216). 
-
-.INPUTS 
-    System.Nullable`1[[System.Guid, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]
-
-
-#>
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
         [Parameter(ParameterSetName = "GetQuery", Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)][ValidateNotNullOrEmpty()][ValidateScript({ if ($_ -is [System.Guid]) { $true } else { throw "TenantId must be of type [System.Guid]." } })][System.guid] $TenantId
