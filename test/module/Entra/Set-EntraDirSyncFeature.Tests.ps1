0163d091a12cf9a3108fdf5add9f979343571530
Snehal Kotwal (Perennial Systems Inc) <v-skotwal@microsoft.com>
2024-08-26 16:54:27 +0530

    updated msol unit test cases

--- test/module/Entra/Set-EntraDirSyncFeature.Tests.ps1
+++ test/module/Entra/Set-EntraDirSyncFeature.Tests.ps1
@@ -1,3 +1,6 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
 BeforeAll {  
     if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
         Import-Module Microsoft.Graph.Entra       
@@ -7,7 +10,7 @@ BeforeAll {
         return @(
             [PSCustomObject]@{
                 "Configuration" = @{ AlertThreshold =500 ; SynchronizationPreventionType = "enabledForCount"}
-                "Id" = "d5aec55f-2d12-4442-8d2f-ccca95d4390e"
+                "Id" = "00aa00aa-bb11-cc22-dd33-44ee44ee44ee"
             }        
         )
         }
@@ -18,25 +21,25 @@ BeforeAll {
 Describe "Set-EntraDirSyncFeature" {
     Context "Test for Set-EntraDirSyncFeature" {
         It "Should sets identity synchronization features for a tenant." {
-            $result =  Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable $false -TenantId "d5aec55f-2d12-4442-8d2f-ccca95d4390e" -Force
+            $result =  Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable $false -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force
             $result | Should -BeNullOrEmpty
             Should -Invoke -CommandName Update-MgDirectoryOnPremiseSynchronization -ModuleName Microsoft.Graph.Entra -Times 1
         }
         
         It "Should fail when Feature is empty" {
-            {Set-EntraDirSyncFeature -Feature  -Enable $false -TenantId "d5aec55f-2d12-4442-8d2f-ccca95d4390e" -Force} | Should -Throw "Missing an argument for parameter 'Feature'. Specify a parameter*"
+            {Set-EntraDirSyncFeature -Feature  -Enable $false -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force} | Should -Throw "Missing an argument for parameter 'Feature'. Specify a parameter*"
         }
 
         It "Should fail when Feature is invalid" {
-            {Set-EntraDirSyncFeature -Feature "" -Enable $false -TenantId "d5aec55f-2d12-4442-8d2f-ccca95d4390e" -Force} | Should -Throw "Cannot bind argument to parameter 'Feature' because it is an empty string.*"
+            {Set-EntraDirSyncFeature -Feature "" -Enable $false -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force} | Should -Throw "Cannot bind argument to parameter 'Feature' because it is an empty string.*"
         } 
 
         It "Should fail when Enable is empty" {
-            {Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable -TenantId "d5aec55f-2d12-4442-8d2f-ccca95d4390e" -Force } | Should -Throw "Missing an argument for parameter 'Enabled'.*"
+            {Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force } | Should -Throw "Missing an argument for parameter 'Enabled'.*"
         }
 
         It "Should fail when Enable is invalid" {
-            {Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable "" -TenantId "d5aec55f-2d12-4442-8d2f-ccca95d4390e" -Force} | Should -Throw "Cannot process argument transformation on parameter 'Enabled'.*"
+            {Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable "" -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force} | Should -Throw "Cannot process argument transformation on parameter 'Enabled'.*"
         }
 
         It "Should fail when TenantId is empty" {
@@ -48,7 +51,29 @@ Describe "Set-EntraDirSyncFeature" {
         }   
 
         It "Should fail when Force parameter is passes with argument" {
-            {Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable $false -TenantId "d5aec55f-2d12-4442-8d2f-ccca95d4390e" -Force "xy"} | Should -Throw "A positional parameter cannot be found that accepts argument*"
-        }   
+            {Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable $false -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force "xy"} | Should -Throw "A positional parameter cannot be found that accepts argument*"
+        }
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Set-EntraDirSyncFeature"
+
+            Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable $false -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Force | Out-Null
+            Should -Invoke -CommandName Update-MgDirectoryOnPremiseSynchronization -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }
+        }
+        It "Should execute successfully without throwing an error" {
+                # Disable confirmation prompts       
+                $originalDebugPreference = $DebugPreference
+                $DebugPreference = 'Continue'
+        
+                try {
+                    # Act & Assert: Ensure the function doesn't throw an exception
+                    { Set-EntraDirSyncFeature -Feature "BypassDirSyncOverrides" -Enable $false -TenantId "00aa00aa-bb11-cc22-dd33-44ee44ee44ee" -Debug } | Should -Not -Throw
+                } finally {
+                    # Restore original confirmation preference            
+                    $DebugPreference = $originalDebugPreference        
+                }
+        }  
     }
 } 
