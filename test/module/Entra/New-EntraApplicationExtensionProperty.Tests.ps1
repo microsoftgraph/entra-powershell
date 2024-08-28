21cb6c1554569b4613c95dd4903d9ebb85dac5bc
v-ymundhe <v-ymundhe@microsoft.com>
2024-08-28 12:46:38 +0530

    unit tests

---
+++ test/module/Entra/New-EntraApplicationExtensionProperty.Tests.ps1
@@ -0,0 +1,89 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+BeforeAll {  
+    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+        Import-Module Microsoft.Graph.Entra      
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+    
+    $scriptblock = {
+        return @(
+            [PSCustomObject]@{
+              "Id"                           = "aaaaaaaa-bbbb-cccc-1111-222222222222"
+              "AppDisplayName"               = "Mock-App"
+              "DataType"                     = "MockType"
+              "DeletedDateTime"              = $null
+              "IsMultiValued"                = $False
+              "IsSyncedFromOnPremises"       = $False
+              "Name"                         = "Mock-App"
+              "TargetObjects"                = "Application"
+              "AdditionalProperties"         = @{"@odata.context" = "https://graph.microsoft.com/v1.0/$metadata#applications('aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb')/extensionProperties/$entity"}
+              "Parameters"                   = $args
+            }
+        )
+    }
+
+    Mock -CommandName New-MgApplicationExtensionProperty -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
+}
+
+Describe "New-EntraApplicationExtensionProperty" {
+Context "Test for New-EntraApplicationExtensionProperty" {
+        It "Should return created MS application extension property" {
+            $result = New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name "Mock-App" -TargetObjects "Application"
+            $result | Should -Not -BeNullOrEmpty
+            $result.Id | Should -Be "aaaaaaaa-bbbb-cccc-1111-222222222222"
+            $result.Name | Should -Be "Mock-App"
+            $result.TargetObjects | Should -Be "Application"
+            $result.DataType | Should -Be "MockType"
+
+
+            Should -Invoke -CommandName New-MgApplicationExtensionProperty  -ModuleName Microsoft.Graph.Entra -Times 1
+        }
+        It "Should fail when ObjectlId is empty" {
+            { New-EntraApplicationExtensionProperty -ObjectId  -DataType "MockType" -Name "Mock-App" -TargetObjects "Application"  } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
+        }
+        It "Should fail when ObjectlId is invalid" {
+            { New-EntraApplicationExtensionProperty -ObjectId "" -DataType "MockType" -Name "Mock-App" -TargetObjects "Application" } | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
+        }
+        It "Should fail when DataType is empty" {
+            { New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType  -Name "Mock-App" -TargetObjects "Application"  } | Should -Throw "Missing an argument for parameter 'DataType'*"
+        }
+        It "Should fail when Name is empty" {
+            { New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name  -TargetObjects "Application"  } | Should -Throw "Missing an argument for parameter 'Name'*"
+        }
+        It "Should fail when TargetObjects is empty" {
+            { New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name "Mock-App" -TargetObjects   } | Should -Throw "Missing an argument for parameter 'TargetObjects'*"
+        }
+        It "Result should Contain ObjectId" {
+            $result = New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name "Mock-App" -TargetObjects "Application"
+            $result.ObjectId | should -Be "aaaaaaaa-bbbb-cccc-1111-222222222222"
+        }
+        It "Should contain ApplicationId in parameters when passed ObjectId to it" {              
+            $result = New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name "Mock-App" -TargetObjects "Application"
+            $params = Get-Parameters -data $result.Parameters
+            $params.ApplicationId | Should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+        }
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion New-EntraApplicationExtensionProperty"
+
+            $result = New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name "Mock-App" -TargetObjects "Application"
+            $params = Get-Parameters -data $result.Parameters
+            $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+        }
+        It "Should execute successfully without throwing an error " {
+            # Disable confirmation prompts       
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { New-EntraApplicationExtensionProperty -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -DataType "MockType" -Name "Mock-App" -TargetObjects "Application" -Debug } | Should -Not -Throw
+            } finally {
+                # Restore original confirmation preference            
+                $DebugPreference = $originalDebugPreference        
+            }
+        }
+
+    }
+}    
---
+++ test/module/Entra/New-EntraGroupAppRoleAssignment.Tests.ps1
@@ -0,0 +1,99 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+BeforeAll {  
+    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+        Import-Module Microsoft.Graph.Entra      
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+    
+    $scriptblock = {
+        return @(
+            [PSCustomObject]@{
+              "Id"                           = "00001111-aaaa-2222-bbbb-3333cccc4444"
+              "AppRoleId"                    = "bbbbbbbb-1111-2222-3333-cccccccccccc"
+              "CreatedDateTime"              = "06-05-2024 05:42:01"
+              "DeletedDateTime"              = $null
+              "PrincipalDisplayName"         = "Mock-Group"
+              "PrincipalId"                  = "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+              "ResourceId"                   = "aaaaaaaa-bbbb-cccc-1111-222222222222"
+              "ResourceDisplayName"          = "Mock-Group"
+              "PrincipalType"                = "PrincipalType"
+              "AdditionalProperties"         = @{"@odata.context" = "https://graph.microsoft.com/v1.0/$metadata#groups('aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb')/appRoleAssignments/$entity"}
+              "Parameters"                   = $args
+            }
+        )
+    }
+
+    Mock -CommandName New-MgGroupAppRoleAssignment -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
+}
+
+Describe "New-EntraGroupAppRoleAssignment" {
+Context "Test for New-EntraGroupAppRoleAssignment" {
+        It "Should return created Group AppRole Assignment" {
+            $result = New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result | Should -Not -BeNullOrEmpty
+            $result.Id | Should -Be "00001111-aaaa-2222-bbbb-3333cccc4444"
+            $result.ResourceId | Should -Be "aaaaaaaa-bbbb-cccc-1111-222222222222"
+            $result.PrincipalId | Should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+            $result.AppRoleId | Should -Be "bbbbbbbb-1111-2222-3333-cccccccccccc"
+
+
+            Should -Invoke -CommandName New-MgGroupAppRoleAssignment  -ModuleName Microsoft.Graph.Entra -Times 1
+        }
+        It "Should fail when ObjectlId is empty" {
+            { New-EntraGroupAppRoleAssignment -ObjectId   -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc" } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
+        }
+        It "Should fail when ObjectlId is invalid" {
+            { New-EntraGroupAppRoleAssignment -ObjectId ""  -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc" } | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
+        }
+        It "Should fail when PrincipalId is empty" {
+            { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId  -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc" } | Should -Throw "Missing an argument for parameter 'PrincipalId'*"
+        }
+        It "Should fail when PrincipalId is invalid" {
+            { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc" } | Should -Throw "Cannot bind argument to parameter 'PrincipalId' because it is an empty string."
+        }
+        It "Should fail when ResourceId is empty" {
+            { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId  -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"  } | Should -Throw "Missing an argument for parameter 'ResourceId'*"
+        }
+        It "Should fail when ResourceId is invalid" {
+            { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc" } | Should -Throw "Cannot bind argument to parameter 'ResourceId' because it is an empty string."
+        }
+        It "Should fail when Id is empty" {
+            { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id   } | Should -Throw "Missing an argument for parameter 'Id'*"
+        }
+        It "Should fail when Id is invalid" {
+            { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "" } | Should -Throw "Cannot bind argument to parameter 'Id' because it is an empty string."
+        }
+        It "Result should Contain ObjectId" {
+            $result = New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result.ObjectId | should -Be "00001111-aaaa-2222-bbbb-3333cccc4444"
+        }
+        It "Should contain AppRoleId in parameters when passed Id to it" {              
+            $result = New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $params = Get-Parameters -data $result.Parameters
+            $params.AppRoleId | Should -Be "bbbbbbbb-1111-2222-3333-cccccccccccc"
+        }
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion New-EntraGroupAppRoleAssignment"
+
+            $result = New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $params = Get-Parameters -data $result.Parameters
+            $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+        }
+        It "Should execute successfully without throwing an error " {
+            # Disable confirmation prompts       
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { New-EntraGroupAppRoleAssignment -ObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -PrincipalId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -ResourceId "aaaaaaaa-bbbb-cccc-1111-222222222222" -Id "bbbbbbbb-1111-2222-3333-cccccccccccc" -Debug } | Should -Not -Throw
+            } finally {
+                # Restore original confirmation preference            
+                $DebugPreference = $originalDebugPreference        
+            }
+        }
+
+    }
+}
