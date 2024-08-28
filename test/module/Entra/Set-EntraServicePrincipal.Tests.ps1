3dfd495d52649b76a3fe4fab5b928fd3f2fe0a60
Snehal Kotwal (Perennial Systems Inc) <v-skotwal@microsoft.com>
2024-08-28 18:21:28 +0530

    updated

--- test/module/Entra/Set-EntraServicePrincipal.Tests.ps1
+++ test/module/Entra/Set-EntraServicePrincipal.Tests.ps1
@@ -11,22 +11,22 @@ BeforeAll {
 }
 
 Describe "Set-EntraServicePrincipal"{
-    Context "Test for " {
+    Context "Test for Set-EntraServicePrincipal" {
         It "Should update the parameter" {
             $tags = @("Environment=Production", "Department=Finance", "Project=MNO")
-            $result= Set-EntraServicePrincipal -ObjectId "6aa187e3-bbbb-4748-a708-fc380aa9eb17" -AccountEnabled $false -AppId "c5d1b05d-2257-465d-bbe4-a2ad50f09b81" -AppRoleAssignmentRequired $true -DisplayName "test11" -ServicePrincipalNames "c5d1b05d-2257-465d-bbe4-a2ad50f09b81" -Tags $tags
+            $result= Set-EntraServicePrincipal -ObjectId "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -AccountEnabled $false -AppId "00001111-aaaa-2222-bbbb-3333cccc4444" -AppRoleAssignmentRequired $true -DisplayName "test11" -ServicePrincipalNames "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -Tags $tags
             $result | Should -BeNullOrEmpty
 
             Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
         }   
         It "Should update the LogoutUrl and ServicePrincipalType parameter" {
-                $result= Set-EntraServicePrincipal -ObjectId "0008861a-d455-4671-bd24-ce9b3bfce288" -LogoutUrl 'https://securescore.office.com/SignOut' -ServicePrincipalType "Application"
+                $result= Set-EntraServicePrincipal -ObjectId "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -LogoutUrl 'https://securescore.office.com/SignOut' -ServicePrincipalType "Application"
                 $result | Should -BeNullOrEmpty
     
                 Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
         }   
         It "Should update the Homepage, ReplyUrls and AlternativeNames parameter" {
-            $result= Set-EntraServicePrincipal -ObjectId "2dccbb5e-5e71-4ff1-905f-44862bb50909" -Homepage 'https://HomePageurlss.com' -ReplyUrls 'https://admin.microsoft1.com' -AlternativeNames "updatetest"
+            $result= Set-EntraServicePrincipal -ObjectId "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -Homepage 'https://HomePageurlss.com' -ReplyUrls 'https://admin.microsoft1.com' -AlternativeNames "updatetest"
             $result | Should -BeNullOrEmpty
 
             Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Graph.Entra -Times 1
@@ -52,24 +52,30 @@ Describe "Set-EntraServicePrincipal"{
             { Set-EntraServicePrincipal -ObjectId ""} | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string.*"
         }
         It "Should fail when non-mandatory is empty" {
-            { Set-EntraServicePrincipal -ObjectId "6aa187e3-bbbb-4748-a708-fc380aa9eb17" -AppId  -Tags  -ReplyUrls -AccountEnabled -AlternativeNames -KeyCredentials -Homepage} | Should -Throw "Missing an argument for parameter*"
-        }
-        It "Should contain ServicePrincipalId in parameters when passed ObjectId to it" {
-            Mock -CommandName Invoke-GraphRequest -MockWith {$args} -ModuleName Microsoft.Graph.Entra
-
-            $result = Set-EntraServicePrincipal -ObjectId "6aa187e3-bbbb-4748-a708-fc380aa9eb17"
-            $params = Get-Parameters -data $result
-            $params.ServicePrincipalId | Should -Be "6aa187e3-bbbb-4748-a708-fc380aa9eb17"
-        }   
+            { Set-EntraServicePrincipal -ObjectId "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -AppId  -Tags  -ReplyUrls -AccountEnabled -AlternativeNames -KeyCredentials -Homepage} | Should -Throw "Missing an argument for parameter*"
+        }  
         It "Should contain 'User-Agent' header" {
             Mock -CommandName Invoke-GraphRequest -MockWith {$args} -ModuleName Microsoft.Graph.Entra
 
             $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Set-EntraServicePrincipal"
 
             $tags = @("Environment=Production", "Department=Finance", "Project=MNO")
-            $result= Set-EntraServicePrincipal -ObjectId "6aa187e3-bbbb-4748-a708-fc380aa9eb17" -AccountEnabled $false -AppId "c5d1b05d-2257-465d-bbe4-a2ad50f09b81" -AppRoleAssignmentRequired $true -DisplayName "test11" -ServicePrincipalNames "c5d1b05d-2257-465d-bbe4-a2ad50f09b81" -Tags $tags
+            $result= Set-EntraServicePrincipal -ObjectId "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -AccountEnabled $false -AppId "00001111-aaaa-2222-bbbb-3333cccc4444" -AppRoleAssignmentRequired $true -DisplayName "test11" -ServicePrincipalNames "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -Tags $tags
             $params = Get-Parameters -data $result
             $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+        }
+        It "Should execute successfully without throwing an error" {
+            # Disable confirmation prompts       
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+            $tags = @("Environment=Production", "Department=Finance", "Project=MNO")
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { Set-EntraServicePrincipal -ObjectId "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -AccountEnabled $false -AppId "00001111-aaaa-2222-bbbb-3333cccc4444" -AppRoleAssignmentRequired $true -DisplayName "test11" -ServicePrincipalNames "11bb11bb-cc22-dd33-ee44-55ff55ff55ff" -Tags $tags -Debug } | Should -Not -Throw
+            } finally {
+                # Restore original confirmation preference            
+                $DebugPreference = $originalDebugPreference        
+            }
         }
    }          
 }
