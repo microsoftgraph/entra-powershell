3fa0682dca7117c632a1bb92ac83bb4ae1cb9094
Snehal Kotwal (Perennial Systems Inc) <v-skotwal@microsoft.com>
2024-08-30 12:02:03 +0530

    mock test cases for contact

---
+++ test/module/Entra/Remove-EntraDomain.Tests.ps1
@@ -0,0 +1,61 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+BeforeAll {  
+    if((Get-Module -Name Microsoft.Graph.Entra) -eq $null){
+        Import-Module Microsoft.Graph.Entra
+    }
+    Import-Module (Join-Path $psscriptroot "..\Common-Functions.ps1") -Force
+
+    Mock -CommandName Remove-MgDomain -MockWith {} -ModuleName Microsoft.Graph.Entra
+}
+
+Describe "Remove-EntraDomain" {
+    Context "Test for Remove-EntraDomain" {
+        It "Should return empty domain name" {
+            $result = Remove-EntraDomain -Name "Contoso.com"
+            $result | Should -BeNullOrEmpty
+
+            Should -Invoke -CommandName Remove-MgDomain -ModuleName Microsoft.Graph.Entra -Times 1
+        }
+
+        It "Should fail when Name is empty" {
+            { Remove-EntraDomain -Name } | Should -Throw "Missing an argument for parameter 'Name'*"
+        }   
+
+        It "Should fail when Name is invalid" {
+            { Remove-EntraDomain -Name "" } | Should -Throw "Cannot bind argument to parameter 'Name' because it is an empty string."
+        }   
+
+        It "Should contain DomainId in parameters when passed Name to it" {
+            Mock -CommandName Remove-MgDomain -MockWith {$args} -ModuleName Microsoft.Graph.Entra
+
+            $result = Remove-EntraDomain -Name "Contoso.com"
+            $params = Get-Parameters -data $result
+            $params.DomainId | Should -Be "Contoso.com"
+        }
+
+        It "Should contain 'User-Agent' header" {
+            Mock -CommandName Remove-MgDomain -MockWith {$args} -ModuleName Microsoft.Graph.Entra
+
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Remove-EntraDomain"
+            $result = Remove-EntraDomain -Name "Contoso.com"
+            $params = Get-Parameters -data $result
+            $params.Headers["User-Agent"] | Should -Be $userAgentHeaderValue
+        }
+        
+        It "Should execute successfully without throwing an error" {
+            # Disable confirmation prompts       
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { Remove-EntraDomain -Name "Contoso.com" -Debug } | Should -Not -Throw
+            } finally {
+                # Restore original confirmation preference            
+                $DebugPreference = $originalDebugPreference        
+            }
+        }   
+    }
+}
