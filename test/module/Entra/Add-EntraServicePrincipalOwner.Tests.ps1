82b7bb3f16ee8b4ceb885a782e9aec1441759af5
Snehal Kotwal (Perennial Systems Inc) <v-skotwal@microsoft.com>
2024-09-04 13:38:58 +0530

    updated

--- test/module/Entra/Add-EntraServicePrincipalOwner.Tests.ps1
+++ test/module/Entra/Add-EntraServicePrincipalOwner.Tests.ps1
@@ -38,16 +38,14 @@ Describe "Add-EntraServicePrincipalOwner" {
             $params.ServicePrincipalId | Should -Be "aaaaaaaa-0b0b-1c1c-2d2d-333333333333"
         }
         It "Should contain BodyParameter in parameters when passed RefObjectId to it" {
-            Mock -CommandName New-MgServicePrincipalOwnerByRef -MockWith {$args} -ModuleName Microsoft.Graph.Entra
-
-            $result = Add-EntraServicePrincipalOwner -ObjectId "aaaaaaaa-0b0b-1c1c-2d2d-333333333333" -RefObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            Add-EntraServicePrincipalOwner -ObjectId "aaaaaaaa-0b0b-1c1c-2d2d-333333333333" -RefObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $value = @{"@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/bbbbbbbb-1111-2222-3333-cccccccccccc"}
-            $params= $result | Convertto-json -Depth 10 | Convertfrom-json  
-            Write-Host $params[1].AdditionalProperties
-            $additionalProperties = $params[1].AdditionalProperties
-            $additionalProperties.'@odata.id' | Should -Be $value.'@odata.id'
+            Should -Invoke -CommandName New-MgServicePrincipalOwnerByRef -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
+                $BodyParameter.AdditionalProperties.'@odata.id' | Should -Be $value.'@odata.id'
+                Write-Host $BodyParameter.AdditionalProperties.'@odata.id'
+                $true
+            }
         }
-
         It "Should contain 'User-Agent' header" {
                 $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Add-EntraServicePrincipalOwner"
     
