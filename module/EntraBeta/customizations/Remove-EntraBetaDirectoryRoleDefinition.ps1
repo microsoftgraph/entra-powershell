e0a5f0ac129dbab72b27c1d9039f12c6c2114210
Ashwini Karke <v-akarke@microsoft.com>
2024-09-05 20:39:43 +0530

    Fixed build failure

--- module/EntraBeta/customizations/Remove-EntraBetaDirectoryRoleDefinition.ps1
+++ module/EntraBeta/customizations/Remove-EntraBetaDirectoryRoleDefinition.ps1
@@ -2,8 +2,8 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 @{
-    SourceName = "Remove-AzureADMSRoleAssignment"
-    TargetName = "Remove-MgBetaRoleManagementDirectoryRoleAssignment"
+    SourceName = "Remove-AzureADMSRoleDefinition"
+    TargetName = "Remove-MgBetaRoleManagementDirectoryRoleDefinition"
     Parameters = $null
     Outputs = $null
 }
--- test/module/Entra/Customizations.Tests.ps1
+++ test/module/Entra/Customizations.Tests.ps1
@@ -15,12 +15,67 @@ Describe 'Checking Files'{
     }
 
     It 'Checking naming conventios' {
+        $sourceNameMappings = @{
+            "Get-AzureADMSRoleAssignment"            = "Get-AzureADMSDirectoryRoleAssignment"
+            "New-AzureADMSRoleAssignment"            = "New-AzureADMSDirectoryRoleAssignment"
+            "Remove-AzureADMSRoleAssignment"         = "Remove-AzureADMSDirectoryRoleAssignment"
+            "Get-AzureADMSRoleDefinition"            = "Get-AzureADMSDirectoryRoleDefinition"
+            "New-AzureADMSRoleDefinition"            = "New-AzureADMSDirectoryRoleDefinition"
+            "Set-AzureADMSRoleDefinition"            = "Set-AzureADMSDirectoryRoleDefinition"
+            "Remove-AzureADMSRoleDefinition"         = "Remove-AzureADMSDirectoryRoleDefinition"
+            "Get-AzureADServiceAppRoleAssignedTo"    = "Get-AzureADServicePrincipalAppRoleAssignedTo"
+            "Get-AzureADServiceAppRoleAssignment"    = "Get-AzureADServicePrincipalAppRoleAssignment"
+            "New-AzureADServiceAppRoleAssignment"    = "New-AzureADServicePrincipalAppRoleAssignment"
+            "Remove-AzureADServiceAppRoleAssignment" = "Remove-AzureADServicePrincipalAppRoleAssignment"
+        }
         $files | ForEach-Object {
             $name = $_.Name -ireplace ".ps1",""
             $name = $name -ireplace "Entra","AzureAD"
             if(("Generic" -ne $name) -and ("Types" -ne $name)){
                 Write-Host "Checking $name"
                 $value = . $_.FullName
+                # Dynamically update $value.SourceName based on the mappings
+                if ($sourceNameMappings.ContainsKey($value.SourceName)) {
+                    $value.SourceName = $sourceNameMappings[$value.SourceName]
+                }
+                # if($value.SourceName -eq "Get-AzureADMSRoleAssignment"){
+                #     $value.SourceName = "Get-AzureADMSDirectoryRoleAssignment"
+                # }
+                # if($value.SourceName -eq "New-AzureADMSRoleAssignment"){
+                #     $value.SourceName = "New-AzureADMSDirectoryRoleAssignment"
+                # }
+                # if($value.SourceName -eq "Remove-AzureADMSRoleAssignment"){
+                #     $value.SourceName = "Remove-AzureADMSDirectoryRoleAssignment"
+                # }
+                # if($value.SourceName -eq "Remove-AzureADMSRoleAssignment"){
+                #     $value.SourceName = "Remove-AzureADMSDirectoryRoleAssignment"
+                # }
+                # if($value.SourceName -eq "Get-AzureADMSRoleDefinition"){
+                #     $value.SourceName = "Get-AzureADMSDirectoryRoleDefinition"
+                # }
+                # if($value.SourceName -eq "New-AzureADMSRoleDefinition"){
+                #     $value.SourceName = "New-AzureADMSDirectoryRoleDefinition"
+                # }
+                # if($value.SourceName -eq "Set-AzureADMSRoleDefinition"){
+                #     $value.SourceName = "Set-AzureADMSDirectoryRoleDefinition"
+                # }
+                # if($value.SourceName -eq "Remove-AzureADMSRoleDefinition"){
+                #     $value.SourceName = "Remove-AzureADMSDirectoryRoleDefinition"
+                # }
+                # if($value.SourceName -eq "Get-AzureADServiceAppRoleAssignedTo"){
+                #     $value.SourceName = "Get-AzureADServicePrincipalAppRoleAssignedTo"
+                # }
+                # if($value.SourceName -eq "Get-AzureADServiceAppRoleAssignment"){
+                #     $value.SourceName = "Get-AzureADServicePrincipalAppRoleAssignment"
+                # }
+                # if($value.SourceName -eq "New-AzureADServiceAppRoleAssignment"){
+                #     $value.SourceName = "New-AzureADServicePrincipalAppRoleAssignment"
+                # }
+                # if($value.SourceName -eq "Remove-AzureADServiceAppRoleAssignment"){
+                #     $value.SourceName = "Remove-AzureADServicePrincipalAppRoleAssignment"
+                # }
+                
+
                 $name | Should -Be ($value.SourceName -ireplace "AzureADMS","AzureAD")  
             }            
         }
