ff7a226f21c5025ba75022e274253bf88f2406fe
Ashwini Karke <v-akarke@microsoft.com>
2024-09-06 10:27:22 +0530

    updated Get-EntraUser

--- module/Entra/customizations/Set-EntraDevice.ps1
+++ module/Entra/customizations/Set-EntraDevice.ps1
@@ -3,9 +3,67 @@
 # ------------------------------------------------------------------------------
 @{
     SourceName = "Set-AzureADDevice"
-    TargetName = $null
-    Parameters = $null
-    Outputs = $null
-    CustomScript = @'
+    TargetName = "Update-MgDevice"
+    Parameters = @(
+        @{
+            SourceName = "ApproximateLastLogonTimeStamp"
+            TargetName = "ApproximateLastSignInDateTime"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "DeviceId"
+            TargetName = "DeviceId1"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "DeviceObjectVersion"
+            TargetName = "DeviceVersion"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "DeviceOSType"
+            TargetName = "OperatingSystem"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "DeviceOSVersion"
+            TargetName = "OperatingSystemVersion"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "DevicePhysicalIds"
+            TargetName = "PhysicalIds"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "DeviceTrustType"
+            TargetName = "TrustType"
+            ConversionType = "Name"
+            SpecialMapping = $null
+        }
+        @{
+            SourceName = "AlternativeSecurityIds"
+            TargetName = "BodyParameter"
+            ConversionType = "ScriptBlock"
+            SpecialMapping = @'
+            $key = [System.Text.Encoding]::UTF8.GetString($TmpValue.key)
+            $Temp = @{
+                alternativeSecurityIds = @(
+                    @{
+                        type = $TmpValue.type
+                        key = [System.Text.Encoding]::ASCII.GetBytes($key)
+                    }
+                )
+            }
+            $Value = $Temp 
 '@
+        }
+    )
+    Outputs = $null
 }
