d5b5c1755753127f01f127d6c067e07cc68f6658
Snehal Kotwal (Perennial Systems Inc) <v-skotwal@microsoft.com>
2024-09-20 18:29:44 +0530

    updated test cases

--- test/module/Entra/Get-EntraDomainFederationSettings.Tests.ps1
+++ test/module/Entra/Get-EntraDomainFederationSettings.Tests.ps1
@@ -29,29 +29,52 @@ BeforeAll {
     Mock -CommandName Get-MgDomainFederationConfiguration -MockWith $scriptblock -ModuleName Microsoft.Graph.Entra
 }
 
-Describe "Get-EntraDomainFedrationSettings" {
-    Context "Test for Get-EntraDomainFedrationSettings" {
+Describe "Get-EntraDomainFederationSettings" {
+    Context "Test for Get-EntraDomainFederationSettings" {
         It "Should return federation settings" {
-            $result = Get-EntraDomainFedrationSettings -DomainName "test.com"
+            $result = Get-EntraDomainFederationSettings -DomainName "test.com"
             $result | Should -Not -BeNullOrEmpty
             $result.FederationBrandName | Should -Be "Contoso"
             $result.ActiveLogOnUri | Should -Be "https://sts.deverett.info/adfs/services/trust/2005/usernamemixed"
             Should -Invoke -CommandName Get-MgDomainFederationConfiguration -ModuleName Microsoft.Graph.Entra -Times 1
         }
         It "Should fail when TenantId is null" {
-            { Get-EntraDomainFedrationSettings -TenantId } | Should -Throw "Missing an argument for parameter 'TenantId'*"
+            { Get-EntraDomainFederationSettings -TenantId } | Should -Throw "Missing an argument for parameter 'TenantId'*"
         }
         It "Should fail when TenantId is empty" {
-            { Get-EntraDomainFedrationSettings -TenantId "" } | Should -Throw "Cannot process argument transformation on parameter 'TenantId'.*"
+            { Get-EntraDomainFederationSettings -TenantId "" } | Should -Throw "Cannot process argument transformation on parameter 'TenantId'.*"
         }
         It "Should fail when DomainName is null" {
-            { Get-EntraDomainFedrationSettings -DomainName } | Should -Throw "Missing an argument for parameter 'DomainName'*"
+            { Get-EntraDomainFederationSettings -DomainName } | Should -Throw "Missing an argument for parameter 'DomainName'*"
         }
         It "Should fail when DomainName is empty" {
-            { Get-EntraDomainFedrationSettings -DomainName "" } | Should -Throw "Cannot bind argument to parameter 'DomainName'*"
+            { Get-EntraDomainFederationSettings -DomainName "" } | Should -Throw "Cannot bind argument to parameter 'DomainName'*"
         }
         It "Should fail when invalid paramter is passed"{
-            { Get-EntraDomainFedrationSettings -Demo } | Should -Throw "A parameter cannot be found that matches parameter name 'Demo'*"
+            { Get-EntraDomainFederationSettings -Demo } | Should -Throw "A parameter cannot be found that matches parameter name 'Demo'*"
         }
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDomainFederationSettings"
+            $result = Get-EntraDomainFederationSettings -DomainName "test.com"
+            $result | Should -Not -BeNullOrEmpty
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDomainFederationSettings"
+            Should -Invoke -CommandName Get-MgDomainFederationConfiguration -ModuleName Microsoft.Graph.Entra -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }
+        } 
+        It "Should execute successfully without throwing an error " {
+            # Disable confirmation prompts       
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { Get-EntraDomainFederationSettings -DomainName "test.com" -Debug } | Should -Not -Throw
+            } finally {
+                # Restore original confirmation preference            
+                $DebugPreference = $originalDebugPreference        
+            }
+        }   
     }
 }
