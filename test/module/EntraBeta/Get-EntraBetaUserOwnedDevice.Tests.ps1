d286964eec4fea71e33ac4ffb6f0de4e24c07b12
v-uansari <v-uansari@microsoft.com>
2024-09-25 19:08:52 +0530

    alias changes

--- test/module/EntraBeta/Get-EntraBetaUserOwnedDevice.Tests.ps1
+++ test/module/EntraBeta/Get-EntraBetaUserOwnedDevice.Tests.ps1
@@ -38,7 +38,7 @@ BeforeAll {
 Describe "Get-EntraBetaUserOwnedDevice" {
     Context "Test for Get-EntraBetaUserOwnedDevice" {
         It "Should return specific user registered device" {
-            $result = Get-EntraBetaUserOwnedDevice -ObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraBetaUserOwnedDevice -UserId "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $result | Should -Not -BeNullOrEmpty
             $result.Id | Should -Be "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
             $result.AdditionalProperties.deviceId | Should -Be "aaaaaaaa-0b0b-1c1c-2d2d-333333333333"
@@ -47,22 +47,22 @@ Describe "Get-EntraBetaUserOwnedDevice" {
             Should -Invoke -CommandName Get-MgBetaUserOwnedDevice  -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should fail when ObjectId is empty" {
-            { Get-EntraBetaUserOwnedDevice -ObjectId    } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
+            { Get-EntraBetaUserOwnedDevice -UserId    } | Should -Throw "Missing an argument for parameter 'ObjectId'*"
         }
         It "Should fail when ObjectId is invalid" {
-            { Get-EntraBetaUserOwnedDevice -ObjectId  ""} | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
+            { Get-EntraBetaUserOwnedDevice -UserId  ""} | Should -Throw "Cannot bind argument to parameter 'ObjectId' because it is an empty string."
         }
         It "Should return All user registered devices" {
-            $result = Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -All
+            $result = Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -All
             $result | Should -Not -BeNullOrEmpty
 
             Should -Invoke -CommandName Get-MgBetaUserOwnedDevice  -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should fail when All is invalid" {
-            { Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -All xyz } | Should -Throw "A positional parameter cannot be found that accepts argument 'xyz'.*"
+            { Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -All xyz } | Should -Throw "A positional parameter cannot be found that accepts argument 'xyz'.*"
         }
         It "Should return top 1 user registered device" {
-            $result = Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Top 1
+            $result = Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Top 1
             $result | Should -Not -BeNullOrEmpty
             $result.Id | Should -Be "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
             $result.AdditionalProperties.deviceId | Should -Be "aaaaaaaa-0b0b-1c1c-2d2d-333333333333"
@@ -71,30 +71,30 @@ Describe "Get-EntraBetaUserOwnedDevice" {
             Should -Invoke -CommandName Get-MgBetaUserOwnedDevice  -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should fail when Top is empty" {
-            { Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Top } | Should -Throw "Missing an argument for parameter 'Top'*"
+            { Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Top } | Should -Throw "Missing an argument for parameter 'Top'*"
         }
         It "Should fail when Top is invalid" {
-            { Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Top xyz } | Should -Throw "Cannot process argument transformation on parameter 'Top'*"
+            { Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Top xyz } | Should -Throw "Cannot process argument transformation on parameter 'Top'*"
         }
         It "Should contain UserId in parameters when passed ObjectId to it" {              
-            $result = Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $params = Get-Parameters -data $result.Parameters
             $params.UserId | Should -Be "bbbbbbbb-1111-2222-3333-cccccccccccc"
         }
         It "Property parameter should work" {
-            $result =  Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Property Id
+            $result =  Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Property Id
             $result | Should -Not -BeNullOrEmpty
             $result.Id | Should -Be 'aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb'
 
             Should -Invoke -CommandName Get-MgBetaUserOwnedDevice -ModuleName Microsoft.Graph.Entra.Beta -Times 1
         }
         It "Should fail when Property is empty" {
-             {  Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Property } | Should -Throw "Missing an argument for parameter 'Property'*"
+             {  Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Property } | Should -Throw "Missing an argument for parameter 'Property'*"
         }
         It "Should contain 'User-Agent' header" {
             $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraBetaUserOwnedDevice"
             
-            $result = Get-EntraBetaUserOwnedDevice -ObjectId "bbbbbbbb-1111-2222-3333-cccccccccccc"
+            $result = Get-EntraBetaUserOwnedDevice -UserId "bbbbbbbb-1111-2222-3333-cccccccccccc"
             $result | Should -Not -BeNullOrEmpty
 
             $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraBetaUserOwnedDevice"
@@ -110,7 +110,7 @@ Describe "Get-EntraBetaUserOwnedDevice" {
             
             try {
                 # Act & Assert: Ensure the function doesn't throw an exception
-                { Get-EntraBetaUserOwnedDevice -ObjectId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Debug } | Should -Not -Throw
+                { Get-EntraBetaUserOwnedDevice -UserId  "bbbbbbbb-1111-2222-3333-cccccccccccc" -Debug } | Should -Not -Throw
             } finally {
                 # Restore original confirmation preference            
                 $DebugPreference = $originalDebugPreference        
