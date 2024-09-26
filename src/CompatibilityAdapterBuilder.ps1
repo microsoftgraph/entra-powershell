1b9462815de131b642933a4a2583efb82a7da6c1
v-pughosh <v-pughosh@microsoft.com>
2024-09-26 11:52:33 +0530

    updates

--- src/CompatibilityAdapterBuilder.ps1
+++ src/CompatibilityAdapterBuilder.ps1
@@ -39,7 +39,6 @@ class CompatibilityAdapterBuilder {
         'Get-EntraServicePrincipalDelegatedPermissionClassification',
         'Set-EntraServicePrincipal',
         'New-EntraConditionalAccessPolicy',
-        'Set-EntraUserThumbnailPhoto',
         'Reset-EntraLifeCycleGroup',
         'Get-EntraObjectByObjectId',
         'Remove-EntraPermissionGrantConditionSet',
