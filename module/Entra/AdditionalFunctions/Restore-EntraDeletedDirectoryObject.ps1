6315950e4e5104b59a4d3b77edde40c3bf60fe55
v-akarke <142799789+v-akarke@users.noreply.github.com>
2024-09-30 06:31:09 -0700

    Restore-EntraBetaDeletedDirectoryObject (#1071)

---
+++ module/Entra/AdditionalFunctions/Restore-EntraDeletedDirectoryObject.ps1
@@ -0,0 +1,46 @@
+function Restore-EntraDeletedDirectoryObject {
+    [CmdletBinding(DefaultParameterSetName = '')]
+    param (
+    [Alias('ObjectId')]
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.String] $Id,
+    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [switch] $AutoReconcileProxyConflict
+    )
+
+    PROCESS {    
+        $params = @{}
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $params["Uri"] = 'https://graph.microsoft.com/v1.0/directory/deletedItems/'   
+        $params["Method"] = "POST"    
+        if($null -ne $PSBoundParameters["Id"])
+        {
+            $params["Uri"] += $Id+"/microsoft.graph.restore"      
+        }
+        if($PSBoundParameters.ContainsKey("AutoReconcileProxyConflict"))
+        {
+            $params["Body"] = @{
+                autoReconcileProxyConflict = $true
+            }
+        }
+    
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+        
+        $response = Invoke-GraphRequest @params -Headers $customHeaders
+        if($response){
+            $userList = @()
+            foreach ($data in $response) {
+                $userType = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphDirectoryObject
+                $data.PSObject.Properties | ForEach-Object {
+                    $propertyName = $_.Name
+                    $propertyValue = $_.Value
+                    $userType | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+                }
+                $userList += $userType
+            }
+            $userList
+        }
+    }
+}
