4115c361be48a79492e80113dcb2695ef8068bd6
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-10-21 10:58:20 +0300

    Fixed the runner script

--- build/Create-EntraModules.ps1
+++ build/Create-EntraModules.ps1
@@ -1,13 +1,26 @@
+param (
+    [string]$Module = "Entra"  # Default to "Entra" if no argument is provided
+)
+
+# Import the necessary scripts
 . ..\src\EntraModuleSplitter.ps1
 . ..\src\EntraModuleBuilder.ps1
 
 # Split the module and take into account the AzureADAliases as well
 $entraModuleSplitter = [EntraModuleSplitter]::new()
-$entraModuleSplitter.SplitEntraModule('Entra')  # or 'EntraBeta'
-$entraModuleSplitter.ProcessEntraAzureADAliases('Entra')
+$entraModuleSplitter.SplitEntraModule($Module)  # Pass the module argument
+$entraModuleSplitter.ProcessEntraAzureADAliases($Module)
 
-#Build the .psm1 file, 
-#TODO: Generate help-xml file
-#TODO: Generate the .psd1 file
+# Build Entra Module
 $moduleBuilder = [EntraModuleBuilder]::new()
-$moduleBuilder.CreateSubModuleFile("..\module\Entra\Microsoft.Graph.Entra\", ".\Typedefs.txt")
+
+# Determine the output path based on the module
+$outputPath = if ($Module -eq "Entra") {
+    "..\module\Entra\Microsoft.Graph.Entra\"
+} else {
+    "..\module\EntraBeta\Microsoft.Graph.Entra.Beta\"
+}
+
+$moduleBuilder.CreateHelpFile($Module)
+$moduleBuilder.CreateSubModuleFile($outputPath, ".\Typedefs.txt")
+$moduleBuilder.CreateModuleManifest($Module)
