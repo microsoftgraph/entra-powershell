6410a6d0d87d06db83668c2e2e2b3e78d490f7f2
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-10-21 10:07:04 +0300

    Added Comments

--- build/Split-EntraModule.ps1
+++ build/Split-EntraModule.ps1
@@ -1,8 +1,13 @@
 . ..\src\EntraModuleSplitter.ps1
 . ..\src\EntraModuleBuilder.ps1
 
+# Split the module and take into account the AzureADAliases as well
 $entraModuleSplitter = [EntraModuleSplitter]::new()
 $entraModuleSplitter.SplitEntraModule('Entra')  # or 'EntraBeta'
 $entraModuleSplitter.ProcessEntraAzureADAliases('Entra')
+
+#Build the .psm1 file, 
+#TODO: Generate help-xml
+#TODO: Generate the .psd1
 $moduleBuilder = [EntraModuleBuilder]::new()
 $moduleBuilder.CreateSubModuleFile("..\module\Entra\Microsoft.Graph.Entra\", ".\Typedefs.txt")
