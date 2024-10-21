15a2f7de7d19bb5c00d3bbc738c9f76a2165dcef
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-10-21 16:52:55 +0300

    Debugged to add Get-EntraUnsupportedCommand and New-EntraCustomHeaders functions

--- build/Create-EntraModule.ps1
+++ build/Create-EntraModule.ps1
@@ -1,9 +1,14 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
 param (
     [string]$Module = "Entra"  # Default to "Entra" if no argument is provided
 )
 
 # Import the necessary scripts
 . ..\src\EntraModuleSplitter.ps1
+
 
 
 # Split the module and take into account the AzureADAliases as well
