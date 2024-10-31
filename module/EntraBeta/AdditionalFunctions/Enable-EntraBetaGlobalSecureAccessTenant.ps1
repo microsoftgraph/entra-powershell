322349ade830350143b8bdd9cc14465a073754a6
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-10-31 09:56:27 +0300

    Update module/EntraBeta/AdditionalFunctions/Enable-EntraBetaGlobalSecureAccessTenant.ps1
    Co-authored-by: Kennedy Kang'ethe <kemunga@microsoft.com>

--- module/EntraBeta/AdditionalFunctions/Enable-EntraBetaGlobalSecureAccessTenant.ps1
+++ module/EntraBeta/AdditionalFunctions/Enable-EntraBetaGlobalSecureAccessTenant.ps1
@@ -7,7 +7,7 @@ function Enable-EntraBetaGlobalSecureAccessTenant {
     $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
 
 
-			$response = Invoke-GraphRequest -Method POST -Headers $customHeaders -OutputType PSObject -Uri "https://graph.microsoft.com/beta/networkAccess/microsoft.graph.networkaccess.onboard"
+    $response = Invoke-GraphRequest -Method POST -Headers $customHeaders -OutputType PSObject -Uri "https://graph.microsoft.com/beta/networkAccess/microsoft.graph.networkaccess.onboard"
 			$response
             
 		}			
