9eba0cdef1672adfe6502e0b1ebe580f9500e839
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-10-31 10:05:42 +0300

    Applying PowerShell formatting for New-EntraBetaPrivateAccessApplication

--- module/EntraBeta/AdditionalFunctions/New-EntraBetaPrivateAccessApplication.ps1
+++ module/EntraBeta/AdditionalFunctions/New-EntraBetaPrivateAccessApplication.ps1
@@ -1,70 +1,65 @@
 # ------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+#  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 function New-EntraBetaPrivateAccessApplication {
 
-	[CmdletBinding(DefaultParameterSetName = 'AllPrivateAccessApps')]
-	param (
+    [CmdletBinding(DefaultParameterSetName = 'AllPrivateAccessApps')]
+    param (
+        [Parameter(Mandatory = $True, Position = 1)]
+        [string]
+        $ApplicationName,
+        
+        [Parameter(Mandatory = $False, Position = 2)]
+        [string]
+        $ConnectorGroupId
+    )
 
-		[Parameter(Mandatory = $True, Position = 1)]
-		[string]
-		$ApplicationName,
-		
-		[Parameter(Mandatory = $False, Position = 2)]
-		[string]
-		$ConnectorGroupId
-	)
+    PROCESS {
+        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
 
-    PROCESS {  
-    $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
+        $bodyJson = @{ displayName = $ApplicationName } | ConvertTo-Json -Depth 99 -Compress
 
+        # Instantiate the Private Access app
+        try {
+            $newApp = Invoke-GraphRequest -Method POST -Headers $customHeaders -Uri 'https://graph.microsoft.com/beta/applicationTemplates/8adf8e6e-67b2-4cf2-a259-e3dc5476c621/instantiate' -Body $bodyJson
+        }
+        catch {
+            Write-Error "Failed to create the Private Access app. Error: $_"
+            return
+        }
 
-	$bodyJson = @{displayName = $ApplicationName} | ConvertTo-Json -Depth 99 -Compress
+        $bodyJson = @{
+            "onPremisesPublishing" = @{
+                "applicationType" = "nonwebapp"
+                "isAccessibleViaZTNAClient" = $true
+            }
+        } | ConvertTo-Json -Depth 99 -Compress
 
-	# Instantiate the Private Access app
+        $newAppId = $newApp.application.objectId
 
-	try {
-		$newApp = Invoke-GraphRequest -Method POST -Headers $customHeaders -Uri https://graph.microsoft.com/beta/applicationTemplates/8adf8e6e-67b2-4cf2-a259-e3dc5476c621/instantiate -Body $bodyJson
-	}
-	catch {
-		Write-Error "Failed to create the Private Access app. Error: $_"
-		return
-	}
+        # Set the Private Access app to be accessible via the ZTNA client
+        $params = @{
+            Method = 'PATCH'
+            Uri = "https://graph.microsoft.com/beta/applications/$newAppId/"
+            Body = $bodyJson
+        }
 
-	$bodyJson = @{
-		"onPremisesPublishing" = @{
-			"applicationType" = "nonwebapp"
-			"isAccessibleViaZTNAClient" = $true
-		}
-	} | ConvertTo-Json -Depth 99 -Compress
+        Invoke-GraphRequest @params
 
-	$newAppId = $newApp.application.objectId
-
-	# Set the Private Access app to be accessible via the ZTNA client
-	$params = @{
-		Method = 'PATCH'
-		Uri = "https://graph.microsoft.com/beta/applications/$newAppId/"
-		Body = $bodyJson
-
-	}
-
-	Invoke-GraphRequest @params
-
-	# If ConnectorGroupId has been specified, assign the connector group to the app, otherwise the default connector group will be assigned.
-	if ($ConnectorGroupId) {
-
-		$bodyJson = @{
-			"@odata.id" = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationproxy/connectorGroups/$ConnectorGroupId"
-		} | ConvertTo-Json -Depth 99 -Compress
-		
-		$params = @{
-			Method = 'PUT'
-			Uri = "https://graph.microsoft.com/beta/applications/$newAppId/connectorGroup/`$ref"
-			Body = $bodyJson
-		}
-			
-		Invoke-GraphRequest @params
-	}
-    
-	}
+        # If ConnectorGroupId has been specified, assign the connector group to the app, otherwise the default connector group will be assigned.
+        if ($ConnectorGroupId) {
+            $bodyJson = @{
+                "@odata.id" = "https://graph.microsoft.com/beta/onPremisesPublishingProfiles/applicationproxy/connectorGroups/$ConnectorGroupId"
+            } | ConvertTo-Json -Depth 99 -Compress
+            
+            $params = @{
+                Method = 'PUT'
+                Uri = "https://graph.microsoft.com/beta/applications/$newAppId/connectorGroup/`$ref"
+                Body = $bodyJson
+            }
+                
+            Invoke-GraphRequest @params
+        }
+    }
 }
