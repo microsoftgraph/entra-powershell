7f8fd92d12377f26d666c9d48621c9a073bfd9f1
Kennedy Kangethe Munga <kemunga@microsoft.com>
2024-10-31 16:02:15 +0300

    Update name

--- module/EntraBeta/AdditionalFunctions/Get-EntraBetaPrivateAccessApplication.ps1
+++ module/EntraBeta/AdditionalFunctions/Get-EntraBetaPrivateAccessApplication.ps1
@@ -3,7 +3,7 @@
 #  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
-function Get-EntraBetaPrivateAccessApplicationSegment {
+function Get-EntraBetaPrivateAccessApplication {
     
     [CmdletBinding(DefaultParameterSetName = 'AllPrivateAccessApps')]
     param (
