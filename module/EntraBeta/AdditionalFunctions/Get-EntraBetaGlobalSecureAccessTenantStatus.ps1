49a92006af72aacb70a4f950d218a38d36f1e5d5
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-10-31 10:00:41 +0300

    Applying PowerShell formatting for Get-EntraBetaGlobalSecureAccessTenantStatus

--- module/EntraBeta/AdditionalFunctions/Get-EntraBetaGlobalSecureAccessTenantStatus.ps1
+++ module/EntraBeta/AdditionalFunctions/Get-EntraBetaGlobalSecureAccessTenantStatus.ps1
@@ -1,15 +1,11 @@
 # ------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+#  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 function Get-EntraBetaGlobalSecureAccessTenantStatus {
-
-    PROCESS {  
-    $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-
-
-			$response = Invoke-GraphRequest -Method GET -Headers $customHeaders -OutputType PSObject -Uri "https://graph.microsoft.com/beta/networkAccess/tenantStatus"
-			$response
-            
-		}			
-
+	PROCESS {
+		$customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
+		$response = Invoke-GraphRequest -Method GET -Headers $customHeaders -OutputType PSObject -Uri "https://graph.microsoft.com/beta/networkAccess/tenantStatus"
+		$response
+	}
 }
