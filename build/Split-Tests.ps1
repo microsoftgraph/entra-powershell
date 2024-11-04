cebb0095b2ba5aebf4e1861f8758b0e846173d79
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-11-04 17:32:27 +0300

    common-functions import change for tests

--- build/Split-Tests.ps1
+++ build/Split-Tests.ps1
@@ -180,41 +180,5 @@ function Split-Tests {
     Log-Message -Message "Split-Tests completed for source: $Module" -Level 'SUCCESS'
 }
 
-function Update-CommonFunctionsImport {
-    param (
-        [string]$Module='Entra'  # Default to the current directory if no path is provided
-    )
- 
-   $rootPath=if($Module -eq 'Entra'){
-       "../testVNext/Entra"
-   }else{
-        "../testVNext/EntraBeta"
-   }
-    
-    # Get all .Tests.ps1 files in the specified directory and its subdirectories
-    $testFiles = Get-ChildItem -Path $rootPath -Recurse -Filter *.Tests.ps1
-
-    Log-Message "Starting common-functions import update"
-    # Loop through each file
-    foreach ($file in $testFiles) {
-        # Read the content of the file
-        $content = Get-Content -Path $file.FullName
-
-        Log-Message "Processing $file"
-        
-        # Check if the target string exists in the content
-          if ($content -match  'Import-Module\s*\(\s*Join-Path\s*\$psscriptroot\s*["'']\.\.\\Common-Functions\.ps1["'']\s*\)\s*-Force') {
-            # Replace the old string with the new one
-            $newContent = $content -replace 'Import-Module \(Join-Path \$PSScriptRoot "\.\.\\\.\.\\\.\.\\Common-Functions\.ps1"\) -Force', 'Import-Module (Join-Path $PSScriptRoot "..\..\build\Common-Functions.ps1") -Force'
-            
-            # Write the updated content back to the file
-            Set-Content -Path $file.FullName -Value $newContent
-            
-            # Output the change
-            Log-Message "Updated file: $($file.FullName)"
-        }
-    }
-}
-
 
 Split-Tests -Module 'EntraBeta'
