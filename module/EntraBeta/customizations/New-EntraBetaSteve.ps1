b4711e2e5eaae18bf445b2d27438c1fe37bce316
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-11-04 11:05:01 +0000

    Renaming function

--- module/EntraBeta/customizations/New-EntraBetaSteve.ps1
+++ module/EntraBeta/customizations/New-EntraBetaSteve.ps1
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.
 #  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-function New-EntraBetaServicePrincipalPasswordCredential {
+function New-EntraBetaSteve {
     [CmdletBinding(DefaultParameterSetName = '')]
     param (
         [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
