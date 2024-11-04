915d490e98707927b0933f1b77128b3423cfad0c
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-11-04 16:13:45 +0300

    Updated common-functions.ps1 import in every test file

--- build/Update-CommonFunctionsImport.ps1
+++ build/Update-CommonFunctionsImport.ps1
@@ -1,4 +1,4 @@
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------ 
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.
 #  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
@@ -6,30 +6,31 @@
 
 function Update-CommonFunctionsImport {
     param (
-        [string]$Module='Entra'  # Default to the current directory if no path is provided
+        [string]$Module = 'Entra'  # Default to 'Entra' if no path is provided
     )
- 
-   $rootPath=if($Module -eq 'Entra'){
-       "../testVNext/Entra"
-   }else{
+
+    $rootPath = if ($Module -eq 'Entra') {
+        "../testVNext/Entra"
+    } else {
         "../testVNext/EntraBeta"
-   }
+    }
     
     # Get all .Tests.ps1 files in the specified directory and its subdirectories
     $testFiles = Get-ChildItem -Path $rootPath -Recurse -Filter *.Tests.ps1
 
     Log-Message "Starting common-functions import update"
+    
     # Loop through each file
     foreach ($file in $testFiles) {
         # Read the content of the file
-        $content = Get-Content -Path $file.FullName
+        $content = Get-Content -Path $file.FullName -Raw
 
         Log-Message "Processing $file"
         
-        # Check if the target string exists in the content
-          if ($content -match  'Import-Module\s*\(\s*Join-Path\s*\$psscriptroot\s*["'']\.\.\\Common-Functions\.ps1["'']\s*\)\s*-Force') {
+        # Check and replace all occurrences of the target string
+        if ($content -match 'Import-Module\s*\(\s*Join-Path\s*\$psscriptroot\s*["'']\.\.\\Common-Functions\.ps1["'']\s*\)\s*-Force') {
             # Replace the old string with the new one
-            $newContent = $content -replace 'Import-Module \(Join-Path \$PSScriptRoot "\.\.\\\.\.\\\.\.\\Common-Functions\.ps1"\) -Force', 'Import-Module (Join-Path $PSScriptRoot "..\..\build\Common-Functions.ps1") -Force'
+            $newContent = $content -replace 'Import-Module\s*\(\s*Join-Path\s*\$psscriptroot\s*["'']\.\.\\Common-Functions\.ps1["'']\s*\)\s*-Force', 'Import-Module (Join-Path $PSScriptRoot "..\..\build\Common-Functions.ps1") -Force'
             
             # Write the updated content back to the file
             Set-Content -Path $file.FullName -Value $newContent
@@ -40,5 +41,6 @@ function Update-CommonFunctionsImport {
     }
 }
 
+# Run the function for both modules
 Update-CommonFunctionsImport -Module 'Entra'
 Update-CommonFunctionsImport -Module 'EntraBeta'
