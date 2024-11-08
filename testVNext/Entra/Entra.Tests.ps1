a4046fe2d4c5dff38b07c229b30faeb381eca42a
Kennedy Kangethe Munga <kemunga@microsoft.com>
2024-11-08 15:51:48 +0300

    Update tests configs

--- testVNext/Entra/Entra.Tests.ps1
+++ testVNext/Entra/Entra.Tests.ps1
@@ -2,8 +2,29 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.Authentication)){
+    Import-Module .\bin\Microsoft.Graph.Entra.Authentication.psd1 -Force
+}
 if($null -eq (Get-Module -Name Microsoft.Graph.Entra.Applications)){
     Import-Module .\bin\Microsoft.Graph.Entra.Applications.psd1 -Force
+}
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.DirectoryManagement)){
+    Import-Module .\bin\Microsoft.Graph.Entra.DirectoryManagement.psd1 -Force
+}
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.Governance)){
+    Import-Module .\bin\Microsoft.Graph.Entra.Governance.psd1 -Force
+}
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.Users)){
+    Import-Module .\bin\Microsoft.Graph.Entra.Users.psd1 -Force
+}
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.Groups)){
+    Import-Module .\bin\Microsoft.Graph.Entra.Groups.psd1 -Force
+}
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.Reports)){
+    Import-Module .\bin\Microsoft.Graph.Entra.Reports.psd1 -Force
+}
+if($null -eq (Get-Module -Name Microsoft.Graph.Entra.SignIns)){
+    Import-Module .\bin\Microsoft.Graph.Entra.SignIns.psd1 -Force
 }
 
 Import-Module Pester
