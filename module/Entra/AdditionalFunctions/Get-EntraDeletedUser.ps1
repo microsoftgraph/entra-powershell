07e86f5e8ec162a84501381a10c776f0f47fbb12
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-11-13 18:11:07 +0300

    Adding code to get deleted users

---
+++ module/Entra/AdditionalFunctions/Get-EntraDeletedUser.ps1
@@ -0,0 +1,111 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+function Get-EntraDeletedUser {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+                
+        [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $Filter,
+                
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [switch] $All,
+                
+        [Parameter(ParameterSetName = "GetVague", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $SearchString,
+        [Alias('Id')]            
+        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.String] $UserId,
+                
+        [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.Nullable`1[System.Int32]] $Top,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
+        [System.String[]] $Property
+    )
+
+    PROCESS {    
+        $params = @{}
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $keysChanged = @{SearchString = "Filter" }
+        if ($null -ne $PSBoundParameters["PipelineVariable"]) {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if ($null -ne $PSBoundParameters["ProgressAction"]) {
+            $params["ProgressAction"] = $PSBoundParameters["ProgressAction"]
+        }
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $PSBoundParameters["Debug"]
+        }
+        if ($null -ne $PSBoundParameters["WarningAction"]) {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        if ($null -ne $PSBoundParameters["ErrorVariable"]) {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if ($null -ne $PSBoundParameters["Filter"]) {
+            $TmpValue = $PSBoundParameters["Filter"]
+            foreach ($i in $keysChanged.GetEnumerator()) {
+                $TmpValue = $TmpValue.Replace($i.Key, $i.Value)
+            }
+            $Value = $TmpValue
+            $params["Filter"] = $Value
+        }
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $PSBoundParameters["Verbose"]
+        }
+        if ($null -ne $PSBoundParameters["ErrorAction"]) {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if ($null -ne $PSBoundParameters["InformationAction"]) {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if ($null -ne $PSBoundParameters["All"]) {
+            if ($PSBoundParameters["All"]) {
+                $params["All"] = $PSBoundParameters["All"]
+            }
+        }
+        if ($null -ne $PSBoundParameters["WarningVariable"]) {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if ($null -ne $PSBoundParameters["InformationVariable"]) {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+        if ($null -ne $PSBoundParameters["SearchString"]) {
+            $TmpValue = $PSBoundParameters["SearchString"]
+            $Value = "mailNickName eq '$TmpValue' or (mail eq '$TmpValue' or (displayName eq '$TmpValue' or startswith(displayName,'$TmpValue')))"
+            $params["Filter"] = $Value
+        }
+        if ($null -ne $PSBoundParameters["UserId"]) {
+            $params["DirectoryObjectId"] = $PSBoundParameters["UserId"]
+        }
+        if ($null -ne $PSBoundParameters["OutBuffer"]) {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if ($null -ne $PSBoundParameters["OutVariable"]) {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if ($PSBoundParameters.ContainsKey("Top")) {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+        if ($null -ne $PSBoundParameters["Property"]) {
+            $params["Property"] = $PSBoundParameters["Property"]
+        }
+
+        Write-Debug("============================ TRANSFORMATIONS ============================")
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+        Write-Debug("=========================================================================`n")
+
+        # Make the API call
+        $response = Get-MgDirectoryDeletedItemAsUser @params -Headers $customHeaders
+
+        # Process the response
+        foreach ($item in $response) {
+            if ($null -ne $item) {
+                $item | Add-Member -MemberType AliasProperty -Name ObjectId -Value $item.Id
+            }
+        }
+
+        $response
+    }    
+}
