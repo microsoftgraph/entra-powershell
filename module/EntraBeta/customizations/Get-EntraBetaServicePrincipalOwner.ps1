d7c87388079ddbff9a06fa63fb2d31c8315676c9
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-11-13 11:38:44 +0300

    Bug fix 1185 - Get-EntraBetaServicePrincipalOwner not returning results (#1188)

--- module/EntraBeta/customizations/Get-EntraBetaServicePrincipalOwner.ps1
+++ module/EntraBeta/customizations/Get-EntraBetaServicePrincipalOwner.ps1
@@ -1,74 +1,78 @@
 # ------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  
+#  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 @{
-    SourceName = "Get-AzureADServicePrincipalOwner"
-    TargetName = $null
-    Parameters = $null
-    Outputs = $null
+    SourceName   = "Get-AzureADServicePrincipalOwner"
+    TargetName   = $null
+    Parameters   = $null
+    Outputs      = $null
     CustomScript = @'
-    function Get-EntraBetaServicePrincipalOwner {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
-    [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.Nullable`1[System.Int32]] $Top,
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [Alias("ObjectId")]
-    [System.String] $ServicePrincipalId,
-    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [switch] $All,
-    [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
-    [System.String[]] $Property
+        [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.Nullable`1[System.Int32]] $Top,
+        
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias("ObjectId")]
+        [System.String] $ServicePrincipalId,
+        
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        # Switch to include all service principal owners
+        [switch] $All,
+        
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true)]
+        [System.String[]] $Property
     )
 
     PROCESS {    
         $params = @{}
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
 
-        if($null -ne $PSBoundParameters["ServicePrincipalId"])
-        {
+        if ($null -ne $PSBoundParameters["ServicePrincipalId"]) {
             $params["ServicePrincipalId"] = $PSBoundParameters["ServicePrincipalId"]
         }
-        if($null -ne $PSBoundParameters["All"])
-        {
-            if($PSBoundParameters["All"])
-            {
+        if ($null -ne $PSBoundParameters["All"]) {
+            if ($PSBoundParameters["All"]) {
                 $params["All"] = $PSBoundParameters["All"]
             }
         }
-        if($PSBoundParameters.ContainsKey("Debug"))
-        {
+        if ($PSBoundParameters.ContainsKey("Debug")) {
             $params["Debug"] = $PSBoundParameters["Debug"]
         }
-        if($PSBoundParameters.ContainsKey("Top"))
-        {
+        if ($PSBoundParameters.ContainsKey("Top")) {
             $params["Top"] = $PSBoundParameters["Top"]
         }
-        if($null -ne $PSBoundParameters["Property"])
-        {
+        if ($null -ne $PSBoundParameters["Property"]) {
             $params["Property"] = $PSBoundParameters["Property"]
+        }
+        # Add common parameters if they exist
+        $commonParams = @("WarningVariable", "InformationVariable", "InformationAction", "OutVariable", "OutBuffer", "ErrorVariable", "PipelineVariable", "ErrorAction", "WarningAction")
+        foreach ($param in $commonParams) {
+            if ($PSBoundParameters.ContainsKey($param)) {
+                $params[$param] = $PSBoundParameters[$param]
+            }
         }
     
         Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
         
         $response = Get-MgBetaServicePrincipalOwner @params -Headers $customHeaders
         $response | ForEach-Object {
-            if($null -ne $_) {
+            if ($null -ne $_) {
                 Add-Member -InputObject $_ -NotePropertyMembers $_.AdditionalProperties
                 Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
-                $propsToConvert = @('appRoles','publishedPermissionScopes')
-                try{
+                $propsToConvert = @('appRoles', 'publishedPermissionScopes')
+                try {
                     foreach ($prop in $propsToConvert) {
                         $value = $_.$prop | ConvertTo-Json -Depth 10 | ConvertFrom-Json
                         $_ | Add-Member -MemberType NoteProperty -Name $prop -Value ($value) -Force
                     }
-                }catch{}  
+                } catch {}
             }
         }
         $response
     }    
-}
 '@
 }
