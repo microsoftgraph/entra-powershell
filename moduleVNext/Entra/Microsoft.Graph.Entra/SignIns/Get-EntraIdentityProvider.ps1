dbff7652fea0d97beb4ccd53f15fd802b36d528d
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-11-14 15:14:38 +0300

    Groups changes

--- moduleVNext/Entra/Microsoft.Graph.Entra/SignIns/Get-EntraIdentityProvider.ps1
+++ moduleVNext/Entra/Microsoft.Graph.Entra/SignIns/Get-EntraIdentityProvider.ps1
@@ -2,6 +2,7 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
+
 function Get-EntraIdentityProvider {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
--- moduleVNext/Entra/Microsoft.Graph.Entra/SignIns/Get-EntraOAuth2PermissionGrant.ps1
+++ moduleVNext/Entra/Microsoft.Graph.Entra/SignIns/Get-EntraOAuth2PermissionGrant.ps1
@@ -2,6 +2,7 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
+
 function Get-EntraOAuth2PermissionGrant {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
