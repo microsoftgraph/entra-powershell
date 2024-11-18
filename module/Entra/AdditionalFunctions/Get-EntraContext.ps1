015ca94ab398e911a0320d80ad6667cf9057c172
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-11-18 18:41:02 +0300

    Get-EntraContext updates

--- module/Entra/AdditionalFunctions/Get-EntraContext.ps1
+++ module/Entra/AdditionalFunctions/Get-EntraContext.ps1
@@ -1,73 +1,72 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-
 function Get-EntraContext {
     [CmdletBinding(DefaultParameterSetName = '')]
     param ()
 
     PROCESS {
         $params = @{}
-        if($null -ne $PSBoundParameters["ErrorAction"])
-        {
+        if ($null -ne $PSBoundParameters["ErrorAction"]) {
             $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
         }
-        if($null -ne $PSBoundParameters["PipelineVariable"])
-        {
+        if ($null -ne $PSBoundParameters["PipelineVariable"]) {
             $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
         }
-        if($null -ne $PSBoundParameters["OutVariable"])
-        {
+        if ($null -ne $PSBoundParameters["OutVariable"]) {
             $params["OutVariable"] = $PSBoundParameters["OutVariable"]
         }
-        if($null -ne $PSBoundParameters["InformationAction"])
-        {
+        if ($null -ne $PSBoundParameters["InformationAction"]) {
             $params["InformationAction"] = $PSBoundParameters["InformationAction"]
         }
-        if($null -ne $PSBoundParameters["WarningVariable"])
-        {
+        if ($null -ne $PSBoundParameters["WarningVariable"]) {
             $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
         }
-        if($PSBoundParameters.ContainsKey("Verbose"))
-        {
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
             $params["Verbose"] = $PSBoundParameters["Verbose"]
         }
-        if($PSBoundParameters.ContainsKey("Debug"))
-        {
+        if ($PSBoundParameters.ContainsKey("Debug")) {
             $params["Debug"] = $PSBoundParameters["Debug"]
         }
-        if($null -ne $PSBoundParameters["Confirm"])
-        {
+        if ($null -ne $PSBoundParameters["Confirm"]) {
             $params["Confirm"] = $PSBoundParameters["Confirm"]
         }
-        if($null -ne $PSBoundParameters["ErrorVariable"])
-        {
+        if ($null -ne $PSBoundParameters["ErrorVariable"]) {
             $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
         }
-        if($null -ne $PSBoundParameters["OutBuffer"])
-        {
+        if ($null -ne $PSBoundParameters["OutBuffer"]) {
             $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
         }
-        if($null -ne $PSBoundParameters["WarningAction"])
-        {
+        if ($null -ne $PSBoundParameters["WarningAction"]) {
             $params["WarningAction"] = $PSBoundParameters["WarningAction"]
         }
-        if($null -ne $PSBoundParameters["WhatIf"])
-        {
+        if ($null -ne $PSBoundParameters["WhatIf"]) {
             $params["WhatIf"] = $PSBoundParameters["WhatIf"]
         }
-        if($null -ne $PSBoundParameters["InformationVariable"])
-        {
+        if ($null -ne $PSBoundParameters["InformationVariable"]) {
             $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
         }
-    
+
         Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
         Write-Debug("=========================================================================`n")
-        
+
         $response = Get-MgContext @params
+
+        if ($null -ne (Get-Module -Name Microsoft.Graph.Entra.Beta)) {
+            $entraVersion = (Get-Module -Name Microsoft.Graph.Entra.Beta | Select-Object -ExpandProperty Version).ToString()
+            $entraModuleName = "Microsoft.Graph.Entra.Beta"
+        }
+        elseif ($null -ne (Get-Module -Name Microsoft.Graph.Entra)) {
+            $entraVersion = (Get-Module -Name Microsoft.Graph.Entra | Select-Object -ExpandProperty Version).ToString()
+            $entraModuleName = "Microsoft.Graph.Entra"
+        }
+
+        $response | Add-Member -MemberType NoteProperty -Name "EntraPowerShellName" -Value $entraModuleName
+        $response | Add-Member -MemberType NoteProperty -Name "EntraPowerShellVersion" -Value $entraVersion
+
         $response
     }
 }
-Set-Alias -Name Get-EntraCurrentSessionInfo -Value Get-EntraContext -Scope Global -Force
 
+Set-Alias -Name Get-EntraCurrentSessionInfo -Value Get-EntraContext -Scope Global -Force
