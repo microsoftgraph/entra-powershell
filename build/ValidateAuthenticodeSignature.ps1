d9bde83ce208cdf2d1f886bdf4f6ce88fac2c2aa
Kennedy Kangethe Munga <kemunga@microsoft.com>
2024-11-19 11:33:44 +0300

    Enable suppression file

--- build/ValidateAuthenticodeSignature.ps1
+++ build/ValidateAuthenticodeSignature.ps1
@@ -1,8 +1,8 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
 
-# [cmdletbinding()]
-# param (
-#     [string]$Module = "Entra"
-# )
+Set-StrictMode -Version 5
 
 . "$psscriptroot/common-functions.ps1"
 
