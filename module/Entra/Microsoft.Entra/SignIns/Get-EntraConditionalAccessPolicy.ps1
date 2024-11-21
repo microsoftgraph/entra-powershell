aeb37c7885c2c461171c957b697f7236df62945a
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-11-21 20:50:24 +0300

    Spacing issue fix

--- module/Entra/Microsoft.Entra/SignIns/Get-EntraConditionalAccessPolicy.ps1
+++ module/Entra/Microsoft.Entra/SignIns/Get-EntraConditionalAccessPolicy.ps1
@@ -2,6 +2,9 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
+
+
+
 function Get-EntraConditionalAccessPolicy {
     [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
