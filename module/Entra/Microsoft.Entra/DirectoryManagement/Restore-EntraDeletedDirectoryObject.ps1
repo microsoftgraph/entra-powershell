3f9b3e18f56d5a408f2caee0fe8b45fc8908950d
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2024-11-21 19:28:39 +0300

    fixes

--- module/Entra/Microsoft.Entra/DirectoryManagement/Restore-EntraDeletedDirectoryObject.ps1
+++ module/Entra/Microsoft.Entra/DirectoryManagement/Restore-EntraDeletedDirectoryObject.ps1
@@ -3,6 +3,8 @@
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
 
+
+
 function Restore-EntraDeletedDirectoryObject {
     [CmdletBinding(DefaultParameterSetName = '')]
     param (
