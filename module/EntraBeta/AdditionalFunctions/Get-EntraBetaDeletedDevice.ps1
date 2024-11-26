3201e762033e2934fafef01ad27900971aeeca9b
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2024-11-26 10:20:24 +0300

    Adding beta code

---
+++ module/EntraBeta/AdditionalFunctions/Get-EntraBetaDeletedDevice.ps1
@@ -0,0 +1,113 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+#  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+function Get-EntraBetaDeletedDevice {
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    param (
+        [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Filter to apply to the query.")]
+        [System.String] $Filter,
+
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Retrieve all deleted devices.")]
+        [switch] $All,
+
+        [Parameter(ParameterSetName = "GetVague", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Search for devices.")]
+        [System.String] $SearchString,
+
+        [Alias('Id')]
+        [Parameter(ParameterSetName = "GetById", Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Device Object ID to retrieve.")]
+        [System.String] $DeviceObjectId,
+
+        [Alias('Limit')]
+        [Parameter(ParameterSetName = "GetQuery", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Maximum number of results to return.")]
+        [System.Nullable`1[System.Int32]] $Top,
+
+        [Alias('Select')]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "Properties to include in the results.")]
+        [System.String[]] $Property
+    )
+
+    PROCESS {
+        $params = @{}
+        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
+        $keysChanged = @{ SearchString = "Filter" }
+
+        if ($null -ne $PSBoundParameters["ErrorAction"]) {
+            $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
+        }
+        if ($null -ne $PSBoundParameters["DeviceObjectId"]) {
+            $params["DirectoryObjectId"] = $PSBoundParameters["DeviceObjectId"]
+        }
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $PSBoundParameters["Verbose"]
+        }
+        if ($null -ne $PSBoundParameters["OutVariable"]) {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
+        }
+        if ($null -ne $PSBoundParameters["InformationAction"]) {
+            $params["InformationAction"] = $PSBoundParameters["InformationAction"]
+        }
+        if ($null -ne $PSBoundParameters["WarningVariable"]) {
+            $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
+        }
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $PSBoundParameters["Debug"]
+        }
+        if ($null -ne $PSBoundParameters["PipelineVariable"]) {
+            $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
+        }
+        if ($null -ne $PSBoundParameters["SearchString"]) {
+            $TmpValue = $PSBoundParameters["SearchString"]
+            $Value = "displayName eq '$TmpValue' or startsWith(displayName,'$TmpValue')"
+            $params["Filter"] = $Value
+        }
+        if ($null -ne $PSBoundParameters["ErrorVariable"]) {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        }
+        if ($null -ne $PSBoundParameters["Top"]) {
+            $params["Top"] = $PSBoundParameters["Top"]
+        }
+        if ($null -ne $PSBoundParameters["OutBuffer"]) {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        }
+        if ($null -ne $PSBoundParameters["All"]) {
+            if ($PSBoundParameters["All"]) {
+                $params["All"] = $PSBoundParameters["All"]
+            }
+        }
+        if ($null -ne $PSBoundParameters["WarningAction"]) {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        if ($null -ne $PSBoundParameters["Filter"]) {
+            $TmpValue = $PSBoundParameters["Filter"]
+            foreach ($i in $keysChanged.GetEnumerator()) {
+                $TmpValue = $TmpValue.Replace($i.Key, $i.Value)
+            }
+            $Value = $TmpValue
+            $params["Filter"] = $Value
+        }
+        if ($null -ne $PSBoundParameters["InformationVariable"]) {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+        if ($null -ne $PSBoundParameters["Property"]) {
+            $params["Property"] = $PSBoundParameters["Property"]
+        }
+
+        # Debug logging for transformations
+        Write-Debug "============================ TRANSFORMATIONS ============================"
+        $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+        Write-Debug "=========================================================================`n"
+
+        try {
+            # Make the API call
+            $response = Get-MgBetaDirectoryDeletedItemAsDevice @params -Headers $customHeaders        
+
+            return $response
+        }
+        catch {
+            # Handle any errors that occur during the API call
+            Write-Error "An error occurred while retrieving the deleted devices: $_"
+        }
+    }
+}
