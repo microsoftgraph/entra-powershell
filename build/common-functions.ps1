5dba6c2a491dcd83174ba1dd610cb4623eac4b28
Steve Mutungi <stevemutungi@microsoft.com>
2024-11-26 16:25:55 +0300

    common-fxns-cross-plat

--- build/common-functions.ps1
+++ build/common-functions.ps1
@@ -1,30 +1,32 @@
 # ------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+#  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 Set-StrictMode -Version 5
 
-$script:_config = Import-PowerShellDataFile -Path "$PSScriptRoot\build.config.psd1"
+$script:_config = Import-PowerShellDataFile -Path (Join-Path -Path $PSScriptRoot -ChildPath "build.config.psd1")
 
 function Get-ConfigValue {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory)]
-		[ArgumentCompleter({ (Import-PowerShellDataFile -Path "$PSScriptRoot\build.config.psd1").Keys })]
+		[ArgumentCompleter({ (Import-PowerShellDataFile -Path (Join-Path -Path $PSScriptRoot -ChildPath "build.config.psd1")).Keys })]
 		[string]
 		$Name
 	)
 
 	$script:_config[$Name]
 }
+
 function Get-ModuleManifestFile {
 	Get-ChildItem (Get-ModuleBasePath) -Filter '*.psd1'
 }
+
 function Get-ModuleName {
 	Get-ChildItem (Get-ModuleBasePath) -Filter '*.psd1' | Select-Object -ExpandProperty basename
 }
 
 function Get-ModuleBasePath {
-	Join-Path (Split-Path -Parent $psscriptroot) (Get-ConfigValue -Name OutputPath)
+	Join-Path (Split-Path -Parent $PSScriptRoot) (Get-ConfigValue -Name OutputPath)
 }
 
 function Get-ModuleVersion {
@@ -44,7 +46,7 @@ function Get-LocalPSRepoName {
 }
 
 function Get-SourceRootDirectory {
-    (Get-Item (Split-Path -Parent $psscriptroot)).fullname
+    (Get-Item (Split-Path -Parent $PSScriptRoot)).FullName
 }
 
 function Get-OutputDirectory {
@@ -56,15 +58,14 @@ function Get-DevRepoDirectory {
 }
 
 function Remove-BuildDirectories {
-
 	$binPath = Get-OutputDirectory
-	if ( Test-Path $binPath ) {
-		$binPath | Remove-Item -Recurse -Force 
+	if (Test-Path $binPath) {
+		Remove-Item -Recurse -Force -Path $binPath
 	}
 
 	$devRepoLocation = Get-DevRepoDirectory
 	if (Test-Path $devRepoLocation) {
-		$devRepoLocation | Remove-Item -Recurse -Force
+		Remove-Item -Recurse -Force -Path $devRepoLocation
 	}
 }
 
@@ -76,20 +77,20 @@ function Register-LocalGallery {
 	$repoPath = Get-DevRepoDirectory
 	if ($Path) {
 		$repoPath = $Path
-	}    
-	if (-not(Test-Path $repoPath)) {
-		$null = New-Item -Path $repoPath -ItemType Directory
+	}
+	if (-not (Test-Path $repoPath)) {
+		New-Item -Path $repoPath -ItemType Directory | Out-Null
 	}
 
-	$null = Register-PSRepository -Name (Get-LocalPSRepoName) -SourceLocation ($repoPath) -ScriptSourceLocation ($repoPath) -InstallationPolicy Trusted
+	Register-PSRepository -Name (Get-LocalPSRepoName) -SourceLocation $repoPath -ScriptSourceLocation $repoPath -InstallationPolicy Trusted | Out-Null
 }
 
-function Unregister-LocalGallery {    
-	$null = Unregister-PSRepository (Get-LocalPSRepoName)
+function Unregister-LocalGallery {
+	Unregister-PSRepository -Name (Get-LocalPSRepoName) | Out-Null
 }
 
 function Update-ModuleVersion {
-	[cmdletbinding()]
+	[CmdletBinding()]
 	param(
 		[switch] $Minor,
 		[switch] $Major,
@@ -97,12 +98,12 @@ function Update-ModuleVersion {
 	)
 
 	$version = Get-ModuleVersion
-	$v = @{        
+	$v = @{
 		Major = $version.Major
 		Minor = $version.Minor
-		Build = $version.Build            
+		Build = $version.Build
 	}
- 
+
 	if ($Build.IsPresent) {
 		$v.Build++
 	}
@@ -115,107 +116,103 @@ function Update-ModuleVersion {
 		$v.Major++
 	}
 
-	$ver = $v.Major, $v.Minor, $v.Build -Join "."
+	$ver = "$($v.Major).$($v.Minor).$($v.Build)"
 	Update-ModuleManifest -Path (Get-ModuleManifestFile).FullName -ModuleVersion $ver
 }
 
 function Create-ModuleFolder {
-	[cmdletbinding()]
+	[CmdletBinding()]
 	param(
-		[string]$OutputDirectory,
-		[switch]$NoClean
+		[string] $OutputDirectory,
+		[switch] $NoClean
 	)
 
-	if ( -not $OutputDirectory ) {
-		$OutputDirectory = Join-Path (Split-Path -Parent $psscriptroot) (Get-ConfigValue -Name OutputPath)
+	if (-not $OutputDirectory) {
+		$OutputDirectory = Join-Path (Split-Path -Parent $PSScriptRoot) (Get-ConfigValue -Name OutputPath)
 	}
 
 	$modulesDirectory = Join-Path $OutputDirectory (Get-ConfigValue -Name ModuleOutputSubdirectoryName)
 
-	if ( (Test-Path $modulesDirectory) -and -not $noclean.ispresent ) {
-		$null = Remove-Item -Recurse -Force $modulesDirectory
+	if ((Test-Path $modulesDirectory) -and -not $NoClean.IsPresent) {
+		Remove-Item -Recurse -Force -Path $modulesDirectory
 	}
 
 	$thisModuleDirectory = Join-Path $modulesDirectory (Get-ModuleName)
-	$targetDirectory = Join-Path $thisModuleDirectory (Get-ModuleVersion).tostring()
+	$targetDirectory = Join-Path $thisModuleDirectory (Get-ModuleVersion).ToString()
 
+	New-Item -Path $targetDirectory -ItemType Directory | Out-Null
 
-	$null = New-Item -Path $targetDirectory -ItemType Directory
-
-	$ignorableSegmentCount = ((Get-ModuleBasePath).replace("`\", '/') -split '/').count
+	$ignorableSegmentCount = ((Get-ModuleBasePath).Replace("\", '/') -split '/').Count
 	$sourceFileList = @()
 	$destinationFileList = @()
 	Get-ModuleFiles | ForEach-Object {
-		$normalizedFile = $_.replace("`\", '/')
+		$normalizedFile = $_.Replace("\", '/')
 		$segments = $normalizedFile -split '/'
-		$relativeSegments = $segments[$ignorableSegmentCount..($segments.length - 1)]
+		$relativeSegments = $segments[$ignorableSegmentCount..($segments.Length - 1)]
 		$relativePath = $relativeSegments -join '/'
 
 		$sourceFileList += Join-Path (Get-ModuleBasePath) $relativePath
 		$destinationFileList += Join-Path $targetDirectory $relativePath
 	}
 
-	0..($sourceFileList.length - 1) | ForEach-Object {
-		$parent = Split-Path -Parent $destinationFileList[ $_ ]
-		if ( -not (Test-Path $parent) ) {
-			$null = New-Item -Path $parent -ItemType Directory
+	0..($sourceFileList.Length - 1) | ForEach-Object {
+		$parent = Split-Path -Parent $destinationFileList[$_]
+		if (-not (Test-Path $parent)) {
+			New-Item -Path $parent -ItemType Directory | Out-Null
 		}
 
-		$destinationName = Split-Path -Leaf $destinationFileList[ $_ ]
-		$syntaxOnlySourceName = Split-Path -Leaf $sourceFileList[ $_ ]
-		$sourceActualName = (Get-ChildItem (Split-Path -Parent $sourceFileList[ $_ ]) -Filter $syntaxOnlySourceName).name
+		$destinationName = Split-Path -Leaf $destinationFileList[$_]
+		$syntaxOnlySourceName = Split-Path -Leaf $sourceFileList[$_]
+		$sourceActualName = (Get-ChildItem (Split-Path -Parent $sourceFileList[$_]) -Filter $syntaxOnlySourceName).Name
 
-		if ( $destinationName -cne $sourceActualName ) {
+		if ($destinationName -cne $sourceActualName) {
 			throw "The case-sensitive name of the file at source path '$($sourceFileList[$_])' is actually '$sourceActualName' and it does not match the case of the last element of destination path '$($destinationFileList[$_])' -- the case of the file names must match exactly in order to support environments with case-sensitive file systems. This can be corrected in the module manifest by specifying the case of the file exactly as it exists in the module source code directory"
 		}
 
-		Copy-Item $sourceFileList[ $_ ] $destinationFileList[ $_ ]
+		Copy-Item -Path $sourceFileList[$_] -Destination $destinationFileList[$_]
 	}
 }
 
 function Get-CustomizationFiles {
-	[cmdletbinding()]
+	[CmdletBinding()]
 	param(
-		[string]
-		$Module = "Entra",
-
-		[string]
-		$Directory
+		[string] $Module = "Entra",
+		[string] $Directory
 	)
 
-	$path = Split-Path -Parent $psscriptroot
+	$path = Split-Path -Parent $PSScriptRoot
 
-	if ( -not $Directory ) {
+	if (-not $Directory) {
 		$path = Join-Path $path 'module'
 		$path = Join-Path $path $Module
 		$path = Join-Path $path (Get-ConfigValue -Name CustomizationPath)
 	}
-	else {
-		$path = Join-Path $path 'module'      
+ else {
+		$path = Join-Path $path 'module'
 		$path = Join-Path $path $Module
 		$path = Join-Path $path $Directory
 	}
+
 	$customizationFileList = @()
 	$files = Get-ChildItem -Path $path -Filter '*.ps1'
 	foreach ($file in $files) {
-		$customizationFileList += $file.FullName        
+		$customizationFileList += $file.FullName
 	}
-    
+
 	$customizationFileList
 }
 
 function Create-ModuleHelp {
 	param (
-		[string]
-		$Module = "Entra"
+		[string] $Module = "Entra"
 	)
 
-	$binPath = Join-Path (Split-Path -Parent $psscriptroot) (Get-ConfigValue -Name OutputPath)
-	if(!(Test-Path $binPath)){
+	$binPath = Join-Path (Split-Path -Parent $PSScriptRoot) (Get-ConfigValue -Name OutputPath)
+	if (-not (Test-Path $binPath)) {
 		New-Item -ItemType Directory -Path $binPath | Out-Null
 	}
-	$moduleDocsPath = Join-Path (Split-Path -Parent $psscriptroot) (Get-ConfigValue -Name ModuleSubdirectoryName)
-	$moduleDocsPath = Join-Path ($moduleDocsPath) (Get-ConfigValue -Name docsPath)
-	$moduleDocsPath = Join-Path ($moduleDocsPath) (Get-ConfigValue -Name ($Module + "Path"))
+	$moduleDocsPath = Join-Path (Split-Path -Parent $PSScriptRoot) (Get-ConfigValue -Name ModuleSubdirectoryName)
+	$moduleDocsPath = Join-Path $moduleDocsPath (Get-ConfigValue -Name docsPath)
+	$moduleDocsPath = Join-Path $moduleDocsPath (Get-ConfigValue -Name ($Module + "Path"))
 	New-ExternalHelp -Path $moduleDocsPath -OutputPath $binPath -Force
 }
