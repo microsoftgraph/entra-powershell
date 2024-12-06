494bada2b36cfc78a98af22bc2f96239dcb3707d
stevemutungi <stevemutungi@microsoft.com>
2024-12-06 10:30:17 +0300

    Adding comments

--- module/Entra/AdditionalFunctions/Connect-Entra.ps1
+++ module/Entra/AdditionalFunctions/Connect-Entra.ps1
@@ -1,7 +1,13 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-$cmd = Get-Command  Connect-MgGraph -CommandType cmdlet
+$cmd = Get-Command -Name Connect-MgGraph -CommandType Cmdlet
+
+# Create metadata object based on the original command
 $metaData = New-Object System.Management.Automation.CommandMetaData $cmd
 
-[System.Management.Automation.ProxyCommand]::Create($metaData) | Set-Item Path function:Connect-Entra
+# Generate a proxy command for Connect-Entra using the metadata
+$proxyCommand = [System.Management.Automation.ProxyCommand]::Create($metaData)
+
+# Set the generated proxy command as a new function named Connect-Entra
+Set-Item -Path Function:Connect-Entra -Value $proxyCommand
