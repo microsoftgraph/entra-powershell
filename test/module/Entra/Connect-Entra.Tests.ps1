3b59bcebdce2f3f6b2e433199d51e9dc7b3a2af5
stevemutungi <stevemutungi@microsoft.com>
2024-12-06 22:15:15 +0300

    Formatting for readability

--- test/module/Entra/Connect-Entra.Tests.ps1
+++ test/module/Entra/Connect-Entra.Tests.ps1
@@ -2,9 +2,9 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
-BeforeAll{
-    if($null -eq (Get-Module -Name Microsoft.Graph.Entra)){
-        Import-Module Microsoft.Graph.Entra      
+BeforeAll {
+    if ($null -eq (Get-Module -Name Microsoft.Graph.Entra)) {
+        Import-Module Microsoft.Graph.Entra
     }
 
     Mock -CommandName Connect-MgGraph -MockWith {} -ModuleName Microsoft.Graph.Entra
@@ -12,31 +12,20 @@ BeforeAll{
     $ConnectEntraCommand = Get-Command Connect-Entra
 }
 
-Describe "Connect-Entra Mock"{
-    It "should return empty object"{
-        $result = Connect-Entra -TenantId "aaaabbbb-0000-cccc-1111-dddd2222eeee" -ApplicationId "00001111-aaaa-2222-bbbb-3333cccc4444"
-        $result | Should -BeNullOrEmpty
-        Should -Invoke -CommandName Connect-MgGraph -ModuleName Microsoft.Graph.Entra -Times 1
-    }
+Describe "Connect-Entra Mock" {
 
-    It "Should connect to an environment as a different identity"{
-        $result = Connect-Entra -ContextScope "Process"
-        $result | Should -BeNullOrEmpty
-        Should -Invoke -CommandName Connect-MgGraph -ModuleName Microsoft.Graph.Entra -Times 1
-    }
-
-    It "Should return error when TenantId is null"{
+    It "Should return error when TenantId is null" {
         { Connect-Entra -TenantId } | Should -Throw "Missing an argument for parameter 'TenantId'*"
     }
-    It "Should return error when Environment is null"{
+    It "Should return error when Environment is null" {
         { Connect-Entra -Environment } | Should -Throw "Missing an argument for parameter 'Environment'*"
     }
-    It "Should return error when invalid parameter is provided"{
+    It "Should return error when invalid parameter is provided" {
         { Connect-Entra -DisplayName } | Should -Throw "A parameter cannot be found that matches parameter name 'DisplayName'*"
     }
 }
 
-Describe "Connect-Entra ParameterSets"{
+Describe "Connect-Entra ParameterSets" {
     It 'Should have six ParameterSets' {
         $ConnectEntraCommand | Should -Not -BeNullOrEmpty
         $ConnectEntraCommand.ParameterSets | Should -HaveCount 6
@@ -51,7 +40,7 @@ Describe "Connect-Entra ParameterSets"{
     It 'Should have AppCertificateParameterSet' {
         $AppCertificateParameterSet = $ConnectEntraCommand.ParameterSets | Where-Object Name -eq 'AppCertificateParameterSet'
         $AppCertificateParameterSet | Should -Not -BeNull
-        @('ClientId', 'TenantId', 'CertificateSubjectName', 'CertificateThumbprint', 'ContextScope', 'Environment', 'ClientTimeout') | Should -BeIn $AppCertificateParameterSet.Parameters.Name                
+        @('ClientId', 'TenantId', 'CertificateSubjectName', 'CertificateThumbprint', 'ContextScope', 'Environment', 'ClientTimeout') | Should -BeIn $AppCertificateParameterSet.Parameters.Name
     }
 
     It 'Should have AppSecretCredentialParameterSet' {
@@ -77,16 +66,16 @@ Describe "Connect-Entra ParameterSets"{
     }
 
     It "Should execute successfully without throwing an error" {
-        # Disable confirmation prompts       
+        # Disable confirmation prompts
         $originalDebugPreference = $DebugPreference
         $DebugPreference = 'Continue'
-        
+
         try {
             # Act & Assert: Ensure the function doesn't throw an exception
-            {  Connect-Entra -TenantId "aaaabbbb-0000-cccc-1111-dddd2222eeee" -ApplicationId "00001111-aaaa-2222-bbbb-3333cccc4444" -Debug } | Should -Not -Throw
+            { Connect-Entra -TenantId "aaaabbbb-0000-cccc-1111-dddd2222eeee" -ApplicationId "00001111-aaaa-2222-bbbb-3333cccc4444" -Debug } | Should -Not -Throw
         } finally {
-            # Restore original confirmation preference            
-            $DebugPreference = $originalDebugPreference        
+            # Restore original confirmation preference
+            $DebugPreference = $originalDebugPreference
         }
-    } 
+    }
 }
