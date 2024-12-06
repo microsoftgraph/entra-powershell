1873d2218ef389d791151b81d8ce3c8e44a0a3f0
stevemutungi <stevemutungi@microsoft.com>
2024-12-06 10:26:12 +0300

    Fix - typo

--- module/Entra/AdditionalFunctions/Connect-Entra.ps1
+++ module/Entra/AdditionalFunctions/Connect-Entra.ps1
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-$cmd = Get-Command  Connect-MgGraph -CommandType cmdlets
+$cmd = Get-Command  Connect-MgGraph -CommandType cmdlet
 $metaData = New-Object System.Management.Automation.CommandMetaData $cmd
 
 [System.Management.Automation.ProxyCommand]::Create($metaData) | Set-Item Path function:Connect-Entra
