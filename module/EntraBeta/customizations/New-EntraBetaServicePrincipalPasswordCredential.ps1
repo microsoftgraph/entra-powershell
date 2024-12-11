f4e694d5b2d42c8aa54b4153e8cb84c7eb3f615b
stevemutungi <stevemutungi@microsoft.com>
2024-12-11 10:53:25 +0300

    Formatting for readability

--- module/EntraBeta/customizations/New-EntraBetaServicePrincipalPasswordCredential.ps1
+++ module/EntraBeta/customizations/New-EntraBetaServicePrincipalPasswordCredential.ps1
@@ -8,71 +8,82 @@
     Outputs = $null
     CustomScript = @'
     function New-EntraBetaServicePrincipalPasswordCredential {
-    [CmdletBinding(DefaultParameterSetName = '')]
-    param (
-    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.String] $CustomKeyIdentifier,
-    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.Nullable`1[System.DateTime]] $StartDate,
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [Alias("ObjectId")]
-    [System.String] $ServicePrincipalId,
-    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.String] $Value,
-    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.Nullable`1[System.DateTime]] $EndDate,
-    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.String] $DisplayName
-    )
+        [CmdletBinding(DefaultParameterSetName = '')]
+        param (
+            [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [System.String] $CustomKeyIdentifier,
+            [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [System.Nullable`1[System.DateTime]] $StartDate,
+            [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [Alias("ObjectId")]
+            [System.String] $ServicePrincipalId,
+            [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [System.String] $Value,
+            [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [System.Nullable`1[System.DateTime]] $EndDate,
+            [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [System.String] $DisplayName
+        )
 
-    PROCESS{
-        $params = @{}
-        $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-        $baseUri = 'https://graph.microsoft.com/beta/servicePrincipals'
-        $Method = "POST"
-        if($null -ne $PSBoundParameters["ServicePrincipalId"])
-        {
-            $params["ServicePrincipalId"] = $PSBoundParameters["ServicePrincipalId"]
-            $params["StartDate"] = $PSBoundParameters["StartDate"]
-            $params["EndDate"] = $PSBoundParameters["EndDate"]
-            $params["DisplayName"] = $PSBoundParameters["DisplayName"]
+        PROCESS {
+            $params = @{}
+            $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
 
-            $URI = "$baseUri/$($params.ServicePrincipalId)/addPassword"
+            if ($null -ne $PSBoundParameters["ServicePrincipalId"]) {
+                $params["ServicePrincipalId"] = $PSBoundParameters["ServicePrincipalId"]
+            }
+            if ($null -ne $PSBoundParameters["DisplayName"]) {
+                $params["DisplayName"] = $PSBoundParameters["DisplayName"]
+            }
+            if ($null -ne $PSBoundParameters["StartDate"]) {
+                $params["StartDate"] = $PSBoundParameters["StartDate"]
+            }
+            if ($null -ne $PSBoundParameters["EndDate"]) {
+                $params["EndDate"] = $PSBoundParameters["EndDate"]
+            }
+            if ($null -ne $PSBoundParameters["Value"]) {
+                $params["Value"] = $PSBoundParameters["Value"]
+            }
+            if ($null -ne $PSBoundParameters["CustomKeyIdentifier"]) {
+                $params["CustomKeyIdentifier"] = $PSBoundParameters["CustomKeyIdentifier"]
+            }
+
+            $params["Url"] = "https://graph.microsoft.com/beta/servicePrincipals/$ServicePrincipalId/addPassword"
             $body = @{
                 passwordCredential = @{
                     startDateTime = $PSBoundParameters["StartDate"];
                     endDateTime = $PSBoundParameters["EndDate"];
                     displayName = $PSBoundParameters["DisplayName"];
                 }
+            } | ConvertTo-Json -Depth 10
+
+            Write-Debug("============================ TRANSFORMATIONS ============================")
+            $params.Keys | ForEach-Object { "$_ : $($params[$_])" } | Write-Debug
+            Write-Debug("=========================================================================`n")
+
+            $response = Invoke-GraphRequest -Headers $customHeaders -Uri $params.Url -Method POST -Body $body -ContentType "application/json"
+
+            $response = $response | ConvertTo-Json | ConvertFrom-Json
+
+            $response | ForEach-Object {
+                if ($null -ne $_) {
+                    Add-Member -InputObject $_ -MemberType AliasProperty -Name StartDate -Value StartDateTime
+                    Add-Member -InputObject $_ -MemberType AliasProperty -Name EndDate -Value EndDateTime
+                }
             }
-        }
 
-        Write-Debug("============================ TRANSFORMATIONS ============================")
-        $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-        Write-Debug("=========================================================================`n")
-
-        $response = (Invoke-GraphRequest -Headers $customHeaders -Uri $URI -Method $Method -Body $body)
-        $response = $response | ConvertTo-Json -Depth 10 | ConvertFrom-Json
-
-        $response | ForEach-Object {
-            if($null -ne $_) {
-            Add-Member -InputObject $_ -MemberType AliasProperty -Name StartDate -Value StartDateTime
-            Add-Member -InputObject $_ -MemberType AliasProperty -Name EndDate -Value EndDateTime
+            $targetTypeList = @()
+            foreach ($data in $response) {
+                $target = New-Object Microsoft.Graph.Beta.PowerShell.Models.MicrosoftGraphPasswordCredential
+                $data.PSObject.Properties | ForEach-Object {
+                    $propertyName = $_.Name.Substring(0, 1).ToUpper() + $_.Name.Substring(1)
+                    $propertyValue = $_.Value
+                    $target | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+                }
+                $targetTypeList += $target
             }
+            $targetTypeList
         }
-
-        $targetTypeList = @()
-        foreach($data in $response){
-            $target = New-Object Microsoft.Graph.Beta.PowerShell.Models.MicrosoftGraphPasswordCredential
-            $data.PSObject.Properties | ForEach-Object {
-                $propertyName = $_.Name.Substring(0,1).ToUpper() + $_.Name.Substring(1)
-                $propertyValue = $_.Value
-                $target | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
-            }
-            $targetTypeList += $target
-        }
-        $targetTypeList
-    }    
-}
+    }
 '@
 }
