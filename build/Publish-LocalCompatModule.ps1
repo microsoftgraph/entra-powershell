550a86e779858a79cd4ed91014f3870c092558d9
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2025-01-08 18:48:57 +0300

    Remove only Entra dependencies

--- build/Publish-LocalCompatModule.ps1
+++ build/Publish-LocalCompatModule.ps1
@@ -35,9 +35,11 @@ $metadataPath = "$PSScriptRoot/../module/$ModuleName/config/ModuleMetadata.json"
 $metadata = Get-Content -Path $metadataPath | ConvertFrom-Json
 
 foreach ($destinationModuleName in $content.destinationModuleName){
-	if(Get-Module -ListAvailable -Name $destinationModuleName){
+	if($moduleName -eq 'Entra'){
+        if(Get-Module -ListAvailable -Name $destinationModuleName){
           Uninstall-Module -Name $destinationModuleName -Force -Verbose
 	}
+  }	
 }
 
 if($moduleName -eq 'Entra'){
