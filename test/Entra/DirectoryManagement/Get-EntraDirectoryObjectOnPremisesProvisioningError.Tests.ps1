893f4ec57f4da98b527fabb0b3c04f4e13262707
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2025-01-08 12:12:20 +0300

    Fixed scriptblock

--- test/Entra/DirectoryManagement/Get-EntraDirectoryObjectOnPremisesProvisioningError.Tests.ps1
+++ test/Entra/DirectoryManagement/Get-EntraDirectoryObjectOnPremisesProvisioningError.Tests.ps1
@@ -11,7 +11,7 @@ BeforeAll {
         return @{
             value = @(
                  @{
-                    onPremisesProvisioningErrors = @()
+                    "onPremisesProvisioningErrors"= @()
                  }
             )
         }
