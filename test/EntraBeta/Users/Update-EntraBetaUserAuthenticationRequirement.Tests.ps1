8079de45b01fbdcf59a33e428707a5ec0afa0d18
stevemutungi <stevemutungi@microsoft.com>
2025-01-13 21:21:55 +0300

    Hotfix - Update-EntraBetaUserAuthenticationRequirement

---
+++ test/EntraBeta/Users/Update-EntraBetaUserAuthenticationRequirement.Tests.ps1
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+#  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+BeforeAll {  
+    if (-not (Get-Module -Name Microsoft.Entra.Beta.Users -ListAvailable)) {
+        Import-Module Microsoft.Entra.Beta.Users
+    }
+    Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
+
+    Mock -CommandName Invoke-GraphRequest -MockWith {} -ModuleName Microsoft.Entra.Beta.Users
+}
+
+Describe "Update-EntraBetaUserAuthenticationRequirement" {
+    Context "Test for Update-EntraBetaUserAuthenticationRequirement" {
+        It "Should return empty object" {
+            $result = Update-EntraBetaUserAuthenticationRequirement -UserId "SawyerM@Contoso.com" -PerUserMfaState "enabled"
+            $result | Should -BeNullOrEmpty
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Beta.Users -Times 1
+        }
+
+        It "Should fail when UserId is empty" {
+            { Update-EntraBetaUserAuthenticationRequirement -UserId -PerUserMfaState "enabled" } | Should -Throw "Missing an argument for parameter 'UserId'. Specify a parameter*"
+        }
+
+        It "Should fail when PerUserMfaState is empty" {
+            { Update-EntraBetaUserAuthenticationRequirement -UserId "SawyerM@Contoso.com" -PerUserMfaState } | Should -Throw "Missing an argument for parameter 'PerUserMfaState'. Specify a parameter*"
+        }
+
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Update-EntraBetaUserAuthenticationRequirement"
+
+            $result = Update-EntraBetaUserAuthenticationRequirement -UserId "SawyerM@Contoso.com" -PerUserMfaState "enabled"
+            $result | Should -BeNullOrEmpty
+
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Update-EntraBetaUserAuthenticationRequirement"
+
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Beta.Users -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }
+        }
+
+        It "Should execute successfully without throwing an error" {
+            # Disable confirmation prompts       
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+    
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { Update-EntraBetaUserAuthenticationRequirement -UserId "SawyerM@Contoso.com" -PerUserMfaState "enabled" -Debug } | Should -Not -Throw
+            }
+            finally {
+                # Restore original confirmation preference            
+                $DebugPreference = $originalDebugPreference        
+            }
+        }
+    }
+}
