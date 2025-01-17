debbe3e501712a9df4e2dc5334a144b7cbfeef45
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-01-17 09:20:13 +0300

    [Modularize] Enhancement - Connect-Entra as a proxy function (#1272)
    * Adding proxy command for Connect-Entra
    
    * Fixing module dependency
    
    ---------
    
    Co-authored-by: stevemutungi <stevemutungi@microsoft.com>

--- test/Entra/Authentication/Connect-Entra.Tests.ps1
+++ test/Entra/Authentication/Connect-Entra.Tests.ps1
@@ -2,9 +2,9 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
-BeforeAll{
-    if($null -eq (Get-Module -Name Microsoft.Entra.Authentication)){
-        Import-Module Microsoft.Entra.Authentication      
+BeforeAll {
+    if ($null -eq (Get-Module -Name Microsoft.Entra.Authentication)) {
+        Import-Module Microsoft.Entra.Authentication
     }
 
     Mock -CommandName Connect-MgGraph -MockWith {} -ModuleName Microsoft.Entra.Authentication
@@ -12,39 +12,20 @@ BeforeAll{
     $ConnectEntraCommand = Get-Command Connect-Entra
 }
 
-Describe "Connect-Entra Mock"{
-    It "should return empty object"{
-        $result = Connect-Entra -TenantId "aaaabbbb-0000-cccc-1111-dddd2222eeee" -ApplicationId "00001111-aaaa-2222-bbbb-3333cccc4444" -CertificateThumbprint "0a0a0a0a-1111-bbbb-2222-3c3c3c3c3c3c"
-        $result | Should -BeNullOrEmpty
-        Should -Invoke -CommandName Connect-MgGraph -ModuleName Microsoft.Entra.Authentication -Times 1
-    }
-    It "Should connect to specified environment"{
-        $result = Connect-Entra -Environment Global
-        $result | Should -BeNullOrEmpty
-        Should -Invoke -CommandName Connect-MgGraph -ModuleName Microsoft.Entra.Authentication -Times 1
-    }
-    It "Should connect to an environment as a different identity"{
-        $result = Connect-Entra -ContextScope "Process"
-        $result | Should -BeNullOrEmpty
-        Should -Invoke -CommandName Connect-MgGraph -ModuleName Microsoft.Entra.Authentication -Times 1
-    }
-    It "Should allow for authentication using environment variables"{
-        $result = Connect-Entra -EnvironmentVariable
-        $result | Should -BeNullOrEmpty
-        Should -Invoke -CommandName Connect-MgGraph -ModuleName Microsoft.Entra.Authentication -Times 1
-    }
-    It "Should return error when TenantId is null"{
+Describe "Connect-Entra Mock" {
+
+    It "Should return error when TenantId is null" {
         { Connect-Entra -TenantId } | Should -Throw "Missing an argument for parameter 'TenantId'*"
     }
-    It "Should return error when Environment is null"{
+    It "Should return error when Environment is null" {
         { Connect-Entra -Environment } | Should -Throw "Missing an argument for parameter 'Environment'*"
     }
-    It "Should return error when invalid parameter is provided"{
+    It "Should return error when invalid parameter is provided" {
         { Connect-Entra -DisplayName } | Should -Throw "A parameter cannot be found that matches parameter name 'DisplayName'*"
     }
 }
 
-Describe "Connect-Entra ParameterSets"{
+Describe "Connect-Entra ParameterSets" {
     It 'Should have six ParameterSets' {
         $ConnectEntraCommand | Should -Not -BeNullOrEmpty
         $ConnectEntraCommand.ParameterSets | Should -HaveCount 6
@@ -59,7 +40,7 @@ Describe "Connect-Entra ParameterSets"{
     It 'Should have AppCertificateParameterSet' {
         $AppCertificateParameterSet = $ConnectEntraCommand.ParameterSets | Where-Object Name -eq 'AppCertificateParameterSet'
         $AppCertificateParameterSet | Should -Not -BeNull
-        @('ClientId', 'TenantId', 'CertificateSubjectName', 'CertificateThumbprint', 'ContextScope', 'Environment', 'ClientTimeout') | Should -BeIn $AppCertificateParameterSet.Parameters.Name                
+        @('ClientId', 'TenantId', 'CertificateSubjectName', 'CertificateThumbprint', 'ContextScope', 'Environment', 'ClientTimeout') | Should -BeIn $AppCertificateParameterSet.Parameters.Name
     }
 
     It 'Should have AppSecretCredentialParameterSet' {
@@ -85,16 +66,17 @@ Describe "Connect-Entra ParameterSets"{
     }
 
     It "Should execute successfully without throwing an error" {
-        # Disable confirmation prompts       
+        # Disable confirmation prompts
         $originalDebugPreference = $DebugPreference
         $DebugPreference = 'Continue'
-        
+
         try {
             # Act & Assert: Ensure the function doesn't throw an exception
-            {  Connect-Entra -TenantId "aaaabbbb-0000-cccc-1111-dddd2222eeee" -ApplicationId "00001111-aaaa-2222-bbbb-3333cccc4444" -CertificateThumbprint "0a0a0a0a-1111-bbbb-2222-3c3c3c3c3c3c" -Debug } | Should -Not -Throw
-        } finally {
-            # Restore original confirmation preference            
-            $DebugPreference = $originalDebugPreference        
+            { Connect-Entra -TenantId "aaaabbbb-0000-cccc-1111-dddd2222eeee" -ApplicationId "00001111-aaaa-2222-bbbb-3333cccc4444" -Debug } | Should -Not -Throw
         }
-    } 
+        finally {
+            # Restore original confirmation preference
+            $DebugPreference = $originalDebugPreference
+        }
+    }
 }
