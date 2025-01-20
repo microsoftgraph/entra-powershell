da7e2272252fbc72203dd361bb175eb5e94ecced
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-01-20 13:44:02 +0300

    [Modularize] - Get-EntraUserGroup (#1268)
    * Get-EntraUserGroup command (code, tests, docs)
    
    * Fixing script path
    
    ---------
    
    Co-authored-by: stevemutungi <stevemutungi@microsoft.com>

---
+++ test/Entra/Users/Get-EntraUserGroup.Tests.ps1
@@ -0,0 +1,78 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+#  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+BeforeAll {
+    if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
+        Import-Module Microsoft.Entra.Users
+    }
+    Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
+
+    $scriptblock = {
+        return @(
+            [PSCustomObject]@{
+                "DisplayName"  = "Contoso Marketing Group"
+                "Id"           = "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+                "Description"  = "Contoso Marketing Group"
+                "MailNickname" = "contosomarketing"
+                "GroupTypes"   = "{Unified}"
+            }
+        )
+    }
+
+    Mock -CommandName Get-MgUserMemberOfAsGroup -MockWith $scriptblock -ModuleName Microsoft.Entra.Users
+}
+
+Describe "Get-EntraUserGroup" {
+    Context "Test for Get-EntraUserGroup" {
+        It "Should return all user roles" {
+            $result = Get-EntraUserGroup -UserId 'SawyerM@contoso.com'
+            $result | Should -Not -BeNullOrEmpty
+            Should -Invoke -CommandName Get-MgUserMemberOfAsGroup -ModuleName Microsoft.Entra.Users -Times 1
+        }
+
+        It "Should return top user role" {
+            $result = Get-EntraUserGroup -UserId 'SawyerM@contoso.com' -Top 1
+            $result | Should -Not -BeNullOrEmpty
+            Should -Invoke -CommandName Get-MgUserMemberOfAsGroup -ModuleName Microsoft.Entra.Users -Times 1
+        }
+
+        It "Property parameter should work" {
+            $result = Get-EntraUserGroup -UserId 'SawyerM@contoso.com' -Property "DisplayName"
+            $result | Should -Not -BeNullOrEmpty
+            $result.DisplayName | Should -Be "Contoso Marketing Group"
+            Should -Invoke -CommandName Get-MgUserMemberOfAsGroup -ModuleName Microsoft.Entra.Users -Times 1
+        }
+
+        It "Should fail when Property is empty" {
+            { Get-EntraUserGroup -UserId 'SawyerM@contoso.com' -Property } | Should -Throw "Missing an argument for parameter 'Property'*"
+        }
+
+        It "Should contain 'User-Agent' header" {
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraUserGroup"
+            $result = Get-EntraUserGroup -UserId 'SawyerM@contoso.com'
+            $result | Should -Not -BeNullOrEmpty
+            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraUserGroup"
+            Should -Invoke -CommandName Get-MgUserMemberOfAsGroup -ModuleName Microsoft.Entra.Users -Times 1 -ParameterFilter {
+                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+                $true
+            }
+        }
+
+        It "Should execute successfully without throwing an error" {
+            # Disable confirmation prompts
+            $originalDebugPreference = $DebugPreference
+            $DebugPreference = 'Continue'
+
+            try {
+                # Act & Assert: Ensure the function doesn't throw an exception
+                { Get-EntraUserGroup -UserId 'SawyerM@contoso.com' -Debug } | Should -Not -Throw
+            }
+            finally {
+                # Restore original confirmation preference
+                $DebugPreference = $originalDebugPreference
+            }
+        }
+    }
+}
