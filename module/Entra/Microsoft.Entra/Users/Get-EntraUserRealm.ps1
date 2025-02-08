c4e86f0e739c75c81d0d4d07ab5ff49cdfa2e1bb
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-02-08 15:46:48 +0300

    Adding formatting.

--- module/Entra/Microsoft.Entra/Users/Get-EntraUserRealm.ps1
+++ module/Entra/Microsoft.Entra/Users/Get-EntraUserRealm.ps1
@@ -2,24 +2,24 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
-
 function Get-EntraUserRealm {
     [CmdletBinding()]
     [OutputType([PsCustomObject[]])]
     param (
         # User Principal Name
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
-        [string[]] $User,
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, HelpMessage = 'A list of UserIds or User Principal Names')]
+        [ValidateNotNullOrEmpty()]
+        [string[]] $UserIds,
         # Check For Microsoft Account
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = 'Check for Microsoft Account')]
         [switch] $CheckForMicrosoftAccount,
         # API Version
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = 'API Version, will default to 2.1')]
         [string] $ApiVersion = '2.1'
     )
 
     process {
-        foreach ($_User in $User) {
+        foreach ($_User in $UserIds) {
             $uriUserRealm = New-Object System.UriBuilder 'https://login.microsoftonline.com/common/userrealm'
             $uriUserRealm.Query = ConvertTo-QueryString @{
                 'api-version'              = $ApiVersion
