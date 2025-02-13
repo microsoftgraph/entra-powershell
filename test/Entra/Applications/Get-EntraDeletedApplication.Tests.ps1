410ea2ad53b29941b732e204ddcf9b26e1576ae9
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-02-13 16:29:05 +0300

    Fixing tests

--- test/Entra/Applications/Get-EntraDeletedApplication.Tests.ps1
+++ test/Entra/Applications/Get-EntraDeletedApplication.Tests.ps1
@@ -1,138 +1,6 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-BeforeAll {  
-    if ((Get-Module -Name Microsoft.Entra.Applications) -eq $null) {
-        Import-Module Microsoft.Entra.Applications      
-    }
-    Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
-
-    $scriptblock = {
-        return @(
-            [PSCustomObject]@{
-                "AddIns"                    = {}
-                "AppRoles"                  = {}
-                "GroupMembershipClaims"     = {}
-                "IdentifierUris"            = {}
-                "Info"                      = @{
-                    LogoUrl = "";
-                }
-                "IsDeviceOnlyAuthSupported" = $null
-                "KeyCredentials"            = {}
-                "OptionalClaims"            = {}
-                "ParentalControlSettings"   = @{
-                    CountriesBlockedForMinors = @{}; 
-                    LegalAgeGroupRule         = "Allow";
-                }
-                "PasswordCredentials"       = {}
-                "Api"                       = @{
-                    KnownClientApplications   = @{};
-                    PreAuthorizedApplications = @{};
-                }
-                "PublicClient"              = @{
-                    RedirectUris = @{};
-                }
-                "PublisherDomain"           = "contoso.com"
-                "Web"                       = @{
-                    HomePageUrl             = "";  
-                    LogoutUrl               = ""; 
-                    RedirectUris            = @{};
-                    Oauth2AllowImplicitFlow = ""
-                }
-                "RequiredResourceAccess"    = $null                
-                "DisplayName"               = "Mock-test-App"
-                "Id"                        = "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
-                "Logo"                      = $null
-                "Parameters"                = $args
-                "DeletedDateTime"           = "02/12/2025 11:07:07"
-                "DeletionAgeInDays"         = 0
-            }
-        )
-    }
-
-    Mock -CommandName Get-MgDirectoryDeletedItemAsApplication -MockWith $scriptblock -ModuleName Microsoft.Entra.Applications
-}
-
-Describe "Get-EntraDeletedApplication" {
-    Context "Test for Get-EntraDeletedApplication" {        
-        It "Should return all deleted applications" {
-            $result = Get-EntraDeletedApplication -All | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $result | Should -Not -BeNullOrEmpty
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItemAsApplication -ModuleName Microsoft.Entra.Applications -Times 1
-        }
-
-        It "Should fail when invalid parameter is passed" {
-            { Get-EntraDeletedApplication -DisplayName "abc" } | Should -Throw "A parameter cannot be found that matches parameter name 'DisplayName'*"
-        }         
-        It "Should return specific application by SearchString" {
-            $result = Get-EntraDeletedApplication -SearchString 'Mock-test-App' | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $result | Should -Not -BeNullOrEmpty
-
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItemAsApplication -ModuleName Microsoft.Entra.Applications -Times 1
-        } 
-        It "Should return specific application by filter" {
-            $result = Get-EntraDeletedApplication -Filter "DisplayName -eq 'Mock-test-App'" | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $result | Should -Not -BeNullOrEmpty
-
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItemAsApplication -ModuleName Microsoft.Entra.Applications -Times 1
-        }  
-        It "Should return top application" {
-            $result = Get-EntraDeletedApplication -Top 1 | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $result | Should -Not -BeNullOrEmpty
-
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItemAsApplication -ModuleName Microsoft.Entra.Applications -Times 1
-        }  
-        It "Result should Contain ObjectId" {
-            $result = Get-EntraDeletedApplication -Filter "DisplayName -eq 'Mock-test-App'" | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $result.ObjectId | should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
-        }
-        It "Should contain Filter in parameters when passed SearchString to it" {               
-            $result = Get-EntraDeletedApplication -SearchString 'Mock-test-App' | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $params = Get-Parameters -data $result.Parameters
-            $params.Filter | Should -Match "Mock-test-App"
-        }
-        It "Property parameter should work" {
-            $result = Get-EntraDeletedApplication -Property "DisplayName"  | ConvertTo-Json | ConvertFrom-Json
-            $result | Should -Not -BeNullOrEmpty
-            $result.DisplayName | Should -Be "Mock-test-App"
-
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItemAsApplication -ModuleName Microsoft.Entra.Applications -Times 1
-        }
-
-        It "Should fail when Property is empty" {
-            { Get-EntraDeletedApplication  -Property } | Should -Throw "Missing an argument for parameter 'Property'*"
-        }
-        It "Should contain 'User-Agent' header" {
-            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDeletedApplication"
-            $result = Get-EntraDeletedApplication -Filter "DisplayName -eq 'Mock-test-App'" | ConvertTo-Json -Depth 5 | ConvertFrom-Json
-            $result | Should -Not -BeNullOrEmpty
-            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDeletedApplication"    
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItemAsApplication -ModuleName Microsoft.Entra.Applications -Times 1 -ParameterFilter {
-                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
-                $true
-            }
-        }
-        It "Should execute successfully without throwing an error " {
-            # Disable confirmation prompts       
-            $originalDebugPreference = $DebugPreference
-            $DebugPreference = 'Continue'
-    
-            try {
-                # Act & Assert: Ensure the function doesn't throw an exception
-                { Get-EntraDeletedApplication -Debug } | Should -Not -Throw
-            }
-            finally {
-                # Restore original confirmation preference            
-                $DebugPreference = $originalDebugPreference        
-            }
-        } 
-    }
-}
-
-
-# ------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
-# ------------------------------------------------------------------------------
 BeforeAll {
     if ((Get-Module -Name Microsoft.Entra.Applications) -eq $null) {
         Import-Module Microsoft.Entra.Applications      
@@ -160,12 +28,12 @@ BeforeAll {
 }
 Describe "Tests for Get-EntraDeletedApplication" {
     It "Result should not be empty" {
-        $result = Get-EntraDeletedApplication -AdministrativeUnitId "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+        $result = Get-EntraDeletedApplication -ApplicationId "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
         $result | Should -Not -BeNullOrEmpty
         Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Applications -Times 1
     }
     It "Result should not be empty objectid" {
-        $result = Get-EntraDeletedApplication -ObjectId "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
+        $result = Get-EntraDeletedApplication -ApplicationId "aaaaaaaa-1111-2222-3333-bbbbbbbbbbbb"
         $result | Should -Not -BeNullOrEmpty
         Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Applications -Times 1
     }    
