398d6509a0239e89e3b9adc31d6b8f25012a5a03
stevemutungi <stevemutungi@microsoft.com>
2025-02-13 09:48:52 +0300

    Formatting improvements

--- module/Entra/Microsoft.Entra/DirectoryManagement/Resolve-EntraIdTenant.ps1
+++ module/Entra/Microsoft.Entra/DirectoryManagement/Resolve-EntraIdTenant.ps1
@@ -3,92 +3,113 @@
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------
 function Resolve-EntraIdTenant {
-    [CmdletBinding(DefaultParameterSetName = 'TenantId',
+    [CmdletBinding(
+        DefaultParameterSetName = 'TenantId',
         SupportsShouldProcess = $false,
         PositionalBinding = $false,
-        HelpUri = 'http://www.microsoft.com/',
-        ConfirmImpact = 'Medium')]
+        HelpUri = 'https://aka.ms/entra/ps/docs',
+        ConfirmImpact = 'Medium'
+    )]
     [Alias()]
-    [OutputType([String])]
+    [OutputType([PSCustomObject])]
     Param (
         # The TenantId in GUID format
-        [Parameter(ParameterSetName = 'TenantId',Mandatory = $true,Position = 0,ValueFromPipeline = $true,ValueFromPipelineByPropertyName = $true,HelpMessage = "Unique Id of the Tenant.")]
+        [Parameter(
+            ParameterSetName = 'TenantId',
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Unique Id of the Tenant in GUID format."
+        )]
         [ValidateScript({
             if ($_ -match "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$") {
                 $true
             } else {
-                throw "Invalid GUID format for TenantId"
+                throw "Invalid GUID format for TenantId."
             }
         })]
         [string]
         $TenantId,
 
         # The TenantDomainName in DNS Name format
-        [Parameter(ParameterSetName = 'DomainName',Mandatory = $true,Position = 0,ValueFromPipeline = $true,ValueFromPipelineByPropertyName = $true,HelpMessage = "Unique Domain Name of the Tenant.")]
+        [Parameter(
+            ParameterSetName = 'DomainName',
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Unique Domain Name of the Tenant (e.g., contoso.com)."
+        )]
         [ValidateScript({
             $_ -match "^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,})+$"
         })]
         [string]
         $DomainName,
 
-         # Environment to resolve Azure AD Tenant In (Global, USGov, China, USGovDoD, Germany)
-        [Parameter(Mandatory = $false,Position = 1,ValueFromPipeline = $true,ValueFromPipelineByPropertyName = $true, HelpMessage = "Tenant Environment Name.")]
+        # Environment to resolve Azure AD Tenant
+        [Parameter(
+            Mandatory = $false,
+            Position = 1,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Tenant Environment Name (Global, USGov, China, USGovDoD, Germany)."
+        )]
         [ValidateSet("Global", "USGov", "China", "USGovDoD", "Germany")]
         [string]
         $Environment = "Global",
-        # Include resolving the value to an Azure AD tenant by the OIDC Metadata endpoint
+
+        # Skip resolving via the OIDC Metadata endpoint
         [switch]
         $SkipOidcMetadataEndpoint
     )
 
     begin {
+        # Retrieve endpoint information based on the environment
         $graphEndpoint = (Get-EntraEnvironment -Name $Environment).GraphEndpoint
         $azureAdEndpoint = (Get-EntraEnvironment -Name $Environment).AzureAdEndpoint
 
-        Write-Verbose ("$(Get-Date -f T) - Using $Environment login endpoint of $azureAdEndpoint")
-        Write-Verbose ("$(Get-Date -f T) - Using $Environment Graph endpoint of $graphEndpoint")
+        Write-Verbose ("Using $Environment login endpoint: $azureAdEndpoint")
+        Write-Verbose ("Using $Environment Graph endpoint: $graphEndpoint")
     }
 
     process {
+        # Initialize headers and result object
         $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
         $resolveUri = $null
-        $resolvedTenant = [ordered]@{}
-        $resolvedTenant.Environment = $Environment
+        $resolvedTenant = [ordered]@{
+            Environment = $Environment
+        }
 
+        # Set URI based on parameter set
         if ($PSCmdlet.ParameterSetName -eq 'TenantId') {
-            Write-Verbose ("$(Get-Date -f T) - Attempting to resolve Azure AD Tenant by TenantId $TenantId")
-            $resolveUri = ("{0}/v1.0/tenantRelationships/findTenantInformationByTenantId(tenantId='{1}')" -f $graphEndpoint, $TenantId)
+            Write-Verbose ("Resolving Azure AD Tenant by TenantId: $TenantId")
+            $resolveUri = "$graphEndpoint/v1.0/tenantRelationships/findTenantInformationByTenantId(tenantId='$TenantId')"
             $resolvedTenant.ValueFormat = "TenantId"
         }
         elseif ($PSCmdlet.ParameterSetName -eq 'DomainName') {
-            Write-Verbose ("$(Get-Date -f T) - Attempting to resolve Azure AD Tenant by DomainName $DomainName")
-            $resolveUri = ("{0}/v1.0/tenantRelationships/findTenantInformationByDomainName(domainName='{1}')" -f $graphEndpoint, $DomainName)
+            Write-Verbose ("Resolving Azure AD Tenant by DomainName: $DomainName")
+            $resolveUri = "$graphEndpoint/v1.0/tenantRelationships/findTenantInformationByDomainName(domainName='$DomainName')"
             $resolvedTenant.ValueFormat = "DomainName"
         }
 
-        if ($null -ne $resolveUri) {
+        if ($resolveUri) {
             try {
-                Write-Verbose ("$(Get-Date -f T) - Resolving Tenant Information using MS Graph API")
-                $resolve = Invoke-MgGraphRequest -Method Get -Uri $resolveUri -ErrorAction Stop -Headers $customHeaders | 
+                Write-Verbose ("Resolving Tenant Information using MS Graph API.")
+                $resolve = Invoke-MgGraphRequest -Method Get -Uri $resolveUri -ErrorAction Stop -Headers $customHeaders |
                     Select-Object tenantId, displayName, defaultDomainName, federationBrandName
 
+                # Populate resolved tenant details
                 $resolvedTenant.Result = "Resolved"
-                $resolvedTenant.ResultMessage = "Resolved Tenant"
-                $resolvedTenant.TenantId = $resolve.TenantId
-                $resolvedTenant.DisplayName = $resolve.DisplayName
+                $resolvedTenant.ResultMessage = "Tenant resolved successfully."
+                $resolvedTenant.TenantId = $resolve.tenantId
+                $resolvedTenant.DisplayName = $resolve.displayName
                 $resolvedTenant.DefaultDomainName = $resolve.defaultDomainName
                 $resolvedTenant.FederationBrandName = $resolve.federationBrandName
             }
             catch {
-                if ($_.Exception.Message -eq 'Response status code does not indicate success: NotFound (Not Found).') {
-                    $resolvedTenant.Result = "NotFound"
-                    $resolvedTenant.ResultMessage = "NotFound (Not Found)"
-                }
-                else {
-                    $resolvedTenant.Result = "Error"
-                    $resolvedTenant.ResultMessage = $_.Exception.Message
-                }
-
+                $resolvedTenant.Result = "Error"
+                $resolvedTenant.ResultMessage = $_.Exception.Message
                 $resolvedTenant.TenantId = $null
                 $resolvedTenant.DisplayName = $null
                 $resolvedTenant.DefaultDomainName = $null
@@ -96,8 +117,9 @@ function Resolve-EntraIdTenant {
             }
         }
 
+        # Handle OIDC Metadata endpoint resolution
         if (-not $SkipOidcMetadataEndpoint) {
-            $oidcMetadataUri = ("{0}/{1}/v2.0/.well-known/openid-configuration" -f $azureAdEndpoint, ($TenantId ?? $DomainName))
+            $oidcMetadataUri = "$azureAdEndpoint/$($TenantId ?? $DomainName)/v2.0/.well-known/openid-configuration"
 
             try {
                 $oidcMetadata = Invoke-RestMethod -Method Get -Uri $oidcMetadataUri -ErrorAction Stop -Headers $customHeaders
@@ -106,7 +128,7 @@ function Resolve-EntraIdTenant {
                 $resolvedTenant.OidcMetadataTenantRegionScope = $oidcMetadata.tenant_region_scope
             }
             catch {
-                $resolvedTenant.OidcMetadataResult = "NotFound"
+                $resolvedTenant.OidcMetadataResult = "Error"
                 $resolvedTenant.OidcMetadataTenantId = $null
                 $resolvedTenant.OidcMetadataTenantRegionScope = $null
             }
@@ -114,8 +136,7 @@ function Resolve-EntraIdTenant {
         else {
             $resolvedTenant.OidcMetadataResult = "Skipped"
         }
-        Write-Output ([pscustomobject]$resolvedTenant)
+
+        Write-Output ([PSCustomObject]$resolvedTenant)
     }
 }
-
-
