13b289d490bb7885a4665a8c355420c705354d08
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-02-14 23:32:54 +0300

    Reset everything

--- test/Entra/DirectoryManagement/Get-EntraDeletedDirectoryObject.Tests.ps1
+++ test/Entra/DirectoryManagement/Get-EntraDeletedDirectoryObject.Tests.ps1
@@ -7,98 +7,76 @@ BeforeAll {
     }
     Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
 
-    $deletedDirectoryObject = {
+    $scriptblock = {
         return @(
             [PSCustomObject]@{
                 "Id"                   = "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+                "AdditionalProperties" = @{DisplayName = "Test-App"; }
                 "DeletedDateTime"      = "2/2/2024 5:33:56 AM"
                 "Parameters"           = $args
-                "DeletionAgeInDays"    = 2
-                "DisplayName"          = "Test-App"
-                "AdditionalProperties" = @{appId = "2f9ce505-5855-47ad-b427-1ca1171a69ba"; createdDateTime = "2024-09-15T10:08:47Z"; publisherDomain = "contoso.com" }
             }
         )
     }
-
-    $scriptblock = {
-        return @{
-            value = @(
-                @{
-                    "Id"                           = "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" 
-                    "OnPremisesSyncEnabled"        = $null
-                    "userPrincipalName"            = "Adams@M365x99297270.OnMicrosoft.com"
-                    "accountEnabled"               = $true
-                    "usageLocation"                = "DE"
-                    "displayName"                  = "Mock-App"
-                    "userType"                     = "User"
-                    "OnPremisesLastSyncDateTime"   = $null
-                    "onPremisesProvisioningErrors" = @{}
-                    "Parameters"                   = $args
-                    "AdditionalProperties"         = @{appId = "2f9ce505-5855-47ad-b427-1ca1171a69ba"; createdDateTime = "2024-09-15T10:08:47Z"; publisherDomain = "contoso.com" }
-                }
-            )
-        }
-    }
-    
 
     Mock -CommandName Get-MgDirectoryDeletedItem -MockWith $scriptblock -ModuleName Microsoft.Entra.DirectoryManagement
 }
 
 Describe "Get-EntraDeletedDirectoryObject" {
-    Context "Test for Get-EntraDeletedDirectoryObject" {
-
-        It "Should return specific Deleted Directory Object" {
-            $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
-            $result | Should -Not -BeNullOrEmpty
-
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItem -ModuleName Microsoft.Entra.DirectoryManagement -Times 1
-        }
-
-        It "Should fail when DirectoryObjectId is empty" {
-            { Get-EntraDeletedDirectoryObject -DirectoryObjectId "" } | Should -Throw "Cannot bind argument to parameter 'DirectoryObjectId'*"
-        }
-        It "Should fail when DirectoryObjectId is null" {
-            { Get-EntraDeletedDirectoryObject -DirectoryObjectId } | Should -Throw "Missing an argument for parameter 'DirectoryObjectId'*"
-        }
-        It "Should fail when invalid parameter is passed" {
-            { Get-EntraDeletedDirectoryObject -DisplayName "abc" } | Should -Throw "A parameter cannot be found that matches parameter name 'DisplayName'*"
-        }
-        
-        It "Property parameter should work" {
-            $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"  -Property Id 
-            $result | Should -Not -BeNullOrEmpty
-            $result.Id | Should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
-
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItem -ModuleName Microsoft.Entra.DirectoryManagement -Times 1
-        }
-
-        It "Should fail when Property is empty" {
-            { Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"  -Property } | Should -Throw "Missing an argument for parameter 'Property'*"
-        }
-        It "Should contain 'User-Agent' header" {
-            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDeletedDirectoryObject"
-            $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId  "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
-            $result | Should -Not -BeNullOrEmpty
-            $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDeletedDirectoryObject"    
-            Should -Invoke -CommandName Get-MgDirectoryDeletedItem -ModuleName Microsoft.Entra.DirectoryManagement -Times 1 -ParameterFilter {
-                $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
-                $true
-            }
-        }
-        It "Should execute successfully without throwing an error " {
-            # Disable confirmation prompts       
-            $originalDebugPreference = $DebugPreference
-            $DebugPreference = 'Continue'
-
-            try {
-                # Act & Assert: Ensure the function doesn't throw an exception
-                { Get-EntraDeletedDirectoryObject -DirectoryObjectId  "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -Debug } | Should -Not -Throw
-            }
-            finally {
-                # Restore original confirmation preference            
-                $DebugPreference = $originalDebugPreference        
-            }
-        } 
+    It "Result should return DeletedDirectoryObject using alias" {
+        $result = Get-EntraDeletedDirectoryObject -Id "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+        $result.ObjectId | should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
     }
-}
+    It "Should fail when DirectoryObjectId is empty" {
+        { Get-EntraDeletedDirectoryObject -DirectoryObjectId "" } | Should -Throw "Cannot bind argument to parameter 'DirectoryObjectId'*"
+    }
+    It "Should fail when DirectoryObjectId is null" {
+        { Get-EntraDeletedDirectoryObject -DirectoryObjectId } | Should -Throw "Missing an argument for parameter 'DirectoryObjectId'*"
+    }
+    It "Should fail when invalid parameter is passed" {
+        { Get-EntraDeletedDirectoryObject -DisplayName "abc" } | Should -Throw "A parameter cannot be found that matches parameter name 'DisplayName'*"
+    }
+    It "Result should Contain ObjectId" {
+        $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+        $result.ObjectId | should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+    }    
+    It "Should contain DirectoryObjectId in parameters when passed Id to it" {              
+        $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+        $params = Get-Parameters -data $result.Parameters
+        $params.DirectoryObjectId | Should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+    }
+    It "Property parameter should work" {
+        $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"  -Property Id 
+        $result | Should -Not -BeNullOrEmpty
+        $result.Id | Should -Be "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
 
+        Should -Invoke -CommandName Get-MgDirectoryDeletedItem -ModuleName Microsoft.Entra.DirectoryManagement -Times 1
+    }
+
+    It "Should fail when Property is empty" {
+        { Get-EntraDeletedDirectoryObject -DirectoryObjectId "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"  -Property } | Should -Throw "Missing an argument for parameter 'Property'*"
+    }
+    It "Should contain 'User-Agent' header" {
+        $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDeletedDirectoryObject"
+        $result = Get-EntraDeletedDirectoryObject -DirectoryObjectId  "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb"
+        $result | Should -Not -BeNullOrEmpty
+        $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Get-EntraDeletedDirectoryObject"    
+        Should -Invoke -CommandName Get-MgDirectoryDeletedItem -ModuleName Microsoft.Entra.DirectoryManagement -Times 1 -ParameterFilter {
+            $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
+            $true
+        }
+    }
+    It "Should execute successfully without throwing an error " {
+        # Disable confirmation prompts       
+        $originalDebugPreference = $DebugPreference
+        $DebugPreference = 'Continue'
+
+        try {
+            # Act & Assert: Ensure the function doesn't throw an exception
+            { Get-EntraDeletedDirectoryObject -DirectoryObjectId  "aaaaaaaa-0000-1111-2222-bbbbbbbbbbbb" -Debug } | Should -Not -Throw
+        }
+        finally {
+            # Restore original confirmation preference            
+            $DebugPreference = $originalDebugPreference        
+        }
+    } 
+}
