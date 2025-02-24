cac5862d5e24f0593a223d84f1fcd26fa44529ef
stevemutungi <stevemutungi@microsoft.com>
2025-02-24 21:50:03 +0300

    Add script block

--- test/Entra/Users/Update-EntraUserFromFederated.Tests.ps1
+++ test/Entra/Users/Update-EntraUserFromFederated.Tests.ps1
@@ -1,66 +1,72 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
+param()
+
 BeforeAll{
-    if((Get-Module -Name Microsoft.Entra.Users) -eq $null){
+    if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
         Import-Module Microsoft.Entra.Users      
     }
     Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
 
-    Mock -CommandName Invoke-GraphRequest -MockWith {} -ModuleName Microsoft.Entra.Users
+    $scriptblock = {
+        @{
+            Name  = "newPassword"
+            Value = "Mufu9887"
+        },
+        @{
+            Name  = "@odata.context"
+            Value = 'https://graph.microsoft.com/v1.0/$metadata#microsoft.graph.passwordResetResponse'
+        }
+    }
 
-    $CurrentPassword = ConvertTo-SecureString 'test@123' -AsPlainText -Force
-    $NewPassword = ConvertTo-SecureString 'test@1234' -AsPlainText -Force
+    Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Entra.Users
 }
 Describe "Tests for Update-EntraUserFromFederated"{
-    Context "Test for Update-EntraUserFromFederated" {
-    It "should return empty object"{
-        $result = Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com" -NewPassword $NewPassword
-        $result | Should -BeNullOrEmpty
+    It "Result should not be empty"{
+        $result = Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com"
+        $result | Should -Not -BeNullOrEmpty
         Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Users -Times 1
+    }
+   
+    It "Should fail when UserPrincipalName is empty" {
+        { Update-EntraUserFromFederated -UserPrincipalName "" } | Should -Throw "Cannot bind argument to parameter 'UserPrincipalName'*"
     }
     It "Should fail when UserPrincipalName is null" {
         { Update-EntraUserFromFederated -UserPrincipalName } | Should -Throw "Missing an argument for parameter 'UserPrincipalName'*"
-    }  
-    It "Should fail when UserPrincipalName is empty" {
-        { Update-EntraUserFromFederated -UserPrincipalName "" } | Should -Throw "Cannot process argument transformation on parameter 'UserPrincipalName'*"
-    }
-    It "Should fail when NewPassword is null" {
-        { Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com" -NewPassword } | Should -Throw "Missing an argument for parameter 'NewPassword'*"
-    }  
-    It "Should fail when NewPassword is empty" {
-        { Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com" -NewPassword "" } | Should -Throw "Cannot process argument transformation on parameter 'NewPassword'*"
+    }    
+    It "Should fail when UserPrincipalName is not passed" {
+        { Update-EntraUserFromFederated } | Should -Throw "Missing an argument for parameter 'UserPrincipalName'*"
+    }    
+    It "Should fail when invalid parameter is passed" {
+        { Update-EntraUserFromFederated -xyz } | Should -Throw "A parameter cannot be found that matches parameter name 'xyz'*"
     }
 
     It "Should contain 'User-Agent' header" {
         $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Update-EntraUserFromFederated"
-
-        Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com" -NewPassword $NewPassword
-
+        $result = Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com"
+        $result | Should -Not -BeNullOrEmpty
         $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Update-EntraUserFromFederated"
-
         Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Users -Times 1 -ParameterFilter {
             $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
             $true
         }
-    }   
-
-        It "Should execute successfully without throwing an error " {
-            # Disable confirmation prompts       
-            $originalDebugPreference = $DebugPreference
-            $DebugPreference = 'Continue'
-    
-            try {
-                # Act & Assert: Ensure the function doesn't throw an exception
-                { 
-                    Update-EntraUserFromFederated -UserPrincipalName "sawyerM@contoso.com" -NewPassword $NewPassword -Debug 
-                } | Should -Not -Throw
-            } finally {
-                # Restore original confirmation preference
-                $DebugPreference = $originalDebugPreference
-            }
-        }
-        
     }
+    It "Should execute successfully without throwing an error " {
+        # Disable confirmation prompts       
+        $originalDebugPreference = $DebugPreference
+        $DebugPreference = 'Continue'
+
+        try {
+            # Act & Assert: Ensure the function doesn't throw an exception
+            { Update-EntraUserFromFederated -Top 1 -Debug } | Should -Not -Throw
+        } finally {
+            # Restore original confirmation preference            
+            $DebugPreference = $originalDebugPreference        
+        }
+    } 
 }
+
+
 
