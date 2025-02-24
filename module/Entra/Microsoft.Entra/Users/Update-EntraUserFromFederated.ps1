94724759f0a80ffe5a3695fe3da7dceff7f66928
stevemutungi <stevemutungi@microsoft.com>
2025-02-24 13:53:12 +0300

    Fix empty object warning

--- module/Entra/Microsoft.Entra/Users/Update-EntraUserFromFederated.ps1
+++ module/Entra/Microsoft.Entra/Users/Update-EntraUserFromFederated.ps1
@@ -3,47 +3,54 @@
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
 function Update-EntraUserFromFederated {
-        [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
+    [CmdletBinding(DefaultParameterSetName = 'GetQuery')]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "UserPrincipalName of the user to update.")]
         [Alias('UserId')]
         [System.String] $UserPrincipalName,
 
-        [Parameter(Mandatory=$false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName=$true, HelpMessage = "New password for the user.")]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "New password for the user.")]
         [SecureString] $NewPassword,
 
-        [Parameter(Mandatory=$false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName=$true, HelpMessage = "TenantId of the user to update.")]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "TenantId of the user to update.")]
         [guid] $TenantId          
     )
 
-        PROCESS {    
-            $params = @{}
-            $authenticationMethodId = "28c10230-6103-485e-b985-444c60001490"
-            $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+    PROCESS {    
+        # Define essential variables
+        $authenticationMethodId = "28c10230-6103-485e-b985-444c60001490"
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+        $params = @{
+            "UserId" = $UserPrincipalName
+            "Url"   = "https://graph.microsoft.com/v1.0/users/$($UserPrincipalName)/authentication/methods/$($authenticationMethodId)/resetPassword"
+        }
 
-            if ($null -ne $PSBoundParameters["UserPrincipalName"]) {
-                $UserId = $PSBoundParameters["UserPrincipalName"]
+        # Handle password conversion securely
+        if ($PSBoundParameters.ContainsKey("NewPassword") -and $NewPassword) {
+            $newSecurePtr = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode($NewPassword)
+            try {
+                $new = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($newSecurePtr)
+                $params["NewPassword"] = $new
+            } finally {
+                [System.Runtime.InteropServices.Marshal]::ZeroFreeGlobalAllocUnicode($newSecurePtr)  # Securely free memory
             }
-            if($null -ne $PSBoundParameters["NewPassword"])
-            {
-                $params["NewPassword"] = $PSBoundParameters["NewPassword"]
-            }        
-        
-            $newsecur = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode($params.NewPassword)
-            $new = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($newsecur)
-        
-            $params["Url"]  = "https://graph.microsoft.com/v1.0/users/$($UserId)/authentication/methods/$($authenticationMethodId)/resetPassword"
-            $body = @{
-                newPassword = $new
-            }
-            $body = $body | ConvertTo-Json
-        
-            Write-Debug("============================ TRANSFORMATIONS ============================")
-            $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-            Write-Debug("=========================================================================`n")
-            
-            $response = Invoke-GraphRequest -Headers $customHeaders -Uri $params.Url -Method POST -Body $body
-            $response
-        }   
+        } else {
+            Write-Warning "NewPassword is missing. No password update will be performed."
+            return
+        }
+
+        # Create request body
+        $body = @{
+            newPassword = $params["NewPassword"]
+        } | ConvertTo-Json
+
+        # Debugging output
+        Write-Debug "============================ TRANSFORMATIONS ============================"
+        $params.Keys | ForEach-Object { Write-Debug "$_ : $($params[$_])" }
+        Write-Debug "=========================================================================`n"
+
+        # Invoke request
+        $response = Invoke-GraphRequest -Headers $customHeaders -Uri $params.Url -Method POST -Body $body
+        return $response
+    }
 }
-
