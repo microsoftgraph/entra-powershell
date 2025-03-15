04389d3c5ca3aa8a2c1194df7a4b35d17d735ed9
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-03-16 00:08:06 +0300

    Fixing tests

--- test/Entra/Users/Set-EntraUserExtension.Tests.ps1
+++ test/Entra/Users/Set-EntraUserExtension.Tests.ps1
@@ -1,10 +1,6 @@
 # ------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
-
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
-param()
-
 BeforeAll {
     if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
         Import-Module Microsoft.Entra.Users      
@@ -14,25 +10,25 @@ BeforeAll {
     Mock -CommandName Invoke-GraphRequest -MockWith {} -ModuleName Microsoft.Entra.Users
 
 }
-Describe "Tests for Set-EntraUser" {
+Describe "Tests for Set-EntraUserExtension" {
 
     It "Should return empty object" {
-        $result = Set-EntraUser -UserId "sawyerM@contoso.com" -AccountEnabled $true
+        $result = Set-EntraUserExtension -UserId "sawyerM@contoso.com" -ExtensionName 'extension_e5e29b8a85d941eab8d12162bd004528_JobGroup' -ExtensionValue 'Job Group D'
         $result | Should -BeNull
     }
 
     It "Should return empty object when UserId is not provided" {
-        $result = Set-EntraUser -AccountEnabled $true
+        $result = Set-EntraUserExtension -ExtensionName 'extension_e5e29b8a85d941eab8d12162bd004528_JobGroup' -ExtensionValue 'Job Group D'
         $result | Should -BeNull
     }
 
     It "Should fail when UserId is missing" {
-        { Set-EntraUser -UserId } | Should -Throw "Missing an argument for parameter 'UserId'. Specify a parameter of type 'System.String' and try again."
+        { Set-EntraUserExtension -UserId } | Should -Throw "Missing an argument for parameter 'UserId'. Specify a parameter of type 'System.String' and try again."
     }
     
     It "Should contain 'User-Agent' header" {
-        $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Set-EntraUser"
-        $result = Set-EntraUser -UserId "sawyerM@contoso.com" -AccountEnabled $true
+        $userAgentHeaderValue = "PowerShell/$psVersion EntraPowershell/$entraVersion Set-EntraUserExtension"
+        $result = Set-EntraUserExtension -UserId "sawyerM@contoso.com" -ExtensionName 'extension_e5e29b8a85d941eab8d12162bd004528_JobGroup' -ExtensionValue 'Job Group D'
         $result | Should -BeNull
         Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Users -Times 1 -ParameterFilter {
             $Headers.'User-Agent' | Should -Be $userAgentHeaderValue
@@ -46,7 +42,7 @@ Describe "Tests for Set-EntraUser" {
 
         try {
             # Act & Assert: Ensure the function doesn't throw an exception
-            { Set-EntraUser -UserId "sawyerM@contoso.com" -AccountEnabled $true -Debug } | Should -Not -Throw
+            { Set-EntraUserExtension -UserId "sawyerM@contoso.com" -ExtensionName 'extension_e5e29b8a85d941eab8d12162bd004528_JobGroup' -ExtensionValue 'Job Group D' -Debug } | Should -Not -Throw
         }
         finally {
             # Restore original confirmation preference            
