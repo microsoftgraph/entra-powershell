8caae025250e92102941e2eca1947e89d2032c8c
Peichen Sun <peichensun@microsoft.com>
2025-03-26 12:29:37 +0000

    add test

--- module/Entra/Microsoft.Entra/Users/Set-EntraCBACertificateUserId.ps1
+++ module/Entra/Microsoft.Entra/Users/Set-EntraCBACertificateUserId.ps1
@@ -25,29 +25,22 @@ function  Set-EntraCBACertificateUserId {
         $userList = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users?`$select=id"
         $userIdPattern = '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
               
-        try {
-            # Validate user exists
-            if (-not ($UserId -match $userIdPattern) -or -not $userList.value.id -contains $UserId) {
-                Write-Error "User '$UserId' not found in Entra ID"
-                return
+        
+        # Validate user exists
+        if (-not ($UserId -match $userIdPattern) -or -not $userList.value.id -contains $UserId) {
+            throw "User '$UserId' not found in Entra ID"
+        }
+
+        # Prepare the request body
+        $body = @{
+            authorizationInfo = @{
+                certificateUserIds = @($certUserIdObj) 
             }
-
-            # Prepare the request body
-            $body = @{
-                authorizationInfo = @{
-                    certificateUserIds = @($certUserIdObj) 
-                }
-            } | ConvertTo-Json
+        } | ConvertTo-Json
 
 
-            $apiCallUrl = "https://graph.microsoft.com/v1.0/users/$($UserId)?`$select=authorizationInfo"
-            $response = Invoke-GraphRequest -Uri $apiCallUrl -Method PATCH -Body $body
-            Write-Debug "Response: $($response | ConvertTo-Json)"
-            
-        }
-        catch {
-            Write-Error "Failed to update certificateUserIds for user $UserId. Error: $_"
-        }
+        $apiCallUrl = "https://graph.microsoft.com/v1.0/users/$($UserId)?`$select=authorizationInfo"
+        $response = Invoke-GraphRequest -Uri $apiCallUrl -Method PATCH -Body $body
+        $response
     }
 }
-Set-EntraCBACertificateUserId
--- test/Entra/Users/Set-EntraCBACertificateUserId.Tests.ps1
+++ test/Entra/Users/Set-EntraCBACertificateUserId.Tests.ps1
@@ -1,129 +1,49 @@
 BeforeAll {
-    if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
+    if (-not (Get-Module -Name Microsoft.Entra.Users)) {
         Import-Module Microsoft.Entra.Users
     }
     Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
 
-    $scriptblock = {
-        return @{
-            UserId = '00000000-0000-0000-0000-000000000001'
-            CertificateUserIds = 'X509:<PN>test_peichen@1vbhqz.onmicrosoft.com'
-        }
+    # Ensure function is loaded
+    if (-not (Get-Command Set-EntraCBACertificateUserId -ErrorAction SilentlyContinue)) {
+        throw "Set-EntraCBACertificateUserId function is not available"
     }
-
-    Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Entra.Users
 }
+
 Describe "Certificate User ID Update Script" {
-        # Mock Get-Certificate function
-        function Get-Certificate {
-            param ([string]$filePath)
-            # Simulate certificate creation
-            $mockCert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new()
-            return $mockCert
-        }
 
-    Context "Test-EntraUserId Function" {
-        It "Should return true for a valid GUID user ID" {
-            $result = Test-EntraUserId -UserId '00000000-0000-0000-0000-000000000001'
-            $result | Should -BeTrue
-        }
-
-        It "Should return false for an invalid user ID" {
-            $result = Test-EntraUserId -UserId 'invalid-guid'
-            $result | Should -BeFalse
-        }
-    }
-
-    Context "Get-Certificate Function" {
-        It "Should throw for unsupported file format" {
-            { Get-Certificate -filePath "test.txt" } | Should -Throw "Unsupported certificate format*"
-        }
-
-        It "Should successfully load .cer file" {
-            $certPath = Join-Path -Path $TestDrive -ChildPath "test.cer"
-            New-Item -Path $certPath -ItemType File -Force
-            $cert = Get-Certificate -filePath $certPath
-            $cert | Should -Not -BeNullOrEmpty
-        }
-
-        It "Should successfully load .pem file" {
-            $pemContent = @"
------BEGIN CERTIFICATE-----
-MIIDazCCAlOgAwIBAgIUJOZcxb3zzzzzzzzzzzzzzzzzzz0wDQYJKoZIhvcNAQEL
-BQAwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoM
-GEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0yMzA3MjcxMjM0NTZaFw0yNDA3
-...
------END CERTIFICATE-----
-"@
-            $pemPath = Join-Path -Path $TestDrive -ChildPath "test.pem"
-            $pemContent | Out-File -FilePath $pemPath -Encoding utf8
-            $cert = Get-Certificate -filePath $pemPath
-            $cert | Should -Not -BeNullOrEmpty
-        }
-    }
-
-    Context "Get-CertificateUserIds Function" {
-        BeforeAll {
-            # Create a mock certificate
-            $mockCert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-        }
-
-        It "Should return dictionary with all keys" {
-            $userIds = Get-CertificateUserIds -cert $mockCert
-            $expectedKeys = @(
-                "PrincipalName", 
-                "RFC822Name", 
-                "IssuerAndSubject", 
-                "Subject", 
-                "SKI", 
-                "SHA1PublicKey", 
-                "IssuerAndSerialNumber"
-            )
-            
-            foreach ($key in $expectedKeys) {
-                $userIds.ContainsKey($key) | Should -BeTrue
+    BeforeEach {
+        # Mock API response for user retrieval
+        Mock -CommandName Invoke-MgGraphRequest -MockWith {
+            return @{
+                value = @(
+                    @{ id = "11111111-1111-1111-1111-111111111111" }
+                    @{ id = "22222222-2222-2222-2222-222222222222" }
+                )
             }
         }
     }
 
-    Context "Script Parameter Validation" {
-        It "Should throw when UserId is not provided" {
-            { 
-                & $PSCommandPath -Path "test.cer" -CertificateMapping "PrincipalName" 
-            } | Should -Throw
-        }
-
-        It "Should throw when Path is not provided" {
-            { 
-                & $PSCommandPath -UserId "00000000-0000-0000-0000-000000000001" -CertificateMapping "PrincipalName" 
-            } | Should -Throw
-        }
-
-        It "Should throw for invalid CertificateMapping" {
-            { 
-                & $PSCommandPath -UserId "00000000-0000-0000-0000-000000000001" -Path "test.cer" -CertificateMapping "InvalidMapping" 
-            } | Should -Throw
-        }
-    }
-
     Context "Main Script Execution" {
-        It "Should fail for non-existent user" {
-            Mock Test-EntraUserId { return $false }
-            Mock Write-Error { throw $Message }
-            
-            { 
-                & $PSCommandPath -UserId "non-existent-id" -Path "test.cer" -CertificateMapping "PrincipalName"
-            } | Should -Throw "User 'non-existent-id' not found in Entra ID"
+
+        It "Should throw an error when user is not found in Entra ID" {   
+            { Set-EntraCBACertificateUserId -UserId "non-existent-id" -CertPath "C:\temp\testcert.cer" -CertificateMapping "PrincipalName" } |
+                Should -Throw
         }
 
-        It "Should successfully update certificate user IDs" {
-            Mock Test-EntraUserId { return $true }
-            Mock Invoke-MgGraphRequest { return @{} }
-            
-            $result = & $PSCommandPath -UserId "00000000-0000-0000-0000-000000000001" -Path "test.cer" -CertificateMapping "PrincipalName"
-            
-            $result | Should -Not -BeNullOrEmpty
-            $result.UserId | Should -Be "00000000-0000-0000-0000-000000000001"
+        It "Should throw an error when certificate is not found" {
+            { Set-EntraCBACertificateUserId -UserId "11111111-1111-1111-1111-111111111111" -CertPath "C:\temp\non-existent.cer" -CertificateMapping "PrincipalName" } |
+                Should -Throw
+        }
+
+        It "Should throw an error when certificate mapping is not valid" {
+            { Set-EntraCBACertificateUserId -UserId "11111111-1111-1111-1111-111111111111" -CertPath "C:\temp\testcert.cer" -CertificateMapping "InvalidMapping" } |
+                Should -Throw 
+        }
+
+        It "Should update the user ID when all parameters are valid" {
+            { Set-EntraCBACertificateUserId -UserId "11111111-1111-1111-1111-111111111111" -CertPath "C:\temp\testcert.cer" -CertificateMapping "PrincipalName" } |
+                Should -Not -Throw
         }
     }
 }
