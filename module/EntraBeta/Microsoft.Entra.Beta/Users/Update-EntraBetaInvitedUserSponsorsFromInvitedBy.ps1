7ae84d452ffa620be9bf96a0329fb8e0b5a232c2
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2025-03-28 09:39:34 +0300

    Migrate to use Graph API

--- module/EntraBeta/Microsoft.Entra.Beta/Users/Update-EntraBetaInvitedUserSponsorsFromInvitedBy.ps1
+++ module/EntraBeta/Microsoft.Entra.Beta/Users/Update-EntraBetaInvitedUserSponsorsFromInvitedBy.ps1
@@ -7,86 +7,102 @@ function Update-EntraBetaInvitedUserSponsorsFromInvitedBy {
         ConfirmImpact = 'High',
         DefaultParameterSetName = 'AllInvitedGuests')]
     param (
-
-        # UserId of Guest User
         [Parameter(ParameterSetName = 'ByUsers', HelpMessage = "The Unique ID of the User (User ID).")]
-        [String[]]
-        $UserId,
+        [String[]] $UserId,
 
-        # Enumerate and Update All Guest Users.
         [Parameter(ParameterSetName = 'AllInvitedGuests', HelpMessage = "A Flag indicating whether to include all invited guests.")]
-        [switch]
-        $All
+        [switch] $All
     )
 
-    begin {      
+    begin {
         $guestFilter = "(CreationType eq 'Invitation')"
     }
 
     process {
-
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-        $environment=(Get-EntraContext).Environment
-        $baseUri=(Get-EntraEnvironment -Name $environment).GraphEndpoint
+        $environment = (Get-EntraContext).Environment
+        $baseUri = (Get-EntraEnvironment -Name $environment).GraphEndpoint
 
-      if ((-not $UserId -or $UserId.Count -eq 0) -and -not $All) {
+        if ((-not $UserId -or $UserId.Count -eq 0) -and -not $All) {
             throw "Please specify either -UserId or -All"
         }
-        $invitedUsers=@()
+
+        $invitedUsers = @()
+        $uri = $baseUri+"/beta/users?$filter=$($guestFilter)&$expand=sponsors"
+
         if ($All) {
-            #TODO: Change to Get-EntraBetaUser when -ExpandProperty is implemented
-            $invitedUsers = Get-MgBetaUser -Filter $guestFilter -All -ExpandProperty Sponsors
+            $invitedUsers = (Invoke-GraphRequest -Method GET -Uri $uri).value
         }
         else {
             foreach ($user in $UserId) {
-                  #TODO: Change to Get-EntraBetaUser when -ExpandProperty is implemented
-                $invitedUsers += Get-MgBetaUser -UserId $user -ExpandProperty Sponsors
+                $userUri = $baseUri+"/beta/users/$user`?\$expand=sponsors"
+                $invitedUsers += (Invoke-GraphRequest -Method GET -Uri $userUri).value
             }
         }
 
-        if ($null -eq $invitedUsers) {
+        if (-not $invitedUsers) {
             Write-Error "No guest users to process"
+            return
         }
-        else {
-            foreach ($invitedUser in $invitedUsers) {
-                $invitedBy = $null
-                $splatArgumentsGetInvitedBy = @{
-                    Method = 'Get'
-                    Uri    = $baseUri +"/beta/users/" + $invitedUser.id + "/invitedBy"
+
+        foreach ($invitedUser in $invitedUsers) {
+            $invitedByUri = "$baseUri/beta/users/$($invitedUser.id)/invitedBy"
+            $invitedBy = Invoke-GraphRequest -Method GET -Uri $invitedByUri -Headers $customHeaders
+
+            Write-Verbose ($invitedBy | ConvertTo-Json -Depth 10)
+
+            if ($invitedBy -and $invitedBy.value -and $invitedBy.value.id) {
+                $inviterId = $invitedBy.value.id
+                Write-Verbose ("InvitedBy for Guest User {0}: {1}" -f $invitedUser.displayName, $inviterId)
+
+                # Get current sponsors
+                $currentSponsorIds = @()
+                if ($invitedUser.sponsors) {
+                    foreach ($s in $invitedUser.sponsors) {
+                        if ($s.id) {
+                            $currentSponsorIds += $s.id
+                        }
+                    }
                 }
 
-                $invitedBy = Invoke-GraphRequest @splatArgumentsGetInvitedBy -Headers $customHeaders
+                if (-not ($currentSponsorIds -contains $inviterId)) {
+                    Write-Verbose "Sponsors does not contain the user who invited them!"
 
-                Write-Verbose ($invitedBy | ConvertTo-Json -Depth 10)
-
-                if ($null -ne $invitedBy -and $null -ne $invitedBy.value -and $null -ne $invitedBy.value.id) {
-                    Write-Verbose ("InvitedBy for Guest User {0}: {1}" -f $invitedUser.displayName, $invitedBy.value.id)
-
-                    if (($null -like $invitedUser.sponsors) -or ($invitedUser.sponsors.id -notcontains $invitedBy.value.id)) {
-                        Write-Verbose "Sponsors does not contain the user who invited them!"
-
-                        if ($PSCmdlet.ShouldProcess(("$($invitedUser.displayName) ($($invitedUser.userPrincipalName) - $($invitedUser.id))"), "Update Sponsors")) {
-                            try {
-                                $sponsorUrl = ("https://graph.microsoft.com/beta/users/{0}" -f $invitedBy.value.id)
-                                $dirObj = @{"sponsors@odata.bind" = @($sponsorUrl) }
-                                $sponsorsRequestBody = $dirObj | ConvertTo-Json
-
-                                Update-MgBetaUser -UserId $invitedUser.id -BodyParameter $sponsorsRequestBody -Header $customHeaders
-                                Write-Output "$($invitedUser.userPrincipalName) - Sponsor updated successfully for this user."
+                    if ($PSCmdlet.ShouldProcess("$($invitedUser.displayName) ($($invitedUser.userPrincipalName) - $($invitedUser.id))", "Update Sponsors")) {
+                        try {
+                            $sponsorUrl = "https://graph.microsoft.com/beta/users/$inviterId"
+                            $dirObj = @{
+                                "sponsors@odata.bind" = @($sponsorUrl)
                             }
-                            catch {
-                                Write-Output "$($invitedUser.userPrincipalName) - Failed updating sponsor for this user."
-                                Write-Error $_
+                            $sponsorsRequestBody = $dirObj | ConvertTo-Json -Depth 5
+
+                            $updateSponsorUri = $baseUri+"/beta/users/$($invitedUser.Id)"
+                            Invoke-GraphRequest -Method PATCH -Uri $updateSponsorUri -Body $sponsorsRequestBody -Headers $customHeaders -ContentType "application/json"
+
+                            Write-Output "$($invitedUser.userPrincipalName) - Sponsor updated successfully."
+                        }
+                        catch {
+                            $errorMessage = $_.Exception.Message
+                            $responseContent = $_.ErrorDetails
+
+                            if ($responseContent -match "One or more added object references already exist for the following modified properties: 'sponsors'" -or $responseContent -match "One or more added object references already exist for the following modified properties: 'sponsors'") {
+                                Write-Warning "$($invitedUser.userPrincipalName) - Sponsor already set. Skipping."
+                            }
+                            elseif ($_.Exception.Response.StatusCode.Value__ -eq 400) {
+                                Write-Warning "$($invitedUser.userPrincipalName) - Bad request: $responseContent"
+                            }
+                            else {
+                                Write-Error "$($invitedUser.userPrincipalName) - Unexpected error: $errorMessage"
                             }
                         }
                     }
-                    else {
-                        Write-Output "$($invitedUser.userPrincipalName) - Sponsor already exists for this user."
-                    }
                 }
                 else {
-                    Write-Output "$($invitedUser.userPrincipalName) - Invited user information not available for this user."
+                    Write-Output "$($invitedUser.userPrincipalName) - Sponsor already exists."
                 }
+            }
+            else {
+                Write-Output "$($invitedUser.userPrincipalName) - InvitedBy info not found."
             }
         }
     }
