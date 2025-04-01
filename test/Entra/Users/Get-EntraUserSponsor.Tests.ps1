37d876ee28c4ebc41f16dc8dac7bf58b080eb944
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-04-01 11:10:52 +0300

    Fixing failing tests

--- test/Entra/Users/Get-EntraUserSponsor.Tests.ps1
+++ test/Entra/Users/Get-EntraUserSponsor.Tests.ps1
@@ -2,37 +2,34 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 BeforeAll {  
-    if((Get-Module -Name Microsoft.Entra.Users) -eq $null){
+    if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
         Import-Module Microsoft.Entra.Users      
     }
     Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
     $scriptblock = {
-    return @{
-        value = @(
-            @{
-                id          = "00aa00aa-bb11-cc22-dd33-44ee44ee44e"
-                country     = "United States"
-                displayName = "Sara Davis"
-                mail        = "z@microsoft.com"
-               '@odata.type' = "#microsoft.graph.user"
-            }
-            @{
-                id          = "10aa00aa-bb11-cc22-dd33-44ee44ee44e"
-                mail        = "d@microsoft.com"
-               '@odata.type' = "#microsoft.graph.group"
-            }
-        )
+        return @{
+            value = @(
+                @{
+                    id            = "00aa00aa-bb11-cc22-dd33-44ee44ee44e"
+                    country       = "United States"
+                    displayName   = "Sara Davis"
+                    mail          = "z@microsoft.com"
+                    '@odata.type' = "#microsoft.graph.user"
+                }
+                @{
+                    id            = "10aa00aa-bb11-cc22-dd33-44ee44ee44e"
+                    mail          = "d@microsoft.com"
+                    '@odata.type' = "#microsoft.graph.group"
+                }
+            )
+        }
     }
-}
 
     Mock -CommandName Invoke-GraphRequest -MockWith $scriptblock -ModuleName Microsoft.Entra.Users
 }
 
 Describe "Get-EntraUserSponsor" {
     Context "Test for Get-EntraUserSponsor" {
-        It "Should fail when UserId is empty string value" {
-            { Get-EntraUserSponsor -UserId "" } | Should -Throw "Cannot bind argument to parameter 'UserId' because it is an empty string."
-        }
 
         It "Should fail when UserId is empty" {
             { Get-EntraUserSponsor -UserId } | Should -Throw "Missing an argument for parameter 'UserId'. Specify a parameter of type 'System.String' and try again."
@@ -45,11 +42,11 @@ Describe "Get-EntraUserSponsor" {
             $result[0].country | should -Be "United States"
             $result[0].displayName | should -Be "Sara Davis"
             $result[0].mail | should -Be "z@microsoft.com"
-            $result[0].'@odata.type'| should -Be  "#microsoft.graph.user"
+            $result[0].'@odata.type' | should -Be "#microsoft.graph.user"
             
             $result[1].id | should -Be "10aa00aa-bb11-cc22-dd33-44ee44ee44e"
             $result[1].mail | should -Be "d@microsoft.com"
-            $result[1].'@odata.type'| should -Be  "#microsoft.graph.group"
+            $result[1].'@odata.type' | should -Be "#microsoft.graph.group"
             Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Users -Times 1
         }
 
@@ -57,7 +54,7 @@ Describe "Get-EntraUserSponsor" {
             $result = Get-EntraUserSponsor -UserId "00aa00aa-bb11-cc22-dd33-44ee44ee44e" -Top 1
             $result | Should -Not -BeNullOrEmpty
 
-            Should -Invoke -CommandName Invoke-GraphRequest  -ModuleName Microsoft.Entra.Users -Times 1
+            Should -Invoke -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Users -Times 1
         }
 
         It "Should fail when top is empty" {
@@ -95,7 +92,8 @@ Describe "Get-EntraUserSponsor" {
                 { 
                     Get-EntraUserSponsor -UserId "00aa00aa-bb11-cc22-dd33-44ee44ee44e" -Debug 
                 } | Should -Not -Throw
-            } finally {
+            }
+            finally {
                 # Restore original confirmation preference
                 $DebugPreference = $originalDebugPreference
             }
