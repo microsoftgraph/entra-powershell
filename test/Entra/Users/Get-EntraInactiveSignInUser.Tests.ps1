4948aaea4be34612af594de024fe7fbb75a27ec5
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2025-04-02 15:15:52 +0300

    added beta tests

--- test/Entra/Users/Get-EntraInactiveSignInUser.Tests.ps1
+++ test/Entra/Users/Get-EntraInactiveSignInUser.Tests.ps1
@@ -1,8 +1,5 @@
-# ------------------------------------------------------------------------------ 
-#  Copyright (c) Microsoft Corporation. 
-#  All Rights Reserved.  
-#  Licensed under the MIT License.  
-#  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
 BeforeAll {
--- test/EntraBeta/Users/Get-EntraBetaInactiveSignInUser.Tests.ps1
+++ test/EntraBeta/Users/Get-EntraBetaInactiveSignInUser.Tests.ps1
@@ -1,12 +1,9 @@
-# ------------------------------------------------------------------------------ 
-#  Copyright (c) Microsoft Corporation. 
-#  All Rights Reserved.  
-#  Licensed under the MIT License.  
-#  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
 BeforeAll {
-    if ((Get-Module -Name Microsoft.Entra.Bets.Users) -eq $null) {
+    if ((Get-Module -Name Microsoft.Entra.Beta.Users) -eq $null) {
         Import-Module Microsoft.Entra.Beta.Users
     }
     Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
