d591c96dec149b2ab4d4c1e1504e195d7ef2ca9c
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-04-11 06:33:57 +0300

    Update module/Entra/Microsoft.Entra/Groups/Select-EntraGroupIdsGroupIsMemberOf.ps1
    Co-authored-by: Kennedy Kang'ethe <kemunga@microsoft.com>

--- module/Entra/Microsoft.Entra/Groups/Select-EntraGroupIdsGroupIsMemberOf.ps1
+++ module/Entra/Microsoft.Entra/Groups/Select-EntraGroupIdsGroupIsMemberOf.ps1
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------------------------ 
 function Select-EntraGroupIdsGroupIsMemberOf {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
-    param (                
+    param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Unique ID of the group. Should be a valid GUID values.")]
         [ValidateNotNullOrEmpty()]
         [Microsoft.Open.AzureAD.Model.GroupIdsForMembershipCheck] $GroupIdsForMembershipCheck,
