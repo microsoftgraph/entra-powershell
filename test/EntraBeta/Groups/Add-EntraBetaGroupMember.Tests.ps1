9ce5fea38ec26fba4749fc394dcb48a3f551a612
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-04-11 06:32:03 +0300

    Update test/EntraBeta/Groups/Add-EntraBetaGroupMember.Tests.ps1
    Co-authored-by: Kennedy Kang'ethe <kemunga@microsoft.com>

--- test/EntraBeta/Groups/Add-EntraBetaGroupMember.Tests.ps1
+++ test/EntraBeta/Groups/Add-EntraBetaGroupMember.Tests.ps1
@@ -63,7 +63,7 @@ Describe "Add-EntraBetaGroupMember" {
                 { Add-EntraBetaGroupMember -GroupId "83ec0ff5-f16a-4ba3-b8db-74919eda4926" -MemberId "ec5813fb-346e-4a33-a014-b55ffee3662b" } | Should -Not -Throw
             }
             finally {
-                # Restore original confirmation preference            
+                # Restore original confirmation preference
                 $DebugPreference = $originalDebugPreference        
             }
         }
