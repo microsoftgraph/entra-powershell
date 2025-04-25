4a299bfebd067c2099e1630f64000b4a402199b8
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-04-25 14:12:09 +0300

    Tests

--- test/Entra/Users/Get-EntraUserExtensions.Tests.ps1
+++ test/Entra/Users/Get-EntraUserExtensions.Tests.ps1
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 # ------------------------------------------------------------------------------
 
-BeforeAll {
+<# BeforeAll {
 
     if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
         Import-Module Microsoft.Entra.Users      
@@ -12,7 +12,47 @@ BeforeAll {
     Mock -CommandName Invoke-GraphRequest -MockWith {} -ModuleName Microsoft.Entra.Users
     Mock -CommandName Get-EntraContext -MockWith { @{Scopes = @("User.Read.All") } } -ModuleName Microsoft.Entra.Users
 
+} #>
+BeforeAll {
+
+    if ((Get-Module -Name Microsoft.Entra.Users) -eq $null) {
+        Import-Module Microsoft.Entra.Users      
+    }
+    Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
+
+    # Mock Invoke-GraphRequest with realistic responses as PSCustomObjects
+    Mock -CommandName Invoke-GraphRequest -ModuleName Microsoft.Entra.Users -MockWith {
+        param($Uri, $Method, $Headers, $Body)
+
+        if ($Uri -eq "/v1.0/directoryObjects/getAvailableExtensionProperties") {
+            return [PSCustomObject]@{
+                value = @(
+                    [PSCustomObject]@{ name = "extension_12345_JobGroup" },
+                    [PSCustomObject]@{ name = "extension_67890_Department" }
+                )
+            }
+        }
+
+        if ($Uri -match "/v1.0/users/.+\?\$select=.+") {
+            return [PSCustomObject]@{
+                id                          = "e92dec24-bcc4-414d-bc13-b0b5a6b97d45"
+                userPrincipalName           = "sawyerM@contoso.com"
+                createdDateTime             = "2024-03-07T03:10:41Z"
+                employeeId                  = "123456"
+                onPremisesDistinguishedName = "CN=SawyerM,OU=Users,DC=contoso,DC=com"
+                identities                  = @()
+                extension_12345_JobGroup    = "Job Group D"
+                extension_67890_Department  = "Finance"
+            }
+        }
+
+        # Default empty response for other URIs
+        return [PSCustomObject]@{}
+    }
+
+    Mock -CommandName Get-EntraContext -MockWith { @{Scopes = @("User.Read.All") } } -ModuleName Microsoft.Entra.Users
 }
+
 Describe "Tests for Get-EntraUserExtensions" {
 
     It "Should return empty object" {
