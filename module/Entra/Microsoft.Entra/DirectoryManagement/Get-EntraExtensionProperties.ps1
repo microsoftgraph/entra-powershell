559e8517ecd81a7f7cff2eddfc17cb99c700ff8f
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-04-26 18:49:33 +0300

    Adding a new command

---
+++ module/Entra/Microsoft.Entra/DirectoryManagement/Get-EntraExtensionProperties.ps1
@@ -0,0 +1,69 @@
+function Get-EntraExtensionProperties {
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param (
+        [Parameter(ParameterSetName = "Default", HelpMessage = "Specify if synced from on-premises")]
+        [System.Nullable[bool]] $IsSyncedFromOnPremises,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Filter by extension property name")]
+        [string] $Name
+    )
+
+    begin {
+        # Ensure connection to Microsoft Entra
+        if (-not (Get-EntraContext)) {
+            $errorMessage = "Not connected to Microsoft Graph. Use 'Connect-Entra -Scopes Directory.Read.All' to authenticate."
+            Write-Error -Message $errorMessage -ErrorAction Stop
+            return
+        }
+    }
+
+    PROCESS {
+        # Build request URI and body
+        $uri = '/v1.0/directoryObjects/microsoft.graph.getAvailableExtensionProperties'
+        
+        # Add base parameters to body
+        $bodyParams = @{
+            isSyncedFromOnPremises = $IsSyncedFromOnPremises
+        }
+
+        $body = $bodyParams | ConvertTo-Json
+        
+        # Get custom headers
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+
+        Write-Verbose "Retrieving all extension properties..."
+        Write-Debug "Request URI: $uri"
+        Write-Debug "Request Body: $body"
+
+        # Call the Graph REST API
+        $resp = Invoke-MgGraphRequest -Method POST -Uri $uri -Body $body -Headers $customHeaders
+
+        $data = $resp.value | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+
+        if (-not [string]::IsNullOrWhiteSpace($Name)) {
+            Write-Verbose "Filtering results for name: $Name"
+            $data = $data | Where-Object { $_.name -eq $Name }
+        }
+
+        if ($data) {
+            $memberList = @()
+            foreach ($response in $data) {
+                $memberType = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphExtensionProperty
+                if (-not ($response -is [PSObject])) {
+                    $response = [PSCustomObject]@{ Value = $response }
+                }
+                $response.PSObject.Properties | ForEach-Object {
+                    $propertyName = $_.Name
+                    $propertyValue = $_.Value
+                    $memberType | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+                }
+                $memberList += $memberType
+            }
+            return $memberList
+        }
+        else {
+            Write-Warning "No extension properties found."
+            return $null
+        }
+    }
+}
