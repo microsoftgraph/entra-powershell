ba8ab4ad5baa4e17aa0b7f8fd08d89021b65519d
Steve Mutungi <132555836+SteveMutungi254@users.noreply.github.com>
2025-05-05 12:16:09 +0300

    [Bug fix 1435] Get-EntraUserExtension (#1451)

--- module/Entra/Microsoft.Entra/DirectoryManagement/Get-EntraExtensionProperty.ps1
+++ module/Entra/Microsoft.Entra/DirectoryManagement/Get-EntraExtensionProperty.ps1
@@ -3,86 +3,83 @@
 #  Licensed under the MIT License.  See License in the project root for license information. 
 # ------------------------------------------------------------------------------ 
 function Get-EntraExtensionProperty {
-    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    [CmdletBinding(DefaultParameterSetName = 'Default', SupportsShouldProcess)]
     param (
-                
-    [Parameter(ParameterSetName = "default")]
-    [System.Nullable`1[System.Boolean]] $IsSyncedFromOnPremises
+        [Parameter(ParameterSetName = "Default", HelpMessage = "Specify if synced from on-premises")]
+        [System.Nullable[bool]] $IsSyncedFromOnPremises,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Filter by extension property name")]
+        [string] $Name
     )
 
-    PROCESS {    
-    $params = @{}
-    $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
-    
-    if ($null -ne $PSBoundParameters["OutVariable"])
-    {
-        $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-    }
-    if($PSBoundParameters.ContainsKey("Debug"))
-    {
-        $params["Debug"] = $PSBoundParameters["Debug"]
-    }
-    if ($null -ne $PSBoundParameters["PipelineVariable"])
-    {
-        $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
-    }
-    if ($null -ne $PSBoundParameters["InformationVariable"])
-    {
-        $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-    }
-    if ($null -ne $PSBoundParameters["OutBuffer"])
-    {
-        $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
-    }
-    if ($null -ne $PSBoundParameters["IsSyncedFromOnPremises"])
-    {
-        $params["IsSyncedFromOnPremises"] = $PSBoundParameters["IsSyncedFromOnPremises"]
-    }
-    if ($null -ne $PSBoundParameters["WarningVariable"])
-    {
-        $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
-    }
-    if($PSBoundParameters.ContainsKey("Verbose"))
-    {
-        $params["Verbose"] = $PSBoundParameters["Verbose"]
-    }
-    if ($null -ne $PSBoundParameters["ErrorVariable"])
-    {
-        $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
-    }
-    if ($null -ne $PSBoundParameters["ErrorAction"])
-    {
-        $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
-    }
-    if ($null -ne $PSBoundParameters["InformationAction"])
-    {
-        $params["InformationAction"] = $PSBoundParameters["InformationAction"]
-    }
-    if ($null -ne $PSBoundParameters["WarningAction"])
-    {
-        $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-    }
-    if ($null -ne $PSBoundParameters["ProgressAction"])
-    {
-        $params["ProgressAction"] = $PSBoundParameters["ProgressAction"]
-    }
-    if($null -ne $PSBoundParameters["Property"])
-    {
-        $params["Property"] = $PSBoundParameters["Property"]
-    }
-
-    Write-Debug("============================ TRANSFORMATIONS ============================")
-    $params.Keys | ForEach-Object {"$_ : $($params[$_])" } | Write-Debug
-    Write-Debug("=========================================================================`n")
-    
-    $response = Get-MgDirectoryObjectAvailableExtensionProperty @params -Headers $customHeaders
-    $response | ForEach-Object {
-        if($null -ne $_) {
-        Add-Member -InputObject $_ -MemberType AliasProperty -Name ObjectId -Value Id
-
+    begin {
+        # Ensure connection to Microsoft Entra
+        if (-not (Get-EntraContext)) {
+            $errorMessage = "Not connected to Microsoft Graph. Use 'Connect-Entra -Scopes Directory.Read.All' to authenticate."
+            Write-Error -Message $errorMessage -ErrorAction Stop
+            return
         }
     }
-    $response
+
+    PROCESS {
+        # Build request URI and body
+        $uri = '/v1.0/directoryObjects/microsoft.graph.getAvailableExtensionProperties'
+        
+        # Add base parameters to body
+        $bodyParams = @{
+            isSyncedFromOnPremises = $IsSyncedFromOnPremises
+        }
+
+        $body = $bodyParams | ConvertTo-Json
+        
+        # Get custom headers
+        $customHeaders = New-EntraCustomHeaders -Command $MyInvocation.MyCommand
+
+        # Create description for -WhatIf
+        $whatIfDescription = "Retrieving extension properties"
+        if ($Name) {
+            $whatIfDescription += " filtered by name '$Name'"
+        }
+        if ($PSBoundParameters.ContainsKey('IsSyncedFromOnPremises')) {
+            $whatIfDescription += " with IsSyncedFromOnPremises=$IsSyncedFromOnPremises"
+        }
+
+        # Add ShouldProcess check
+        if ($PSCmdlet.ShouldProcess($uri, $whatIfDescription)) {
+            Write-Verbose "Retrieving all extension properties..."
+            Write-Debug "Request URI: $uri"
+            Write-Debug "Request Body: $body"
+
+            # Call the Graph REST API
+            $resp = Invoke-MgGraphRequest -Method POST -Uri $uri -Body $body -Headers $customHeaders
+            
+            $data = $resp.value | ConvertTo-Json -Depth 10 | ConvertFrom-Json
+
+            if (-not [string]::IsNullOrWhiteSpace($Name)) {
+                Write-Verbose "Filtering results for name: $Name"
+                $data = $data | Where-Object { $_.name -eq $Name }
+            }
+
+            if ($data) {
+                $memberList = @()
+                foreach ($response in $data) {
+                    $memberType = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphExtensionProperty
+                    if (-not ($response -is [PSObject])) {
+                        $response = [PSCustomObject]@{ Value = $response }
+                    }
+                    $response.PSObject.Properties | ForEach-Object {
+                        $propertyName = $_.Name
+                        $propertyValue = $_.Value
+                        $memberType | Add-Member -MemberType NoteProperty -Name $propertyName -Value $propertyValue -Force
+                    }
+                    $memberList += $memberType
+                }
+                return $memberList
+            }
+            else {
+                Write-Warning "No extension properties found."
+                return $null
+            }
+        }
     }
 }
-
