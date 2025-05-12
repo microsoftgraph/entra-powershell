0c434ddc335e054e429cfe0b93458f9e904add83
stevemutungi <stevemutungi@microsoft.com>
2025-05-12 15:11:37 +0300

    Nit: Formatting

--- test/Entra/Applications/Add-EntraApplicationOwner.Tests.ps1
+++ test/Entra/Applications/Add-EntraApplicationOwner.Tests.ps1
@@ -7,9 +7,9 @@ BeforeAll {
     }
     Import-Module (Join-Path $PSScriptRoot "..\..\Common-Functions.ps1") -Force
 
-    Mock -CommandName Get-EntraContext -MockWith { @{
+    <# Mock -CommandName Get-EntraContext -MockWith { @{
             Environment = "Global"
-        } } -ModuleName Microsoft.Entra.Applications
+        } } -ModuleName Microsoft.Entra.Applications #>
     Mock -CommandName Get-EntraEnvironment -MockWith { return @{
             GraphEndpoint = "https://graph.microsoft.com"
         } } -ModuleName Microsoft.Entra.Applications
