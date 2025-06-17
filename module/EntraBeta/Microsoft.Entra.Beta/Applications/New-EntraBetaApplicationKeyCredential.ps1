fedfa7c5796a8b9c300bcad0fc76748348322661
Kennedy Kangethe Munga <kemunga@microsoft.com>
2025-06-17 10:03:28 +0300

    Update beta cmdlet

--- module/EntraBeta/Microsoft.Entra.Beta/Applications/New-EntraBetaApplicationKeyCredential.ps1
+++ module/EntraBeta/Microsoft.Entra.Beta/Applications/New-EntraBetaApplicationKeyCredential.ps1
@@ -39,65 +39,57 @@ function New-EntraBetaApplicationKeyCredential {
     }
 
     PROCESS {    
-        $params = @{}
         $customHeaders = New-EntraBetaCustomHeaders -Command $MyInvocation.MyCommand
-    
-        if ($null -ne $PSBoundParameters["Value"]) {
-            $params["Value"] = $PSBoundParameters["Value"]
-        }
-        if ($null -ne $PSBoundParameters["ProgressAction"]) {
-            $params["ProgressAction"] = $PSBoundParameters["ProgressAction"]
-        }
-        if ($PSBoundParameters.ContainsKey("Debug")) {
-            $params["Debug"] = $PSBoundParameters["Debug"]
-        }
-        if ($null -ne $PSBoundParameters["OutBuffer"]) {
-            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
+        $params = @{}
+
+        $keyCredential = New-Object Microsoft.Graph.Beta.PowerShell.Models.MicrosoftGraphKeyCredential
+        $keyCredential.CustomKeyIdentifier = [System.Text.Encoding]::ASCII.GetBytes($PSBoundParameters["CustomKeyIdentifier"])
+        $keyCredential.StartDateTime = $PSBoundParameters["StartDate"]
+        $keyCredential.EndDateTime = $PSBoundParameters["EndDate"]
+        $keyCredential.Key = [System.Text.Encoding]::ASCII.GetBytes($PSBoundParameters["Value"])
+        $keyCredential.Type = $PSBoundParameters["Type"]
+        $keyCredential.Usage = $PSBoundParameters["Usage"]
+
+        $params["KeyCredential"] = $keyCredential
+
+        if ($null -ne $PSBoundParameters["ApplicationId"]) {
+            $params["ApplicationId"] = $PSBoundParameters["ApplicationId"]
         }
         if ($null -ne $PSBoundParameters["WarningVariable"]) {
             $params["WarningVariable"] = $PSBoundParameters["WarningVariable"]
         }
-        if ($null -ne $PSBoundParameters["WarningAction"]) {
-            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
-        }
-        if ($null -ne $PSBoundParameters["CustomKeyIdentifier"]) {
-            $params["CustomKeyIdentifier"] = $PSBoundParameters["CustomKeyIdentifier"]
-        }
-        if ($null -ne $PSBoundParameters["ApplicationId"]) {
-            $params["ApplicationId"] = $PSBoundParameters["ApplicationId"]
-        }
-        if ($null -ne $PSBoundParameters["OutVariable"]) {
-            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
-        }
-        if ($null -ne $PSBoundParameters["StartDate"]) {
-            $params["StartDate"] = $PSBoundParameters["StartDate"]
-        }
-        if ($PSBoundParameters.ContainsKey("Verbose")) {
-            $params["Verbose"] = $PSBoundParameters["Verbose"]
+        if ($PSBoundParameters.ContainsKey("Debug")) {
+            $params["Debug"] = $PSBoundParameters["Debug"]
         }
         if ($null -ne $PSBoundParameters["PipelineVariable"]) {
             $params["PipelineVariable"] = $PSBoundParameters["PipelineVariable"]
         }
-        if ($null -ne $PSBoundParameters["Type"]) {
-            $params["Type"] = $PSBoundParameters["Type"]
+        if ($null -ne $PSBoundParameters["InformationVariable"]) {
+            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
+        }
+        if ($null -ne $PSBoundParameters["OutBuffer"]) {
+            $params["OutBuffer"] = $PSBoundParameters["OutBuffer"]
         }
         if ($null -ne $PSBoundParameters["InformationAction"]) {
             $params["InformationAction"] = $PSBoundParameters["InformationAction"]
         }
-        if ($null -ne $PSBoundParameters["InformationVariable"]) {
-            $params["InformationVariable"] = $PSBoundParameters["InformationVariable"]
-        }
-        if ($null -ne $PSBoundParameters["EndDate"]) {
-            $params["EndDate"] = $PSBoundParameters["EndDate"]
-        }
-        if ($null -ne $PSBoundParameters["Usage"]) {
-            $params["Usage"] = $PSBoundParameters["Usage"]
+        if ($null -ne $PSBoundParameters["ErrorVariable"]) {
+            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
         }
         if ($null -ne $PSBoundParameters["ErrorAction"]) {
             $params["ErrorAction"] = $PSBoundParameters["ErrorAction"]
         }
-        if ($null -ne $PSBoundParameters["ErrorVariable"]) {
-            $params["ErrorVariable"] = $PSBoundParameters["ErrorVariable"]
+        if ($PSBoundParameters.ContainsKey("Verbose")) {
+            $params["Verbose"] = $PSBoundParameters["Verbose"]
+        }
+        if ($null -ne $PSBoundParameters["WarningAction"]) {
+            $params["WarningAction"] = $PSBoundParameters["WarningAction"]
+        }
+        if ($null -ne $PSBoundParameters["ProgressAction"]) {
+            $params["ProgressAction"] = $PSBoundParameters["ProgressAction"]
+        }
+        if ($null -ne $PSBoundParameters["OutVariable"]) {
+            $params["OutVariable"] = $PSBoundParameters["OutVariable"]
         }
 
         Write-Debug("============================ TRANSFORMATIONS ============================")
