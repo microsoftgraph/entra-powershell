314ef1e8222e660e69970ab4b77d4193d59e11f3
Kennedy Kang'ethe <kemunga@microsoft.com>
2025-08-27 14:07:55 +0300

    Automate mapping files creation (#1508)

---
+++ src/Create-ModuleMappingFile.ps1
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+function Create-ModuleMappingFile {
+    [cmdletbinding()]
+    param(
+        [ValidateSet('Entra', 'EntraBeta')]
+        [string]
+        $ModuleName = 'Entra'  # Default to "Entra" if no argument is provided
+    )
+
+    PROCESS {
+
+        if($ModuleName -eq 'Entra'){
+            $rootModuleName = 'Microsoft.Entra'
+            $docFolderName = 'entra-powershell-v1.0'
+        }
+        elseif($ModuleName -eq 'EntraBeta'){
+            $rootModuleName = 'Microsoft.Entra.Beta'
+            $docFolderName = 'entra-powershell-beta'
+        }
+
+        $moduleFolderPath = (Join-Path $PSScriptRoot "../module/docs/$docFolderName")
+        Write-Host "[ModuleFolderPath] $moduleFolderPath" -ForegroundColor 'Green'
+        $subModules = @(Get-ChildItem -Path $moduleFolderPath -Directory)
+        Write-Host "[subModules] $($subModules.Count)" -ForegroundColor 'Green'
+
+        $mapping = @{}
+
+        foreach($subModuleName in $subModules.Name){
+            $subModuleFolderPath = (Join-Path $moduleFolderPath $subModuleName)
+            Write-Host "[ModuleFolderPath] $subModuleFolderPath" -ForegroundColor 'Green'
+            $subModulesDocs = @(Get-ChildItem -Path $subModuleFolderPath -File)
+
+            foreach($subModuleDoc in $subModulesDocs){
+                if($subModuleDoc.BaseName -ne 'Enable-EntraAzureADAlias'){
+                    $mapping.Add($subModuleDoc.BaseName,$subModuleName)
+                }
+            }
+        }
+
+        # Save the mapping to a JSON file
+        $mappingFilePath = (Join-Path $PSScriptRoot "$ModuleName-ModuleMapping.json")
+        $mapping | ConvertTo-Json -Depth 10 | Out-File -FilePath $mappingFilePath -Encoding utf8
+    }
+}
