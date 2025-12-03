122754f825c9d4248e0973c3007376cdc6fa5bcb
Givinalis Omachar <giomachar@microsoft.com>
2025-12-03 11:51:29 +0300

    Added verbose logging for pipeline runs to indicate the installed version

--- build/Install-Dependencies.ps1
+++ build/Install-Dependencies.ps1
@@ -20,8 +20,12 @@ if ($ModuleSettingsPath) { $settingPath = $ModuleSettingsPath }
 $content = Get-Content -Path $settingPath | ConvertFrom-Json
 Write-Verbose("Installing Module $($content.sourceModule)")
 Install-Module $content.sourceModule -scope currentuser -Force -AllowClobber
+$installedModule = Get-Module -Name $content.sourceModule -ListAvailable | Select-Object -First 1
+Write-Host "Installed Module: $($content.sourceModule) - version: $($installedModule.Version)" -ForegroundColor Green
 
 foreach ($moduleName in $content.destinationModuleName){
     Write-Verbose("Installing Module $($moduleName)")
     Install-Module $moduleName -scope currentuser -RequiredVersion $content.destinationModuleVersion -Force -AllowClobber
+    $installedModule = Get-Module -Name $moduleName -ListAvailable | Select-Object -First 1
+    Write-Host "-> Installed Module: $($moduleName) - version: $($installedModule.Version)" -ForegroundColor Green
 }
